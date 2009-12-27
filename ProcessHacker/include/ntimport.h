eea2f4779925495ce399a5cb093912b5e480bed5
Wen Jia Liu <wj32.64@gmail.com>
2009-12-27 11:35:22 +0000

    added retrieval of command line information

--- ProcessHacker/include/ntimport.h
+++ ProcessHacker/include/ntimport.h
@@ -3,6 +3,7 @@
 
 #include <ntbasic.h>
 #include <ntexapi.h>
+#include <ntmmapi.h>
 #include <ntobapi.h>
 #include <ntpsapi.h>
 #include <ntseapi.h>
@@ -18,19 +19,25 @@
 
 EXT _NtAlertResumeThread NtAlertResumeThread EQNULL;
 EXT _NtAlertThread NtAlertThread EQNULL;
+EXT _NtAllocateVirtualMemory NtAllocateVirtualMemory EQNULL;
 EXT _NtClose NtClose EQNULL;
 EXT _NtDuplicateObject NtDuplicateObject EQNULL;
+EXT _NtFreeVirtualMemory NtFreeVirtualMemory EQNULL;
 EXT _NtGetNextProcess NtGetNextProcess EQNULL;
 EXT _NtGetNextThread NtGetNextThread EQNULL;
 EXT _NtOpenProcess NtOpenProcess EQNULL;
 EXT _NtOpenProcessToken NtOpenProcessToken EQNULL;
 EXT _NtOpenThread NtOpenThread EQNULL;
+EXT _NtProtectVirtualMemory NtProtectVirtualMemory EQNULL;
 EXT _NtQueryInformationProcess NtQueryInformationProcess EQNULL;
 EXT _NtQueryInformationThread NtQueryInformationThread EQNULL;
 EXT _NtQueryInformationToken NtQueryInformationToken EQNULL;
-EXT _NtQueryObject NtQueryObject EQNULL;
+EXT _NtQueryObject NtQueryObject EQNULL;           
+EXT _NtQuerySection NtQuerySection EQNULL;
 EXT _NtQuerySystemInformation NtQuerySystemInformation EQNULL;
+EXT _NtQueryVirtualMemory NtQueryVirtualMemory EQNULL;
 EXT _NtQueueApcThread NtQueueApcThread EQNULL;
+EXT _NtReadVirtualMemory NtReadVirtualMemory EQNULL;
 EXT _NtResumeProcess NtResumeProcess EQNULL;
 EXT _NtResumeThread NtResumeThread EQNULL; 
 EXT _NtSetInformationProcess NtSetInformationProcess EQNULL;
@@ -40,6 +47,7 @@ EXT _NtSuspendProcess NtSuspendProcess EQNULL;
 EXT _NtSuspendThread NtSuspendThread EQNULL;
 EXT _NtTerminateProcess NtTerminateProcess EQNULL;
 EXT _NtTerminateThread NtTerminateThread EQNULL;
+EXT _NtWriteVirtualMemory NtWriteVirtualMemory EQNULL;
 
 BOOLEAN PhInitializeImports();
 
