93e7907a0ffa8e76595030860ef30cda0cf03c4d
Wen Jia Liu <wj32.64@gmail.com>
2009-12-28 05:42:58 +0000

    * added PhEnumHashtable * ported FastResourceLock to C

---
+++ ProcessHacker/fastlock.c
@@ -0,0 +1,339 @@
+#include <fastlock.h>
+
+// FastLock is a port of FastResourceLock from PH 1.x.
+//
+// This code contains no comments because it is a direct 
+// port. Please see FastResourceLock.cs in PH 1.x for 
+// details.
+
+#define PH_LOCK_OWNED 0x1
+#define PH_LOCK_EXCLUSIVE_WAKING 0x2
+
+#define PH_LOCK_SHARED_OWNERS_SHIFT 2
+#define PH_LOCK_SHARED_OWNERS_MASK 0x3ff
+#define PH_LOCK_SHARED_OWNERS_INC 0x4
+
+#define PH_LOCK_SHARED_WAITERS_SHIFT 12
+#define PH_LOCK_SHARED_WAITERS_MASK 0x3ff
+#define PH_LOCK_SHARED_WAITERS_INC 0x1000
+
+#define PH_LOCK_EXCLUSIVE_WAITERS_SHIFT 22
+#define PH_LOCK_EXCLUSIVE_WAITERS_MASK 0x3ff
+#define PH_LOCK_EXCLUSIVE_WAITERS_INC 0x400000
+
+#define PH_LOCK_EXCLUSIVE_MASK \
+    (PH_LOCK_EXCLUSIVE_WAKING | \
+    (PH_LOCK_EXCLUSIVE_WAITERS_MASK << PH_LOCK_EXCLUSIVE_WAITERS_SHIFT))
+
+static ULONG PhLockSpinCount;
+
+VOID PhFastLockInitialization()
+{
+    SYSTEM_INFO info;
+
+    GetSystemInfo(&info);
+
+    if (info.dwNumberOfProcessors > 1)
+        PhLockSpinCount = 4000;
+    else
+        PhLockSpinCount = 0;
+}
+
+VOID PhInitializeFastLock(
+    __out PPH_FAST_LOCK FastLock
+    )
+{
+    FastLock->Value = 0;
+    FastLock->ExclusiveWakeEvent = NULL;
+    FastLock->SharedWakeEvent = NULL;
+}
+
+VOID PhDeleteFastLock(
+    __inout PPH_FAST_LOCK FastLock
+    )
+{
+    if (FastLock->ExclusiveWakeEvent)
+    {
+        CloseHandle(FastLock->ExclusiveWakeEvent);
+        FastLock->ExclusiveWakeEvent = NULL;
+    }
+
+    if (FastLock->SharedWakeEvent)
+    {
+        CloseHandle(FastLock->SharedWakeEvent);
+        FastLock->SharedWakeEvent = NULL;
+    }
+}
+
+VOID FORCEINLINE PhpEnsureEventCreated(
+    __inout PHANDLE Handle
+    )
+{
+    HANDLE handle;
+
+    if (*Handle != NULL)
+        return;
+
+    handle = CreateSemaphore(NULL, 0, MAXLONG, NULL);
+
+    if (InterlockedCompareExchangePointer(
+        Handle,
+        handle,
+        NULL
+        ) != NULL)
+    {
+        CloseHandle(handle);
+    }
+}
+
+VOID PhAcquireFastLockExclusive(
+    __inout PPH_FAST_LOCK FastLock
+    )
+{
+    ULONG value;
+    ULONG i = 0;
+
+    while (TRUE)
+    {
+        value = FastLock->Value;
+
+        if (!(value & (PH_LOCK_OWNED | PH_LOCK_EXCLUSIVE_WAKING)))
+        {
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value + PH_LOCK_OWNED,
+                value
+                ) == value)
+                break;
+        }
+        else if (i >= PhLockSpinCount)
+        {
+            PhpEnsureEventCreated(&FastLock->ExclusiveWakeEvent);
+
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value + PH_LOCK_EXCLUSIVE_WAITERS_INC,
+                value
+                ) == value)
+            {
+                if (WaitForSingleObject(
+                    FastLock->ExclusiveWakeEvent,
+                    INFINITE
+                    ) != WAIT_OBJECT_0)
+                    PhRaiseStatus(STATUS_UNSUCCESSFUL);
+
+                do
+                {
+                    value = FastLock->Value;
+                } while (InterlockedCompareExchange(
+                    &FastLock->Value,
+                    value + PH_LOCK_OWNED - PH_LOCK_EXCLUSIVE_WAKING,
+                    value
+                    ) != value);
+
+                break;
+            }
+        }
+
+        i++;
+    }
+}
+
+VOID PhAcquireFastLockShared(
+    __inout PPH_FAST_LOCK FastLock
+    )
+{
+    ULONG value;
+    ULONG i = 0;
+
+    while (TRUE)
+    {
+        value = FastLock->Value;
+
+        if (!(value & (
+            PH_LOCK_OWNED |
+            (PH_LOCK_SHARED_OWNERS_MASK << PH_LOCK_SHARED_OWNERS_SHIFT) |
+            PH_LOCK_EXCLUSIVE_MASK
+            )))
+        {
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value + PH_LOCK_OWNED + PH_LOCK_SHARED_OWNERS_INC,
+                value
+                ) == value)
+                break;
+        }
+        else if (
+            (value & PH_LOCK_OWNED) &&
+            ((value >> PH_LOCK_SHARED_OWNERS_SHIFT) & PH_LOCK_SHARED_OWNERS_MASK) > 0 &&
+            !(value & PH_LOCK_EXCLUSIVE_MASK)
+            )
+        {
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value + PH_LOCK_SHARED_OWNERS_INC,
+                value
+                ) == value)
+                break;
+        }
+        else if (i >= PhLockSpinCount)
+        {
+            PhpEnsureEventCreated(&FastLock->SharedWakeEvent);
+
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value + PH_LOCK_SHARED_WAITERS_INC,
+                value
+                ) == value)
+            {
+                if (WaitForSingleObject(
+                    FastLock->SharedWakeEvent,
+                    INFINITE
+                    ) != WAIT_OBJECT_0)
+                    PhRaiseStatus(STATUS_UNSUCCESSFUL);
+
+                continue;
+            }
+        }
+    }
+}
+
+VOID PhReleaseFastLockExclusive(
+    __inout PPH_FAST_LOCK FastLock
+    )
+{
+    ULONG value;
+
+    while (TRUE)
+    {
+        value = FastLock->Value;
+
+        if ((value >> PH_LOCK_EXCLUSIVE_WAITERS_SHIFT) & PH_LOCK_EXCLUSIVE_WAITERS_MASK)
+        {
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value - PH_LOCK_OWNED + PH_LOCK_EXCLUSIVE_WAKING - PH_LOCK_EXCLUSIVE_WAITERS_INC,
+                value
+                ) == value)
+            {
+                ReleaseSemaphore(FastLock->ExclusiveWakeEvent, 1, NULL);
+
+                break;
+            }
+        }
+        else
+        {
+            ULONG sharedWaiters;
+
+            sharedWaiters = (value >> PH_LOCK_SHARED_WAITERS_SHIFT) & PH_LOCK_SHARED_WAITERS_MASK;
+
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value & ~(PH_LOCK_OWNED | (PH_LOCK_SHARED_WAITERS_MASK << PH_LOCK_SHARED_WAITERS_SHIFT)),
+                value
+                ) == value)
+            {
+                if (sharedWaiters)
+                    ReleaseSemaphore(FastLock->SharedWakeEvent, sharedWaiters, 0);
+
+                break;
+            }
+        }
+    }
+}
+
+VOID PhReleaseFastLockShared(
+    __inout PPH_FAST_LOCK FastLock
+    )
+{
+    ULONG value;
+
+    while (TRUE)
+    {
+        value = FastLock->Value;
+
+        if (((value >> PH_LOCK_SHARED_OWNERS_SHIFT) & PH_LOCK_SHARED_OWNERS_MASK) > 1)
+        {
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value - PH_LOCK_SHARED_OWNERS_INC,
+                value
+                ) == value)
+                break;
+        }
+        else if ((value >> PH_LOCK_EXCLUSIVE_WAITERS_SHIFT) & PH_LOCK_EXCLUSIVE_WAITERS_MASK)
+        {
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value - PH_LOCK_OWNED + PH_LOCK_EXCLUSIVE_WAKING -
+                PH_LOCK_SHARED_OWNERS_INC - PH_LOCK_EXCLUSIVE_WAITERS_INC,
+                value
+                ) == value)
+            {
+                ReleaseSemaphore(FastLock->ExclusiveWakeEvent, 1, NULL);
+
+                break;
+            }
+        }
+        else
+        {
+            if (InterlockedCompareExchange(
+                &FastLock->Value,
+                value - PH_LOCK_OWNED - PH_LOCK_SHARED_OWNERS_INC,
+                value
+                ) == value)
+                break;
+        }
+    }
+}
+
+BOOLEAN PhTryAcquireFastLockExclusive(
+    __inout PPH_FAST_LOCK FastLock
+    )
+{
+    ULONG value;
+
+    value = FastLock->Value;
+
+    if (value & (PH_LOCK_OWNED | PH_LOCK_EXCLUSIVE_WAKING))
+        return FALSE;
+
+    return InterlockedCompareExchange(
+        &FastLock->Value,
+        value + PH_LOCK_OWNED,
+        value
+        ) == value;
+}
+
+BOOLEAN PhTryAcquireFastLockShared(
+    __inout PPH_FAST_LOCK FastLock
+    )
+{
+    ULONG value;
+
+    value = FastLock->Value;
+
+    if (value & PH_LOCK_EXCLUSIVE_MASK)
+        return FALSE;
+
+    if (!(value & PH_LOCK_OWNED))
+    {
+        return InterlockedCompareExchange(
+            &FastLock->Value,
+            value + PH_LOCK_OWNED + PH_LOCK_SHARED_OWNERS_INC,
+            value
+            ) == value;
+    }
+    else if ((value >> PH_LOCK_SHARED_OWNERS_SHIFT) & PH_LOCK_SHARED_OWNERS_MASK)
+    {
+        return InterlockedCompareExchange(
+            &FastLock->Value,
+            value + PH_LOCK_SHARED_OWNERS_INC,
+            value
+            ) == value;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
---
+++ ProcessHacker/include/fastlock.h
@@ -0,0 +1,49 @@
+#ifndef FASTLOCK_H
+#define FASTLOCK_H
+
+#include <phbase.h>
+
+// FastLock is a port of FastResourceLock from PH 1.x.
+
+typedef struct _PH_FAST_LOCK
+{
+    ULONG Value;
+    HANDLE ExclusiveWakeEvent;
+    HANDLE SharedWakeEvent;
+} PH_FAST_LOCK, *PPH_FAST_LOCK;
+
+VOID PhFastLockInitialization();
+
+VOID PhInitializeFastLock(
+    __out PPH_FAST_LOCK FastLock
+    );
+
+VOID PhDeleteFastLock(
+    __inout PPH_FAST_LOCK FastLock
+    );
+
+VOID PhAcquireFastLockExclusive(
+    __inout PPH_FAST_LOCK FastLock
+    );
+
+VOID PhAcquireFastLockShared(
+    __inout PPH_FAST_LOCK FastLock
+    );
+
+VOID PhReleaseFastLockExclusive(
+    __inout PPH_FAST_LOCK FastLock
+    );
+
+VOID PhReleaseFastLockShared(
+    __inout PPH_FAST_LOCK FastLock
+    );
+
+BOOLEAN PhTryAcquireFastLockExclusive(
+    __inout PPH_FAST_LOCK FastLock
+    );
+
+BOOLEAN PhTryAcquireFastLockShared(
+    __inout PPH_FAST_LOCK FastLock
+    );
+
+#endif
