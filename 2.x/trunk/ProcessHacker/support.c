e95e885755238276d5fb94f800cad3dc50a7b445
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2009-12-29 07:35:42 +0000

    added version info code

--- 2.x/trunk/ProcessHacker/support.c
+++ 2.x/trunk/ProcessHacker/support.c
@@ -33,6 +33,88 @@ INT PhShowMessage_V(
     return MessageBox(hWnd, message, PH_APP_NAME, Type);
 }
 
+PVOID PhGetFileVersionInfo(
+    __in PWSTR FileName
+    )
+{
+    ULONG versionInfoSize;
+    ULONG dummy;
+    PVOID versionInfo;
+
+    versionInfoSize = GetFileVersionInfoSize(
+        FileName,
+        &dummy
+        );
+
+    if (versionInfoSize)
+    {
+        versionInfo = PhAllocate(versionInfoSize);
+
+        if (!GetFileVersionInfo(
+            FileName,
+            0,
+            versionInfoSize,
+            versionInfo
+            ))
+        {
+            PhFree(versionInfo);
+
+            return NULL;
+        }
+    }
+
+    return versionInfo;
+}
+
+ULONG PhGetFileVersionInfoLangCodePage(
+    __in PVOID VersionInfo
+    )
+{
+    PVOID buffer;
+    ULONG length;
+
+    if (VerQueryValue(VersionInfo, L"\\VarFileInfo\\Translation", &buffer, &length))
+    {
+        // Combine the language ID and code page.
+        return (*(PUSHORT)buffer << 16) + *((PUSHORT)buffer + 1);
+    }
+    else
+    {
+        return 0x40904e4;
+    }
+}
+
+PPH_STRING PhGetFileVersionInfoString(
+    __in PVOID VersionInfo,
+    __in PWSTR SubBlock
+    )
+{
+    PVOID buffer;
+    ULONG length;
+
+    if (VerQueryValue(VersionInfo, SubBlock, &buffer, &length))
+    {
+        return PhCreateStringEx((PWSTR)buffer, length * sizeof(WCHAR));
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+PPH_STRING PhGetFileVersionInfoString2(
+    __in PVOID VersionInfo,
+    __in ULONG LangCodePage,
+    __in PWSTR StringName
+    )
+{
+    WCHAR subBlock[64];
+
+    _snwprintf(subBlock, 64, L"\\StringFileInfo\\%08X\\%s", LangCodePage, StringName);
+
+    return PhGetFileVersionInfoString(VersionInfo, subBlock);
+}
+
 PPH_STRING PhGetSystemDirectory()
 {
     PPH_STRING systemDirectory;
