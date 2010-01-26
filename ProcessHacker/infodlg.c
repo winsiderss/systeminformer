6cbcde2260880e894ccec290d9663830d7a63733
Wen Jia Liu <wj32.64@gmail.com>
2010-01-26 07:47:14 +0000

    implemented layout for infodlg

--- ProcessHacker/infodlg.c
+++ ProcessHacker/infodlg.c
@@ -12,10 +12,30 @@ static INT_PTR CALLBACK PhpInformationDlgProc(
     case WM_INITDIALOG:
         {
             PWSTR string = (PWSTR)lParam;
+            PPH_LAYOUT_MANAGER layoutManager;
 
             PhCenterWindow(hwndDlg, GetParent(hwndDlg));
 
             SetDlgItemText(hwndDlg, IDC_TEXT, string);
+
+            layoutManager = PhAllocate(sizeof(PH_LAYOUT_MANAGER));
+            PhInitializeLayoutManager(layoutManager, hwndDlg);
+            PhAddLayoutItem(layoutManager, GetDlgItem(hwndDlg, IDC_TEXT), NULL,
+                PH_ANCHOR_ALL);
+            PhAddLayoutItem(layoutManager, GetDlgItem(hwndDlg, IDOK), NULL,
+                PH_ANCHOR_RIGHT | PH_ANCHOR_BOTTOM);
+
+            SetProp(hwndDlg, L"LayoutManager", (HANDLE)layoutManager);
+        }
+        break;
+    case WM_DESTROY:
+        {
+            PPH_LAYOUT_MANAGER layoutManager;
+
+            layoutManager = (PPH_LAYOUT_MANAGER)GetProp(hwndDlg, L"LayoutManager");
+            PhDeleteLayoutManager(layoutManager);
+            PhFree(layoutManager);
+            RemoveProp(hwndDlg, L"LayoutManager");
         }
         break;
     case WM_COMMAND:
@@ -28,6 +48,20 @@ static INT_PTR CALLBACK PhpInformationDlgProc(
                 break;
             }
         }
+        break;
+    case WM_SIZE:
+        {
+            PPH_LAYOUT_MANAGER layoutManager;
+
+            layoutManager = (PPH_LAYOUT_MANAGER)GetProp(hwndDlg, L"LayoutManager");
+            PhLayoutManagerLayout(layoutManager);
+        }
+        break;
+    case WM_SIZING:
+        {
+            PhResizingMinimumSize((PRECT)lParam, wParam, 180, 160);
+        }
+        break;
     }
 
     return FALSE;
