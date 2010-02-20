627c9430eaae5e2852f7a4f4232698a99d343827
Wen Jia Liu <wj32.64@gmail.com>
2010-02-20 04:57:57 +0000

    implemented session ID menu

--- ProcessHacker/runas.c
+++ ProcessHacker/runas.c
@@ -21,7 +21,15 @@
  */
 
 #include <phgui.h>
+#include <wtsapi32.h>
 #include <windowsx.h>
+
+typedef struct _RUNAS_DIALOG_CONTEXT
+{
+    HANDLE ProcessId;
+
+    PPH_LIST SessionIdList;
+} RUNAS_DIALOG_CONTEXT, *PRUNAS_DIALOG_CONTEXT;
 
 INT_PTR CALLBACK PhpRunAsDlgProc(
     __in HWND hwndDlg,
@@ -46,12 +54,17 @@ VOID PhShowRunAsDialog(
     __in_opt HANDLE ProcessId
     )
 {
+    RUNAS_DIALOG_CONTEXT context;
+
+    context.ProcessId = ProcessId;
+    context.SessionIdList = NULL;
+
     DialogBoxParam(
         PhInstanceHandle,
         MAKEINTRESOURCE(IDD_RUNAS),
         ParentWindowHandle,
         PhpRunAsDlgProc,
-        (LPARAM)ProcessId
+        (LPARAM)&context
         );
 }
 
@@ -83,6 +96,21 @@ INT_PTR CALLBACK PhpRunAsDlgProc(
     __in LPARAM lParam
     )
 {
+    PRUNAS_DIALOG_CONTEXT context;
+
+    if (uMsg != WM_INITDIALOG)
+    {
+        context = (PRUNAS_DIALOG_CONTEXT)GetProp(hwndDlg, L"Context");
+    }
+    else
+    {
+        context = (PRUNAS_DIALOG_CONTEXT)lParam;
+        SetProp(hwndDlg, L"Context", (HANDLE)context);
+    }
+
+    if (!context)
+        return FALSE;
+
     switch (uMsg)
     {
     case WM_INITDIALOG:
@@ -148,6 +176,119 @@ INT_PTR CALLBACK PhpRunAsDlgProc(
                     }
 
                     PhFreeFileDialog(fileDialog);
+                }
+                break;
+            case IDC_SESSIONS:
+                {
+                    HMENU sessionsMenu;
+                    PWTS_SESSION_INFO sessions;
+                    ULONG numberOfSessions;
+                    ULONG i;
+                    RECT buttonRect;
+                    POINT point;
+
+                    sessionsMenu = CreatePopupMenu();
+
+                    if (WTSEnumerateSessions(
+                        WTS_CURRENT_SERVER_HANDLE,
+                        0,
+                        1,
+                        &sessions,
+                        &numberOfSessions
+                        ))
+                    {
+                        if (!context->SessionIdList)
+                            context->SessionIdList = PhCreateList(numberOfSessions);
+                        else
+                            PhClearList(context->SessionIdList);
+
+                        for (i = 0; i < numberOfSessions; i++)
+                        {
+                            ULONG length;
+                            PWSTR domainName = NULL;
+                            PWSTR userName = NULL;
+                            PPH_STRING menuString;
+
+                            WTSQuerySessionInformation(
+                                WTS_CURRENT_SERVER_HANDLE,
+                                sessions[i].SessionId,
+                                WTSDomainName,
+                                &domainName,
+                                &length
+                                );
+                            WTSQuerySessionInformation(
+                                WTS_CURRENT_SERVER_HANDLE,
+                                sessions[i].SessionId,
+                                WTSUserName,
+                                &userName,
+                                &length
+                                );
+
+                            if (
+                                userName &&
+                                !WSTR_EQUAL(userName, L"") &&
+                                sessions[i].pWinStationName &&
+                                !WSTR_EQUAL(sessions[i].pWinStationName, L"")
+                                )
+                            {
+                                menuString = PhFormatString(
+                                    L"%u: %s (%s\\%s)",
+                                    sessions[i].SessionId,
+                                    sessions[i].pWinStationName,
+                                    domainName ? domainName : L"",
+                                    userName
+                                    );
+                            }
+                            else if (
+                                sessions[i].pWinStationName &&
+                                !WSTR_EQUAL(sessions[i].pWinStationName, L"")
+                                )
+                            {
+                                menuString = PhFormatString(
+                                    L"%u: %s",
+                                    sessions[i].SessionId,
+                                    sessions[i].pWinStationName
+                                    );
+                            }
+                            else
+                            {
+                                menuString = PhFormatString(L"%u", sessions[i].SessionId);
+                            }
+
+                            if (domainName) WTSFreeMemory(domainName);
+                            if (userName) WTSFreeMemory(userName);
+
+                            AppendMenu(sessionsMenu, MF_STRING, IDDYNAMIC + i, menuString->Buffer);
+                            PhDereferenceObject(menuString);
+
+                            PhAddListItem(context->SessionIdList, (PVOID)sessions[i].SessionId);
+                        }
+
+                        WTSFreeMemory(sessions);
+
+                        GetClientRect(GetDlgItem(hwndDlg, IDC_SESSIONS), &buttonRect);
+                        point.x = buttonRect.right;
+                        point.y = 0;
+                        PhShowContextMenu(hwndDlg, GetDlgItem(hwndDlg, IDC_SESSIONS), sessionsMenu, point);
+
+                        DestroyMenu(sessionsMenu);
+                    }
+                }
+                break;
+            default:
+                {
+                    if (LOWORD(wParam) >= IDDYNAMIC)
+                    {
+                        ULONG index = LOWORD(wParam) - IDDYNAMIC;
+
+                        if (context->SessionIdList && index < context->SessionIdList->Count)
+                        {
+                            ULONG sessionId;
+
+                            sessionId = (ULONG)context->SessionIdList->Items[index];
+                            SetDlgItemInt(hwndDlg, IDC_SESSIONID, sessionId, FALSE);
+                        }
+                    }
                 }
                 break;
             }
