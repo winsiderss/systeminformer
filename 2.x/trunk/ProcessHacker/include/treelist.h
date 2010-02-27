9192484b38f93fb628c484532c6d5a79a18a5562
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-02-27 09:42:45 +0000

    show "Unknown" instead of "Static"

--- 2.x/trunk/ProcessHacker/include/treelist.h
+++ 2.x/trunk/ProcessHacker/include/treelist.h
@@ -10,9 +10,21 @@ HWND PhCreateTreeListControl(
     __in INT_PTR Id
     );
 
-typedef struct _PH_TREELIST_ITEM
+typedef struct _PH_TREELIST_NODE
 {
     ULONG Flags;
-} PH_TREELIST_ITEM, *PPH_TREELIST_ITEM;
+
+    ULONG NumberOfItems;
+    PVOID Items[1];
+} PH_TREELIST_NODE, *PPH_TREELIST_NODE;
+
+typedef struct _PH_TREELIST_NODE_DISPLAY
+{
+    ULONG Flags;
+    PPH_TREELIST_NODE Node;
+
+    ULONG NumberOfChildren;
+    struct _PH_TREELIST_NODE_DISPLAY *Children[1];
+} PH_TREELIST_NODE_DISPLAY, *PPH_TREELIST_NODE_DISPLAY;
 
 #endif
