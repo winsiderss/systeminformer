f3f5c9e142b1286e1e4a9dfbde3a9e92548fefdb
Wen Jia Liu <wj32.64@gmail.com>
2010-03-14 10:04:39 +0000

    completed ntlpcapi.h

--- phlib/include/ntlpcapi.h
+++ phlib/include/ntlpcapi.h
@@ -4,4 +4,274 @@
 #define PORT_CONNECT 0x0001
 #define PORT_ALL_ACCESS (STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | 0x1)
 
+typedef struct _PORT_MESSAGE
+{
+    union
+    {
+        struct
+        {
+            CSHORT DataLength;
+            CSHORT TotalLength;
+        } s1;
+        ULONG Length;
+    } u1;
+    union
+    {
+        struct
+        {
+            CSHORT Type;
+            CSHORT DataInfoOffset;
+        } s2;
+        ULONG ZeroInit;
+    } u2;
+    union
+    {
+        CLIENT_ID ClientId;
+        QUAD DoNotUseThisField;
+    };
+    ULONG MessageId;
+    union
+    {
+        SIZE_T ClientViewSize; // only valid for LPC_CONNECTION_REQUEST messages
+        ULONG CallbackId; // only valid for LPC_REQUEST messages
+    };
+} PORT_MESSAGE, *PPORT_MESSAGE;
+
+#define LPC_REQUEST 1
+#define LPC_REPLY 2
+#define LPC_DATAGRAM 3
+#define LPC_LOST_REPLY 4
+#define LPC_PORT_CLOSED 5
+#define LPC_CLIENT_DIED 6
+#define LPC_EXCEPTION 7
+#define LPC_DEBUG_EVENT 8
+#define LPC_ERROR_EVENT 9
+#define LPC_CONNECTION_REQUEST 10
+
+#define LPC_KERNELMODE_MESSAGE  (CSHORT)0x8000
+#define PORT_VALID_OBJECT_ATTRIBUTES OBJ_CASE_INSENSITIVE
+
+#ifdef _M_IX86
+#define PORT_MAXIMUM_MESSAGE_LENGTH 256
+#else
+#define PORT_MAXIMUM_MESSAGE_LENGTH 512
+#endif
+
+#define LPC_MAX_CONNECTION_INFO_SIZE (16 * sizeof(ULONG_PTR))
+
+#define PORT_TOTAL_MAXIMUM_MESSAGE_LENGTH \
+    ((PORT_MAXIMUM_MESSAGE_LENGTH + sizeof(PORT_MESSAGE) + LPC_MAX_CONNECTION_INFO_SIZE + 0xf) & ~0xf)
+
+typedef struct _LPC_CLIENT_DIED_MSG
+{
+    PORT_MESSAGE PortMsg;
+    LARGE_INTEGER CreateTime;
+} LPC_CLIENT_DIED_MSG, *PLPC_CLIENT_DIED_MSG;
+
+typedef struct _PORT_VIEW
+{
+    ULONG Length;
+    HANDLE SectionHandle;
+    ULONG SectionOffset;
+    SIZE_T ViewSize;
+    PVOID ViewBase;
+    PVOID ViewRemoteBase;
+} PORT_VIEW, *PPORT_VIEW;
+
+typedef struct _REMOTE_PORT_VIEW
+{
+    ULONG Length;
+    SIZE_T ViewSize;
+    PVOID ViewBase;
+} REMOTE_PORT_VIEW, *PREMOTE_PORT_VIEW;
+
+// Port creation
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtCreatePort(
+    __out PHANDLE PortHandle,
+    __in POBJECT_ATTRIBUTES ObjectAttributes,
+    __in ULONG MaxConnectionInfoLength,
+    __in ULONG MaxMessageLength,
+    __in_opt ULONG MaxPoolUsage
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtCreateWaitablePort(
+    __out PHANDLE PortHandle,
+    __in POBJECT_ATTRIBUTES ObjectAttributes,
+    __in ULONG MaxConnectionInfoLength,
+    __in ULONG MaxMessageLength,
+    __in_opt ULONG MaxPoolUsage
+    );
+
+// Port connection (client)
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtConnectPort(
+    __out PHANDLE PortHandle,
+    __in PUNICODE_STRING PortName,
+    __in PSECURITY_QUALITY_OF_SERVICE SecurityQos,
+    __inout_opt PPORT_VIEW ClientView,
+    __inout_opt PREMOTE_PORT_VIEW ServerView,
+    __out_opt PULONG MaxMessageLength,
+    __inout_opt PVOID ConnectionInformation,
+    __inout_opt PULONG ConnectionInformationLength
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtSecureConnectPort(
+    __out PHANDLE PortHandle,
+    __in PUNICODE_STRING PortName,
+    __in PSECURITY_QUALITY_OF_SERVICE SecurityQos,
+    __inout_opt PPORT_VIEW ClientView,
+    __in_opt PSID RequiredServerSid,
+    __inout_opt PREMOTE_PORT_VIEW ServerView,
+    __out_opt PULONG MaxMessageLength,
+    __inout_opt PVOID ConnectionInformation,
+    __inout_opt PULONG ConnectionInformationLength
+    );
+
+// Port connection (server)
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtListenPort(
+    __in HANDLE PortHandle,
+    __out PPORT_MESSAGE ConnectionRequest
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtAcceptConnectPort(
+    __out PHANDLE PortHandle,
+    __in_opt PVOID PortContext,
+    __in PPORT_MESSAGE ConnectionRequest,
+    __in BOOLEAN AcceptConnection,
+    __inout_opt PPORT_VIEW ServerView,
+    __out_opt PREMOTE_PORT_VIEW ClientView
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtCompleteConnectPort(
+    __in HANDLE PortHandle
+    );
+
+// General
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtRequestPort(
+    __in HANDLE PortHandle,
+    __in PPORT_MESSAGE RequestMessage
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtRequestWaitReplyPort(
+    __in HANDLE PortHandle,
+    __in PPORT_MESSAGE RequestMessage,
+    __out PPORT_MESSAGE ReplyMessage
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtReplyPort(
+    __in HANDLE PortHandle,
+    __in PPORT_MESSAGE ReplyMessage
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtReplyWaitReplyPort(
+    __in HANDLE PortHandle,
+    __inout PPORT_MESSAGE ReplyMessage
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtReplyWaitReceivePort(
+    __in HANDLE PortHandle,
+    __out_opt PVOID *PortContext,
+    __in_opt PPORT_MESSAGE ReplyMessage,
+    __out PPORT_MESSAGE ReceiveMessage
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtReplyWaitReceivePortEx(
+    __in HANDLE PortHandle,
+    __out_opt PVOID *PortContext,
+    __in_opt PPORT_MESSAGE ReplyMessage,
+    __out PPORT_MESSAGE ReceiveMessage,
+    __in_opt PLARGE_INTEGER Timeout
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtImpersonateClientOfPort(
+    __in HANDLE PortHandle,
+    __in PPORT_MESSAGE Message
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtReadRequestData(
+    __in HANDLE PortHandle,
+    __in PPORT_MESSAGE Message,
+    __in ULONG DataEntryIndex,
+    __out_bcount(BufferSize) PVOID Buffer,
+    __in SIZE_T BufferSize,
+    __out_opt PSIZE_T NumberOfBytesRead
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtWriteRequestData(
+    __in HANDLE PortHandle,
+    __in PPORT_MESSAGE Message,
+    __in ULONG DataEntryIndex,
+    __in_bcount(BufferSize) PVOID Buffer,
+    __in SIZE_T BufferSize,
+    __out_opt PSIZE_T NumberOfBytesWritten
+    );
+
+typedef enum _PORT_INFORMATION_CLASS
+{
+    PortBasicInformation,
+    PortDumpInformation
+} PORT_INFORMATION_CLASS;
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtQueryInformationPort(
+    __in HANDLE PortHandle,
+    __in PORT_INFORMATION_CLASS PortInformationClass,
+    __out_bcount(Length) PVOID PortInformation,
+    __in ULONG Length,
+    __out_opt PULONG ReturnLength
+    );
+
 #endif
