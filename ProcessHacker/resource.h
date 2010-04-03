3889f0d90cc0a8de8431ff13e9346d59e731e879
Wen Jia Liu <wj32.64@gmail.com>
2010-04-03 10:24:45 +0000

    fixed compiler warnings

--- ProcessHacker/resource.h
+++ ProcessHacker/resource.h
@@ -177,7 +177,6 @@
 #define IDC_CPU17                       1116
 #define IDC_OPTION                      1116
 #define IDC_CPU18                       1117
-#define IDC_COMBO1                      1117
 #define IDC_CHOICEUSER                  1117
 #define IDC_CPU19                       1118
 #define IDC_CPU20                       1119
@@ -315,7 +314,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        157
+#define _APS_NEXT_RESOURCE_VALUE        156
 #define _APS_NEXT_COMMAND_VALUE         40133
 #define _APS_NEXT_CONTROL_VALUE         1118
 #define _APS_NEXT_SYMED_VALUE           111
