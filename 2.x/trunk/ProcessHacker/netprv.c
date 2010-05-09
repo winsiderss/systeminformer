f1d7ca5efe2f2f72ecbe29d35ec0c05b461c02da
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-05-09 03:47:51 +0000

    added host lookup to the network provider

--- 2.x/trunk/ProcessHacker/netprv.c
+++ 2.x/trunk/ProcessHacker/netprv.c
@@ -37,6 +37,21 @@ typedef struct _PH_NETWORK_CONNECTION
     ULONGLONG OwnerInfo[PH_NETWORK_OWNER_INFO_SIZE];
 } PH_NETWORK_CONNECTION, *PPH_NETWORK_CONNECTION;
 
+typedef struct _PH_NETWORK_ITEM_QUERY_DATA
+{
+    PPH_NETWORK_ITEM NetworkItem;
+
+    PH_IP_ADDRESS Address;
+    BOOLEAN Remote;
+    PPH_STRING HostString;
+} PH_NETWORK_ITEM_QUERY_DATA, *PPH_NETWORK_ITEM_QUERY_DATA;
+
+typedef struct _PHP_RESOLVE_CACHE_ITEM
+{
+    PH_IP_ADDRESS Address;
+    PPH_STRING HostString;
+} PHP_RESOLVE_CACHE_ITEM, *PPHP_RESOLVE_CACHE_ITEM;
+
 typedef DWORD (WINAPI *_GetExtendedTcpTable)(
     __out_bcount_opt(*pdwSize) PVOID pTcpTable,
     __inout PDWORD pdwSize,
@@ -55,6 +70,16 @@ typedef DWORD (WINAPI *_GetExtendedUdpTable)(
     __in ULONG Reserved
     );
 
+typedef INT (WSAAPI *_GetNameInfoW)(
+    __in_bcount(SockaddrLength) const SOCKADDR *pSockaddr,
+    __in socklen_t SockaddrLength,
+    __out_ecount_opt(NodeBufferSize) PWCHAR pNodeBuffer,
+    __in DWORD NodeBufferSize,
+    __out_ecount_opt(ServiceBufferSize) PWCHAR pServiceBuffer,
+    __in DWORD ServiceBufferSize,
+    __in INT Flags
+    );
+
 VOID NTAPI PhpNetworkItemDeleteProcedure(
     __in PVOID Object,
     __in ULONG Flags
@@ -69,6 +94,15 @@ ULONG NTAPI PhpNetworkHashtableHashFunction(
     __in PVOID Entry
     );
 
+BOOLEAN PhpResolveCacheHashtableCompareFunction(
+    __in PVOID Entry1,
+    __in PVOID Entry2
+    );
+
+ULONG NTAPI PhpResolveCacheHashtableHashFunction(
+    __in PVOID Entry
+    );
+
 BOOLEAN PhGetNetworkConnections(
     __out PPH_NETWORK_CONNECTION *Connections,
     __out PULONG NumberOfConnections
@@ -77,15 +111,24 @@ BOOLEAN PhGetNetworkConnections(
 PPH_OBJECT_TYPE PhNetworkItemType;
 
 PPH_HASHTABLE PhNetworkHashtable;
-PH_QUEUED_LOCK PhNetworkHashtableLock;
+PH_QUEUED_LOCK PhNetworkHashtableLock = PH_QUEUED_LOCK_INIT;
 
 PH_CALLBACK PhNetworkItemAddedEvent;
 PH_CALLBACK PhNetworkItemModifiedEvent;
 PH_CALLBACK PhNetworkItemRemovedEvent;
 PH_CALLBACK PhNetworkItemsUpdatedEvent;
 
+PH_INITONCE PhNetworkProviderWorkQueueInitOnce = PH_INITONCE_INIT;
+PH_WORK_QUEUE PhNetworkProviderWorkQueue;
+PPH_QUEUE PhNetworkItemQueryQueue;
+PH_MUTEX PhNetworkItemQueryQueueLock;
+
+static PPH_HASHTABLE PhpResolveCacheHashtable;
+static PH_QUEUED_LOCK PhpResolveCacheHashtableLock = PH_QUEUED_LOCK_INIT;
+
 static _GetExtendedTcpTable GetExtendedTcpTable_I;
 static _GetExtendedUdpTable GetExtendedUdpTable_I;
+static _GetNameInfoW GetNameInfoW_I;
 
 BOOLEAN PhInitializeNetworkProvider()
 {
@@ -104,12 +147,20 @@ BOOLEAN PhInitializeNetworkProvider()
         40
         );
 
-    PhInitializeQueuedLock(&PhNetworkHashtableLock);
-
     PhInitializeCallback(&PhNetworkItemAddedEvent);
     PhInitializeCallback(&PhNetworkItemModifiedEvent);
     PhInitializeCallback(&PhNetworkItemRemovedEvent);
     PhInitializeCallback(&PhNetworkItemsUpdatedEvent);
+
+    PhNetworkItemQueryQueue = PhCreateQueue(20);
+    PhInitializeMutex(&PhNetworkItemQueryQueueLock);
+
+    PhpResolveCacheHashtable = PhCreateHashtable(
+        sizeof(PHP_RESOLVE_CACHE_ITEM),
+        PhpResolveCacheHashtableCompareFunction,
+        PhpResolveCacheHashtableHashFunction,
+        20
+        );
 
     return TRUE;
 }
@@ -158,8 +209,6 @@ BOOLEAN PhpNetworkHashtableCompareFunction(
         PhIpEndpointEquals(&networkItem1->LocalEndpoint, &networkItem2->LocalEndpoint) &&
         PhIpEndpointEquals(&networkItem1->RemoteEndpoint, &networkItem2->RemoteEndpoint) &&
         networkItem1->ProcessId == networkItem2->ProcessId;
-
-    return FALSE;
 }
 
 ULONG NTAPI PhpNetworkHashtableHashFunction(
@@ -222,6 +271,197 @@ __assumeLocked VOID PhpRemoveNetworkItem(
     PhDereferenceObject(NetworkItem);
 }
 
+BOOLEAN PhpResolveCacheHashtableCompareFunction(
+    __in PVOID Entry1,
+    __in PVOID Entry2
+    )
+{
+    PPHP_RESOLVE_CACHE_ITEM cacheItem1 = *(PPHP_RESOLVE_CACHE_ITEM *)Entry1;
+    PPHP_RESOLVE_CACHE_ITEM cacheItem2 = *(PPHP_RESOLVE_CACHE_ITEM *)Entry2;
+
+    return PhIpAddressEquals(&cacheItem1->Address, &cacheItem2->Address);
+}
+
+ULONG NTAPI PhpResolveCacheHashtableHashFunction(
+    __in PVOID Entry
+    )
+{
+    PPHP_RESOLVE_CACHE_ITEM cacheItem = *(PPHP_RESOLVE_CACHE_ITEM *)Entry;
+
+    return PhHashIpAddress(&cacheItem->Address);
+}
+
+__assumeLocked PPHP_RESOLVE_CACHE_ITEM PhpLookupResolveCacheItem(
+    __in PPH_IP_ADDRESS Address
+    )
+{
+    PHP_RESOLVE_CACHE_ITEM lookupCacheItem;
+    PPHP_RESOLVE_CACHE_ITEM lookupCacheItemPtr = &lookupCacheItem;
+    PPHP_RESOLVE_CACHE_ITEM *cacheItemPtr;
+
+    // Construct a temporary cache item for the lookup.
+    lookupCacheItem.Address = *Address;
+
+    cacheItemPtr = (PPHP_RESOLVE_CACHE_ITEM *)PhGetHashtableEntry(
+        PhpResolveCacheHashtable,
+        &lookupCacheItemPtr
+        );
+
+    if (cacheItemPtr)
+        return *cacheItemPtr;
+    else
+        return NULL;
+}
+
+PPH_STRING PhGetHostNameFromAddress(
+    __in PPH_IP_ADDRESS Address
+    )
+{
+    struct sockaddr_in ipv4Address;
+    struct sockaddr_in6 ipv6Address;
+    struct sockaddr *address;
+    socklen_t length;
+    PPH_STRING hostName;
+
+    if (Address->Type == PH_IPV4_NETWORK_TYPE)
+    {
+        ipv4Address.sin_family = AF_INET;
+        ipv4Address.sin_port = 0;
+        ipv4Address.sin_addr = Address->InAddr;
+        address = (struct sockaddr *)&ipv4Address;
+        length = sizeof(ipv4Address);
+    }
+    else if (Address->Type == PH_IPV6_NETWORK_TYPE)
+    {
+        ipv6Address.sin6_family = AF_INET6;
+        ipv6Address.sin6_port = 0;
+        ipv6Address.sin6_flowinfo = 0;
+        ipv6Address.sin6_addr = Address->In6Addr;
+        ipv6Address.sin6_scope_id = 0;
+        address = (struct sockaddr *)&ipv6Address;
+        length = sizeof(ipv6Address);
+    }
+    else
+    {
+        return NULL;
+    }
+
+    hostName = PhCreateStringEx(NULL, 128);
+
+    if (GetNameInfoW_I(
+        address,
+        length,
+        hostName->Buffer,
+        hostName->Length + 2,
+        NULL,
+        0,
+        NI_NAMEREQD
+        ) != 0)
+    {
+        // Try with the maximum host name size.
+        PhDereferenceObject(hostName);
+        hostName = PhCreateStringEx(NULL, NI_MAXHOST * 2);
+
+        if (GetNameInfoW_I(
+            address,
+            length,
+            hostName->Buffer,
+            hostName->Length + 2,
+            NULL,
+            0,
+            NI_NAMEREQD
+            ) != 0)
+        {
+            PhDereferenceObject(hostName);
+            return NULL;
+        }
+    }
+
+    PhTrimStringToNullTerminator(hostName);
+
+    return hostName;
+}
+
+NTSTATUS PhpNetworkItemQueryWorker(
+    __in PVOID Parameter
+    )
+{
+    PPH_NETWORK_ITEM_QUERY_DATA data = (PPH_NETWORK_ITEM_QUERY_DATA)Parameter;
+    PPH_STRING hostString;
+    PPHP_RESOLVE_CACHE_ITEM cacheItem;
+
+    // Last minute check of the cache.
+
+    PhAcquireQueuedLockSharedFast(&PhpResolveCacheHashtableLock);
+    cacheItem = PhpLookupResolveCacheItem(&data->Address);
+    PhReleaseQueuedLockSharedFast(&PhpResolveCacheHashtableLock);
+
+    if (!cacheItem)
+    {
+        hostString = PhGetHostNameFromAddress(&data->Address);
+
+        if (hostString)
+        {
+            // Update the cache.
+
+            PhAcquireQueuedLockExclusiveFast(&PhpResolveCacheHashtableLock);
+
+            cacheItem = PhpLookupResolveCacheItem(&data->Address);
+
+            if (!cacheItem)
+            {
+                cacheItem = PhAllocate(sizeof(PHP_RESOLVE_CACHE_ITEM));
+                cacheItem->Address = data->Address;
+                cacheItem->HostString = hostString;
+                PhReferenceObject(hostString);
+
+                PhAddHashtableEntry(PhpResolveCacheHashtable, &cacheItem);
+            }
+
+            PhReleaseQueuedLockExclusiveFast(&PhpResolveCacheHashtableLock);
+        }
+    }
+    else
+    {
+        PhReferenceObject(cacheItem->HostString);
+        data->HostString = cacheItem->HostString;
+    }
+
+    PhAcquireMutex(&PhNetworkItemQueryQueueLock);
+    PhEnqueueQueueItem(PhNetworkItemQueryQueue, data);
+    PhReleaseMutex(&PhNetworkItemQueryQueueLock);
+
+    return STATUS_SUCCESS;
+}
+
+VOID PhpQueueNetworkItemQuery(
+    __in PPH_NETWORK_ITEM NetworkItem,
+    __in BOOLEAN Remote
+    )
+{
+    PPH_NETWORK_ITEM_QUERY_DATA data;
+
+    data = PhAllocate(sizeof(PH_NETWORK_ITEM_QUERY_DATA));
+    memset(data, 0, sizeof(PH_NETWORK_ITEM_QUERY_DATA));
+    data->NetworkItem = NetworkItem;
+    data->Remote = Remote;
+
+    if (Remote)
+        data->Address = NetworkItem->RemoteEndpoint.Address;
+    else
+        data->Address = NetworkItem->LocalEndpoint.Address;
+
+    PhReferenceObject(NetworkItem);
+
+    if (PhBeginInitOnce(&PhNetworkProviderWorkQueueInitOnce))
+    {
+        PhInitializeWorkQueue(&PhNetworkProviderWorkQueue, 0, 3, 500);
+        PhEndInitOnce(&PhNetworkProviderWorkQueueInitOnce);
+    }
+
+    PhQueueWorkQueueItem(&PhNetworkProviderWorkQueue, PhpNetworkItemQueryWorker, data);
+}
+
 VOID PhNetworkProviderUpdate(
     __in PVOID Object
     )
@@ -230,11 +470,13 @@ VOID PhNetworkProviderUpdate(
     ULONG numberOfConnections;
     ULONG i;
 
-    if (!GetExtendedTcpTable_I || !GetExtendedUdpTable_I)
+    if (!GetExtendedTcpTable_I || !GetExtendedUdpTable_I || !GetNameInfoW_I)
     {
         LoadLibrary(L"iphlpapi.dll");
         GetExtendedTcpTable_I = PhGetProcAddress(L"iphlpapi.dll", "GetExtendedTcpTable");
         GetExtendedUdpTable_I = PhGetProcAddress(L"iphlpapi.dll", "GetExtendedUdpTable");
+        LoadLibrary(L"ws2_32.dll");
+        GetNameInfoW_I = PhGetProcAddress(L"ws2_32.dll", "GetNameInfoW");
     }
 
     if (!PhGetNetworkConnections(&connections, &numberOfConnections))
@@ -288,6 +530,31 @@ VOID PhNetworkProviderUpdate(
         }
     }
 
+    // Go through the queued network item query data.
+    {
+        PPH_NETWORK_ITEM_QUERY_DATA data;
+
+        PhAcquireMutex(&PhNetworkItemQueryQueueLock);
+
+        while (PhDequeueQueueItem(PhNetworkItemQueryQueue, &data))
+        {
+            PhReleaseMutex(&PhNetworkItemQueryQueueLock);
+
+            if (data->Remote)
+                PhSwapReference2(&data->NetworkItem->RemoteHostString, data->HostString);
+            else
+                PhSwapReference2(&data->NetworkItem->LocalHostString, data->HostString);
+
+            data->NetworkItem->JustResolved = TRUE;
+
+            PhDereferenceObject(data->NetworkItem);
+            PhFree(data);
+            PhAcquireMutex(&PhNetworkItemQueryQueueLock);
+        }
+
+        PhReleaseMutex(&PhNetworkItemQueryQueueLock);
+    }
+
     for (i = 0; i < numberOfConnections; i++)
     {
         PPH_NETWORK_ITEM networkItem;
@@ -302,15 +569,21 @@ VOID PhNetworkProviderUpdate(
 
         if (!networkItem)
         {
+            PPHP_RESOLVE_CACHE_ITEM cacheItem;
+
             // Network item not found, create it.
 
             networkItem = PhCreateNetworkItem();
+
+            // Fill in basic information.
             networkItem->ProtocolType = connections[i].ProtocolType;
             networkItem->LocalEndpoint = connections[i].LocalEndpoint;
             networkItem->RemoteEndpoint = connections[i].RemoteEndpoint;
             networkItem->State = connections[i].State;
             networkItem->ProcessId = connections[i].ProcessId;
             memcpy(networkItem->OwnerInfo, connections[i].OwnerInfo, sizeof(ULONGLONG) * PH_NETWORK_OWNER_INFO_SIZE);
+
+            // Format various strings.
 
             networkItem->ProtocolTypeString = PhGetProtocolTypeName(networkItem->ProtocolType);
 
@@ -339,6 +612,43 @@ VOID PhNetworkProviderUpdate(
             if (networkItem->RemoteEndpoint.Address.Type != 0 && networkItem->RemoteEndpoint.Port != 0)
                 PhPrintUInt32(networkItem->RemotePortString, networkItem->RemoteEndpoint.Port);
 
+            // Get host names.
+
+            // Local
+            {
+                PhAcquireQueuedLockSharedFast(&PhpResolveCacheHashtableLock);
+                cacheItem = PhpLookupResolveCacheItem(&networkItem->LocalEndpoint.Address);
+                PhReleaseQueuedLockSharedFast(&PhpResolveCacheHashtableLock);
+
+                if (cacheItem)
+                {
+                    PhReferenceObject(cacheItem->HostString);
+                    networkItem->LocalHostString = cacheItem->HostString;
+                }
+                else
+                {
+                    PhpQueueNetworkItemQuery(networkItem, FALSE);
+                }
+            }
+
+            // Remote
+            if (!PhIsIpAddressNull(&networkItem->RemoteEndpoint.Address))
+            {
+                PhAcquireQueuedLockSharedFast(&PhpResolveCacheHashtableLock);
+                cacheItem = PhpLookupResolveCacheItem(&networkItem->RemoteEndpoint.Address);
+                PhReleaseQueuedLockSharedFast(&PhpResolveCacheHashtableLock);
+
+                if (cacheItem)
+                {
+                    PhReferenceObject(cacheItem->HostString);
+                    networkItem->RemoteHostString = cacheItem->HostString;
+                }
+                else
+                {
+                    PhpQueueNetworkItemQuery(networkItem, TRUE);
+                }
+            }
+
             // Add the network item to the hashtable.
             PhAcquireQueuedLockExclusive(&PhNetworkHashtableLock);
             PhAddHashtableEntry(PhNetworkHashtable, &networkItem);
@@ -349,7 +659,26 @@ VOID PhNetworkProviderUpdate(
         }
         else
         {
-            // TODO
+            BOOLEAN modified = FALSE;
+
+            if (networkItem->JustResolved)
+                modified = TRUE;
+
+            if (networkItem->State != connections[i].State)
+            {
+                networkItem->State = connections[i].State;
+                modified = TRUE;
+            }
+
+            networkItem->JustResolved = FALSE;
+
+            if (modified)
+            {
+                // Raise the network item modified event.
+                PhInvokeCallback(&PhNetworkItemModifiedEvent, networkItem);
+            }
+
+            PhDereferenceObject(networkItem);
         }
     }
 
