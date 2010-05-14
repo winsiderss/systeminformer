0b9736256a8ce51539f54726499c17624cbaa1dd
Wen Jia Liu <wj32.64@gmail.com>
2010-05-14 08:02:18 +0000

    added detection for suspended processes

--- ProcessHacker/include/providers.h
+++ ProcessHacker/include/providers.h
@@ -69,6 +69,7 @@ typedef struct _PH_PROCESS_ITEM
     ULONG IsInSignificantJob : 1;
     ULONG IsPacked : 1;
     ULONG IsPosix : 1;
+    ULONG IsSuspended : 1;
     ULONG IsWow64 : 1;
 
     // Misc.
