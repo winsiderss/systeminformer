52336ac0d053df57499690d69c9bede248ea566c
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-07-06 11:01:51 +0000

    fixed graph tooltip size limit

--- 2.x/trunk/phlib/include/graph.h
+++ 2.x/trunk/phlib/include/graph.h
@@ -111,7 +111,7 @@ typedef struct _PH_GRAPH_GETTOOLTIPTEXT
     ULONG Index;
     ULONG TotalCount;
 
-    PH_STRINGREF Text;
+    PH_STRINGREF Text; // must be null-terminated
 } PH_GRAPH_GETTOOLTIPTEXT, *PPH_GRAPH_GETTOOLTIPTEXT;
 
 #define PH_GRAPH_DATA_COUNT(Width, Step) (((Width) + (Step) - 1) / (Step) + 1) // round up in division
