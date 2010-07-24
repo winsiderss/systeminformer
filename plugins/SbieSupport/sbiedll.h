5cd36db89af894d1c56ea1262b77019ca712d408
Wen Jia Liu <wj32.64@gmail.com>
2010-07-24 11:10:26 +0000

    added SbieSupport plugin

---
+++ plugins/SbieSupport/sbiedll.h
@@ -0,0 +1,14 @@
+#ifndef SBIEDLL_H
+#define SBIEDLL_H
+
+typedef LONG (__stdcall *P_SbieApi_EnumBoxes)(
+        LONG index,                 // initialize to -1
+        WCHAR *box_name);           // pointer to WCHAR [34]
+
+typedef LONG (__stdcall *P_SbieApi_EnumProcessEx)(
+    const WCHAR *box_name,      // pointer to WCHAR [34]
+    BOOLEAN all_sessions,
+    ULONG which_session,
+    ULONG *boxed_pids);         // pointer to ULONG [512]
+
+#endif
