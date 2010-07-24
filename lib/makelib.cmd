471214b3053824675f1d888534d39a6fb1c99621
Wen Jia Liu <wj32.64@gmail.com>
2010-07-24 02:38:41 +0000

    added samlib

--- lib/makelib.cmd
+++ lib/makelib.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-lib /machine:x86 /def:lib32/ntdll.def /out:lib32/ntdll.lib
-"../tools/fixlib/bin/Release/fixlib.exe" lib32/ntdll.lib
-lib /machine:x64 /def:lib64/ntdll.def /out:lib64/ntdll.lib
+lib /machine:x86 /def:lib32/%1.def /out:lib32/%1.lib
+"../tools/fixlib/bin/Release/fixlib.exe" lib32/%1.lib
+lib /machine:x64 /def:lib64/%1.def /out:lib64/%1.lib
