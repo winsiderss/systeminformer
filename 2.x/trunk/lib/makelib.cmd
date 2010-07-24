394ac6b01f65a3f1e551c024dbca7db08453c0dc
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-07-24 01:38:01 +0000

    ntdll32 -> lib32; ntdll64 -> lib64

--- 2.x/trunk/lib/makelib.cmd
+++ 2.x/trunk/lib/makelib.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-lib /machine:x86 /def:ntdll32/ntdll.def /out:ntdll32/ntdll.lib
-"../tools/fixlib/bin/Release/fixlib.exe" ntdll32/ntdll.lib
-lib /machine:x64 /def:ntdll64/ntdll.def /out:ntdll64/ntdll.lib
+lib /machine:x86 /def:lib32/ntdll.def /out:lib32/ntdll.lib
+"../tools/fixlib/bin/Release/fixlib.exe" lib32/ntdll.lib
+lib /machine:x64 /def:lib64/ntdll.def /out:lib64/ntdll.lib
