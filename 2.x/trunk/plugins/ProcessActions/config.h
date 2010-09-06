cf8435b4c41932eca434c0285ee092a4839f555c
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-09-06 02:51:49 +0000

    added config dialog

---
+++ 2.x/trunk/plugins/ProcessActions/config.h
@@ -0,0 +1,9 @@
+#ifndef PA_CONFIG_H
+#define PA_CONFIG_H
+
+VOID PaShowConfigurationDialog(
+    __in HWND ParentWindowHandle,
+    __in PVOID DllBase
+    );
+
+#endif
--- 2.x/trunk/plugins/ProcessActions/resource.h
+++ 2.x/trunk/plugins/ProcessActions/resource.h
@@ -4,6 +4,21 @@
 //
 #define IDD_DIALOG1                     101
 #define IDD_CONFIG                      101
+#define IDC_ENTRIES                     1001
+#define IDC_PROCESSNAME_NAME            1003
+#define IDC_PROCESSNAME_CHECK           1006
+#define IDC_FILENAME_CHECK              1007
+#define IDC_FILENAME_NAME               1008
+#define IDC_REMOVE                      1009
+#define IDC_ADD                         1010
+#define IDC_BROWSE                      1011
+#define IDC_SELECT                      1012
+#define IDC_ENABLED                     1013
+#define IDC_TERMINATE                   1014
+#define IDC_SETPRIORITYCLASS            1015
+#define IDC_SETPRIORITYCLASS_VALUE      1016
+#define IDC_CHECK7                      1017
+#define IDC_SETAFFINITY                 1017
 
 // Next default values for new objects
 // 
@@ -11,7 +26,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
