65ac040ccc8e2d547d42eaf0269f7b1126ae60f8
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-09-10 09:29:27 +0000

    added CLR support for plugins

---
+++ 2.x/trunk/plugins/ProcessHacker2.Api/ProcessHacker/CallbackRegistration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ProcessHacker2.Api
+{
+    public unsafe class CallbackRegistration : IDisposable
+    {
+        private PhCallback* _callback;
+        private PhCallbackRegistration* _registration;
+        private PhCallbackFunction _function;
+        private IntPtr _functionNative;
+
+        public CallbackRegistration(PhCallback* callback, PhCallbackFunction function)
+        {
+            _callback = callback;
+            _function = function;
+
+            _registration = (PhCallbackRegistration*)Marshal.AllocHGlobal(Marshal.SizeOf(typeof(PhCallbackRegistration)));
+
+            _functionNative = Marshal.GetFunctionPointerForDelegate(function);
+
+            NativeApi.PhRegisterCallback(_callback, _functionNative, IntPtr.Zero, _registration);
+        }
+
+        public void Dispose()
+        {
+            if (_registration != null)
+            {
+                NativeApi.PhUnregisterCallback(_callback, _registration);
+                Marshal.FreeHGlobal((IntPtr)_registration);
+            }
+        }
+    }
+}
