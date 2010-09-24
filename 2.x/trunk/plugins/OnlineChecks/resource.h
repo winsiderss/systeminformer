0562b0631407e3812ffc43c8f1fd0ee22c369edf
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-09-24 22:30:58 +0000

    VirusTotal uploading works!

--- 2.x/trunk/plugins/OnlineChecks/resource.h
+++ 2.x/trunk/plugins/OnlineChecks/resource.h
@@ -3,14 +3,17 @@
 // Used by OnlineChecks.rc
 //
 #define ID_SENDTO_SERVICE1              101
+#define IDD_PROGRESS                    101
+#define IDC_BUTTON1                     1001
+#define IDC_MESSAGE                     1002
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
