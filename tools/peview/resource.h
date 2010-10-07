96db3143ab057d886e3bcf5b9bff23e18e3ebe92
Wen Jia Liu <wj32.64@gmail.com>
2010-10-07 23:28:16 +0000

    added time stamp display to peview

--- tools/peview/resource.h
+++ tools/peview/resource.h
@@ -12,7 +12,7 @@
 #define IDC_SUBSYSTEMVERSION            1006
 #define IDC_CHARACTERISTICS             1007
 #define IDC_LIST                        1008
-#define IDC_EDIT1                       1009
+#define IDC_TIMESTAMP                   1010
 
 // Next default values for new objects
 // 
@@ -20,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
