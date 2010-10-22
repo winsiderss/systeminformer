57ae9d619e4509671044a324158fdfb403f4392c
Wen Jia Liu <wj32.64@gmail.com>
2010-10-22 02:25:55 +0000

    added CLR tab to peview

--- tools/peview/resource.h
+++ tools/peview/resource.h
@@ -6,6 +6,7 @@
 #define IDD_PEIMPORTS                   103
 #define IDD_PEEXPORTS                   104
 #define IDD_LIBEXPORTS                  105
+#define IDD_PECLR                       106
 #define IDC_TARGETMACHINE               1003
 #define IDC_CHECKSUM                    1004
 #define IDC_SUBSYSTEM                   1005
@@ -13,14 +14,17 @@
 #define IDC_CHARACTERISTICS             1007
 #define IDC_LIST                        1008
 #define IDC_TIMESTAMP                   1010
+#define IDC_RUNTIMEVERSION              1011
+#define IDC_FLAGS                       1012
+#define IDC_EDIT1                       1013
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
