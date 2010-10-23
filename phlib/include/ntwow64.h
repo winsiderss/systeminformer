621da2657840bfc6f67159edb372d4c587fa30fd
Wen Jia Liu <wj32.64@gmail.com>
2010-10-23 07:43:36 +0000

    added WOW64_SHARED_INFORMATION

--- phlib/include/ntwow64.h
+++ phlib/include/ntwow64.h
@@ -1,6 +1,30 @@
 #ifndef _NTWOW64_H
 #define _NTWOW64_H
 
+#define WOW64_SYSTEM_DIRECTORY "SysWOW64"
+#define WOW64_SYSTEM_DIRECTORY_U L"SysWOW64"
+#define WOW64_X86_TAG " (x86)"
+#define WOW64_X86_TAG_U L" (x86)"
+
+// In USER_SHARED_DATA
+// symbols
+typedef enum _WOW64_SHARED_INFORMATION
+{
+    SharedNtdll32LdrInitializeThunk = 0,
+    SharedNtdll32KiUserExceptionDispatcher = 1,
+    SharedNtdll32KiUserApcDispatcher = 2,
+    SharedNtdll32KiUserCallbackDispatcher = 3,
+    SharedNtdll32LdrHotPatchRoutine = 4,
+    SharedNtdll32ExpInterlockedPopEntrySListFault = 5,
+    SharedNtdll32ExpInterlockedPopEntrySListResume = 6,
+    SharedNtdll32ExpInterlockedPopEntrySListEnd = 7,
+    SharedNtdll32RtlUserThreadStart = 8,
+    SharedNtdll32pQueryProcessDebugInformationRemote = 9,
+    SharedNtdll32EtwpNotificationThread = 10,
+    SharedNtdll32BaseAddress = 11,
+    Wow64SharedPageEntriesCount = 12
+} WOW64_SHARED_INFORMATION;
+
 // 32-bit definitions
 
 #define WOW64_POINTER(Type) ULONG
