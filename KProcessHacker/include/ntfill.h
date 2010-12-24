266fa8465c033875797565169bfa5d6477736d91
Wen Jia Liu <wj32.64@gmail.com>
2010-12-24 23:07:49 +0000

    * completed basic process functions * added thread functions

--- KProcessHacker/include/ntfill.h
+++ KProcessHacker/include/ntfill.h
@@ -1,7 +1,7 @@
 #ifndef NTFILL_H
 #define NTFILL_H
 
-// Processes
+// Process manager
 
 typedef NTSTATUS (NTAPI *_PsSuspendProcess)(
     __in PEPROCESS Process
@@ -9,6 +9,47 @@ typedef NTSTATUS (NTAPI *_PsSuspendProcess)(
 
 typedef NTSTATUS (NTAPI *_PsResumeProcess)(
     __in PEPROCESS Process
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+ZwQueryInformationProcess(
+    __in HANDLE ProcessHandle,
+    __in PROCESSINFOCLASS ProcessInformationClass,
+    __out_bcount(ProcessInformationLength) PVOID ProcessInformation,
+    __in ULONG ProcessInformationLength,
+    __out_opt PULONG ReturnLength
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+ZwSetInformationProcess(
+    __in HANDLE ProcessHandle,
+    __in PROCESSINFOCLASS ProcessInformationClass,
+    __in_bcount(ProcessInformationLength) PVOID ProcessInformation,
+    __in ULONG ProcessInformationLength
+    );
+
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+ZwQueryInformationThread(
+    __in HANDLE ThreadHandle,
+    __in THREADINFOCLASS ThreadInformationClass,
+    __out_bcount(ThreadInformationLength) PVOID ThreadInformation,
+    __in ULONG ThreadInformationLength,
+    __out_opt PULONG ReturnLength
+    );
+
+NTKERNELAPI
+NTSTATUS
+NTAPI
+PsLookupProcessThreadByCid(
+    __in PCLIENT_ID ClientId,
+    __out_opt PEPROCESS *Process,
+    __out PETHREAD *Thread
     );
 
 typedef struct _EJOB *PEJOB;
