54ddda7fba76bc4d90ee023ebd73d70bb8ef7a1c
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2010-12-26 04:04:56 +0000

    * removed useless casts in PhReadVirtualMemory/PhWriteVirtualMemory * updated auto.cmd

--- 2.x/trunk/KProcessHacker/auto.cmd
+++ 2.x/trunk/KProcessHacker/auto.cmd
@@ -4,4 +4,6 @@ build -cZ
 if not %errorlevel%==0 goto end
 copy bin\i386\kprocesshacker.sys ..\bin\Release32\
 copy bin\i386\kprocesshacker.pdb ..\bin\Release32\
+copy bin\amd64\kprocesshacker.sys ..\bin\Release64\
+copy bin\amd64\kprocesshacker.pdb ..\bin\Release64\
 :end
