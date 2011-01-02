ca1e89df94987a7db75f1870030978189c8e52e8
Wen Jia Liu <wj32.64@gmail.com>
2011-01-02 10:53:02 +0000

    build script now creates everything for release

--- build/internal/dobuildrelease.cmd
+++ build/internal/dobuildrelease.cmd
@@ -3,6 +3,15 @@
 rem The first parameter specifies the ProcessHacker2 
 rem base directory.
 rem The second parameter specifies the output directory.
+rem Variables:
+rem INNOBIN - Specify the path to Inno Setup.
+rem     e.g. C:\Program Files\Inno Setup 5
+rem SVNBIN - Specify the path to the SVN client.
+rem SEVENZIPBIN - Specify the path to 7-Zip.
+rem     e.g. C:\Program Files\7-Zip
+rem MINORVERSION - Specify the minor version of 
+rem     the Process Hacker release being built.
+rem     e.g. 8
 
 rem Build the main projects
 
@@ -20,11 +29,11 @@ call %1\build\internal\wait.cmd 2
 
 rem Build the installer (HARDCODED PATH)
 
-if exist "C:\Program Files\Inno Setup 5\iscc.exe". (
-pushd %1\build\Installer\
-del *.exe
-"C:\Program Files\Inno Setup 5\iscc.exe" Process_Hacker2_installer.iss
-popd
+if exist "%INNOBIN%\iscc.exe". (
+    pushd %1\build\Installer\
+    del *.exe
+    "%INNOBIN%\iscc.exe" Process_Hacker2_installer.iss
+    popd
 )
 
 call %1\build\internal\dorelease.cmd %1 %2
