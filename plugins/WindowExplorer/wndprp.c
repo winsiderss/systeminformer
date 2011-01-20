0e2549b818bcc14cc1a5688d83b0ca301b637907
Wen Jia Liu <wj32.64@gmail.com>
2011-01-20 09:49:13 +0000

    implemented Properties page

--- plugins/WindowExplorer/wndprp.c
+++ plugins/WindowExplorer/wndprp.c
@@ -26,6 +26,8 @@
 #include "wndexp.h"
 #include "resource.h"
 
+#define NUMBER_OF_PAGES 2
+
 typedef struct _WINDOW_PROPERTIES_CONTEXT
 {
     HWND ParentWindowHandle;
@@ -36,8 +38,40 @@ HWND WepCreateWindowProperties(
     __in PWINDOW_PROPERTIES_CONTEXT Context
     );
 
+INT CALLBACK WepPropSheetProc(
+    __in HWND hwndDlg,
+    __in UINT uMsg,
+    __in LPARAM lParam
+    );
+
+LRESULT CALLBACK WepPropSheetWndProc(
+    __in HWND hwnd,
+    __in UINT uMsg,
+    __in WPARAM wParam,
+    __in LPARAM lParam
+    );
+
+HPROPSHEETPAGE WepCommonCreatePage(
+    __in PWINDOW_PROPERTIES_CONTEXT Context,
+    __in PWSTR Template,
+    __in DLGPROC DlgProc
+    );
+
+INT CALLBACK WepCommonPropPageProc(
+    __in HWND hwnd,
+    __in UINT uMsg,
+    __in LPPROPSHEETPAGE ppsp
+    );
+
 NTSTATUS WepPropertiesThreadStart(
     __in PVOID Parameter
+    );
+
+INT_PTR CALLBACK WepWindowGeneralDlgProc(
+    __in HWND hwndDlg,
+    __in UINT uMsg,
+    __in WPARAM wParam,
+    __in LPARAM lParam
     );
 
 INT_PTR CALLBACK WepWindowPropertiesDlgProc(
@@ -58,7 +92,6 @@ VOID WeShowWindowProperties(
     __in HWND WindowHandle
     )
 {
-    HWND hwnd;
     PWINDOW_PROPERTIES_CONTEXT context;
 
     if (!WePropertiesCreateList)
@@ -72,45 +105,182 @@ VOID WeShowWindowProperties(
         PhWaitForEvent(&WePropertiesThreadReadyEvent, NULL);
     }
 
-    context = PhAllocate(sizeof(WINDOW_PROPERTIES_CONTEXT));
-    memset(context, 0, sizeof(WINDOW_PROPERTIES_CONTEXT));
-    context->ParentWindowHandle = ParentWindowHandle;
-    context->WindowHandle = WindowHandle;
+    if (NT_SUCCESS(PhCreateAlloc(&context, sizeof(WINDOW_PROPERTIES_CONTEXT))))
+    {
+        memset(context, 0, sizeof(WINDOW_PROPERTIES_CONTEXT));
+        context->ParentWindowHandle = ParentWindowHandle;
+        context->WindowHandle = WindowHandle;
 
-    // Queue the window for creation and wake up the host thread.
-    PhAddItemList(WePropertiesCreateList, context);
-    PostThreadMessage((ULONG)WePropertiesThreadClientId.UniqueThread, WM_NULL, 0, 0);
+        // Queue the window for creation and wake up the host thread.
+        PhAddItemList(WePropertiesCreateList, context);
+        PostThreadMessage((ULONG)WePropertiesThreadClientId.UniqueThread, WM_NULL, 0, 0);
+    }
 }
 
-HWND WepCreateWindowProperties(
+static HWND WepCreateWindowProperties(
     __in PWINDOW_PROPERTIES_CONTEXT Context
     )
 {
     PROPSHEETHEADER propSheetHeader = { sizeof(propSheetHeader) };
-    PROPSHEETPAGE propSheetPage;
-    HPROPSHEETPAGE pages[4];
+    HPROPSHEETPAGE pages[NUMBER_OF_PAGES];
 
     propSheetHeader.dwFlags =
         PSH_MODELESS |
         PSH_NOAPPLYNOW |
         PSH_NOCONTEXTHELP |
-        PSH_PROPTITLE;
+        PSH_PROPTITLE |
+        PSH_USECALLBACK;
     propSheetHeader.hwndParent = Context->ParentWindowHandle;
     propSheetHeader.pszCaption = PhaFormatString(L"Window %Ix", Context->WindowHandle)->Buffer;
     propSheetHeader.nPages = 0;
     propSheetHeader.nStartPage = 0;
     propSheetHeader.phpage = pages;
+    propSheetHeader.pfnCallback = WepPropSheetProc;
 
-    // Properties page
-    memset(&propSheetPage, 0, sizeof(PROPSHEETPAGE));
-    propSheetPage.dwSize = sizeof(PROPSHEETPAGE);
-    propSheetPage.hInstance = PluginInstance->DllBase;
-    propSheetPage.pszTemplate = MAKEINTRESOURCE(IDD_WNDPROPS);
-    propSheetPage.pfnDlgProc = WepWindowPropertiesDlgProc;
-    propSheetPage.lParam = (LPARAM)Context;
-    pages[propSheetHeader.nPages++] = CreatePropertySheetPage(&propSheetPage);
+    // General
+    pages[propSheetHeader.nPages++] = WepCommonCreatePage(
+        Context,
+        MAKEINTRESOURCE(IDD_WNDGENERAL),
+        WepWindowGeneralDlgProc
+        );
+    // Properties
+    pages[propSheetHeader.nPages++] = WepCommonCreatePage(
+        Context,
+        MAKEINTRESOURCE(IDD_WNDPROPS),
+        WepWindowPropertiesDlgProc
+        );
 
     return (HWND)PropertySheet(&propSheetHeader);
+}
+
+static INT CALLBACK WepPropSheetProc(
+    __in HWND hwndDlg,
+    __in UINT uMsg,
+    __in LPARAM lParam
+    )
+{
+    switch (uMsg)
+    {
+    case PSCB_INITIALIZED:
+        {
+            WNDPROC oldWndProc;
+            HWND refreshButtonHandle;
+
+            oldWndProc = (WNDPROC)GetWindowLongPtr(hwndDlg, GWLP_WNDPROC);
+            SetWindowLongPtr(hwndDlg, GWLP_WNDPROC, (LONG_PTR)WepPropSheetWndProc);
+            SetProp(hwndDlg, L"OldWndProc", (HANDLE)oldWndProc);
+
+            // Hide the Cancel button.
+            ShowWindow(GetDlgItem(hwndDlg, IDCANCEL), SW_HIDE);
+            // Set the OK button's text to "Close".
+            SetDlgItemText(hwndDlg, IDOK, L"Close");
+            // Add the Refresh button.
+            refreshButtonHandle = CreateWindow(L"BUTTON", L"Refresh", WS_CHILD | WS_TABSTOP | WS_VISIBLE, 0, 0, 3, 3, hwndDlg, (HMENU)IDC_REFRESH,
+                PluginInstance->DllBase, NULL);
+            SendMessage(refreshButtonHandle, WM_SETFONT, (WPARAM)SendMessage(GetDlgItem(hwndDlg, IDOK), WM_GETFONT, 0, 0), FALSE);
+        }
+        break;
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WepPropSheetWndProc(
+    __in HWND hwnd,
+    __in UINT uMsg,
+    __in WPARAM wParam,
+    __in LPARAM lParam
+    )
+{
+    WNDPROC oldWndProc = (WNDPROC)GetProp(hwnd, L"OldWndProc");
+
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        {
+            SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)oldWndProc);
+            RemoveProp(hwnd, L"OldWndProc");
+            RemoveProp(hwnd, L"Moved");
+        }
+        break;
+    case WM_SHOWWINDOW:
+        {
+            if (!GetProp(hwnd, L"Moved"))
+            {
+                // Move the Refresh button to where the OK button is, and move the OK button to 
+                // where the Cancel button is.
+                // This must be done here because in the prop sheet callback the buttons are not 
+                // in the right places.
+                PhCopyControlRectangle(hwnd, GetDlgItem(hwnd, IDOK), GetDlgItem(hwnd, IDC_REFRESH));
+                PhCopyControlRectangle(hwnd, GetDlgItem(hwnd, IDCANCEL), GetDlgItem(hwnd, IDOK));
+                SetProp(hwnd, L"Moved", (HANDLE)1);
+            }
+        }
+        break;
+    case WM_COMMAND:
+        {
+            switch (LOWORD(wParam))
+            {
+            case IDC_REFRESH:
+                {
+                    ULONG i;
+                    HWND pageHandle;
+
+                    // Broadcast the message to all property pages.
+                    for (i = 0; i < NUMBER_OF_PAGES; i++)
+                    {
+                        if (pageHandle = PropSheet_IndexToHwnd(hwnd, i))
+                            SendMessage(pageHandle, WM_COMMAND, IDC_REFRESH, 0);
+                    }
+                }
+                break;
+            }
+        }
+        break;
+    }
+
+    return CallWindowProc(oldWndProc, hwnd, uMsg, wParam, lParam);
+}
+
+static HPROPSHEETPAGE WepCommonCreatePage(
+    __in PWINDOW_PROPERTIES_CONTEXT Context,
+    __in PWSTR Template,
+    __in DLGPROC DlgProc
+    )
+{
+    HPROPSHEETPAGE propSheetPageHandle;
+    PROPSHEETPAGE propSheetPage;
+
+    memset(&propSheetPage, 0, sizeof(PROPSHEETPAGE));
+    propSheetPage.dwSize = sizeof(PROPSHEETPAGE);
+    propSheetPage.dwFlags = PSP_USECALLBACK;
+    propSheetPage.hInstance = PluginInstance->DllBase;
+    propSheetPage.pszTemplate = Template;
+    propSheetPage.pfnDlgProc = DlgProc;
+    propSheetPage.lParam = (LPARAM)Context;
+    propSheetPage.pfnCallback = WepCommonPropPageProc;
+
+    propSheetPageHandle = CreatePropertySheetPage(&propSheetPage);
+
+    return propSheetPageHandle;
+}
+
+static INT CALLBACK WepCommonPropPageProc(
+    __in HWND hwnd,
+    __in UINT uMsg,
+    __in LPPROPSHEETPAGE ppsp
+    )
+{
+    PWINDOW_PROPERTIES_CONTEXT context;
+
+    context = (PWINDOW_PROPERTIES_CONTEXT)ppsp->lParam;
+
+    if (uMsg == PSPCB_ADDREF)
+        PhReferenceObject(context);
+    else if (uMsg == PSPCB_RELEASE)
+        PhDereferenceObject(context);
+
+    return 1;
 }
 
 NTSTATUS WepPropertiesThreadStart(
@@ -140,9 +310,12 @@ NTSTATUS WepPropertiesThreadStart(
         {
             for (i = 0; i < WePropertiesCreateList->Count; i++)
             {
+                PWINDOW_PROPERTIES_CONTEXT context;
                 HWND hwnd;
 
-                hwnd = WepCreateWindowProperties(WePropertiesCreateList->Items[i]);
+                context = WePropertiesCreateList->Items[i];
+                hwnd = WepCreateWindowProperties(context);
+                PhDereferenceObject(context);
                 PhAddItemList(WePropertiesWindowList, hwnd);
             }
 
@@ -166,8 +339,6 @@ NTSTATUS WepPropertiesThreadStart(
             DispatchMessage(&message);
         }
 
-        PhDrainAutoPool(&autoPool);
-
         // Destroy properties windows when necessary.
         for (i = 0; i < WePropertiesWindowList->Count; i++)
         {
@@ -178,11 +349,98 @@ NTSTATUS WepPropertiesThreadStart(
                 i--;
             }
         }
+
+        PhDrainAutoPool(&autoPool);
     }
 
     PhDeleteAutoPool(&autoPool);
 
     return STATUS_SUCCESS;
+}
+
+FORCEINLINE BOOLEAN WepPropPageDlgProcHeader(
+    __in HWND hwndDlg,
+    __in UINT uMsg,
+    __in LPARAM lParam,
+    __out_opt LPPROPSHEETPAGE *PropSheetPage,
+    __out_opt PWINDOW_PROPERTIES_CONTEXT *Context
+    )
+{
+    LPPROPSHEETPAGE propSheetPage;
+
+    if (uMsg == WM_INITDIALOG)
+    {
+        propSheetPage = (LPPROPSHEETPAGE)lParam;
+        // Save the context.
+        SetProp(hwndDlg, L"PropSheetPage", (HANDLE)lParam);
+    }
+    else
+    {
+        propSheetPage = (LPPROPSHEETPAGE)GetProp(hwndDlg, L"PropSheetPage");
+
+        if (uMsg == WM_DESTROY)
+            RemoveProp(hwndDlg, L"PropSheetPage");
+    }
+
+    if (!propSheetPage)
+        return FALSE;
+
+    if (PropSheetPage)
+        *PropSheetPage = propSheetPage;
+    if (Context)
+        *Context = (PWINDOW_PROPERTIES_CONTEXT)propSheetPage->lParam;
+
+    return TRUE;
+}
+
+INT_PTR CALLBACK WepWindowGeneralDlgProc(
+    __in HWND hwndDlg,
+    __in UINT uMsg,
+    __in WPARAM wParam,
+    __in LPARAM lParam
+    )
+{
+    return FALSE;
+}
+
+static BOOL CALLBACK EnumPropsExCallback(
+    __in HWND hwnd,
+    __in LPTSTR lpszString,
+    __in HANDLE hData,
+    __in ULONG_PTR dwData
+    )
+{
+    INT lvItemIndex;
+    PWSTR propName;
+    WCHAR value[PH_PTR_STR_LEN_1];
+
+    propName = lpszString;
+
+    if ((ULONG_PTR)lpszString < 0x10000)
+    {
+        // The property has an atom name.
+        propName = PhaFormatString(L"(Atom %u)", (ULONG)lpszString)->Buffer;
+    }
+
+    lvItemIndex = PhAddListViewItem((HWND)dwData, MAXINT, propName, NULL);
+
+    PhPrintPointer(value, (PVOID)hData);
+    PhSetListViewSubItem((HWND)dwData, lvItemIndex, 1, value);
+
+    return TRUE;
+}
+
+static VOID WepRefreshWindowProps(
+    __in HWND hwndDlg,
+    __in HWND ListViewHandle,
+    __in PWINDOW_PROPERTIES_CONTEXT Context
+    )
+{
+    ExtendedListView_SetRedraw(ListViewHandle, FALSE);
+    ListView_DeleteAllItems(ListViewHandle);
+    EnumPropsEx(Context->WindowHandle, EnumPropsExCallback, (LPARAM)ListViewHandle);
+    ExtendedListView_SortItems(ListViewHandle);
+    ExtendedListView_SetRedraw(ListViewHandle, TRUE);
 }
 
 INT_PTR CALLBACK WepWindowPropertiesDlgProc(
@@ -192,5 +450,39 @@ INT_PTR CALLBACK WepWindowPropertiesDlgProc(
     __in LPARAM lParam
     )
 {
+    PWINDOW_PROPERTIES_CONTEXT context;
+
+    if (!WepPropPageDlgProcHeader(hwndDlg, uMsg, lParam, NULL, &context))
+        return FALSE;
+
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+        {
+            HWND lvHandle;
+
+            lvHandle = GetDlgItem(hwndDlg, IDC_LIST);
+            PhSetListViewStyle(lvHandle, FALSE, TRUE);
+            PhSetControlTheme(lvHandle, L"explorer");
+
+            PhAddListViewColumn(lvHandle, 0, 0, 0, LVCFMT_LEFT, 160, L"Name");
+            PhAddListViewColumn(lvHandle, 1, 1, 1, LVCFMT_LEFT, 100, L"Value");
+            PhSetExtendedListView(lvHandle);
+
+            WepRefreshWindowProps(hwndDlg, lvHandle, context);
+        }
+        break;
+    case WM_COMMAND:
+        {
+            switch (LOWORD(wParam))
+            {
+            case IDC_REFRESH:
+                WepRefreshWindowProps(hwndDlg, GetDlgItem(hwndDlg, IDC_LIST), context);
+                break;
+            }
+        }
+        break;
+    }
+
     return FALSE;
 }
