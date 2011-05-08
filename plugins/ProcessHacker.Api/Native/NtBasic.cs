692ab019098ccbf721514b7d25b7b403cc905e99
dmex <dmex04@gmail.com>
2011-05-08 13:03:36 +0000

    Updated Managed plugin wrapper: -Removed unnecessary code -Fixed typo -Added GPL3 License information

--- plugins/ProcessHacker.Api/Native/NtBasic.cs
+++ plugins/ProcessHacker.Api/Native/NtBasic.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+ * Process Hacker - 
+ *   NT Basic definitions
+ * 
+ * Copyright (C) 2011 wj32
+ * Copyright (C) 2011 dmex
+ * 
+ * This file is part of Process Hacker.
+ * 
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Runtime.InteropServices;
 
 namespace ProcessHacker.Api
--- plugins/ProcessHacker.Api/ProcessHacker/ProcessTreeFilter.cs
+++ plugins/ProcessHacker.Api/ProcessHacker/ProcessTreeFilter.cs
@@ -1,5 +1,27 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿/*
+ * Process Hacker - 
+ *   Process Tree Filter API
+ * 
+ * Copyright (C) 2011 wj32
+ * Copyright (C) 2011 dmex
+ * 
+ * This file is part of Process Hacker.
+ * 
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 
 namespace ProcessHacker.Api
 {
@@ -11,16 +33,10 @@ namespace ProcessHacker.Api
         }
 
         private void* _filter;
-        private PhProcessTreeFilter _function;
-        private IntPtr _functionNative;
 
         public ProcessTreeFilter(PhProcessTreeFilter function)
         {
-            _function = function;
-
-            _functionNative = Marshal.GetFunctionPointerForDelegate(function);
-
-            _filter = NativeApi.PhAddProcessTreeFilter(_functionNative, IntPtr.Zero);
+            _filter = NativeApi.PhAddProcessTreeFilter(function, IntPtr.Zero);
         }
 
         public void Dispose()
