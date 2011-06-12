73a89fcc03cbf390c49960f80d8f3654e4dfe244
Wen Jia Liu <wj32.64@gmail.com>
2011-06-12 11:28:23 +0000

    ExtendedServices: added support for editing preshutdown time-out and required privileges

--- plugins/ExtendedServices/other.c
+++ plugins/ExtendedServices/other.c
@@ -29,7 +29,12 @@ typedef struct _SERVICE_OTHER_CONTEXT
 {
     PPH_SERVICE_ITEM ServiceItem;
 
+    BOOLEAN Ready;
+    BOOLEAN Dirty;
+    BOOLEAN PreshutdownTimeoutValid;
+    BOOLEAN RequiredPrivilegesValid;
     HWND PrivilegesLv;
+    PPH_LIST PrivilegeList;
     HWND TriggersLv;
     struct _ES_TRIGGER_CONTEXT *TriggerContext;
 } SERVICE_OTHER_CONTEXT, *PSERVICE_OTHER_CONTEXT;
@@ -42,7 +47,7 @@ NTSTATUS EspLoadOtherInfo(
     NTSTATUS status = STATUS_SUCCESS;
     SC_HANDLE serviceHandle;
     SERVICE_PRESHUTDOWN_INFO preshutdownInfo;
-    LPSERVICE_REQUIRED_PRIVILEGES_INFO requiredPrivileges;
+    LPSERVICE_REQUIRED_PRIVILEGES_INFO requiredPrivilegesInfo;
     ULONG returnLength;
 
     if (!(serviceHandle = PhOpenService(Context->ServiceItem->Name->Buffer, SERVICE_QUERY_CONFIG)))
@@ -58,19 +63,21 @@ NTSTATUS EspLoadOtherInfo(
         ))
     {
         SetDlgItemInt(hwndDlg, IDC_PRESHUTDOWNTIMEOUT, preshutdownInfo.dwPreshutdownTimeout, FALSE);
+        Context->PreshutdownTimeoutValid = TRUE;
     }
 
     // Required privileges
 
-    if (requiredPrivileges = PhQueryServiceVariableSize(serviceHandle, SERVICE_CONFIG_REQUIRED_PRIVILEGES_INFO))
+    if (requiredPrivilegesInfo = PhQueryServiceVariableSize(serviceHandle, SERVICE_CONFIG_REQUIRED_PRIVILEGES_INFO))
     {
         PWSTR privilege;
         ULONG privilegeLength;
         INT lvItemIndex;
         PH_STRINGREF privilegeSr;
+        PPH_STRING privilegeString;
         PPH_STRING displayName;
 
-        privilege = requiredPrivileges->pmszRequiredPrivileges;
+        privilege = requiredPrivilegesInfo->pmszRequiredPrivileges;
 
         if (privilege)
         {
@@ -81,7 +88,10 @@ NTSTATUS EspLoadOtherInfo(
                 if (privilegeLength == 0)
                     break;
 
-                lvItemIndex = PhAddListViewItem(Context->PrivilegesLv, MAXINT, privilege, NULL);
+                privilegeString = PhCreateStringEx(privilege, privilegeLength * sizeof(WCHAR));
+                PhAddItemList(Context->PrivilegeList, privilegeString);
+
+                lvItemIndex = PhAddListViewItem(Context->PrivilegesLv, MAXINT, privilege, privilegeString);
                 privilegeSr.Buffer = privilege;
                 privilegeSr.Length = (USHORT)(privilegeLength * sizeof(WCHAR));
 
@@ -97,7 +107,8 @@ NTSTATUS EspLoadOtherInfo(
 
         ExtendedListView_SortItems(Context->PrivilegesLv);
 
-        PhFree(requiredPrivileges);
+        PhFree(requiredPrivilegesInfo);
+        Context->RequiredPrivilegesValid = TRUE;
     }
 
     // Triggers
@@ -110,6 +121,17 @@ NTSTATUS EspLoadOtherInfo(
     CloseServiceHandle(serviceHandle);
 
     return status;
+}
+
+static int __cdecl PrivilegeNameCompareFunction(
+    __in const void *elem1,
+    __in const void *elem2
+    )
+{
+    PWSTR string1 = *(PWSTR *)elem1;
+    PWSTR string2 = *(PWSTR *)elem2;
+
+    return wcscmp(string1, string2);
 }
 
 INT_PTR CALLBACK EspServiceOtherDlgProc(
@@ -158,6 +180,9 @@ INT_PTR CALLBACK EspServiceOtherDlgProc(
             PhAddListViewColumn(privilegesLv, 1, 1, 1, LVCFMT_LEFT, 220, L"Display Name");
             PhSetExtendedListView(privilegesLv);
 
+            context->PrivilegeList = PhCreateList(32);
+            EnableWindow(GetDlgItem(hwndDlg, IDC_REMOVE), FALSE);
+
             context->TriggersLv = triggersLv = GetDlgItem(hwndDlg, IDC_TRIGGERS);
 
             if (WindowsVersion >= WINDOWS_7)
@@ -184,10 +209,18 @@ INT_PTR CALLBACK EspServiceOtherDlgProc(
                 PhShowWarning(hwndDlg, L"Unable to query service information: %s",
                     ((PPH_STRING)PHA_DEREFERENCE(PhGetNtMessage(status)))->Buffer);
             }
+
+            context->Ready = TRUE;
         }
         break;
     case WM_DESTROY:
         {
+            if (context->PrivilegeList)
+            {
+                PhDereferenceObjects(context->PrivilegeList->Items, context->PrivilegeList->Count);
+                PhDereferenceObject(context->PrivilegeList);
+            }
+
             if (context->TriggerContext)
                 EsDestroyServiceTriggerContext(context->TriggerContext);
 
@@ -198,6 +231,144 @@ INT_PTR CALLBACK EspServiceOtherDlgProc(
         {
             switch (LOWORD(wParam))
             {
+            case IDC_ADD:
+                {
+                    NTSTATUS status;
+                    LSA_HANDLE policyHandle;
+                    LSA_ENUMERATION_HANDLE enumContext;
+                    PPOLICY_PRIVILEGE_DEFINITION buffer;
+                    ULONG count;
+                    ULONG i;
+                    PPH_LIST choices;
+                    PPH_STRING selectedChoice = NULL;
+
+                    choices = PhCreateList(100);
+
+                    if (!NT_SUCCESS(status = PhOpenLsaPolicy(&policyHandle, POLICY_VIEW_LOCAL_INFORMATION, NULL)))
+                    {
+                        PhShowStatus(hwndDlg, L"Unable to open LSA policy", status, 0);
+                        break;
+                    }
+
+                    enumContext = 0;
+
+                    while (TRUE)
+                    {
+                        status = LsaEnumeratePrivileges(
+                            policyHandle,
+                            &enumContext,
+                            &buffer,
+                            0x100,
+                            &count
+                            );
+
+                        if (status == STATUS_NO_MORE_ENTRIES)
+                            break;
+                        if (!NT_SUCCESS(status))
+                            break;
+
+                        for (i = 0; i < count; i++)
+                        {
+                            PhAddItemList(choices, PhaCreateStringEx(buffer[i].Name.Buffer, buffer[i].Name.Length)->Buffer);
+                        }
+                    }
+
+                    LsaClose(policyHandle);
+
+                    qsort(choices->Items, choices->Count, sizeof(PWSTR), PrivilegeNameCompareFunction);
+
+                    while (PhaChoiceDialog(
+                        hwndDlg,
+                        L"Add privilege",
+                        L"Select a privilege to add:",
+                        (PWSTR *)choices->Items,
+                        choices->Count,
+                        NULL,
+                        PH_CHOICE_DIALOG_CHOICE,
+                        &selectedChoice,
+                        NULL,
+                        NULL
+                        ))
+                    {
+                        BOOLEAN found = FALSE;
+                        PPH_STRING privilegeString;
+                        INT lvItemIndex;
+                        PPH_STRING displayName;
+
+                        // Check for duplicates.
+                        for (i = 0; i < context->PrivilegeList->Count; i++)
+                        {
+                            if (PhEqualString(context->PrivilegeList->Items[i], selectedChoice, FALSE))
+                            {
+                                found = TRUE;
+                                break;
+                            }
+                        }
+
+                        if (found)
+                        {
+                            if (PhShowMessage(
+                                hwndDlg,
+                                MB_OKCANCEL | MB_ICONERROR,
+                                L"The selected privilege has already been added."
+                                ) == IDOK)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        privilegeString = selectedChoice;
+                        PhReferenceObject(privilegeString);
+
+                        PhAddItemList(context->PrivilegeList, privilegeString);
+
+                        lvItemIndex = PhAddListViewItem(context->PrivilegesLv, MAXINT, privilegeString->Buffer, privilegeString);
+
+                        if (PhLookupPrivilegeDisplayName(&privilegeString->sr, &displayName))
+                        {
+                            PhSetListViewSubItem(context->PrivilegesLv, lvItemIndex, 1, displayName->Buffer);
+                            PhDereferenceObject(displayName);
+                        }
+
+                        ExtendedListView_SortItems(context->PrivilegesLv);
+
+                        context->Dirty = TRUE;
+                        context->RequiredPrivilegesValid = TRUE;
+
+                        break;
+                    }
+
+                    PhDereferenceObject(choices);
+                }
+                break;
+            case IDC_REMOVE:
+                {
+                    INT lvItemIndex;
+                    PPH_STRING privilegeString;
+                    ULONG index;
+
+                    lvItemIndex = ListView_GetNextItem(context->PrivilegesLv, -1, LVNI_SELECTED);
+
+                    if (lvItemIndex != -1 && PhGetListViewItemParam(context->PrivilegesLv, lvItemIndex, (PPVOID)&privilegeString))
+                    {
+                        index = PhFindItemList(context->PrivilegeList, privilegeString);
+
+                        if (index != -1)
+                        {
+                            PhDereferenceObject(privilegeString);
+                            PhRemoveItemList(context->PrivilegeList, index);
+                            PhRemoveListViewItem(context->PrivilegesLv, lvItemIndex);
+
+                            context->Dirty = TRUE;
+                            context->RequiredPrivilegesValid = TRUE;
+                        }
+                    }
+                }
+                break;
             case IDC_NEW:
                 if (context->TriggerContext)
                     EsHandleEventServiceTrigger(context->TriggerContext, ES_TRIGGER_EVENT_NEW);
@@ -209,6 +380,19 @@ INT_PTR CALLBACK EspServiceOtherDlgProc(
             case IDC_DELETE:
                 if (context->TriggerContext)
                     EsHandleEventServiceTrigger(context->TriggerContext, ES_TRIGGER_EVENT_DELETE);
+                break;
+            }
+
+            switch (HIWORD(wParam))
+            {
+            case EN_CHANGE:
+                {
+                    if (context->Ready)
+                    {
+                        context->Dirty = TRUE;
+                        context->PreshutdownTimeoutValid = TRUE;
+                    }
+                }
                 break;
             }
         }
@@ -226,9 +410,72 @@ INT_PTR CALLBACK EspServiceOtherDlgProc(
                 return TRUE;
             case PSN_APPLY:
                 {
-                    ULONG win32Result;
+                    SC_HANDLE serviceHandle;
+                    ULONG win32Result = 0;
 
                     SetWindowLongPtr(hwndDlg, DWLP_MSGRESULT, PSNRET_NOERROR);
+
+                    if (context->Dirty)
+                    {
+                        if (serviceHandle = PhOpenService(context->ServiceItem->Name->Buffer, SERVICE_CHANGE_CONFIG))
+                        {
+                            SERVICE_PRESHUTDOWN_INFO preshutdownInfo;
+                            SERVICE_REQUIRED_PRIVILEGES_INFO requiredPrivilegesInfo;
+
+                            if (context->PreshutdownTimeoutValid)
+                            {
+                                preshutdownInfo.dwPreshutdownTimeout = GetDlgItemInt(hwndDlg, IDC_PRESHUTDOWNTIMEOUT, NULL, FALSE);
+
+                                if (!ChangeServiceConfig2(serviceHandle, SERVICE_CONFIG_PRESHUTDOWN_INFO, &preshutdownInfo))
+                                {
+                                    win32Result = GetLastError();
+                                }
+                            }
+
+                            if (context->RequiredPrivilegesValid)
+                            {
+                                PH_STRING_BUILDER sb;
+                                ULONG i;
+
+                                PhInitializeStringBuilder(&sb, 100);
+
+                                for (i = 0; i < context->PrivilegeList->Count; i++)
+                                {
+                                    PhAppendStringBuilder(&sb, context->PrivilegeList->Items[i]);
+                                    PhAppendCharStringBuilder(&sb, 0);
+                                }
+
+                                requiredPrivilegesInfo.pmszRequiredPrivileges = sb.String->Buffer;
+
+                                if (win32Result == 0 && !ChangeServiceConfig2(serviceHandle, SERVICE_CONFIG_REQUIRED_PRIVILEGES_INFO, &requiredPrivilegesInfo))
+                                {
+                                    win32Result = GetLastError();
+                                }
+
+                                PhDeleteStringBuilder(&sb);
+                            }
+
+                            CloseServiceHandle(serviceHandle);
+                        }
+                        else
+                        {
+                            win32Result = GetLastError();
+                        }
+
+                        if (win32Result != 0)
+                        {
+                            if (PhShowMessage(
+                                hwndDlg,
+                                MB_ICONERROR | MB_RETRYCANCEL,
+                                L"Unable to change service information: %s",
+                                ((PPH_STRING)PHA_DEREFERENCE(PhGetWin32Message(win32Result)))->Buffer
+                                ) == IDRETRY)
+                            {
+                                SetWindowLongPtr(hwndDlg, DWLP_MSGRESULT, PSNRET_INVALID);
+                                goto EndOfSave;
+                            }
+                        }
+                    }
 
                     if (context->TriggerContext)
                     {
@@ -242,16 +489,24 @@ INT_PTR CALLBACK EspServiceOtherDlgProc(
                                 ) == IDRETRY)
                             {
                                 SetWindowLongPtr(hwndDlg, DWLP_MSGRESULT, PSNRET_INVALID);
+                                goto EndOfSave;
                             }
                         }
                     }
+
+EndOfSave:
+                    return TRUE;
                 }
-                return TRUE;
+                break;
             case LVN_ITEMCHANGED:
                 {
                     if (header->hwndFrom == context->TriggersLv && context->TriggerContext)
                     {
                         EsHandleEventServiceTrigger(context->TriggerContext, ES_TRIGGER_EVENT_SELECTIONCHANGED);
+                    }
+                    else if (header->hwndFrom == context->PrivilegesLv)
+                    {
+                        EnableWindow(GetDlgItem(hwndDlg, IDC_REMOVE), ListView_GetSelectedCount(context->PrivilegesLv) == 1);
                     }
                 }
                 break;
