659caff5a692b7c929d34d384f50f1d9c33fafb9
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-07-02 13:18:14 +0000

    finished basic tree list (incremental search, tooltips)

--- 2.x/trunk/phlib/include/treenew.h
+++ 2.x/trunk/phlib/include/treenew.h
@@ -3,6 +3,9 @@
 
 #define PH_TREENEW_CLASSNAME L"PhTreeNew"
 
+#define PH_TREENEW_SEARCH_TIMEOUT 1000
+#define PH_TREENEW_SEARCH_MAXIMUM_LENGTH 1023
+
 typedef struct _PH_TREENEW_COLUMN
 {
     union
@@ -103,10 +106,15 @@ typedef struct _PH_TREENEW_NODE
 #define TN_CACHE_FONT 0x2
 #define TN_CACHE_ICON 0x4
 
+// Cell part input flags
+#define TN_MEASURE_TEXT 0x1
+
 // Cell part flags
-#define TN_PART_PLUSMINUS 0x1
-#define TN_PART_ICON 0x2
-#define TN_PART_CONTENT 0x4
+#define TN_PART_CELL 0x1
+#define TN_PART_PLUSMINUS 0x2
+#define TN_PART_ICON 0x4
+#define TN_PART_CONTENT 0x8
+#define TN_PART_TEXT 0x10
 
 // Hit test input flags
 #define TN_TEST_COLUMN 0x1
@@ -123,13 +131,21 @@ typedef struct _PH_TREENEW_NODE
 #define TN_HIT_ITEM_CONTENT 0x80 // requires TN_TEST_SUBITEM
 #define TN_HIT_DIVIDER 0x100
 
+// Selection flags
+#define TN_SELECT_DESELECT 0x1
+#define TN_SELECT_TOGGLE 0x2
+#define TN_SELECT_RESET 0x4
+
 typedef struct _PH_TREENEW_CELL_PARTS
 {
     ULONG Flags;
     RECT RowRect;
-    RECT PlusMinusRect;
-    RECT IconRect;
-    RECT ContentRect;
+    RECT CellRect; // TN_PART_CELL
+    RECT PlusMinusRect; // TN_PART_PLUSMINUS
+    RECT IconRect; // TN_PART_ICON
+    RECT ContentRect; // TN_PART_CONTENT
+    RECT TextRect; // TN_PART_TEXT
+    PH_STRINGREF Text; // TN_PART_TEXT
 } PH_TREENEW_CELL_PARTS, *PPH_TREENEW_CELL_PARTS;
 
 typedef struct _PH_TREENEW_HIT_TEST
@@ -150,7 +166,7 @@ typedef enum _PH_TREENEW_MESSAGE
     TreeNewGetNodeColor, // PPH_TREENEW_GET_NODE_COLOR Parameter1
     TreeNewGetNodeFont, // PPH_TREENEW_GET_NODE_FONT Parameter1
     TreeNewGetNodeIcon, // PPH_TREENEW_GET_NODE_ICON Parameter1
-    TreeNewGetNodeTooltip, // PPH_TREENEW_GET_NODE_TOOLTIP Parameter1
+    TreeNewGetCellTooltip, // PPH_TREENEW_GET_CELL_TOOLTIP Parameter1
     TreeNewCustomDraw, // PPH_TREENEW_CUSTOM_DRAW Parameter1
 
     // Notifications
@@ -165,9 +181,10 @@ typedef enum _PH_TREENEW_MESSAGE
     TreeNewRightClick, // PPH_TREENEW_MOUSE_EVENT Parameter1
     TreeNewLeftDoubleClick, // PPH_TREENEW_MOUSE_EVENT Parameter1
     TreeNewRightDoubleClick, // PPH_TREENEW_MOUSE_EVENT Parameter1
-    TreeNewContextMenu, // PPH_TREENEW_MOUSE_EVENT Parameter1
+    TreeNewContextMenu, // PPOINT Parameter1
 
     TreeNewHeaderRightClick,
+    TreeNewIncrementalSearch, // PPH_TREENEW_SEARCH_EVENT Parameter1
 
     MaxTreeNewMessage
 } PH_TREENEW_MESSAGE;
@@ -231,13 +248,15 @@ typedef struct _PH_TREENEW_GET_NODE_ICON
     HICON Icon;
 } PH_TREENEW_GET_NODE_ICON, *PPH_TREENEW_GET_NODE_ICON;
 
-typedef struct _PH_TREENEW_GET_NODE_TOOLTIP
+typedef struct _PH_TREENEW_GET_CELL_TOOLTIP
 {
     ULONG Flags;
     PPH_TREENEW_NODE Node;
+    PPH_TREENEW_COLUMN Column;
+    BOOLEAN Unfolding;
 
     PH_STRINGREF Text;
-} PH_TREENEW_GET_NODE_TOOLTIP, *PPH_TREENEW_GET_NODE_TOOLTIP;
+} PH_TREENEW_GET_CELL_TOOLTIP, *PPH_TREENEW_GET_CELL_TOOLTIP;
 
 typedef struct _PH_TREENEW_CUSTOM_DRAW
 {
@@ -272,6 +291,13 @@ typedef struct _PH_TREENEW_NODE_EVENT
     PVOID Reserved2;
 } PH_TREENEW_NODE_EVENT, *PPH_TREENEW_NODE_EVENT;
 
+typedef struct _PH_TREENEW_SEARCH_EVENT
+{
+    LONG FoundIndex;
+    LONG StartIndex;
+    PH_STRINGREF String;
+} PH_TREENEW_SEARCH_EVENT, *PPH_TREENEW_SEARCH_EVENT;
+
 #define TNM_FIRST (WM_USER + 1)
 #define TNM_SETCALLBACK (WM_USER + 1)
 #define TNM_NODESADDED (WM_USER + 2) // unimplemented
@@ -289,14 +315,21 @@ typedef struct _PH_TREENEW_NODE_EVENT
 #define TNM_SETTRISTATE (WM_USER + 14)
 #define TNM_ENSUREVISIBLE (WM_USER + 15)
 #define TNM_SCROLL (WM_USER + 16)
-#define TNM_GETVISIBLENODECOUNT (WM_USER + 17)
-#define TNM_GETVISIBLENODE (WM_USER + 18)
-#define TNM_GETNODETEXT (WM_USER + 19)
-#define TNM_SETNODESTATE (WM_USER + 20)
-#define TNM_SETNODEEXPANDED (WM_USER + 21)
-#define TNM_GETMAXID (WM_USER + 22)
-#define TNM_SETMAXID (WM_USER + 23)
-#define TNM_LAST (WM_USER + 23)
+#define TNM_GETFLATNODECOUNT (WM_USER + 17)
+#define TNM_GETFLATNODE (WM_USER + 18)
+#define TNM_GETCELLTEXT (WM_USER + 19)
+#define TNM_SETNODEEXPANDED (WM_USER + 20)
+#define TNM_GETMAXID (WM_USER + 21)
+#define TNM_SETMAXID (WM_USER + 22)
+#define TNM_INVALIDATENODE (WM_USER + 23)
+#define TNM_INVALIDATENODES (WM_USER + 24)
+#define TNM_GETFIXEDHEADER (WM_USER + 25)
+#define TNM_GETHEADER (WM_USER + 26)
+#define TNM_GETTOOLTIPS (WM_USER + 27)
+#define TNM_SETREDRAW (WM_USER + 28)
+#define TNM_SELECTRANGE (WM_USER + 29)
+#define TNM_DESELECTRANGE (WM_USER + 30)
+#define TNM_LAST (WM_USER + 30)
 
 #define TreeNew_SetCallback(hWnd, Callback, Context) \
     SendMessage((hWnd), TNM_SETCALLBACK, (WPARAM)(Context), (LPARAM)(Callback))
@@ -333,6 +366,54 @@ typedef struct _PH_TREENEW_NODE_EVENT
 
 #define TreeNew_SetTriState(hWnd, TriState) \
     SendMessage((hWnd), TNM_SETTRISTATE, (WPARAM)(TriState), 0)
+
+#define TreeNew_EnsureVisible(hWnd, Node) \
+    SendMessage((hWnd), TNM_ENSUREVISIBLE, 0, (LPARAM)(Node))
+
+#define TreeNew_Scroll(hWnd, DeltaRows, DeltaX) \
+    SendMessage((hWnd), TNM_SCROLL, (WPARAM)(DeltaRows), (LPARAM)(DeltaX))
+
+#define TreeNew_GetFlatNodeCount(hWnd) \
+    ((ULONG)SendMessage((hWnd), TNM_GETFLATNODECOUNT, 0, 0))
+
+#define TreeNew_GetFlatNode(hWnd, Index) \
+    SendMessage((hWnd), TNM_GETFLATNODE, (WPARAM)(Index), 0)
+
+#define TreeNew_GetCellText(hWnd, GetCellText) \
+    SendMessage((hWnd), TNM_GETCELLTEXT, 0, (LPARAM)(GetCellText))
+
+#define TreeNew_SetNodeExpanded(hWnd, Node, Expanded) \
+    SendMessage((hWnd), TNM_SETNODEEXPANDED, (WPARAM)(Expanded), (LPARAM)(Node))
+
+#define TreeNew_GetMaxId(hWnd) \
+    ((ULONG)SendMessage((hWnd), TNM_GETMAXID, 0, 0))
+
+#define TreeNew_SetMaxId(hWnd, MaxId) \
+    SendMessage((hWnd), TNM_SETMAXID, (WPARAM)(MaxId), 0)
+
+#define TreeNew_InvalidateNode(hWnd, Node) \
+    SendMessage((hWnd), TNM_INVALIDATENODE, 0, (LPARAM)(Node))
+
+#define TreeNew_InvalidateNodes(hWnd, Start, End) \
+    SendMessage((hWnd), TNM_INVALIDATENODES, (WPARAM)(Start), (LPARAM)(End))
+
+#define TreeNew_GetFixedHeader(hWnd) \
+    ((HWND)SendMessage((hWnd), TNM_GETFIXEDHEADER, 0, 0))
+
+#define TreeNew_GetHeader(hWnd) \
+    ((HWND)SendMessage((hWnd), TNM_GETHEADER, 0, 0))
+
+#define TreeNew_GetTooltips(hWnd) \
+    ((HWND)SendMessage((hWnd), TNM_GETTOOLTIPS, 0, 0))
+
+#define TreeNew_SetRedraw(hWnd, Redraw) \
+    SendMessage((hWnd), TNM_SETREDRAW, (WPARAM)(Redraw), 0)
+
+#define TreeNew_SelectRange(hWnd, Start, End) \
+    SendMessage((hWnd), TNM_SELECTRANGE, (WPARAM)(Start), (LPARAM)(End))
+
+#define TreeNew_DeselectRange(hWnd, Start, End) \
+    SendMessage((hWnd), TNM_DESELECTRANGE, (WPARAM)(Start), (LPARAM)(End))
 
 BOOLEAN PhTreeNewInitialization();
 
