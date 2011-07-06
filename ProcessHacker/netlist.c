d5b577514acead9f58f44f08fc87d9176878f2da
Wen Jia Liu <wj32.64@gmail.com>
2011-07-06 10:04:41 +0000

    fixed network and service list restructuring for plugin columns

--- ProcessHacker/netlist.c
+++ ProcessHacker/netlist.c
@@ -288,6 +288,13 @@ VOID PhUpdateNetworkNode(
 
 VOID PhTickNetworkNodes()
 {
+    if (NetworkTreeListSortOrder != NoSortOrder && NetworkTreeListSortColumn >= PHNETLC_MAXIMUM)
+    {
+        // Sorting is on, but it's not one of our columns. Force a rebuild. (If it was one of our 
+        // columns, the restructure would have been handled in PhUpdateNetworkNode.)
+        TreeNew_NodesStructured(NetworkTreeListHandle);
+    }
+
     PH_TICK_SH_STATE_TN(PH_NETWORK_NODE, ShState, NetworkNodeStateList, PhpRemoveNetworkNode, PhCsHighlightingDuration, NetworkTreeListHandle, TRUE, NULL);
 }
 
