cb2ad858adc71f874a35e386c7dab8fcb22ebb39
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-07-11 02:08:37 +0000

    added BUG comment

--- 2.x/trunk/plugins/DotNetTools/asmpage.c
+++ 2.x/trunk/plugins/DotNetTools/asmpage.c
@@ -874,12 +874,16 @@ ULONG UpdateDotNetTraceInfoWithTimeout(
 
     Context->TraceClrV2 = ClrV2;
     Context->TraceResult = 0;
+    Context->TraceHandleActive = 0;
+    Context->TraceResult = 0;
 
     threadHandle = PhCreateThread(0, UpdateDotNetTraceInfoThreadStart, Context);
 
     if (NtWaitForSingleObject(threadHandle, FALSE, Timeout) != STATUS_WAIT_0)
     {
         // Timeout has expired. Stop the trace processing if it's still active.
+        // BUG: This assumes that the thread is in ProcessTrace. It might still be 
+        // setting up though!
         if (_InterlockedExchange(&Context->TraceHandleActive, 0) == 1)
         {
             CloseTrace(Context->TraceHandle);
