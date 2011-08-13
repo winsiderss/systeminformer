c648d007edd37616349b9fab08dd9704a3760be1
dmex <dmex@users.noreply.github.com>
2011-08-13 22:16:19 +0000

    Updater: hash result

--- plugins/Updater/output.c
+++ plugins/Updater/output.c
@@ -336,6 +336,8 @@ static NTSTATUS DownloadWorkerThreadStart(
 
 	DisposeHandles();
 
+	HashFile();
+
 	SetDlgItemText(hwndDlg, IDC_STATUS, L"Download Complete");
 
 	Install = TRUE;
@@ -545,3 +547,111 @@ VOID DisposeHandles()
 	//if (localFilePath)
 		//PhDereferenceObject(localFilePath);
 }
+
+
+DWORD HashFile()
+{
+    DWORD dwStatus = 0;
+    BOOL bResult = FALSE;
+    HCRYPTPROV hProv = 0;
+    HCRYPTHASH hHash = 0;
+    HANDLE hFile = NULL;
+    BYTE rgbFile[BUFFER_LEN];
+    DWORD cbRead = 0;
+    BYTE rgbHash[MD5LEN];
+    DWORD cbHash = 0;
+    CHAR rgbDigits[] = "0123456789abcdef";
+
+    // Copyright (C) Microsoft.  All rights reserved.
+    // Logic to check usage goes here.
+
+    hFile = CreateFile(localFilePath->Buffer,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        FILE_FLAG_SEQUENTIAL_SCAN,
+        NULL);
+
+    if (INVALID_HANDLE_VALUE == hFile)
+    {
+        dwStatus = GetLastError();
+        printf("Error opening file %s\nError: %d\n", L"", 
+            dwStatus); 
+        return dwStatus;
+    }
+
+    // Get handle to the crypto provider
+    if (!CryptAcquireContext(&hProv,
+        NULL,
+        NULL,
+        PROV_RSA_FULL,
+        CRYPT_VERIFYCONTEXT))
+    {
+        dwStatus = GetLastError();
+        printf("CryptAcquireContext failed: %d\n", dwStatus); 
+        CloseHandle(hFile);
+        return dwStatus;
+    }
+
+    if (!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
+    {
+        dwStatus = GetLastError();
+        printf("CryptAcquireContext failed: %d\n", dwStatus); 
+        CloseHandle(hFile);
+        CryptReleaseContext(hProv, 0);
+        return dwStatus;
+    }
+
+    while (bResult = ReadFile(hFile, rgbFile, BUFFER_LEN, &cbRead, NULL))
+    {
+        if (0 == cbRead)
+        {
+            break;
+        }
+
+		        if (!CryptHashData(hHash, rgbFile, cbRead, 0))
+        {
+            dwStatus = GetLastError();
+            printf("CryptHashData failed: %d\n", dwStatus); 
+            CryptReleaseContext(hProv, 0);
+            CryptDestroyHash(hHash);
+            CloseHandle(hFile);
+            return dwStatus;
+        }
+    }
+
+    if (!bResult)
+    {
+        dwStatus = GetLastError();
+        printf("ReadFile failed: %d\n", dwStatus); 
+        CryptReleaseContext(hProv, 0);
+        CryptDestroyHash(hHash);
+        CloseHandle(hFile);
+        return dwStatus;
+    }
+
+    cbHash = MD5LEN;
+    if (CryptGetHashParam(hHash, HP_HASHVAL, rgbHash, &cbHash, 0))
+    {
+		DWORD i = 0;
+		LogEvent(PhFormatString(L"Updater: MD5 hash of file is: (%d)", cbHash));
+
+        for (i = 0; i < cbHash; i++)
+        {
+            LogEvent(PhFormatString(L"%d%d", rgbDigits[rgbHash[i] >> 4], rgbDigits[rgbHash[i] & 0xf]));
+        }
+    }
+    else
+    {
+        dwStatus = GetLastError();
+        //printf("CryptGetHashParam failed: %d\n", dwStatus); 
+    }
+
+    CryptDestroyHash(hHash);
+    CryptReleaseContext(hProv, 0);
+    CloseHandle(hFile);
+
+    return dwStatus; 
+}   
+
