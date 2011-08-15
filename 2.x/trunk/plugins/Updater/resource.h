38eb876cf170ca9ab102096a706e5aa35e5a6408
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-08-15 10:36:31 +0000

    Updater: updated resources

--- 2.x/trunk/plugins/Updater/resource.h
+++ 2.x/trunk/plugins/Updater/resource.h
@@ -16,6 +16,7 @@
 #define IDC_EDIT1                       1008
 #define IDC_HASHCOMBOBOX                1009
 #define IDDOWNLOAD                      1010
+#define IDDOWNLOAD2                     1011
 
 // Next default values for new objects
 // 
@@ -23,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           108
 #endif
 #endif
