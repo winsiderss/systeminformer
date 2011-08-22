461902bf846023c754098203890f21f397c8e171
Wen Jia Liu <wj32.64@gmail.com>
2011-08-22 01:21:43 +0000

    Updater: tabified files

--- plugins/Updater/updater.h
+++ plugins/Updater/updater.h
@@ -9,8 +9,8 @@
 typedef enum _PH_UPDATER_STATE
 {
 	Default,
-    Downloading,
-    Installing
+	Downloading,
+	Installing
 } PH_UPDATER_STATE;
 
 #pragma endregion
@@ -41,17 +41,17 @@ typedef enum _PH_UPDATER_STATE
 
 typedef struct _PH_UPDATER_CONTEXT
 {
-    HWND MainWindowHandle;
-    PVOID Parameter;
+	HWND MainWindowHandle;
+	PVOID Parameter;
 } PH_UPDATER_CONTEXT, *PPH_UPDATER_CONTEXT;
 
 typedef struct _UPDATER_XML_DATA
 {
-    ULONG MinorVersion;
-    ULONG MajorVersion;
-    PPH_STRING RelDate;
-    PPH_STRING Size;
-    PPH_STRING Hash;
+	ULONG MinorVersion;
+	ULONG MajorVersion;
+	PPH_STRING RelDate;
+	PPH_STRING Size;
+	PPH_STRING Hash;
 } UPDATER_XML_DATA, *PUPDATER_XML_DATA;
 
 #pragma endregion
@@ -80,17 +80,17 @@ VOID DisposeFileHandles();
 BOOL ConnectionAvailable();
 
 BOOL ParseVersionString(
-    __in PWSTR String,
-    __out PULONG MajorVersion,
-    __out PULONG MinorVersion
-    );
+	__in PWSTR String,
+	__out PULONG MajorVersion,
+	__out PULONG MinorVersion
+	);
 
 LONG CompareVersions(
-    __in ULONG MajorVersion1,
-    __in ULONG MinorVersion1,
-    __in ULONG MajorVersion2,
-    __in ULONG MinorVersion2
-    );
+	__in ULONG MajorVersion1,
+	__in ULONG MinorVersion1,
+	__in ULONG MajorVersion2,
+	__in ULONG MinorVersion2
+	);
 
 VOID StartInitialCheck();
 
@@ -99,19 +99,19 @@ VOID ShowUpdateDialog();
 BOOL PhInstalledUsingSetup();
 
 BOOL ReadRequestString(
-    __in HINTERNET Handle,
-    __out PSTR *Data,
-    __out_opt PULONG DataLength
-    );
+	__in HINTERNET Handle,
+	__out PSTR *Data,
+	__out_opt PULONG DataLength
+	);
 
 BOOL QueryXmlData(
-    __in PVOID Buffer,
-    __out PUPDATER_XML_DATA XmlData
-    );
+	__in PVOID Buffer,
+	__out PUPDATER_XML_DATA XmlData
+	);
 
 VOID FreeXmlData(
-    __in PUPDATER_XML_DATA XmlData
-    );
+	__in PUPDATER_XML_DATA XmlData
+	);
 
 BOOL InitializeConnection(
 	__in PCWSTR host,
@@ -125,32 +125,32 @@ VOID LogEvent(
 	);
 
 VOID NTAPI MenuItemCallback(
-    __in_opt PVOID Parameter,
-    __in_opt PVOID Context
-    );
+	__in_opt PVOID Parameter,
+	__in_opt PVOID Context
+	);
 
 VOID NTAPI MainWindowShowingCallback(
-    __in_opt PVOID Parameter,
-    __in_opt PVOID Context
-    );
+	__in_opt PVOID Parameter,
+	__in_opt PVOID Context
+	);
 
 VOID NTAPI ShowOptionsCallback(
-    __in_opt PVOID Parameter,
-    __in_opt PVOID Context
-    );
+	__in_opt PVOID Parameter,
+	__in_opt PVOID Context
+	);
 
 INT_PTR CALLBACK MainWndProc(      
-    __in HWND hwndDlg,
-    __in UINT uMsg,
-    __in WPARAM wParam,
-    __in LPARAM lParam
-    );
+	__in HWND hwndDlg,
+	__in UINT uMsg,
+	__in WPARAM wParam,
+	__in LPARAM lParam
+	);
 
 INT_PTR CALLBACK OptionsDlgProc(
-    __in HWND hwndDlg,
-    __in UINT uMsg,
-    __in WPARAM wParam,
-    __in LPARAM lParam
-    );
+	__in HWND hwndDlg,
+	__in UINT uMsg,
+	__in WPARAM wParam,
+	__in LPARAM lParam
+	);
 
 #pragma endregion
