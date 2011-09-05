d95198d483e195f794ff9493f73faf4f2dab83be
dmex <dmex04@gmail.com>
2011-09-05 11:05:28 +0000

    Updater: updated UI, added changelog text

--- plugins/Updater/resource.h
+++ plugins/Updater/resource.h
@@ -12,12 +12,11 @@
 #define IDC_STATUSTEXT                  1004
 #define IDC_RELDATE                     1005
 #define IDC_DLSIZE                      1006
-#define IDC_STATUSTEXT2                 1008
+#define IDC_SPEEDTEXT                   1008
 #define IDC_HASHCOMBOBOX                1009
-#define IDC_STATUSTEXT3                 1009
+#define IDC_RTIMETEXT                   1009
 #define IDC_DOWNLOAD                    1010
-#define IDC_SYSLINK1                    1014
-#define IDC_SYSLINK2                    1015
+#define IDC_EDIT1                       1015
 
 // Next default values for new objects
 // 
@@ -25,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           108
 #endif
 #endif
