325a53b296ad8e1288e124e077ebc657366f98c9
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-09-09 07:43:17 +0000

    Updater/gfxinfo: local sync

--- 2.x/trunk/plugins/GraphicsInfo/resources/resource.h
+++ 2.x/trunk/plugins/GraphicsInfo/resources/resource.h
@@ -5,21 +5,18 @@
 #define IDD_SYSGFX                      107
 #define IDD_SYSGFX_PANEL                108
 #define IDD_OPTIONS                     109
-#define IDC_MEMGRAPH                    109
-#define IDC_GPUGRAPH                    110
-#define IDC_CONTGRAPH                   111
 #define IDC_ZREADS_V                    1023
 #define IDC_ZREADBYTES_V                1024
-#define IDC_ZWRITES_V                   1026
 #define IDC_ZWRITEBYTES_V               1027
+#define IDC_ZWRITEBYTES_V3              1028
 #define IDC_ZRECEIVES_V                 1029
 #define IDC_ZRECEIVEBYTES_V             1030
+#define IDC_ZWRITEBYTES_V2              1031
+#define IDC_FANSPEED                    1031
 #define IDC_ZSENDS_V                    1032
-#define IDC_ZSENDBYTES_V                1033
 #define IDC_GROUPGPU                    1035
 #define IDC_GROUPMEM                    1036
 #define IDC_ALWAYSONTOP                 1037
-#define IDC_GROUPMEM2                   1039
 #define IDC_GROUPCONTROLLER             1039
 
 // Next default values for new objects
