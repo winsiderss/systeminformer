9e3b8f581ab9fc0109d4cdfaa319a53c3a02c52f
Wen Jia Liu <wj32.64@gmail.com>
2011-10-11 09:05:16 +0000

    ExtendedNotifications: added Growl support

--- plugins/ExtendedNotifications/resource.h
+++ plugins/ExtendedNotifications/resource.h
@@ -6,6 +6,7 @@
 #define IDC_TEXT_RETURN                 101
 #define IDD_SERVICES                    102
 #define IDD_LOGGING                     103
+#define IDD_GROWL                       104
 #define IDC_INCLUDE                     1006
 #define IDC_EXCLUDE                     1007
 #define IDC_REMOVE                      1008
@@ -16,15 +17,17 @@
 #define IDC_TEXT                        1013
 #define IDC_EDIT1                       1014
 #define IDC_LOGFILENAME                 1014
+#define IDC_LICENSE                     1014
 #define IDC_BROWSE                      1015
+#define IDC_ENABLEGROWL                 1016
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
 #endif
