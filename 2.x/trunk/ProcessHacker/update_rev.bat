dbf4c9fefe445337639a0e00eb944d464988109e
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2011-11-15 09:41:42 +0000

    patch by xhmikosr

--- 2.x/trunk/ProcessHacker/update_rev.bat
+++ 2.x/trunk/ProcessHacker/update_rev.bat
@@ -17,7 +17,12 @@ ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
 ECHO You should (re)install TortoiseSVN.
 ECHO I'll use PHAPP_VERSION_REVISION=0 for now.
 
-ECHO #define PHAPP_VERSION_REVISION 0 > "include\phapprev.h"
+ECHO #ifndef PHAPPREV_H> "include\phapprev.h"
+ECHO #define PHAPPREV_H>> "include\phapprev.h"
+ECHO.>> "include\phapprev.h"
+ECHO #define PHAPP_VERSION_REVISION 0 >> "include\phapprev.h"
+ECHO.>> "include\phapprev.h"
+ECHO #endif // PHAPPREV_H>> "include\phapprev.h"
 
 ENDLOCAL
 EXIT /B
