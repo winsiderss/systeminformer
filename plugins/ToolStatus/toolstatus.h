e00fabe70e8b38d6c3b9eb84860f92b36d7d9f1f
dmex <dmex04@gmail.com>
2012-03-19 13:21:20 +0000

    ToolStatus: Added experimental Service/Network searching

--- plugins/ToolStatus/toolstatus.h
+++ plugins/ToolStatus/toolstatus.h
@@ -1,6 +1,15 @@
 #ifndef TOOLSTATUS_H
 #define TOOLSTATUS_H
 
+#include <phdk.h>
+
+#include "resource.h"
+
+#include <phapppub.h>
+#include <phplug.h>
+#include <phappresource.h>
+#include <windowsx.h>
+
 #define TIDC_REFRESH 0
 #define TIDC_OPTIONS 1
 #define TIDC_FINDOBJ 2
@@ -22,6 +31,17 @@
 #define STATUS_MAXCPUPROCESS 0x100
 #define STATUS_MAXIOPROCESS 0x200
 #define STATUS_MAXIMUM 0x400
+
+typedef enum _TOOLBAR_DISPLAY_STYLE
+{
+    ImageOnly = 0,
+    SelectiveText = 1,
+    AllText = 2
+} TOOLBAR_DISPLAY_STYLE;
+
+BOOLEAN EnableToolBar;
+BOOLEAN EnableStatusBar;
+TOOLBAR_DISPLAY_STYLE DisplayStyle;
 
 VOID NTAPI LoadCallback(
     __in_opt PVOID Parameter,
