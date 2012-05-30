0a993f00f93c1326724e86aae5d04ad459a0e581
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-05-30 11:24:56 +0000

    installer patch by xhmikosr

--- 2.x/trunk/ProcessHacker/include/phappres.h
+++ 2.x/trunk/ProcessHacker/include/phappres.h
@@ -1,8 +1,5 @@
 // Notes:
 // * Do not use /* comments */ since ISPP is buggy and it will throw an error.
-//
-// * ISPP_IS_BUGGY is defined in the installer script only and it's just a workaround
-//   for ISPP being buggy and throwing an error for the MAKE_STR macro.
 
 #ifndef PHAPPRES_H
 #define PHAPPRES_H
@@ -14,15 +11,15 @@
 #define PHAPP_VERSION_BUILD 0
 
 #if (PHAPP_VERSION_BUILD == 0)
-#define TWO_DIGIT_VER 1
+#define TWO_DIGIT_VER   1
 #else
 #define THREE_DIGIT_VER 1
 #endif
 
 #define DO_MAKE_STR(x) #x
-#define MAKE_STR(x) DO_MAKE_STR(x)
+#define MAKE_STR(x)    DO_MAKE_STR(x)
 
-#ifndef ISPP_IS_BUGGY
+#ifndef ISPP_INVOKED
 
 #if defined(TWO_DIGIT_VER)
 #define PHAPP_VERSION_STRING MAKE_STR(PHAPP_VERSION_MAJOR) "." MAKE_STR(PHAPP_VERSION_MINOR) ".0" "." MAKE_STR(PHAPP_VERSION_REVISION)
@@ -32,6 +29,6 @@
 
 #define PHAPP_VERSION_NUMBER PHAPP_VERSION_MAJOR,PHAPP_VERSION_MINOR,PHAPP_VERSION_BUILD,PHAPP_VERSION_REVISION
 
-#endif // ISPP_IS_BUGGY
+#endif // ISPP_INVOKED
 
 #endif // PHAPPRES_H
