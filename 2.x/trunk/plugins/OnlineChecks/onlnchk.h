0bf4028043e82bd43dc40113c7361c85c31b8adb
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-07-06 08:42:12 +0000

    OnlineChecks: reverted to r4989 for 2.28 release

--- 2.x/trunk/plugins/OnlineChecks/onlnchk.h
+++ 2.x/trunk/plugins/OnlineChecks/onlnchk.h
@@ -1,52 +1,6 @@
 #ifndef ONLNCHK_H
 #define ONLNCHK_H
 
-#pragma comment(lib, "OleAut32.lib")
-#pragma comment(lib, "Winhttp.lib")
-
-#define COBJMACROS
-#include <time.h>
-#include <phdk.h>
-#include <OleCtl.h>
-#include <winhttp.h>
-#include <Netlistmgr.h>
-#include <ShObjIdl.h>
-
-#include "sha256.h"
-#include "resource.h"
-
-#define HASH_SHA1 1
-#define HASH_SHA256 2
-
-#define UM_LAUNCH_COMMAND (WM_APP + 1)
-#define UM_ERROR (WM_APP + 2)
-
-#define BUFFER_LEN 0x1000
-
-typedef struct _UPLOAD_CONTEXT
-{
-    LONG RefCount;
-
-    PPH_STRING FileName;
-    ULONG Service;
-    HWND WindowHandle;
-    HANDLE ThreadHandle;
-
-    PPH_STRING LaunchCommand;
-    PPH_STRING ErrorMessage;
-} UPLOAD_CONTEXT, *PUPLOAD_CONTEXT;
-
-typedef struct _SERVICE_INFO
-{
-    ULONG Id;
-    PWSTR HostName;
-    INTERNET_PORT HostPort;
-    ULONG HostFlags;
-    PWSTR UploadObjectName;
-    PWSTR FileNameFieldName;
-} SERVICE_INFO, *PSERVICE_INFO;
-
-
 // main
 
 #ifndef MAIN_PRIVATE
@@ -55,24 +9,14 @@ extern PPH_PLUGIN PluginInstance;
 
 // upload
 
-#define UPLOAD_SERVICE_VIRUSTOTAL 101
-#define UPLOAD_SERVICE_JOTTI 102
-#define UPLOAD_SERVICE_CIMA 103
+#define UPLOAD_SERVICE_VIRUSTOTAL 1
+#define UPLOAD_SERVICE_JOTTI 2
+#define UPLOAD_SERVICE_CIMA 3
 
 VOID UploadToOnlineService(
     __in HWND hWnd,
     __in PPH_STRING FileName,
     __in ULONG Service
     );
-
-INT_PTR CALLBACK UploadDlgProc(
-    __in HWND hwndDlg,
-    __in UINT uMsg,
-    __in WPARAM wParam,
-    __in LPARAM lParam
-    );
-
-
-
 
 #endif
