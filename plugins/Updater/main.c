0e697e913d7b72d42b2ebe332916a84c632bcb94
dmex <dmex04@gmail.com>
2012-12-07 09:37:11 +0000

    Updater: Switched to WinHTTP, Fixed Revision checking, Fixed HOBJ/Font leak, Improved error-checking

---
+++ plugins/Updater/main.c
@@ -0,0 +1,232 @@
+/*
+ * Process Hacker Update Checker -
+ *   Main program
+ *
+ * Copyright (C) 2010-2012 wj32
+ * Copyright (C) 2012 dmex
+ *
+ * This file is part of Process Hacker.
+ *
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "updater.h"
+
+VOID NTAPI LoadCallback(
+    __in_opt PVOID Parameter,
+    __in_opt PVOID Context
+    );
+VOID NTAPI MenuItemCallback(
+    __in_opt PVOID Parameter,
+    __in_opt PVOID Context
+    );
+VOID NTAPI MainWindowShowingCallback(
+    __in_opt PVOID Parameter,
+    __in_opt PVOID Context
+    );
+VOID NTAPI ShowOptionsCallback(
+    __in_opt PVOID Parameter,
+    __in_opt PVOID Context
+    );
+
+PPH_PLUGIN PluginInstance;
+static PH_CALLBACK_REGISTRATION PluginMenuItemCallbackRegistration;
+static PH_CALLBACK_REGISTRATION MainWindowShowingCallbackRegistration;
+static PH_CALLBACK_REGISTRATION PluginShowOptionsCallbackRegistration;
+
+LOGICAL DllMain(
+    __in HINSTANCE Instance,
+    __in ULONG Reason,
+    __reserved PVOID Reserved
+    )
+{
+    switch (Reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        {
+            PPH_PLUGIN_INFORMATION info;
+
+            PluginInstance = PhRegisterPlugin(L"ProcessHacker.UpdateChecker", Instance, &info);
+
+            if (!PluginInstance)
+                return FALSE;
+
+            info->DisplayName = L"Update Checker";
+            info->Author = L"dmex";
+            info->Description = L"Plugin for checking new Process Hacker releases via the Help menu.";
+            info->HasOptions = TRUE;
+
+            PhRegisterCallback(
+                PhGetGeneralCallback(GeneralCallbackMainWindowShowing),
+                MainWindowShowingCallback,
+                NULL,
+                &MainWindowShowingCallbackRegistration
+                );
+            PhRegisterCallback(
+                PhGetPluginCallback(PluginInstance, PluginCallbackMenuItem),
+                MenuItemCallback,
+                NULL,
+                &PluginMenuItemCallbackRegistration
+                );
+            PhRegisterCallback(
+                PhGetPluginCallback(PluginInstance, PluginCallbackShowOptions),
+                ShowOptionsCallback,
+                NULL,
+                &PluginShowOptionsCallbackRegistration
+                );
+
+            {
+                PH_SETTING_CREATE settings[] =
+                {
+                    { IntegerSettingType, SETTING_AUTO_CHECK, L"1" },
+                };
+
+                PhAddSettings(settings, _countof(settings));
+            }
+        }
+        break;
+    }
+
+    return TRUE;
+}
+
+static VOID NTAPI MainWindowShowingCallback(
+    __in_opt PVOID Parameter,
+    __in_opt PVOID Context
+    )
+{
+    // Add our menu item, 4 = Help menu.
+    PhPluginAddMenuItem(PluginInstance, 4, NULL, UPDATE_MENUITEM, L"Check for Updates", NULL);
+
+    // Check if the user want's us to auto-check for updates.
+    if (PhGetIntegerSetting(SETTING_AUTO_CHECK))
+    {
+        // All good, queue up our update check.
+        StartInitialCheck();
+    }
+}
+
+static VOID NTAPI MenuItemCallback(
+    __in_opt PVOID Parameter,
+    __in_opt PVOID Context
+    )
+{
+    PPH_PLUGIN_MENU_ITEM menuItem = (PPH_PLUGIN_MENU_ITEM)Parameter;
+
+    if (menuItem != NULL && menuItem->Id == UPDATE_MENUITEM)
+    {
+        ShowUpdateDialog(NULL);
+    }
+}
+
+static VOID NTAPI ShowOptionsCallback(
+    __in_opt PVOID Parameter,
+    __in_opt PVOID Context
+    )
+{
+    DialogBox(
+        (HINSTANCE)PluginInstance->DllBase,
+        MAKEINTRESOURCE(IDD_OPTIONS),
+        (HWND)Parameter,
+        OptionsDlgProc
+        );
+}
+
+PPH_STRING PhGetOpaqueXmlNodeText(
+    __in mxml_node_t *xmlNode
+    )
+{
+    if (xmlNode && xmlNode->child && xmlNode->child->type == MXML_OPAQUE && xmlNode->child->value.opaque)
+    {
+        return PhCreateStringFromAnsi(xmlNode->child->value.opaque);
+    }
+    
+    return PhReferenceEmptyString();
+}
+
+
+BOOL PhInstalledUsingSetup(
+    VOID
+    )
+{
+    static PH_STRINGREF keyName = PH_STRINGREF_INIT(L"Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Process_Hacker2_is1");
+
+    HANDLE keyHandle = NULL;
+
+    // Check uninstall entries for the 'Process_Hacker2_is1' registry key.
+    if (NT_SUCCESS(PhOpenKey(
+        &keyHandle,
+        KEY_READ,
+        PH_KEY_LOCAL_MACHINE,
+        &keyName,
+        0
+        )))
+    {
+        NtClose(keyHandle);
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+BOOL ConnectionAvailable(
+    VOID
+    )
+{
+    if (WindowsVersion > WINDOWS_XP)
+    {
+        INetworkListManager *pNetworkListManager;
+
+        // Create an instance of the INetworkListManger COM object.
+        if (SUCCEEDED(CoCreateInstance(&CLSID_NetworkListManager, NULL, CLSCTX_ALL, &IID_INetworkListManager, &pNetworkListManager)))
+        {
+            VARIANT_BOOL isConnected = VARIANT_FALSE;
+            VARIANT_BOOL isConnectedInternet = VARIANT_FALSE;
+
+            // Query the relevant properties.
+            INetworkListManager_get_IsConnected(pNetworkListManager, &isConnected);
+            INetworkListManager_get_IsConnectedToInternet(pNetworkListManager, &isConnectedInternet);
+
+            // Cleanup the INetworkListManger COM object.
+            INetworkListManager_Release(pNetworkListManager);
+            pNetworkListManager = NULL;
+
+            // Check if Windows is connected to a network and it's connected to the internet.
+            if (isConnected == VARIANT_TRUE && isConnectedInternet == VARIANT_TRUE)
+            {
+                // We're online and connected to the internet.
+                return TRUE;
+            }
+
+            // We're not connected to anything.
+            return FALSE;
+        }
+
+        // If we reached here, we were unable to init the INetworkListManager, fall back to InternetGetConnectedState.
+        goto NOT_SUPPORTED;
+    }
+    else
+    {
+        // DWORD dwType;   
+        // Import WinInet for InternetGetConnectedState
+        //#include <WinInet.h>
+
+        //if (InternetGetConnectedState(&dwType, 0))
+        //{
+        //    return TRUE;
+        //}
+    }
+NOT_SUPPORTED:
+    return FALSE;
+}
