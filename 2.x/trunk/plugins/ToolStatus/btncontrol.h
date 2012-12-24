3b7b16df131089351152dfda1f8f7a23f065e99a
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2012-12-24 16:02:06 +0000

    ToolStatus: fixes

---
+++ 2.x/trunk/plugins/ToolStatus/btncontrol.h
@@ -0,0 +1,43 @@
+#ifndef __BTN_CONTROL_H
+#define __BTN_CONTROL_H
+
+#include <phdk.h>
+
+typedef struct _NC_CONTROL
+{
+    UINT uCmdId; // sent in a WM_COMMAND message
+    UINT uState;
+    BOOLEAN IsButtonDown; // is the button up/down?
+    BOOLEAN IsMouseDown; // is the mouse activating the button?
+    BOOLEAN IsMouseActive;
+    BOOLEAN oldstate;
+
+    INT nButSize; // horizontal size of button   
+    INT cxLeftEdge; // size of the current window borders.
+    INT cxRightEdge;  // size of the current window borders.
+    INT cyTopEdge; 
+    INT cyBottomEdge; 
+   
+    POINT pt;
+    RECT rect;
+    RECT oldrect;
+    RECT* prect;
+    HINSTANCE DllBase;
+    WNDPROC NCAreaWndProc;
+} NC_CONTROL;
+
+BOOLEAN InsertButton(
+    __in HINSTANCE DllBase,
+    __in HWND WindowHandle, 
+    __in UINT uCmdId, 
+    __in INT nSize
+    );
+
+LRESULT CALLBACK InsButProc(
+    __in HWND hwndDlg,
+    __in UINT uMsg,
+    __in WPARAM wParam,
+    __in LPARAM lParam
+    );
+
+#endif
