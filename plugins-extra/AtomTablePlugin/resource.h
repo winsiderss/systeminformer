217b1a8088be2b181e47fad25cff2d8944d8a0fc
dmex <dmex@users.noreply.github.com>
2013-06-22 13:49:21 +0000

    PluginsExtra: Added NT Atom Table plugin

---
+++ plugins-extra/AtomTablePlugin/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AtomTablePlugin.rc
+//
+#define IDD_ATOMDIALOG                  101
+#define IDC_ATOMLIST                    1021
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_SYMED_VALUE           108
+#endif
+#endif
