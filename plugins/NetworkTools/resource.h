f644b1302adf913a3a0c32781075d7c37ca25557
dmex <dmex04@gmail.com>
2013-08-03 13:49:33 +0000

    NetworkTools: bumped to 1.3; Updated UI; Added native ping/tracert/whois support;

--- plugins/NetworkTools/resource.h
+++ plugins/NetworkTools/resource.h
@@ -6,16 +6,22 @@
 #define IDD_OUTPUT                      101
 #define ID_TOOLS_TRACEROUTE             102
 #define ID_TOOLS_WHOIS                  103
-#define IDC_TEXT                        1001
-#define IDC_MESSAGE                     1002
+#define IDD_OPTIONS                     109
+#define IDC_NETRESOLVECHECK             1001
+#define IDC_NETRETRY                    1004
+#define IDC_MAXPINGTEXT                 1007
+#define IDC_MAXTIMEOUTTEXT              1008
+#define IDC_NETOUTPUTEDIT               1008
+#define IDC_COMBOBOXEX1                 1009
+#define IDC_COMBOCMD                    1009
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
