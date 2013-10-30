a1f6f8edca8865041b070072ea009dca2b234045
Wen Jia Liu <wj32.64@gmail.com>
2013-10-30 12:49:15 +0000

    added timestamp option

--- build/internal/sign.cmd
+++ build/internal/sign.cmd
@@ -5,7 +5,10 @@ if "%1" == "" goto :notset
 set additional=
 if "%2" == "kmcs" set additional=/ac "%PHBASE%\build\internal\DigiCert High Assurance EV Root CA.crt"
 
-signtool sign /t http://timestamp.digicert.com /i "DigiCert High Assurance Code Signing CA-1" %additional% %1
+set timestamp=
+if "%SIGN_TIMESTAMP%" == "1" set timestamp=/t http://timestamp.digicert.com
+
+signtool sign %timestamp% /i "DigiCert High Assurance Code Signing CA-1" %additional% %1
 
 goto :end
 
