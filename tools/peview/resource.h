6b0f99e84bb19bf83adb8a592cbc8e9b86b7d1ad
Wen Jia Liu <wj32.64@gmail.com>
2013-11-18 03:48:53 +0000

    peview: added manifest

--- tools/peview/resource.h
+++ tools/peview/resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by peview.rc
 //
+#define IDR_RT_MANIFEST                 1
 #define IDD_PEGENERAL                   102
 #define IDD_PEIMPORTS                   103
 #define IDD_PEEXPORTS                   104
@@ -32,7 +33,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
