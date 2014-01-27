a0fc93dec9e9bc19c82535a245daa1dc19985003
dmex <dmex@users.noreply.github.com>
2014-01-27 10:30:57 +0000

    plugins-extra: sync WaitChainPlguin changes

--- plugins-extra/WaitChainPlugin/wndtree.c
+++ plugins-extra/WaitChainPlugin/wndtree.c
@@ -1,25 +1,24 @@
 /*
- * Process Hacker Window Explorer -
- *   window treelist
- *
- * Copyright (C) 2011 wj32
- * Copyright (C) 2013 dmex
- *
- * This file is part of Process Hacker.
- *
- * Process Hacker is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Process Hacker is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
- */
+* Process Hacker Extra Plugins -
+*   Wait Chain Traversal (WCT) Plugin
+*
+* Copyright (C) 2014 dmex
+*
+* This file is part of Process Hacker.
+*
+* Process Hacker is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Process Hacker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "main.h"
 
