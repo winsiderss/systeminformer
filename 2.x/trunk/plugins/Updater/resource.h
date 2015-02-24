9176e6674c2907eabe58109836c3f44a1f5fe352
dmex <dmex@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2015-02-24 01:02:10 +0000

    Updater: cleaned up resource file; fixed missing FileVersionInfo for Win32 builds;

--- 2.x/trunk/plugins/Updater/resource.h
+++ 2.x/trunk/plugins/Updater/resource.h
@@ -3,24 +3,24 @@
 // Used by Updater.rc
 //
 #define IDD_UPDATE                      101
-#define IDB_SF_PNG                      108
-#define IDD_OPTIONS                     109
-#define IDC_MESSAGE                     1002
-#define IDC_AUTOCHECKBOX                1003
+#define IDB_SF_PNG                      102
+#define IDD_OPTIONS                     103
+#define IDC_MESSAGE                     1001
+#define IDC_AUTOCHECKBOX                1002
 #define IDC_PROGRESS                    1003
-#define IDC_RELDATE                     1005
-#define IDC_STATUS                      1007
-#define IDC_DOWNLOAD                    1010
-#define IDC_UPDATEICON                  1017
-#define IDC_INFOSYSLINK                 1020
+#define IDC_RELDATE                     1004
+#define IDC_STATUS                      1005
+#define IDC_DOWNLOAD                    1006
+#define IDC_UPDATEICON                  1007
+#define IDC_INFOSYSLINK                 1008
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1021
-#define _APS_NEXT_SYMED_VALUE           108
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           103
 #endif
 #endif
