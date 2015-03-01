43d3647a599f16fc751ef6739ee2a222cca5ebb0
dmex <dmex04@gmail.com>
2015-03-01 14:11:45 +0000

    ExtraPlugins: added new NvGpuPlugin

---
+++ plugins-extra/NvGpuPlugin/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by NvGpuPlugin.rc
+//
+#define IDD_GPU_PANEL                   9
+#define IDD_GPU_DIALOG                  113
+#define IDC_GRAPH_LAYOUT                114
+#define IDC_TITLE                       116
+#define IDC_GPUNAME                     117
+#define IDC_PANEL_LAYOUT                118
+#define IDC_GPU_L                       119
+#define IDC_MEMORY_L                    120
+#define IDC_SHARED_L                    121
+#define IDC_SHARED_BUS                  122
+#define IDC_BUS_L                       122
+#define IDC_CLOCK_CORE                  123
+#define IDC_CLOCK_MEMORY                124
+#define IDC_FAN_PERCENT                 125
+#define IDC_TEMP_VALUE                  126
+#define IDC_CLOCK_SHADER                127
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        127
+#define _APS_NEXT_COMMAND_VALUE         40006
+#define _APS_NEXT_CONTROL_VALUE         1029
+#define _APS_NEXT_SYMED_VALUE           108
+#endif
+#endif
