d07cb74b7d22d0562d79c8e419f9d9f49acbf0a2
xhmikosr <xhmikosr@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2015-04-18 05:05:48 +0000

    Add script to run coverity.
    I'll simplify it later more.

---
+++ 2.x/trunk/build/coverity/coverity-build.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+SET MSBUILD_SWITCHES=/nologo /consoleloggerparameters:Verbosity=minimal /maxcpucount^
+ /nodeReuse:true /target:Rebuild /property:Configuration="Release"
+
+MSBuild "..\..\ProcessHacker.sln" %MSBUILD_SWITCHES%;Platform=Win32
+MSBuild "..\..\ProcessHacker.sln" %MSBUILD_SWITCHES%;Platform=x64
+
+PUSHD ..\sdk
+CALL "makesdk.cmd"
+POPD
+
+MSBuild "..\..\plugins\Plugins.sln" %MSBUILD_SWITCHES%;Platform=Win32
+MSBuild "..\..\plugins\Plugins.sln" %MSBUILD_SWITCHES%;Platform=x64
+
+MSBuild "..\..\plugins-extra\ExtraPlugins.sln" %MSBUILD_SWITCHES%;Platform=Win32
+MSBuild "..\..\plugins-extra\ExtraPlugins.sln" %MSBUILD_SWITCHES%;Platform=x64
