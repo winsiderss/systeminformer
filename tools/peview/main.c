8a1ebf6ca8631b2d0fca0709a2c253bd7dd9790d
Wen Jia Liu <wj32.64@gmail.com>
2015-04-20 12:25:22 +0000

    peview: Add winmd file extension

--- tools/peview/main.c
+++ tools/peview/main.c
@@ -72,7 +72,7 @@ INT WINAPI wWinMain(
     {
         static PH_FILETYPE_FILTER filters[] =
         {
-            { L"Supported files (*.exe;*.dll;*.ocx;*.sys;*.scr;*.cpl;*.ax;*.acm;*.lib)", L"*.exe;*.dll;*.ocx;*.sys;*.scr;*.cpl;*.ax;*.acm;*.lib" },
+            { L"Supported files (*.exe;*.dll;*.ocx;*.sys;*.scr;*.cpl;*.ax;*.acm;*.lib;*.winmd)", L"*.exe;*.dll;*.ocx;*.sys;*.scr;*.cpl;*.ax;*.acm;*.lib;*.winmd" },
             { L"All files (*.*)", L"*.*" }
         };
         PVOID fileDialog;
