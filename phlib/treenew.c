d0bde9a8eb08f7e48c87d20ea151316cdb61056f
Wen Jia Liu <wj32.64@gmail.com>
2015-04-21 13:42:29 +0000

    Added ThinRows setting

--- phlib/treenew.c
+++ phlib/treenew.c
@@ -2014,13 +2014,13 @@ VOID PhTnpUpdateTextMetrics(
         }
         else
         {
-            if (WindowsVersion >= WINDOWS_VISTA)
+            if (WindowsVersion >= WINDOWS_VISTA && !(Context->Style & TN_STYLE_THIN_ROWS))
                 Context->RowHeight += 1; // HACK
         }
 
         Context->RowHeight += 1; // HACK
 
-        if (WindowsVersion >= WINDOWS_VISTA)
+        if (WindowsVersion >= WINDOWS_VISTA && !(Context->Style & TN_STYLE_THIN_ROWS))
             Context->RowHeight += 2; // HACK
 
         ReleaseDC(Context->Handle, hdc);
