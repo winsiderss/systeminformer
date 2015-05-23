e099f28a8572edc1ba40e34057a8cd47aa4f022e
dmex <dmex@users.noreply.github.com>
2015-05-23 12:49:27 +0000

    ExtendedNotifications: added SETTING_PREFIX

--- plugins/ExtendedNotifications/main.c
+++ plugins/ExtendedNotifications/main.c
@@ -111,7 +111,7 @@ LOGICAL DllMain(
         {
             PPH_PLUGIN_INFORMATION info;
 
-            PluginInstance = PhRegisterPlugin(L"ProcessHacker.ExtendedNotifications", Instance, &info);
+            PluginInstance = PhRegisterPlugin(SETTING_PREFIX, Instance, &info);
 
             if (!PluginInstance)
                 return FALSE;
