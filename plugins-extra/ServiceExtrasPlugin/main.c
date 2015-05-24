b9a8aec198c2c8c2c24f75aed36ccababc99a1c1
dmex <dmex04@gmail.com>
2015-05-24 12:45:34 +0000

    ServiceExtrasPlugin: bump version 1.1; Added SID column to services tab;

--- plugins-extra/ServiceExtrasPlugin/main.c
+++ plugins-extra/ServiceExtrasPlugin/main.c
@@ -22,17 +22,39 @@
 
 #include "main.h"
 
-static PPH_PLUGIN PluginInstance = NULL;
+HWND ServiceTreeNewHandle = NULL;
+PPH_PLUGIN PluginInstance = NULL;
+_RtlCreateServiceSid RtlCreateServiceSid_I = NULL;
 static PH_CALLBACK_REGISTRATION PluginLoadCallbackRegistration;
 static PH_CALLBACK_REGISTRATION PluginUnloadCallbackRegistration;
 static PH_CALLBACK_REGISTRATION ServicePropertiesInitializingCallbackRegistration;
+static PH_CALLBACK_REGISTRATION ServiceTreeNewInitializingCallbackRegistration;
+static PH_CALLBACK_REGISTRATION TreeNewMessageCallbackRegistration;
+
+static LONG NTAPI ServiceSidSortFunction(
+    _In_ PVOID Node1,
+    _In_ PVOID Node2,
+    _In_ ULONG SubId,
+    _In_ PVOID Context
+    )
+{
+    PPH_SERVICE_NODE node1 = Node1;
+    PPH_SERVICE_NODE node2 = Node2;
+    PSERVICE_EXTENSION extension1 = PhPluginGetObjectExtension(PluginInstance, node1->ServiceItem, EmServiceItemType);
+    PSERVICE_EXTENSION extension2 = PhPluginGetObjectExtension(PluginInstance, node2->ServiceItem, EmServiceItemType);
+
+    UpdateServiceSid(node1, extension1);
+    UpdateServiceSid(node2, extension2);
+
+    return PhCompareStringWithNull(extension1->ServiceSid, extension2->ServiceSid, TRUE);
+}
 
 static VOID LoadCallback(
     _In_opt_ PVOID Parameter,
     _In_opt_ PVOID Context
     )
 {
-    NOTHING;
+    RtlCreateServiceSid_I = PhGetProcAddress(L"ntdll.dll", "RtlCreateServiceSid");
 }
 
 static VOID NTAPI UnloadCallback(
@@ -62,6 +84,87 @@ static VOID NTAPI ServicePropertiesInitializingCallback(
         propSheetPage.pfnDlgProc = ServiceExtraDlgProc;
         propSheetPage.lParam = (LPARAM)objectProperties->Parameter;
         objectProperties->Pages[objectProperties->NumberOfPages++] = CreatePropertySheetPage(&propSheetPage);
+    }
+}
+
+static VOID ServiceTreeNewInitializingCallback(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    )
+{
+    PPH_PLUGIN_TREENEW_INFORMATION info = Parameter;
+    PH_TREENEW_COLUMN column;
+
+    *(HWND*)Context = info->TreeNewHandle;
+
+    memset(&column, 0, sizeof(PH_TREENEW_COLUMN));
+    column.Text = L"SID";
+    column.Width = 140;
+    column.Alignment = PH_ALIGN_LEFT;
+
+    PhPluginAddTreeNewColumn(PluginInstance, info->CmData, &column, SERVICE_SID_COLUMN_ID, NULL, ServiceSidSortFunction);
+}
+
+static VOID ServiceItemCreateCallback(
+    _In_ PVOID Object,
+    _In_ PH_EM_OBJECT_TYPE ObjectType,
+    _In_ PVOID Extension
+    )
+{
+    //PPH_SERVICE_ITEM processItem = Object;
+    PSERVICE_EXTENSION extension = Extension;
+
+    memset(extension, 0, sizeof(SERVICE_EXTENSION));
+}
+
+static VOID ServiceItemDeleteCallback(
+    _In_ PVOID Object,
+    _In_ PH_EM_OBJECT_TYPE ObjectType,
+    _In_ PVOID Extension
+    )
+{
+    //PPH_SERVICE_ITEM processItem = Object;
+    PSERVICE_EXTENSION extension = Extension;
+
+    PhSwapReference(&extension->ServiceSid, NULL);
+}
+
+static VOID TreeNewMessageCallback(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    )
+{
+    PPH_PLUGIN_TREENEW_MESSAGE message = Parameter;
+
+    switch (message->Message)
+    {
+    case TreeNewGetCellText:
+        {
+            PPH_TREENEW_GET_CELL_TEXT getCellText = message->Parameter1;
+
+            if (message->TreeNewHandle == ServiceTreeNewHandle)
+            {
+                PPH_SERVICE_NODE node;
+
+                node = (PPH_SERVICE_NODE)getCellText->Node;
+
+                switch (message->SubId)
+                {
+                case SERVICE_SID_COLUMN_ID:
+                    {
+                        PSERVICE_EXTENSION extension;
+
+                        extension = PhPluginGetObjectExtension(PluginInstance, node->ServiceItem, EmServiceItemType);
+
+                        UpdateServiceSid(node, extension);
+
+                        getCellText->Text = PhGetStringRef(extension->ServiceSid);
+                    }
+                    break;
+                }
+            }
+        }
+        break;
     }
 }
 
@@ -104,6 +207,26 @@ LOGICAL DllMain(
                 ServicePropertiesInitializingCallback,
                 NULL,
                 &ServicePropertiesInitializingCallbackRegistration
+                );
+            PhRegisterCallback(
+                PhGetGeneralCallback(GeneralCallbackServiceTreeNewInitializing),
+                ServiceTreeNewInitializingCallback,
+                &ServiceTreeNewHandle,
+                &ServiceTreeNewInitializingCallbackRegistration
+                );
+            PhRegisterCallback(
+                PhGetPluginCallback(PluginInstance, PluginCallbackTreeNewMessage),
+                TreeNewMessageCallback,
+                NULL,
+                &TreeNewMessageCallbackRegistration
+                );     
+
+            PhPluginSetObjectExtension(
+                PluginInstance, 
+                EmServiceItemType, 
+                sizeof(SERVICE_EXTENSION),
+                ServiceItemCreateCallback,
+                ServiceItemDeleteCallback
                 );
         }
         break;
