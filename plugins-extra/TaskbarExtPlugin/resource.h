8afba3702465fa9a466bf8da281358d895da5463
dmex <dmex@users.noreply.github.com>
2015-06-09 16:30:53 +0000

    TaskbarExtPlugin: remove duplicate flag; fix resource control value;

--- plugins-extra/TaskbarExtPlugin/resource.h
+++ plugins-extra/TaskbarExtPlugin/resource.h
@@ -15,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           106
 #endif
 #endif
