f05bba4bd6f358dba972d3fbf56cf2d7817d3e3e
wj32 <wj32@21ef857c-d57f-4fe0-8362-d861dc6d29cd>
2015-06-10 07:53:45 +0000

    Add missing file

---
+++ 2.x/trunk/phlib/include/apiimport.h
@@ -0,0 +1,43 @@
+#ifndef _APIIMPORT_H
+#define _APIIMPORT_H
+
+typedef NTSTATUS (NTAPI *_NtQueryInformationEnlistment)(
+    _In_ HANDLE EnlistmentHandle,
+    _In_ ENLISTMENT_INFORMATION_CLASS EnlistmentInformationClass,
+    _Out_writes_bytes_(EnlistmentInformationLength) PVOID EnlistmentInformation,
+    _In_ ULONG EnlistmentInformationLength,
+    _Out_opt_ PULONG ReturnLength
+    );
+
+typedef NTSTATUS (NTAPI *_NtQueryInformationResourceManager)(
+    _In_ HANDLE ResourceManagerHandle,
+    _In_ RESOURCEMANAGER_INFORMATION_CLASS ResourceManagerInformationClass,
+    _Out_writes_bytes_(ResourceManagerInformationLength) PVOID ResourceManagerInformation,
+    _In_ ULONG ResourceManagerInformationLength,
+    _Out_opt_ PULONG ReturnLength
+    );
+
+typedef NTSTATUS (NTAPI *_NtQueryInformationTransaction)(
+    _In_ HANDLE TransactionHandle,
+    _In_ TRANSACTION_INFORMATION_CLASS TransactionInformationClass,
+    _Out_writes_bytes_(TransactionInformationLength) PVOID TransactionInformation,
+    _In_ ULONG TransactionInformationLength,
+    _Out_opt_ PULONG ReturnLength
+    );
+
+typedef NTSTATUS (NTAPI *_NtQueryInformationTransactionManager)(
+    _In_ HANDLE TransactionManagerHandle,
+    _In_ TRANSACTIONMANAGER_INFORMATION_CLASS TransactionManagerInformationClass,
+    _Out_writes_bytes_(TransactionManagerInformationLength) PVOID TransactionManagerInformation,
+    _In_ ULONG TransactionManagerInformationLength,
+    _Out_opt_ PULONG ReturnLength
+    );
+
+#define PH_DECLARE_IMPORT(Name) _##Name Name##_Import(VOID)
+
+PH_DECLARE_IMPORT(NtQueryInformationEnlistment);
+PH_DECLARE_IMPORT(NtQueryInformationResourceManager);
+PH_DECLARE_IMPORT(NtQueryInformationTransaction);
+PH_DECLARE_IMPORT(NtQueryInformationTransactionManager);
+
+#endif
