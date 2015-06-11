75e1b82cb1bca1fbdfbc876347091988c1ec3546
dmex <dmex04@gmail.com>
2015-06-11 15:56:29 +0000

    Updater: Removed WIC dependencies; Removed PNG image; Removed SF logo;

--- plugins/Updater/updater.c
+++ plugins/Updater/updater.c
@@ -27,151 +27,6 @@ static HANDLE UpdateDialogThreadHandle = NULL;
 static HWND UpdateDialogHandle = NULL;
 static PH_EVENT InitializedEvent = PH_EVENT_INIT;
 
-static HBITMAP LoadImageFromResources(
-    _In_ UINT Width,
-    _In_ UINT Height,
-    _In_ PCWSTR Name
-    )
-{
-    UINT frameCount = 0;
-    BOOLEAN isSuccess = FALSE;
-    ULONG resourceLength = 0;
-    HGLOBAL resourceHandle = NULL;
-    HRSRC resourceHandleSource = NULL;
-    WICInProcPointer resourceBuffer = NULL;
-
-    BITMAPINFO bitmapInfo = { 0 };
-    HBITMAP bitmapHandle = NULL;
-    PBYTE bitmapBuffer = NULL;
-
-    IWICStream* wicStream = NULL;
-    IWICBitmapSource* wicBitmapSource = NULL;
-    IWICBitmapDecoder* wicDecoder = NULL;
-    IWICBitmapFrameDecode* wicFrame = NULL;
-    IWICImagingFactory* wicFactory = NULL;
-    IWICBitmapScaler* wicScaler = NULL;
-    WICPixelFormatGUID pixelFormat;
-
-    WICRect rect = { 0, 0, Width, Height };
-
-    __try
-    {
-        // Create the ImagingFactory
-        if (FAILED(CoCreateInstance(&CLSID_WICImagingFactory1, NULL, CLSCTX_INPROC_SERVER, &IID_IWICImagingFactory, &wicFactory)))
-            __leave;
-
-        // Find the resource
-        if ((resourceHandleSource = FindResource(PluginInstance->DllBase, Name, L"PNG")) == NULL)
-            __leave;
-
-        // Get the resource length
-        resourceLength = SizeofResource(PluginInstance->DllBase, resourceHandleSource);
-
-        // Load the resource
-        if ((resourceHandle = LoadResource(PluginInstance->DllBase, resourceHandleSource)) == NULL)
-            __leave;
-
-        if ((resourceBuffer = (WICInProcPointer)LockResource(resourceHandle)) == NULL)
-            __leave;
-
-        // Create the Stream
-        if (FAILED(IWICImagingFactory_CreateStream(wicFactory, &wicStream)))
-            __leave;
-
-        // Initialize the Stream from Memory
-        if (FAILED(IWICStream_InitializeFromMemory(wicStream, resourceBuffer, resourceLength)))
-            __leave;
-
-        if (FAILED(IWICImagingFactory_CreateDecoder(wicFactory, &GUID_ContainerFormatPng, NULL, &wicDecoder)))
-            __leave;
-
-        if (FAILED(IWICBitmapDecoder_Initialize(wicDecoder, (IStream*)wicStream, WICDecodeMetadataCacheOnDemand)))
-            __leave;
-
-        // Get the Frame count
-        if (FAILED(IWICBitmapDecoder_GetFrameCount(wicDecoder, &frameCount)) || frameCount < 1)
-            __leave;
-
-        // Get the Frame
-        if (FAILED(IWICBitmapDecoder_GetFrame(wicDecoder, 0, &wicFrame)))
-            __leave;
-
-        // Get the WicFrame image format
-        if (SUCCEEDED(IWICBitmapFrameDecode_GetPixelFormat(wicFrame, &pixelFormat)))
-        {
-            // Check if the image format is supported:
-            if (IsEqualGUID(&pixelFormat, &GUID_WICPixelFormat32bppBGRA))
-            {
-                wicBitmapSource = (IWICBitmapSource*)wicFrame;
-            }
-            else
-            {
-                // Convert the image to the correct format:
-                if (FAILED(WICConvertBitmapSource(&GUID_WICPixelFormat32bppBGRA, (IWICBitmapSource*)wicFrame, &wicBitmapSource)))
-                    __leave;
-
-                IWICBitmapFrameDecode_Release(wicFrame);
-            }
-        }
-
-        bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-        bitmapInfo.bmiHeader.biWidth = rect.Width;
-        bitmapInfo.bmiHeader.biHeight = -((LONG)rect.Height);
-        bitmapInfo.bmiHeader.biPlanes = 1;
-        bitmapInfo.bmiHeader.biBitCount = 32;
-        bitmapInfo.bmiHeader.biCompression = BI_RGB;
-
-        HDC hdc = CreateCompatibleDC(NULL);
-        bitmapHandle = CreateDIBSection(hdc, &bitmapInfo, DIB_RGB_COLORS, (PVOID*)&bitmapBuffer, NULL, 0);
-        ReleaseDC(NULL, hdc);
-
-        // Check if it's the same rect as the requested size.
-        //if (width != rect.Width || height != rect.Height)
-        if (FAILED(IWICImagingFactory_CreateBitmapScaler(wicFactory, &wicScaler)))
-            __leave;
-        if (FAILED(IWICBitmapScaler_Initialize(wicScaler, wicBitmapSource, rect.Width, rect.Height, WICBitmapInterpolationModeFant)))
-            __leave;
-        if (FAILED(IWICBitmapScaler_CopyPixels(wicScaler, &rect, rect.Width * 4, rect.Width * rect.Height * 4, bitmapBuffer)))
-            __leave;
-
-        isSuccess = TRUE;
-    }
-    __finally
-    {
-        if (wicScaler)
-        {
-            IWICBitmapScaler_Release(wicScaler);
-        }
-
-        if (wicBitmapSource)
-        {
-            IWICBitmapSource_Release(wicBitmapSource);
-        }
-
-        if (wicStream)
-        {
-            IWICStream_Release(wicStream);
-        }
-
-        if (wicDecoder)
-        {
-            IWICBitmapDecoder_Release(wicDecoder);
-        }
-
-        if (wicFactory)
-        {
-            IWICImagingFactory_Release(wicFactory);
-        }
-
-        if (resourceHandle)
-        {
-            FreeResource(resourceHandle);
-        }
-    }
-
-    return bitmapHandle;
-}
-
 static mxml_type_t QueryXmlDataCallback(
     _In_ mxml_node_t *node
     )
@@ -311,10 +166,10 @@ static VOID FreeUpdateContext(
         Context->FontHandle = NULL;
     }
 
-    if (Context->SourceforgeBitmap)
+    if (Context->IconBitmap)
     {
-        DeleteObject(Context->SourceforgeBitmap);
-        Context->SourceforgeBitmap = NULL;
+        DeleteObject(Context->IconBitmap);
+        Context->IconBitmap = NULL;
     }
 
     PhFree(Context);
@@ -966,12 +821,12 @@ static INT_PTR CALLBACK UpdaterWndProc(
                 GetModuleHandle(NULL),
                 MAKEINTRESOURCE(PHAPP_IDI_PROCESSHACKER),
                 IMAGE_ICON,
-                GetSystemMetrics(SM_CXICON),
-                GetSystemMetrics(SM_CYICON),
+                32,
+                32,
                 LR_SHARED
                 );
-
-            context->SourceforgeBitmap = LoadImageFromResources(48, 48, MAKEINTRESOURCE(IDB_SF_PNG));
+            
+            context->IconBitmap = PhIconToBitmap(context->IconHandle, 32, 32);
 
             // Set the window icons
             if (context->IconHandle)
@@ -980,8 +835,8 @@ static INT_PTR CALLBACK UpdaterWndProc(
             if (context->FontHandle)
                 SendMessage(GetDlgItem(hwndDlg, IDC_MESSAGE), WM_SETFONT, (WPARAM)context->FontHandle, FALSE);
 
-            if (context->SourceforgeBitmap)
-                SendMessage(GetDlgItem(hwndDlg, IDC_UPDATEICON), STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)context->SourceforgeBitmap);
+            if (context->IconBitmap)
+                SendMessage(GetDlgItem(hwndDlg, IDC_UPDATEICON), STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)context->IconBitmap);
 
             // Center the update window on PH if it's visible else we center on the desktop.
             PhCenterWindow(hwndDlg, (IsWindowVisible(parentWindow) && !IsIconic(parentWindow)) ? parentWindow : NULL);
