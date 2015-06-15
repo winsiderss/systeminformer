cdb3e197a48f781128defcc5c7fa178e8549d490
dmex <dmex@users.noreply.github.com>
2015-06-15 12:11:41 +0000

    ToolStatus: fix last commit

--- plugins/ToolStatus/main.c
+++ plugins/ToolStatus/main.c
@@ -474,7 +474,7 @@ static LRESULT CALLBACK MainWndSubclassProc(
                             menuItem = PhCreateEMenuItem(0, button.idCommand, ToolbarGetText(button.idCommand), NULL, NULL);
 
                             // Add Toolbar icons to the menu (doesn't work very well on XP).
-                            //if (WindowsVersion >= WINDOWS_VISTA)
+                            if (WindowsVersion >= WINDOWS_VISTA)
                             {
                                 HICON menuIcon;
 
