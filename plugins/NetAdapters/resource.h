4fd43991bd1145940ae8239a892171b234cce912
dmex <dmex@users.noreply.github.com>
2015-08-28 13:04:11 +0000

    NetAdapters: added details button (initial commit)

--- plugins/NetAdapters/resource.h
+++ plugins/NetAdapters/resource.h
@@ -6,6 +6,7 @@
 #define IDD_NETADAPTER_DIALOG           113
 #define IDC_GRAPH_LAYOUT                114
 #define IDD_NETADAPTER_PANEL            115
+#define IDD_NETADAPTER_DETAILS          117
 #define IDC_NETADAPTERS_LISTVIEW        1023
 #define IDC_LINK_SPEED                  1026
 #define IDC_ADAPTERNAME                 1026
@@ -15,14 +16,15 @@
 #define IDC_STAT_BRECIEVED              1029
 #define IDC_STAT_BTOTAL                 1030
 #define IDC_DETAILS_BUTTON              1031
+#define IDC_DETAILS_LIST                1032
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_RESOURCE_VALUE        119
 #define _APS_NEXT_COMMAND_VALUE         40006
-#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_CONTROL_VALUE         1033
 #define _APS_NEXT_SYMED_VALUE           108
 #endif
 #endif
