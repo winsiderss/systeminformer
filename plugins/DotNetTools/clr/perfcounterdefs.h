5a0d2294da97291baecbb1ab486390178a17ca0c
dmex <dmex@users.noreply.github.com>
2016-01-28 10:02:17 +0000

    DotNetTools: Added version checking for .NET structures; Fixed some capitalization; Added missing licence info and licence.txt;

--- plugins/DotNetTools/clr/perfcounterdefs.h
+++ plugins/DotNetTools/clr/perfcounterdefs.h
@@ -1,8 +1,34 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license.
-//
+/*
+ * Process Hacker .NET Tools -
+ *   .NET Process IPC definitions
+ *
+ * Copyright (C) 2015-2016 dmex
+ *
+ * This file is part of Process Hacker.
+ *
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+// Licensed to the .NET Foundation under one or more agreements. 
+// The .NET Foundation licenses this file to you under the MIT license. 
+// See the LICENSE file in the current folder for more information. 
+//----------------------------------------------------------------------------- 
+// PerfCounterDefs.h 
+// 
+// Internal Interface for CLR to use Performance counters.
+//----------------------------------------------------------------------------- 
+//
 // dmex: This header has been highly modified.
 // Original: https://github.com/dotnet/coreclr/blob/master/src/inc/perfcounterdefs.h
 
