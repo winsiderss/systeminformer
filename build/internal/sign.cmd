6bea6a9fc2f06d9047252a8db0246ea2f3ab72c9
Wen Jia Liu <wj32.64@gmail.com>
2016-02-16 09:28:29 +1100

    Add dual signing (SHA1 & SHA256)

--- build/internal/sign.cmd
+++ build/internal/sign.cmd
@@ -7,8 +7,11 @@ if "%2" == "kmcs" set additional=/ac "%PHBASE%\build\internal\DigiCert High Assu
 
 set timestamp=
 if "%SIGN_TIMESTAMP%" == "1" set timestamp=/t http://timestamp.digicert.com
+set timestamp_rfc=
+if "%SIGN_TIMESTAMP%" == "1" set timestamp_rfc=/tr http://timestamp.digicert.com /td sha256
 
 signtool sign %timestamp% /i "DigiCert High Assurance Code Signing CA-1" %additional% %1
+signtool sign /as /fd sha256 %timestamp_rfc% /i "DigiCert SHA2 High Assurance Code Signing CA" %additional% %1
 
 goto :end
 
