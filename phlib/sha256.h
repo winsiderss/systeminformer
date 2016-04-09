3d6d962a2a170a2972fe91f8d4e5282c37b74e67
Wen Jia Liu <wj32.64@gmail.com>
2016-04-09 15:04:40 +1000

    Add Sha256HashAlgorithm

---
+++ phlib/sha256.h
@@ -0,0 +1,24 @@
+#ifndef _SHA256_H
+#define _SHA256_H
+
+#ifndef uint8
+#define uint8  unsigned char
+#endif
+
+#ifndef uint32
+#define uint32 unsigned long int
+#endif
+
+typedef struct
+{
+    uint32 total[2];
+    uint32 state[8];
+    uint8 buffer[64];
+}
+sha256_context;
+
+void sha256_starts( sha256_context *ctx );
+void sha256_update( sha256_context *ctx, uint8 *input, uint32 length );
+void sha256_finish( sha256_context *ctx, uint8 digest[32] );
+
+#endif /* sha256.h */
