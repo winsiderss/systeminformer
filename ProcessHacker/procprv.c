winsiderss/systeminformer

commit 7fe0dfd3705a4fc5f813182a3f229e240d4328d8

Add highlighting for Pico processes (Windows subsystem for linux) (#57)
* Update process flags
* Add highlighting for Pico processes (Windows subsystem for linux)

ProcessHacker/procprv.c
@@ -92,15 +92,29 @@ typedef struct _PH_PROCESS_QUERY_S1_DATA
     PWSTR IntegrityString;
 
     PPH_STRING JobName;
-    BOOLEAN IsInJob;
-    BOOLEAN IsInSignificantJob;
-
     HANDLE ConsoleHostProcessId;
     PPH_STRING PackageFullName;
 
-    BOOLEAN IsDotNet;
-    BOOLEAN IsWow64;
-    BOOLEAN IsWow64Valid;
+    union
+    {
+        ULONG Flags;
+        struct
+        {
+            ULONG IsInJob : 1;
+            ULONG IsInSignificantJob : 1;
+
+            ULONG IsDotNet : 1;
+            ULONG IsWow64 : 1;
+            ULONG IsWow64Valid : 1;
+
+            ULONG IsProtectedProcess : 1;
+            ULONG IsSecureProcess : 1;
+            ULONG IsPicoProcess : 1;
+
+            ULONG Spare : 24;
+        };
+    };
+
 } PH_PROCESS_QUERY_S1_DATA, *PPH_PROCESS_QUERY_S1_DATA;
 
 typedef struct _PH_PROCESS_QUERY_S2_DATA
@@ -982,16 +996,21 @@ VOID PhpProcessQueryStage1(
         }
     }
 
-#ifdef _WIN64
-    // WOW64
+    // Process flags
     if (processHandleLimited)
     {
-        if (NT_SUCCESS(PhGetProcessIsWow64(processHandleLimited, &Data->IsWow64)))
-            Data->IsWow64Valid = TRUE;
+        PROCESS_EXTENDED_BASIC_INFORMATION basicInfo;
+
+        if (NT_SUCCESS(PhGetProcessExtendedBasicInformation(processHandleLimited, &basicInfo)))
+        {
+            if (Data->IsWow64 = basicInfo.IsWow64Process)
+                Data->IsWow64Valid = TRUE;
+
+            Data->IsProtectedProcess = basicInfo.IsProtectedProcess;
+            Data->IsSecureProcess = basicInfo.IsSecureProcess;
+            Data->IsPicoProcess = basicInfo.IsPicoProcess;
+        }
     }
-#else
-    Data->IsWow64Valid = TRUE;
-#endif
 
     // Command line, .NET
     {
@@ -1299,6 +1318,9 @@ VOID PhpFillProcessItemStage1(
     processItem->IsInSignificantJob = Data->IsInSignificantJob;
     processItem->IsWow64 = Data->IsWow64;
     processItem->IsWow64Valid = Data->IsWow64Valid;
+    processItem->IsProtectedProcess = Data->IsProtectedProcess;
+    processItem->IsSecureProcess = Data->IsSecureProcess;
+    processItem->IsPicoProcess = Data->IsPicoProcess;
 
     PhSwapReference(&processItem->Record->CommandLine, processItem->CommandLine);
 }

ProcessHacker/include/procprv.h
@@ -166,7 +166,11 @@ typedef struct _PH_PROCESS_ITEM
             ULONG IsWow64Valid : 1;
             ULONG IsPartiallySuspended : 1;
             ULONG AddedEventSent : 1;
-            ULONG Spare : 18;
+            ULONG IsProtectedProcess : 1;
+            ULONG IsSecureProcess : 1;
+            ULONG IsPicoProcess : 1;
+
+            ULONG Spare : 15;
         };
     };
 

ProcessHacker/include/settings.h
@@ -197,6 +197,8 @@ EXT ULONG PhCsUseColorDebuggedProcesses;
 EXT ULONG PhCsColorDebuggedProcesses;
 EXT ULONG PhCsUseColorElevatedProcesses;
 EXT ULONG PhCsColorElevatedProcesses;
+EXT ULONG PhCsUseColorPicoProcesses;
+EXT ULONG PhCsColorPicoProcesses;
 EXT ULONG PhCsUseColorImmersiveProcesses;
 EXT ULONG PhCsColorImmersiveProcesses;
 EXT ULONG PhCsUseColorSuspended;

ProcessHacker/options.c
@@ -1044,6 +1044,7 @@ static COLOR_ITEM ColorItems[] =
 #endif
     COLOR_ITEM(L"ColorDebuggedProcesses", L"Debugged processes", L"Processes that are currently being debugged."),
     COLOR_ITEM(L"ColorElevatedProcesses", L"Elevated processes", L"Processes with full privileges on a system with UAC enabled."),
+    COLOR_ITEM(L"ColorPicoProcesses", L"Pico processes", L"Processes that belong to the Windows subsystem for Linux."),
     COLOR_ITEM(L"ColorImmersiveProcesses", L"Immersive processes and DLLs", L"Processes and DLLs that belong to a Modern UI app."),
     COLOR_ITEM(L"ColorSuspended", L"Suspended processes and threads", L"Processes and threads that are suspended from execution."),
     COLOR_ITEM(L"ColorDotNet", L".NET processes and DLLs", L".NET (i.e. managed) processes and DLLs."),

ProcessHacker/proctree.c
@@ -2805,6 +2805,8 @@ BOOLEAN NTAPI PhpProcessTreeNewCallback(
                 getNodeColor->BackColor = PhCsColorSuspended;
             else if (PhCsUseColorElevatedProcesses && processItem->IsElevated)
                 getNodeColor->BackColor = PhCsColorElevatedProcesses;
+            else if (PhCsUseColorDotNet && processItem->IsPicoProcess)
+                getNodeColor->BackColor = PhCsColorPicoProcesses;
             else if (PhCsUseColorImmersiveProcesses && processItem->IsImmersive)
                 getNodeColor->BackColor = PhCsColorImmersiveProcesses;
             else if (PhCsUseColorDotNet && processItem->IsDotNet)

ProcessHacker/settings.c
@@ -201,6 +201,8 @@ VOID PhSettingsInitialization(
     PhpAddIntegerSetting(L"ColorDebuggedProcesses", L"ffbbcc");
     PhpAddIntegerSetting(L"UseColorElevatedProcesses", L"1");
     PhpAddIntegerSetting(L"ColorElevatedProcesses", L"00aaff");
+    PhpAddIntegerSetting(L"UseColorPicoProcesses", L"1");
+    PhpAddIntegerSetting(L"ColorPicoProcesses", L"ff8000"); // Blue
     PhpAddIntegerSetting(L"UseColorImmersiveProcesses", L"1");
     PhpAddIntegerSetting(L"ColorImmersiveProcesses", L"cbc0ff"); // Pink
     PhpAddIntegerSetting(L"UseColorSuspended", L"1");
@@ -260,6 +262,8 @@ VOID PhUpdateCachedSettings(
     UPDATE_INTEGER_CS(ColorDebuggedProcesses);
     UPDATE_INTEGER_CS(UseColorElevatedProcesses);
     UPDATE_INTEGER_CS(ColorElevatedProcesses);
+    UPDATE_INTEGER_CS(UseColorPicoProcesses);
+    UPDATE_INTEGER_CS(ColorPicoProcesses);
     UPDATE_INTEGER_CS(UseColorImmersiveProcesses);
     UPDATE_INTEGER_CS(ColorImmersiveProcesses);
     UPDATE_INTEGER_CS(UseColorSuspended);

commit 4c4937d3bfedd6fb3915faa636110178abbbc3de

Fix process bitness regression from PR #57

ProcessHacker/procprv.c
@@ -1000,10 +1000,11 @@ VOID PhpProcessQueryStage1(
 
         if (NT_SUCCESS(PhGetProcessExtendedBasicInformation(processHandleLimited, &basicInfo)))
         {
-            Data->IsWow64Valid = Data->IsWow64 = basicInfo.IsWow64Process;
             Data->IsProtectedProcess = basicInfo.IsProtectedProcess;
             Data->IsSecureProcess = basicInfo.IsSecureProcess;
             Data->IsPicoProcess = basicInfo.IsPicoProcess;
+            Data->IsWow64 = basicInfo.IsWow64Process;
+            Data->IsWow64Valid = TRUE;
         }
     }
 