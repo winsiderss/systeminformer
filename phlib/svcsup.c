664be1faf09d8643e43a1ffae8ec8c68344af6d9
dmex <dmex04@gmail.com>
2016-12-03 13:51:06 +1100

    Fix empty services tab on LTSB and 10240 builds of Windows 10
    Bug introducted in 14393 sdk update and the SERVICE_TYPE_ALL macro.

--- phlib/svcsup.c
+++ phlib/svcsup.c
@@ -92,7 +92,20 @@ PVOID PhEnumServices(
     ULONG servicesReturned;
 
     if (!Type)
-        Type = WindowsVersion >= WINDOWS_10 ? SERVICE_TYPE_ALL : (SERVICE_DRIVER | SERVICE_WIN32);
+    {
+        if (PhOsVersion.dwBuildNumber < 14393)
+        {
+            Type = WindowsVersion >= WINDOWS_10 ? SERVICE_WIN32 | 
+                SERVICE_ADAPTER |  SERVICE_DRIVER | 
+                SERVICE_INTERACTIVE_PROCESS |  SERVICE_USER_SERVICE | 
+                SERVICE_USERSERVICE_INSTANCE : (SERVICE_DRIVER | SERVICE_WIN32);
+        }
+        else
+        {
+            Type = WindowsVersion >= WINDOWS_10 ? SERVICE_TYPE_ALL : (SERVICE_DRIVER | SERVICE_WIN32);
+        }
+    }
+
     if (!State)
         State = SERVICE_STATE_ALL;
 
