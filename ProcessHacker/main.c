b3937b56978887a1900ba58073c4a320e9fdcaa5
dmex <dmex04@gmail.com>
2017-03-11 07:50:02 +1100

    *Fix previous commit

--- ProcessHacker/main.c
+++ ProcessHacker/main.c
@@ -289,7 +289,7 @@ INT WINAPI wWinMain(
     result = PhMainMessageLoop();
 
     if (PhGetIntegerSetting(L"KphUnloadOnShutdown"))
-        PhUnloadDriver(NULL, L"KProcessHacker3");
+        PhUnloadDriver(NULL, KPH_DEVICE_SHORT_NAME);
 
     RtlExitUserProcess(result);
 }
