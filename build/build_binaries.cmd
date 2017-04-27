d49a09be50df547deee2f88c876b9400a527e7e6
dmex <dmex04@gmail.com>
2017-04-27 23:39:21 +1000

    CustomBuildTool: Add release builds, Fix build_release.cmd, Improve nightly builds

--- build/build_binaries.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-start /B /W "" "..\tools\CustomBuildTool\bin\Release\CustomBuildTool.exe" "-bin"
-start /B /W "" "..\tools\CustomBuildTool\bin\Release\CustomBuildTool.exe" "-exe"
-
-pause
