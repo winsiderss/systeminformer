1f49a46b4c3216c021396650bdab6fe32cec5bd0
ForeverOf14 <ForeverOf14@users.noreply.github.com>
2017-05-11 19:44:16 +0800

    PHNT header - new APIs (#137)
    * Add API-Def: LdrDisableThreadCalloutsForDll
    * Add API-Def EtwEventRegister
    * Create ntsmss.h
    * Add APIs
    * Complete basic API def
    * Update ntsmss.h

---
+++ phnt/include/ntsmss.h
@@ -0,0 +1,22 @@
+#ifndef _NTSMSS_H
+#define _NTSMSS_H
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+RtlConnectToSm(
+  _In_  PUNICODE_STRING ApiPortName,
+  _In_  HANDLE ApiPortHandle,
+  _In_  DWORD ProcessImageType,
+  _Out_ PHANDLE SmssConnection
+    );
+
+NTSYSAPI
+NTSTATUS
+NTAPI
+RtlSendMsgToSm(
+  _In_  HANDLE ApiPortHandle,
+  _In_  PPORT_MESSAGE MessageData
+    );
+
+#endif
