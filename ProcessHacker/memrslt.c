35b0b98971d217ce52cd49cf6112b494f174ad46
dmex <dmex04@gmail.com>
2017-05-24 17:37:49 +1000

    Add missing window icons

--- ProcessHacker/memrslt.c
+++ ProcessHacker/memrslt.c
@@ -278,6 +278,9 @@ INT_PTR CALLBACK PhpMemoryResultsDlgProc(
         {
             HWND lvHandle;
 
+            SendMessage(hwndDlg, WM_SETICON, ICON_SMALL, (LPARAM)PH_LOAD_SHARED_ICON_SMALL(PhLibImageBase, MAKEINTRESOURCE(IDI_PROCESSHACKER)));
+            SendMessage(hwndDlg, WM_SETICON, ICON_BIG, (LPARAM)PH_LOAD_SHARED_ICON_LARGE(PhLibImageBase, MAKEINTRESOURCE(IDI_PROCESSHACKER)));
+
             PhRegisterDialog(hwndDlg);
 
             {
