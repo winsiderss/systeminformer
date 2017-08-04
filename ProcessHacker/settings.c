51606aff2b6b8b0754f9d470f28bd7433367888b
dmex <dmex04@gmail.com>
2017-08-04 16:08:50 +1000

    Disable ShowHexId setting by default

--- ProcessHacker/settings.c
+++ ProcessHacker/settings.c
@@ -137,7 +137,7 @@ VOID PhAddDefaultSettings(
     PhpAddIntegerPairSetting(L"SessionShadowHotkey", L"106,2"); // VK_MULTIPLY,KBDCTRL
     PhpAddIntegerSetting(L"ShowCommitInSummary", L"1");
     PhpAddIntegerSetting(L"ShowCpuBelow001", L"0");
-    PhpAddIntegerSetting(L"ShowHexId", L"1");
+    PhpAddIntegerSetting(L"ShowHexId", L"0");
     PhpAddIntegerSetting(L"StartHidden", L"0");
     PhpAddIntegerSetting(L"SysInfoWindowAlwaysOnTop", L"0");
     PhpAddIntegerSetting(L"SysInfoWindowOneGraphPerCpu", L"0");
