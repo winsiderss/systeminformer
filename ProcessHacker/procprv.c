winsiderss/systeminformer

commit c63ad12d63ef4c162d2146894bdf43e4a97c05bc

Remove duplicate ProcessPackage query

ProcessHacker/procprv.c
@@ -1171,20 +1171,12 @@ VOID PhpProcessQueryStage2(
 
     if (PhEnableProcessQueryStage2 && processItem->FileName)
     {
-        PPH_STRING packageFullName = NULL;
-
-        if (processItem->QueryHandle)
-            packageFullName = PhGetProcessPackageFullName(processItem->QueryHandle);
-
         Data->VerifyResult = PhVerifyFileCached(
             processItem->FileName,
-            packageFullName,
+            processItem->PackageFullName,
             &Data->VerifySignerName,
             FALSE
             );
-
-        if (packageFullName)
-            PhDereferenceObject(packageFullName);
 
         status = PhIsExecutablePacked(
             processItem->FileName->Buffer,

commit 7853b856525252952f9e897ac807c1b114754dd7

Fix race introduced from commit c63ad12d

ProcessHacker/procprv.c
@@ -1179,8 +1179,6 @@ VOID PhpProcessQueryStage1(
     {
         Data->PackageFullName = PhGetProcessPackageFullName(processHandleLimited);
     }
-
-    PhpQueueProcessQueryStage2(processItem);
 }
 
 VOID PhpProcessQueryStage2(
@@ -1323,6 +1321,9 @@ VOID PhpFillProcessItemStage1(
     processItem->IsImmersive = Data->IsImmersive;
 
     PhSwapReference(&processItem->Record->CommandLine, processItem->CommandLine);
+
+    // Note: Queue stage 2 processing after filling stage1 process data. 
+    PhpQueueProcessQueryStage2(processItem);
 }
 
 VOID PhpFillProcessItemStage2(