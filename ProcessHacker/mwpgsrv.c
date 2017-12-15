winsiderss/systeminformer

commit 9d3ec4439769bd8ed1cc6caf0399904d4cebf527

Add (experimental) EventQueue support, Improve process/service/network tab performance, Remove legacy process/service providor hacks

ProcessHacker/mwpgsrv.c
@@ -3,6 +3,7 @@
  *   Main window: Services tab
  *
  * Copyright (C) 2009-2016 wj32
+ * Copyright (C) 2017 dmex
  *
  * This file is part of Process Hacker.
  *
@@ -37,16 +38,14 @@
 
 PPH_MAIN_TAB_PAGE PhMwpServicesPage;
 HWND PhMwpServiceTreeNewHandle;
+PH_PROVIDER_EVENT_QUEUE PhMwpServiceEventQueue;
 
 static PH_CALLBACK_REGISTRATION ServiceAddedRegistration;
 static PH_CALLBACK_REGISTRATION ServiceModifiedRegistration;
 static PH_CALLBACK_REGISTRATION ServiceRemovedRegistration;
 static PH_CALLBACK_REGISTRATION ServicesUpdatedRegistration;
 
-static PPH_POINTER_LIST ServicesPendingList;
 static BOOLEAN ServiceTreeListLoaded = FALSE;
-static BOOLEAN ServicesNeedsRedraw = FALSE;
-
 static PPH_TN_FILTER_ENTRY DriverFilterEntry = NULL;
 
 BOOLEAN PhMwpServicesPageCallback(
@@ -61,6 +60,8 @@ BOOLEAN PhMwpServicesPageCallback(
     case MainTabPageCreate:
         {
             PhMwpServicesPage = Page;
+
+            PhInitializeProviderEventQueue(&PhMwpServiceEventQueue, 100);
 
             PhRegisterCallback(
                 &PhServiceAddedEvent,
@@ -163,22 +164,6 @@ VOID PhMwpNeedServiceTreeList(
 
         if (PhGetIntegerSetting(L"HideDriverServices"))
             DriverFilterEntry = PhAddTreeNewFilter(PhGetFilterSupportServiceTreeList(), PhMwpDriverServiceTreeFilter, NULL);
-
-        if (ServicesPendingList)
-        {
-            PPH_SERVICE_ITEM serviceItem;
-            ULONG enumerationKey = 0;
-
-            while (PhEnumPointerList(ServicesPendingList, &enumerationKey, (PVOID *)&serviceItem))
-            {
-                PhMwpOnServiceAdded(serviceItem, 1);
-            }
-
-            // Force a re-draw.
-            PhMwpOnServicesUpdated();
-
-            PhClearReference(&ServicesPendingList);
-        }
     }
 }
 
@@ -358,12 +343,7 @@ VOID NTAPI PhMwpServiceAddedHandler(
     PPH_SERVICE_ITEM serviceItem = (PPH_SERVICE_ITEM)Parameter;
 
     PhReferenceObject(serviceItem);
-    PostMessage(
-        PhMainWndHandle,
-        WM_PH_SERVICE_ADDED,
-        PhGetRunIdProvider(&PhMwpServiceProviderRegistration),
-        (LPARAM)serviceItem
-        );
+    PhPushProviderEventQueue(&PhMwpServiceEventQueue, ProviderAddedEvent, Parameter, PhGetRunIdProvider(&PhMwpServiceProviderRegistration));
 }
 
 VOID NTAPI PhMwpServiceModifiedHandler(
@@ -376,7 +356,7 @@ VOID NTAPI PhMwpServiceModifiedHandler(
 
     copy = PhAllocateCopy(serviceModifiedData, sizeof(PH_SERVICE_MODIFIED_DATA));
 
-    PostMessage(PhMainWndHandle, WM_PH_SERVICE_MODIFIED, 0, (LPARAM)copy);
+    PhPushProviderEventQueue(&PhMwpServiceEventQueue, ProviderModifiedEvent, copy, PhGetRunIdProvider(&PhMwpServiceProviderRegistration));
 }
 
 VOID NTAPI PhMwpServiceRemovedHandler(
@@ -386,7 +366,7 @@ VOID NTAPI PhMwpServiceRemovedHandler(
 {
     PPH_SERVICE_ITEM serviceItem = (PPH_SERVICE_ITEM)Parameter;
 
-    PostMessage(PhMainWndHandle, WM_PH_SERVICE_REMOVED, 0, (LPARAM)serviceItem);
+    PhPushProviderEventQueue(&PhMwpServiceEventQueue, ProviderRemovedEvent, Parameter, PhGetRunIdProvider(&PhMwpServiceProviderRegistration));
 }
 
 VOID NTAPI PhMwpServicesUpdatedHandler(
@@ -394,7 +374,7 @@ VOID NTAPI PhMwpServicesUpdatedHandler(
     _In_opt_ PVOID Context
     )
 {
-    PostMessage(PhMainWndHandle, WM_PH_SERVICES_UPDATED, 0, 0);
+    PostMessage(PhMainWndHandle, WM_PH_SERVICES_UPDATED, PhGetRunIdProvider(&PhMwpServiceProviderRegistration), 0);
 }
 
 VOID PhMwpOnServiceAdded(
@@ -404,24 +384,8 @@ VOID PhMwpOnServiceAdded(
 {
     PPH_SERVICE_NODE serviceNode;
 
-    if (ServiceTreeListLoaded)
-    {
-        if (!ServicesNeedsRedraw)
-        {
-            TreeNew_SetRedraw(PhMwpServiceTreeNewHandle, FALSE);
-            ServicesNeedsRedraw = TRUE;
-        }
-
-        serviceNode = PhAddServiceNode(ServiceItem, RunId);
-        // ServiceItem dereferenced below
-    }
-    else
-    {
-        if (!ServicesPendingList)
-            ServicesPendingList = PhCreatePointerList(100);
-
-        PhAddItemPointerList(ServicesPendingList, ServiceItem);
-    }
+    serviceNode = PhAddServiceNode(ServiceItem, RunId);
+    // ServiceItem dereferenced below
 
     if (RunId != 1)
     {
@@ -444,30 +408,20 @@ VOID PhMwpOnServiceAdded(
         }
     }
 
-    if (ServiceTreeListLoaded)
-        PhDereferenceObject(ServiceItem);
+    PhDereferenceObject(ServiceItem);
 }
 
 VOID PhMwpOnServiceModified(
-    _In_ struct _PH_SERVICE_MODIFIED_DATA *ServiceModifiedData
+    _In_ PPH_SERVICE_MODIFIED_DATA ServiceModifiedData
     )
 {
     PH_SERVICE_CHANGE serviceChange;
     UCHAR logEntryType;
 
-    if (ServiceTreeListLoaded)
-    {
-        //if (!ServicesNeedsRedraw)
-        //{
-        //    TreeNew_SetRedraw(PhMwpServiceTreeNewHandle, FALSE);
-        //    ServicesNeedsRedraw = TRUE;
-        //}
+    PhUpdateServiceNode(PhFindServiceNode(ServiceModifiedData->Service));
 
-        PhUpdateServiceNode(PhFindServiceNode(ServiceModifiedData->Service));
-
-        if (DriverFilterEntry)
-            PhApplyTreeNewFilters(PhGetFilterSupportServiceTreeList());
-    }
+    if (DriverFilterEntry)
+        PhApplyTreeNewFilters(PhGetFilterSupportServiceTreeList());
 
     serviceChange = PhGetServiceChange(ServiceModifiedData);
 
@@ -536,15 +490,6 @@ VOID PhMwpOnServiceRemoved(
     _In_ PPH_SERVICE_ITEM ServiceItem
     )
 {
-    if (ServiceTreeListLoaded)
-    {
-        if (!ServicesNeedsRedraw)
-        {
-            TreeNew_SetRedraw(PhMwpServiceTreeNewHandle, FALSE);
-            ServicesNeedsRedraw = TRUE;
-        }
-    }
-
     PhLogServiceEntry(PH_LOG_ENTRY_SERVICE_DELETE, ServiceItem->Name, ServiceItem->DisplayName);
 
     if (PhMwpNotifyIconNotifyMask & PH_NOTIFY_SERVICE_CREATE)
@@ -563,38 +508,47 @@ VOID PhMwpOnServiceRemoved(
         }
     }
 
-    if (ServiceTreeListLoaded)
-    {
-        PhRemoveServiceNode(PhFindServiceNode(ServiceItem));
-    }
-    else
-    {
-        if (ServicesPendingList)
-        {
-            HANDLE pointerHandle;
-
-            // Remove the service from the pending list so we don't try to add it later.
-
-            if (pointerHandle = PhFindItemPointerList(ServicesPendingList, ServiceItem))
-                PhRemoveItemPointerList(ServicesPendingList, pointerHandle);
-
-            PhDereferenceObject(ServiceItem);
-        }
-    }
+    PhRemoveServiceNode(PhFindServiceNode(ServiceItem));
 }
 
 VOID PhMwpOnServicesUpdated(
-    VOID
+    _In_ ULONG RunId
     )
 {
-    if (ServiceTreeListLoaded)
-    {
-        PhTickServiceNodes();
+    PPH_PROVIDER_EVENT events;
+    ULONG count;
+    ULONG i;
 
-        if (ServicesNeedsRedraw)
+    events = PhFlushProviderEventQueue(&PhMwpServiceEventQueue, RunId, &count);
+
+    if (events)
+    {
+        TreeNew_SetRedraw(PhMwpServiceTreeNewHandle, FALSE);
+
+        for (i = 0; i < count; i++)
         {
-            TreeNew_SetRedraw(PhMwpServiceTreeNewHandle, TRUE);
-            ServicesNeedsRedraw = FALSE;
+            PH_PROVIDER_EVENT_TYPE type = PH_PROVIDER_EVENT_TYPE(events[i]);
+            PPH_SERVICE_ITEM serviceItem = PH_PROVIDER_EVENT_OBJECT(events[i]);
+
+            switch (type)
+            {
+            case ProviderAddedEvent:
+                PhMwpOnServiceAdded(serviceItem, events[i].RunId);
+                break;
+            case ProviderModifiedEvent:
+                PhMwpOnServiceModified((PPH_SERVICE_MODIFIED_DATA)serviceItem);
+                break;
+            case ProviderRemovedEvent:
+                PhMwpOnServiceRemoved(serviceItem);
+                break;
+            }
         }
+
+        PhFree(events);
     }
+
+    PhTickServiceNodes();
+
+    if (count != 0)
+        TreeNew_SetRedraw(PhMwpServiceTreeNewHandle, TRUE);
 }

ProcessHacker/hidnproc.c
@@ -565,8 +565,8 @@ static PPH_PROCESS_ITEM PhpCreateProcessItemForHiddenProcess(
     processItem = PhCreateProcessItem(Entry->ProcessId);
 
     // Mark the process as terminated if necessary.
-    if (Entry->Type == TerminatedProcess)
-        processItem->State |= PH_PROCESS_ITEM_REMOVED;
+    //if (Entry->Type == TerminatedProcess)
+    //    processItem->State |= PH_PROCESS_ITEM_REMOVED;
 
     // We need a process record. Just use the record of System Idle Process.
     if (idleProcessItem = PhReferenceProcessItem(SYSTEM_IDLE_PROCESS_ID))

ProcessHacker/include/mainwnd.h
@@ -8,19 +8,19 @@ extern BOOLEAN PhMainWndExiting;
 #define WM_PH_ACTIVATE (WM_APP + 99)
 #define PH_ACTIVATE_REPLY 0x1119
 
-#define WM_PH_PROCESS_ADDED (WM_APP + 101)
-#define WM_PH_PROCESS_MODIFIED (WM_APP + 102)
-#define WM_PH_PROCESS_REMOVED (WM_APP + 103)
+//#define WM_PH_PROCESS_ADDED_DEPRECATED (WM_APP + 101)
+//#define WM_PH_PROCESS_MODIFIED_DEPRECATED (WM_APP + 102)
+//#define WM_PH_PROCESS_REMOVED_DEPRECATED (WM_APP + 103)
 #define WM_PH_PROCESSES_UPDATED (WM_APP + 104)
 
-#define WM_PH_SERVICE_ADDED (WM_APP + 105)
-#define WM_PH_SERVICE_MODIFIED (WM_APP + 106)
-#define WM_PH_SERVICE_REMOVED (WM_APP + 107)
+//#define WM_PH_SERVICE_ADDED_DEPRECATED (WM_APP + 105)
+//#define WM_PH_SERVICE_MODIFIED_DEPRECATED (WM_APP + 106)
+//#define WM_PH_SERVICE_REMOVED_DEPRECATED (WM_APP + 107)
 #define WM_PH_SERVICES_UPDATED (WM_APP + 108)
 
-#define WM_PH_NETWORK_ITEM_ADDED (WM_APP + 109)
-#define WM_PH_NETWORK_ITEM_MODIFIED (WM_APP + 110)
-#define WM_PH_NETWORK_ITEM_REMOVED (WM_APP + 111)
+//#define WM_PH_NETWORK_ITEM_ADDED_DEPRECATED (WM_APP + 109)
+//#define WM_PH_NETWORK_ITEM_MODIFIED_DEPRECATED (WM_APP + 110)
+//#define WM_PH_NETWORK_ITEM_REMOVED_DEPRECATED (WM_APP + 111)
 #define WM_PH_NETWORK_ITEMS_UPDATED (WM_APP + 112)
 
 // begin_phapppub

ProcessHacker/include/mainwndp.h
@@ -121,28 +121,6 @@ ULONG_PTR PhMwpOnUserMessage(
     _In_ ULONG_PTR LParam
     );
 
-// Callbacks
-
-VOID NTAPI PhMwpNetworkItemAddedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    );
-
-VOID NTAPI PhMwpNetworkItemModifiedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    );
-
-VOID NTAPI PhMwpNetworkItemRemovedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    );
-
-VOID NTAPI PhMwpNetworkItemsUpdatedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    );
-
 // Settings
 
 VOID PhMwpLoadSettings(
@@ -276,6 +254,7 @@ extern PPH_MAIN_TAB_PAGE PhMwpProcessesPage;
 extern HWND PhMwpProcessTreeNewHandle;
 extern HWND PhMwpSelectedProcessWindowHandle;
 extern BOOLEAN PhMwpSelectedProcessVirtualizationEnabled;
+extern struct _PH_PROVIDER_EVENT_QUEUE PhMwpProcessEventQueue;
 
 BOOLEAN PhMwpProcessesPageCallback(
     _In_ struct _PH_MAIN_TAB_PAGE *Page,
@@ -352,27 +331,15 @@ VOID NTAPI PhMwpProcessesUpdatedHandler(
     _In_opt_ PVOID Context
     );
 
-VOID PhMwpOnProcessAdded(
-    _In_ _Assume_refs_(1) PPH_PROCESS_ITEM ProcessItem,
-    _In_ ULONG RunId
-    );
-
-VOID PhMwpOnProcessModified(
-    _In_ PPH_PROCESS_ITEM ProcessItem
-    );
-
-VOID PhMwpOnProcessRemoved(
-    _In_ PPH_PROCESS_ITEM ProcessItem
-    );
-
 VOID PhMwpOnProcessesUpdated(
-    VOID
+    _In_ ULONG RunId
     );
 
 // Services
 
 extern PPH_MAIN_TAB_PAGE PhMwpServicesPage;
 extern HWND PhMwpServiceTreeNewHandle;
+extern struct _PH_PROVIDER_EVENT_QUEUE PhMwpServiceEventQueue;
 
 BOOLEAN PhMwpServicesPageCallback(
     _In_ struct _PH_MAIN_TAB_PAGE *Page,
@@ -420,27 +387,15 @@ VOID NTAPI PhMwpServicesUpdatedHandler(
     _In_opt_ PVOID Context
     );
 
-VOID PhMwpOnServiceAdded(
-    _In_ _Assume_refs_(1) PPH_SERVICE_ITEM ServiceItem,
-    _In_ ULONG RunId
-    );
-
-VOID PhMwpOnServiceModified(
-    _In_ struct _PH_SERVICE_MODIFIED_DATA *ServiceModifiedData
-    );
-
-VOID PhMwpOnServiceRemoved(
-    _In_ PPH_SERVICE_ITEM ServiceItem
-    );
-
 VOID PhMwpOnServicesUpdated(
-    VOID
+    _In_ ULONG RunId
     );
 
 // Network
 
 extern PPH_MAIN_TAB_PAGE PhMwpNetworkPage;
 extern HWND PhMwpNetworkTreeNewHandle;
+extern struct _PH_PROVIDER_EVENT_QUEUE PhMwpNetworkEventQueue;
 
 BOOLEAN PhMwpNetworkPageCallback(
     _In_ struct _PH_MAIN_TAB_PAGE *Page,
@@ -469,21 +424,28 @@ VOID PhMwpInitializeNetworkMenu(
     _In_ ULONG NumberOfNetworkItems
     );
 
-VOID PhMwpOnNetworkItemAdded(
-    _In_ ULONG RunId,
-    _In_ _Assume_refs_(1) PPH_NETWORK_ITEM NetworkItem
+VOID PhMwpNetworkItemAddedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
     );
 
-VOID PhMwpOnNetworkItemModified(
-    _In_ PPH_NETWORK_ITEM NetworkItem
+VOID PhMwpNetworkItemModifiedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
     );
 
-VOID PhMwpOnNetworkItemRemoved(
-    _In_ PPH_NETWORK_ITEM NetworkItem
+VOID PhMwpNetworkItemRemovedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    );
+
+VOID PhMwpNetworkItemsUpdatedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
     );
 
 VOID PhMwpOnNetworkItemsUpdated(
-    VOID
+    _In_ ULONG RunId
     );
 
 // Users

ProcessHacker/include/phuisup.h
@@ -44,7 +44,6 @@ FORCEINLINE VOID PhChangeShStateTn(
         ULONG64 tickCount; \
         BOOLEAN preferFullInvalidate; \
         HANDLE stateListHandle; \
-        BOOLEAN redrawDisabled = FALSE; \
         BOOLEAN needsFullInvalidate = FALSE; \
 \
         if (!StateList || StateList->Count == 0) \
@@ -72,21 +71,13 @@ FORCEINLINE VOID PhChangeShStateTn(
                     } \
                     else \
                     { \
-                        TreeNew_InvalidateNode(TreeNewHandleForUpdate, node); \
+                        if (TreeNewHandleForUpdate) \
+                            TreeNew_InvalidateNode((TreeNewHandleForUpdate), node); \
                     } \
                 } \
             } \
             else if (node->ShStateFieldName.State == RemovingItemState) \
             { \
-                if (TreeNewHandleForUpdate) \
-                { \
-                    if (!redrawDisabled) \
-                    { \
-                        TreeNew_SetRedraw((TreeNewHandleForUpdate), FALSE); \
-                        redrawDisabled = TRUE; \
-                    } \
-                } \
-\
                 RemoveFunction(node, __VA_ARGS__); \
                 needsFullInvalidate = TRUE; \
             } \
@@ -96,8 +87,6 @@ FORCEINLINE VOID PhChangeShStateTn(
 \
         if (TreeNewHandleForUpdate) \
         { \
-            if (redrawDisabled) \
-                TreeNew_SetRedraw((TreeNewHandleForUpdate), TRUE); \
             if (needsFullInvalidate) \
             { \
                 InvalidateRect((TreeNewHandleForUpdate), NULL, FALSE); \

ProcessHacker/include/procprv.h
@@ -164,7 +164,7 @@ typedef struct _PH_PROCESS_ITEM
             ULONG IsImmersive : 1;
             ULONG IsWow64Valid : 1;
             ULONG IsPartiallySuspended : 1;
-            ULONG AddedEventSent : 1;
+            ULONG Unused : 1;
             ULONG IsProtectedProcess : 1;
             ULONG IsSecureProcess : 1;
             ULONG IsSubsystemProcess : 1;
@@ -342,7 +342,7 @@ PhGetStatisticsTimeString(
 // end_phapppub
 
 VOID PhFlushProcessQueryData(
-    _In_ BOOLEAN SendModifiedEvent
+    VOID
     );
 
 VOID PhProcessProviderUpdate(

ProcessHacker/mainwnd.c
@@ -385,11 +385,6 @@ VOID PhMwpApplyUpdateInterval(
 {
     PhSetIntervalProviderThread(&PhPrimaryProviderThread, Interval);
     PhSetIntervalProviderThread(&PhSecondaryProviderThread, Interval);
-
-    if (Interval > PH_FLUSH_PROCESS_QUERY_DATA_INTERVAL_LONG_TERM)
-        SetTimer(PhMainWndHandle, TIMER_FLUSH_PROCESS_QUERY_DATA, PH_FLUSH_PROCESS_QUERY_DATA_INTERVAL_LONG_TERM, NULL);
-    else
-        KillTimer(PhMainWndHandle, TIMER_FLUSH_PROCESS_QUERY_DATA); // Might not exist
 }
 
 VOID PhMwpInitializeControls(
@@ -1722,12 +1717,15 @@ VOID PhMwpOnTimer(
                 KillTimer(PhMainWndHandle, TIMER_FLUSH_PROCESS_QUERY_DATA);
 
             break;
-        default:
-            NOTHING;
+        case 3:
+            {
+                KillTimer(PhMainWndHandle, TIMER_FLUSH_PROCESS_QUERY_DATA);
+            }
             break;
         }
 
-        PhFlushProcessQueryData(TRUE);
+        PhBoostProvider(&PhMwpProcessProviderRegistration, NULL);
+        PhBoostProvider(&PhMwpServiceProviderRegistration, NULL);
     }
 }
 
@@ -2056,125 +2054,24 @@ ULONG_PTR PhMwpOnUserMessage(
             PhMwpActivateWindow(!!PhGetIntegerSetting(L"IconTogglesVisibility"));
         }
         break;
-    case WM_PH_PROCESS_ADDED:
-        {
-            ULONG runId = (ULONG)WParam;
-            PPH_PROCESS_ITEM processItem = (PPH_PROCESS_ITEM)LParam;
-
-            PhMwpOnProcessAdded(processItem, runId);
-        }
-        break;
-    case WM_PH_PROCESS_MODIFIED:
-        {
-            PhMwpOnProcessModified((PPH_PROCESS_ITEM)LParam);
-        }
-        break;
-    case WM_PH_PROCESS_REMOVED:
-        {
-            PhMwpOnProcessRemoved((PPH_PROCESS_ITEM)LParam);
-        }
-        break;
     case WM_PH_PROCESSES_UPDATED:
         {
-            PhMwpOnProcessesUpdated();
-        }
-        break;
-    case WM_PH_SERVICE_ADDED:
-        {
-            ULONG runId = (ULONG)WParam;
-            PPH_SERVICE_ITEM serviceItem = (PPH_SERVICE_ITEM)LParam;
-
-            PhMwpOnServiceAdded(serviceItem, runId);
-        }
-        break;
-    case WM_PH_SERVICE_MODIFIED:
-        {
-            PPH_SERVICE_MODIFIED_DATA serviceModifiedData = (PPH_SERVICE_MODIFIED_DATA)LParam;
-
-            PhMwpOnServiceModified(serviceModifiedData);
-            PhFree(serviceModifiedData);
-        }
-        break;
-    case WM_PH_SERVICE_REMOVED:
-        {
-            PhMwpOnServiceRemoved((PPH_SERVICE_ITEM)LParam);
+            PhMwpOnProcessesUpdated((ULONG)WParam);
         }
         break;
     case WM_PH_SERVICES_UPDATED:
         {
-            PhMwpOnServicesUpdated();
-        }
-        break;
-    case WM_PH_NETWORK_ITEM_ADDED:
-        {
-            ULONG runId = (ULONG)WParam;
-            PPH_NETWORK_ITEM networkItem = (PPH_NETWORK_ITEM)LParam;
-
-            PhMwpOnNetworkItemAdded(runId, networkItem);
-        }
-        break;
-    case WM_PH_NETWORK_ITEM_MODIFIED:
-        {
-            PhMwpOnNetworkItemModified((PPH_NETWORK_ITEM)LParam);
-        }
-        break;
-    case WM_PH_NETWORK_ITEM_REMOVED:
-        {
-            PhMwpOnNetworkItemRemoved((PPH_NETWORK_ITEM)LParam);
+            PhMwpOnServicesUpdated((ULONG)WParam);
         }
         break;
     case WM_PH_NETWORK_ITEMS_UPDATED:
         {
-            PhMwpOnNetworkItemsUpdated();
+            PhMwpOnNetworkItemsUpdated((ULONG)WParam);
         }
         break;
     }
 
     return 0;
-}
-
-VOID NTAPI PhMwpNetworkItemAddedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    )
-{
-    PPH_NETWORK_ITEM networkItem = (PPH_NETWORK_ITEM)Parameter;
-
-    PhReferenceObject(networkItem);
-    PostMessage(
-        PhMainWndHandle,
-        WM_PH_NETWORK_ITEM_ADDED,
-        PhGetRunIdProvider(&PhMwpNetworkProviderRegistration),
-        (LPARAM)networkItem
-        );
-}
-
-VOID NTAPI PhMwpNetworkItemModifiedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    )
-{
-    PPH_NETWORK_ITEM networkItem = (PPH_NETWORK_ITEM)Parameter;
-
-    PostMessage(PhMainWndHandle, WM_PH_NETWORK_ITEM_MODIFIED, 0, (LPARAM)networkItem);
-}
-
-VOID NTAPI PhMwpNetworkItemRemovedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    )
-{
-    PPH_NETWORK_ITEM networkItem = (PPH_NETWORK_ITEM)Parameter;
-
-    PostMessage(PhMainWndHandle, WM_PH_NETWORK_ITEM_REMOVED, 0, (LPARAM)networkItem);
-}
-
-VOID NTAPI PhMwpNetworkItemsUpdatedHandler(
-    _In_opt_ PVOID Parameter,
-    _In_opt_ PVOID Context
-    )
-{
-    PostMessage(PhMainWndHandle, WM_PH_NETWORK_ITEMS_UPDATED, 0, 0);
 }
 
 VOID PhMwpLoadSettings(

ProcessHacker/mwpgnet.c
@@ -3,6 +3,7 @@
  *   Main window: Network tab
  *
  * Copyright (C) 2009-2016 wj32
+ * Copyright (C) 2017 dmex
  *
  * This file is part of Process Hacker.
  *
@@ -38,6 +39,7 @@
 
 PPH_MAIN_TAB_PAGE PhMwpNetworkPage;
 HWND PhMwpNetworkTreeNewHandle;
+PH_PROVIDER_EVENT_QUEUE PhMwpNetworkEventQueue;
 
 static PH_CALLBACK_REGISTRATION NetworkItemAddedRegistration;
 static PH_CALLBACK_REGISTRATION NetworkItemModifiedRegistration;
@@ -46,7 +48,6 @@ static PH_CALLBACK_REGISTRATION NetworkItemsUpdatedRegistration;
 
 static BOOLEAN NetworkFirstTime = TRUE;
 static BOOLEAN NetworkTreeListLoaded = FALSE;
-static BOOLEAN NetworkNeedsRedraw = FALSE;
 
 BOOLEAN PhMwpNetworkPageCallback(
     _In_ struct _PH_MAIN_TAB_PAGE *Page,
@@ -60,6 +61,8 @@ BOOLEAN PhMwpNetworkPageCallback(
     case MainTabPageCreate:
         {
             PhMwpNetworkPage = Page;
+
+            PhInitializeProviderEventQueue(&PhMwpNetworkEventQueue, 100);
 
             PhRegisterCallback(
                 &PhNetworkItemAddedEvent,
@@ -309,52 +312,84 @@ VOID PhShowNetworkContextMenu(
     PhFree(networkItems);
 }
 
-VOID PhMwpOnNetworkItemAdded(
-    _In_ ULONG RunId,
-    _In_ _Assume_refs_(1) PPH_NETWORK_ITEM NetworkItem
+VOID NTAPI PhMwpNetworkItemAddedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
     )
 {
-    PPH_NETWORK_NODE networkNode;
+    PPH_NETWORK_ITEM networkItem = (PPH_NETWORK_ITEM)Parameter;
 
-    if (!NetworkNeedsRedraw)
-    {
-        TreeNew_SetRedraw(PhMwpNetworkTreeNewHandle, FALSE);
-        NetworkNeedsRedraw = TRUE;
-    }
-
-    networkNode = PhAddNetworkNode(NetworkItem, RunId);
-    PhDereferenceObject(NetworkItem);
+    PhReferenceObject(networkItem);
+    PhPushProviderEventQueue(&PhMwpNetworkEventQueue, ProviderAddedEvent, Parameter, PhGetRunIdProvider(&PhMwpNetworkProviderRegistration));
 }
 
-VOID PhMwpOnNetworkItemModified(
-    _In_ PPH_NETWORK_ITEM NetworkItem
+VOID NTAPI PhMwpNetworkItemModifiedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
     )
 {
-    PhUpdateNetworkNode(PhFindNetworkNode(NetworkItem));
+    PPH_NETWORK_ITEM networkItem = (PPH_NETWORK_ITEM)Parameter;
+
+    PhPushProviderEventQueue(&PhMwpNetworkEventQueue, ProviderModifiedEvent, Parameter, PhGetRunIdProvider(&PhMwpNetworkProviderRegistration));
 }
 
-VOID PhMwpOnNetworkItemRemoved(
-    _In_ PPH_NETWORK_ITEM NetworkItem
+VOID NTAPI PhMwpNetworkItemRemovedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
     )
 {
-    if (!NetworkNeedsRedraw)
-    {
-        TreeNew_SetRedraw(PhMwpNetworkTreeNewHandle, FALSE);
-        NetworkNeedsRedraw = TRUE;
-    }
+    PPH_NETWORK_ITEM networkItem = (PPH_NETWORK_ITEM)Parameter;
 
-    PhRemoveNetworkNode(PhFindNetworkNode(NetworkItem));
+    PhPushProviderEventQueue(&PhMwpNetworkEventQueue, ProviderRemovedEvent, Parameter, PhGetRunIdProvider(&PhMwpNetworkProviderRegistration));
+}
+
+VOID NTAPI PhMwpNetworkItemsUpdatedHandler(
+    _In_opt_ PVOID Parameter,
+    _In_opt_ PVOID Context
+    )
+{
+    PostMessage(PhMainWndHandle, WM_PH_NETWORK_ITEMS_UPDATED, PhGetRunIdProvider(&PhMwpNetworkProviderRegistration), 0);
 }
 
 VOID PhMwpOnNetworkItemsUpdated(
-    VOID
+    _In_ ULONG RunId
     )
 {
+    PPH_PROVIDER_EVENT events;
+    ULONG count;
+    ULONG i;
+
+    events = PhFlushProviderEventQueue(&PhMwpNetworkEventQueue, RunId, &count);
+
+    if (events)
+    {
+        TreeNew_SetRedraw(PhMwpNetworkTreeNewHandle, FALSE);
+
+        for (i = 0; i < count; i++)
+        {
+            PH_PROVIDER_EVENT_TYPE type = PH_PROVIDER_EVENT_TYPE(events[i]);
+            PPH_NETWORK_ITEM networkItem = PH_PROVIDER_EVENT_OBJECT(events[i]);
+
+            switch (type)
+            {
+            case ProviderAddedEvent:
+                PhAddNetworkNode(networkItem, events[i].RunId);
+                PhDereferenceObject(networkItem);
+                break;
+            case ProviderModifiedEvent:
+                PhUpdateNetworkNode(PhFindNetworkNode(networkItem));
+                break;
+            case ProviderRemovedEvent:
+                PhRemoveNetworkNode(PhFindNetworkNode(networkItem));
+                break;
+            }
+        }
+
+        PhFree(events);
+    }
+
     PhTickNetworkNodes();
 
-    if (NetworkNeedsRedraw)
-    {
+    if (count != 0)
         TreeNew_SetRedraw(PhMwpNetworkTreeNewHandle, TRUE);
-        NetworkNeedsRedraw = FALSE;
-    }
 }

commit a1babb1b56ed2a0985a5eaf58b3a82a190f495f7

Fix HideDriverServices regression from commit 9d3ec4439769bd8ed1cc6caf0399904d4cebf527

ProcessHacker/mwpgsrv.c
@@ -117,7 +117,8 @@ BOOLEAN PhMwpServicesPageCallback(
         return TRUE;
     case MainTabPageLoadSettings:
         {
-            // Nothing
+            if (PhGetIntegerSetting(L"HideDriverServices"))
+                DriverFilterEntry = PhAddTreeNewFilter(PhGetFilterSupportServiceTreeList(), PhMwpDriverServiceTreeFilter, NULL);
         }
         return TRUE;
     case MainTabPageSaveSettings:
@@ -159,11 +160,7 @@ VOID PhMwpNeedServiceTreeList(
     if (!ServiceTreeListLoaded)
     {
         ServiceTreeListLoaded = TRUE;
-
         PhLoadSettingsServiceTreeList();
-
-        if (PhGetIntegerSetting(L"HideDriverServices"))
-            DriverFilterEntry = PhAddTreeNewFilter(PhGetFilterSupportServiceTreeList(), PhMwpDriverServiceTreeFilter, NULL);
     }
 }
 