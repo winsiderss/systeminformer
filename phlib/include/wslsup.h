830487082dc3b09cb2cf67caaa732b9279da2dfc
dmex <dmex04@gmail.com>
2019-01-26 00:35:12 +0100

    Add inital lxss support wrappers

---
+++ phlib/include/wslsup.h
@@ -0,0 +1,47 @@
+/*
+ * Process Hacker -
+ *   LXSS support helpers
+ *
+ * Copyright (C) 2019 dmex
+ *
+ * This file is part of Process Hacker.
+ *
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PH_WSLSUP_H
+#define _PH_WSLSUP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BOOLEAN PhInitializeLxssImageVersionInfo(
+    _Out_ PPH_IMAGE_VERSION_INFO ImageVersionInfo,
+    _In_ PPH_STRING FileName
+    );
+
+ULONG PhCreateProcessLxss(
+    _In_ PWSTR LxssDistribution,
+    _In_ PWSTR LxssCommandLine,
+    _In_opt_ PWSTR LxssCurrentDirectory,
+    _Out_ PPH_STRING *Result,
+    _Out_opt_ PHANDLE ProcessHandle
+    );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
