4576f841cba275eaa93d0790e57a26ef194b315c
dmex <dmex04@gmail.com>
2019-04-28 03:00:56 +0200

    Remove legacy PhCreateThread

--- plugins/DotNetTools/asmpage.c
+++ plugins/DotNetTools/asmpage.c
@@ -1169,7 +1169,8 @@ ULONG UpdateDotNetTraceInfoWithTimeout(
     Context->TraceHandleActive = 0;
     Context->TraceHandle = 0;
 
-    threadHandle = PhCreateThread(0, UpdateDotNetTraceInfoThreadStart, Context);
+    if (!NT_SUCCESS(PhCreateThreadEx(&threadHandle, UpdateDotNetTraceInfoThreadStart, Context)))
+        return ERROR_INVALID_HANDLE;
 
     if (NtWaitForSingleObject(threadHandle, FALSE, Timeout) != STATUS_WAIT_0)
     {
