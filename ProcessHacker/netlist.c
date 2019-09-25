winsiderss/systeminformer

commit b7ad49b6fe0e6446a49ed43c6b1b68ec0a6abb4d

Add PID column to network tab, Update network address sorting

ProcessHacker/netlist.c
@@ -136,6 +136,7 @@ VOID PhInitializeNetworkTreeList(
     PhAddTreeNewColumnEx(hwnd, PHNETLC_TIMESTAMP, FALSE, L"Time stamp", 100, PH_ALIGN_LEFT, -1, 0, TRUE);
     PhAddTreeNewColumn(hwnd, PHNETLC_LOCALHOSTNAME, FALSE, L"Local hostname", 120, PH_ALIGN_LEFT, -1, 0);
     PhAddTreeNewColumn(hwnd, PHNETLC_REMOTEHOSTNAME, FALSE, L"Remote hostname", 120, PH_ALIGN_LEFT, -1, 0);
+    PhAddTreeNewColumn(hwnd, PHNETLC_PID, TRUE, L"PID", 50, PH_ALIGN_RIGHT, 0, DT_RIGHT);
 
     TreeNew_SetRedraw(hwnd, TRUE);
 
@@ -296,6 +297,7 @@ VOID PhpRemoveNetworkNode(
 
     if (NetworkNode->ProcessNameText) PhDereferenceObject(NetworkNode->ProcessNameText);
     if (NetworkNode->TimeStampText) PhDereferenceObject(NetworkNode->TimeStampText);
+    if (NetworkNode->PidText) PhDereferenceObject(NetworkNode->PidText);
     if (NetworkNode->TooltipText) PhDereferenceObject(NetworkNode->TooltipText);
 
     PhDereferenceObject(NetworkNode->NetworkItem);
@@ -371,7 +373,18 @@ END_SORT_FUNCTION
 
 BEGIN_SORT_FUNCTION(LocalAddress)
 {
-    sortResult = PhCompareStringZ(networkItem1->LocalAddressString, networkItem2->LocalAddressString, FALSE);
+    if (networkItem1->ProtocolType & PH_IPV4_NETWORK_TYPE && networkItem2->ProtocolType & PH_IPV4_NETWORK_TYPE)
+    {
+        sortResult = uintcmp(networkItem1->LocalEndpoint.Address.InAddr.s_addr, networkItem2->LocalEndpoint.Address.InAddr.s_addr);
+    }
+    else if (networkItem1->ProtocolType & PH_IPV6_NETWORK_TYPE && networkItem2->ProtocolType & PH_IPV6_NETWORK_TYPE)
+    {
+        sortResult = memcmp(networkItem1->LocalEndpoint.Address.In6Addr.s6_addr, networkItem2->LocalEndpoint.Address.In6Addr.s6_addr, sizeof(IN6_ADDR));
+    }
+    else
+    {
+        sortResult = PhCompareStringZ(networkItem1->LocalAddressString, networkItem2->LocalAddressString, FALSE);
+    }
 }
 END_SORT_FUNCTION
 
@@ -389,7 +402,18 @@ END_SORT_FUNCTION
 
 BEGIN_SORT_FUNCTION(RemoteAddress)
 {
-    sortResult = PhCompareStringZ(networkItem1->RemoteAddressString, networkItem2->RemoteAddressString, FALSE);
+    if (networkItem1->ProtocolType & PH_IPV4_NETWORK_TYPE && networkItem2->ProtocolType & PH_IPV4_NETWORK_TYPE)
+    {
+        sortResult = uintcmp(networkItem1->RemoteEndpoint.Address.InAddr.s_addr, networkItem2->RemoteEndpoint.Address.InAddr.s_addr);
+    }
+    else if (networkItem1->ProtocolType & PH_IPV6_NETWORK_TYPE && networkItem2->ProtocolType & PH_IPV6_NETWORK_TYPE)
+    {
+        sortResult = memcmp(networkItem1->RemoteEndpoint.Address.In6Addr.s6_addr, networkItem2->RemoteEndpoint.Address.In6Addr.s6_addr, sizeof(IN6_ADDR));
+    }
+    else
+    {
+        sortResult = PhCompareStringZ(networkItem1->RemoteAddressString, networkItem2->RemoteAddressString, FALSE);
+    }
 }
 END_SORT_FUNCTION
 
@@ -556,6 +580,19 @@ BOOLEAN NTAPI PhpNetworkTreeNewCallback(
                     }
                 }
                 break;
+            case PHNETLC_PID:
+                {
+                    PH_FORMAT format[1];
+
+                    if (networkItem->ProcessId)
+                        PhInitFormatU(&format[0], HandleToUlong(networkItem->ProcessId));
+                    else
+                        PhInitFormatS(&format[0], L"Waiting connections");
+
+                    PhMoveReference(&node->PidText, PhFormat(format, 1, 96));
+                    getCellText->Text = node->PidText->sr;
+                }
+                break;
             default:
                 return FALSE;
             }
@@ -701,21 +738,21 @@ PPH_STRING PhpGetNetworkItemProcessName(
     _In_ PPH_NETWORK_ITEM NetworkItem
     )
 {
-    PH_FORMAT format[4];
+    PH_FORMAT format[1];
 
-    if (!NetworkItem->ProcessId)
-        return PhCreateString(L"Waiting connections");
-
-    PhInitFormatS(&format[1], L" (");
-    PhInitFormatU(&format[2], HandleToUlong(NetworkItem->ProcessId));
-    PhInitFormatC(&format[3], ')');
-
-    if (NetworkItem->ProcessName)
-        PhInitFormatSR(&format[0], NetworkItem->ProcessName->sr);
+    if (NetworkItem->ProcessId)
+    {
+        if (NetworkItem->ProcessName)
+            PhInitFormatSR(&format[0], NetworkItem->ProcessName->sr);
+        else
+            PhInitFormatS(&format[0], L"Unknown process");
+    }
     else
-        PhInitFormatS(&format[0], L"Unknown process");
+    {
+        PhInitFormatS(&format[0], L"Waiting connections");
+    }
 
-    return PhFormat(format, 4, 96);
+    return PhFormat(format, 1, 96);
 }
 
 PPH_NETWORK_ITEM PhGetSelectedNetworkItem(

ProcessHacker/include/netlist.h
@@ -16,7 +16,8 @@
 #define PHNETLC_TIMESTAMP 8
 #define PHNETLC_LOCALHOSTNAME 9
 #define PHNETLC_REMOTEHOSTNAME 10
-#define PHNETLC_MAXIMUM 11
+#define PHNETLC_PID 11
+#define PHNETLC_MAXIMUM 12
 
 // begin_phapppub
 typedef struct _PH_NETWORK_NODE
@@ -33,6 +34,7 @@ typedef struct _PH_NETWORK_NODE
     ULONG64 UniqueId;
     PPH_STRING ProcessNameText;
     PPH_STRING TimeStampText;
+    PPH_STRING PidText;
 
     PPH_STRING TooltipText;
 // begin_phapppub

commit a769d4b07944f0b3bca6a0fa761caf06636a69be

Fix crash regression from commit b7ad49b

ProcessHacker/netlist.c
@@ -136,7 +136,7 @@ VOID PhInitializeNetworkTreeList(
     PhAddTreeNewColumnEx(hwnd, PHNETLC_TIMESTAMP, FALSE, L"Time stamp", 100, PH_ALIGN_LEFT, -1, 0, TRUE);
     PhAddTreeNewColumn(hwnd, PHNETLC_LOCALHOSTNAME, FALSE, L"Local hostname", 120, PH_ALIGN_LEFT, -1, 0);
     PhAddTreeNewColumn(hwnd, PHNETLC_REMOTEHOSTNAME, FALSE, L"Remote hostname", 120, PH_ALIGN_LEFT, -1, 0);
-    PhAddTreeNewColumn(hwnd, PHNETLC_PID, TRUE, L"PID", 50, PH_ALIGN_RIGHT, 0, DT_RIGHT);
+    PhAddTreeNewColumn(hwnd, PHNETLC_PID, FALSE, L"PID", 50, PH_ALIGN_RIGHT, 0, DT_RIGHT);
 
     TreeNew_SetRedraw(hwnd, TRUE);
 
@@ -453,6 +453,12 @@ BEGIN_SORT_FUNCTION(TimeStamp)
 }
 END_SORT_FUNCTION
 
+BEGIN_SORT_FUNCTION(Pid)
+{
+    sortResult = intptrcmp((LONG_PTR)networkItem1->ProcessId, (LONG_PTR)networkItem2->ProcessId);
+}
+END_SORT_FUNCTION
+
 BOOLEAN NTAPI PhpNetworkTreeNewCallback(
     _In_ HWND hwnd,
     _In_ PH_TREENEW_MESSAGE Message,
@@ -486,7 +492,8 @@ BOOLEAN NTAPI PhpNetworkTreeNewCallback(
                     SORT_FUNCTION(Owner),
                     SORT_FUNCTION(TimeStamp),
                     SORT_FUNCTION(LocalHostname),
-                    SORT_FUNCTION(RemoteHostname)
+                    SORT_FUNCTION(RemoteHostname),
+                    SORT_FUNCTION(Pid)
                 };
                 int (__cdecl *sortFunction)(const void *, const void *);
 