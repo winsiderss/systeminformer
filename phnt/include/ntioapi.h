bb6f88b5bf1f18dc260183fc851e53f0131c8310
dmex <dmex04@gmail.com>
2019-11-07 02:54:11 +1100

    Update ntioapi.h

--- phnt/include/ntioapi.h
+++ phnt/include/ntioapi.h
@@ -1294,6 +1294,24 @@ NtQueryDirectoryFile(
     _In_ BOOLEAN RestartScan
     );
 
+#if (PHNT_VERSION >= PHNT_REDSTONE3)
+NTSYSCALLAPI
+NTSTATUS
+NTAPI
+NtQueryDirectoryFileEx(
+    _In_ HANDLE FileHandle,
+    _In_opt_ HANDLE Event,
+    _In_opt_ PIO_APC_ROUTINE ApcRoutine,
+    _In_opt_ PVOID ApcContext,
+    _Out_ PIO_STATUS_BLOCK IoStatusBlock,
+    _Out_ PVOID FileInformation,
+    _In_ ULONG Length,
+    _In_ FILE_INFORMATION_CLASS FileInformationClass,
+    _In_ ULONG QueryFlags,
+    _In_opt_ PUNICODE_STRING FileName
+    );
+#endif
+
 NTSYSCALLAPI
 NTSTATUS
 NTAPI
