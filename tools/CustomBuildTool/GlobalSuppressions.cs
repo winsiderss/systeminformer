4b03f163756666b0b868627ec068a58da16b5892
dmex <dmex04@gmail.com>
2019-11-07 15:56:32 +1100

    BuildTools: Fix vs2019 warnings

---
+++ tools/CustomBuildTool/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0063:Use simple 'using' statement")]
