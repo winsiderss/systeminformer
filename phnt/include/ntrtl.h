b3197613aeb87354948729e96d71fb93cc415593
diversenok <diversenok@gmail.com>
2019-12-01 16:22:29 +0100

    Add RtlLengthSidAsUnicodeString

--- phnt/include/ntrtl.h
+++ phnt/include/ntrtl.h
@@ -5879,6 +5879,14 @@ RtlReplaceSidInSd(
 NTSYSAPI
 NTSTATUS
 NTAPI
+RtlLengthSidAsUnicodeString(
+    _In_ PSID Sid,
+    _Out_ PULONG StringLength
+    );
+
+NTSYSAPI
+NTSTATUS
+NTAPI
 RtlConvertSidToUnicodeString(
     _Inout_ PUNICODE_STRING UnicodeString,
     _In_ PSID Sid,
