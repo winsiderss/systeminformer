4eee3416fface3a814b6da0a4367ef0d749f806b
dmex <dmex04@gmail.com>
2019-12-05 02:07:18 +1100

    peview: Add .NET binary StrongNameToken

--- tools/peview/resource.h
+++ tools/peview/resource.h
@@ -44,6 +44,7 @@
 #define IDC_IMAGEBASE                   1015
 #define IDC_MVIDSTRING                  1015
 #define IDC_ENTRYPOINT                  1016
+#define IDC_TOKENSTRING                 1016
 #define IDC_SYMSEARCH                   1017
 #define IDC_IMAGETYPE                   1017
 #define IDC_IMAGESIZE                   1017
@@ -60,7 +61,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        126
+#define _APS_NEXT_RESOURCE_VALUE        127
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           115
