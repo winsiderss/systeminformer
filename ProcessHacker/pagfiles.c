0e6b578b24db1f04e5b7b5b7499b43fe07c1f0ad
dmex <dmex04@gmail.com>
2019-12-21 20:58:34 +1100

    Update pagefile dialog #526

--- ProcessHacker/pagfiles.c
+++ ProcessHacker/pagfiles.c
@@ -3,6 +3,7 @@
  *   pagefiles viewer
  *
  * Copyright (C) 2010 wj32
+ * Copyright (C) 2020 dmex
  *
  * This file is part of Process Hacker.
  *
@@ -21,6 +22,17 @@
  */
 
 #include <phapp.h>
+#include <phsettings.h>
+#include <settings.h>
+
+HWND PhPageFileWindowHandle = NULL;
+
+typedef struct _PHP_PAGEFILE_PROPERTIES_CONTEXT
+{
+    HWND WindowHandle;
+    HWND ListViewHandle;
+    PH_LAYOUT_MANAGER LayoutManager;
+} PHP_PAGEFILE_PROPERTIES_CONTEXT, *PPHP_PAGEFILE_PROPERTIES_CONTEXT;
 
 INT_PTR CALLBACK PhpPagefilesDlgProc(
     _In_ HWND hwndDlg,
@@ -33,15 +45,25 @@ VOID PhShowPagefilesDialog(
     _In_ HWND ParentWindowHandle
     )
 {
-    DialogBox(
-        PhInstanceHandle,
-        MAKEINTRESOURCE(IDD_PAGEFILES),
-        ParentWindowHandle,
-        PhpPagefilesDlgProc
-        );
+    if (!PhPageFileWindowHandle)
+    {
+        PhPageFileWindowHandle = CreateDialog(
+            PhInstanceHandle,
+            MAKEINTRESOURCE(IDD_PAGEFILES),
+            PhCsForceNoParent ? NULL : ParentWindowHandle,
+            PhpPagefilesDlgProc
+            );
+        PhRegisterDialog(PhPageFileWindowHandle);
+        ShowWindow(PhPageFileWindowHandle, SW_SHOW);
+    }
+
+    if (IsMinimized(PhPageFileWindowHandle))
+        ShowWindow(PhPageFileWindowHandle, SW_RESTORE);
+    else
+        SetForegroundWindow(PhPageFileWindowHandle);
 }
 
-static VOID PhpAddPagefileItems(
+VOID PhpAddPagefileItems(
     _In_ HWND ListViewHandle,
     _In_ PVOID Pagefiles
     )
@@ -61,9 +83,7 @@ static VOID PhpAddPagefileItems(
         newFileName = PhGetFileName(fileName);
         PhDereferenceObject(fileName);
 
-        lvItemIndex = PhAddListViewItem(ListViewHandle, MAXINT,
-            newFileName->Buffer, NULL);
-
+        lvItemIndex = PhAddListViewItem(ListViewHandle, MAXINT, newFileName->Buffer, NULL);
         PhDereferenceObject(newFileName);
 
         // Usage
@@ -85,6 +105,58 @@ static VOID PhpAddPagefileItems(
     }
 }
 
+VOID PhpAddPagefileItemsEx(
+    _In_ HWND ListViewHandle,
+    _In_ PVOID Pagefiles
+    )
+{
+    PSYSTEM_PAGEFILE_INFORMATION_EX pagefile;
+
+    pagefile = PH_FIRST_PAGEFILE_EX(Pagefiles);
+
+    while (pagefile)
+    {
+        INT lvItemIndex;
+        PPH_STRING fileName;
+        PPH_STRING newFileName;
+        PPH_STRING usage;
+
+        fileName = PhCreateStringFromUnicodeString(&pagefile->Info.PageFileName);
+        newFileName = PhGetFileName(fileName);
+        PhDereferenceObject(fileName);
+
+        lvItemIndex = PhAddListViewItem(ListViewHandle, MAXINT, newFileName->Buffer, NULL);
+        PhDereferenceObject(newFileName);
+
+        // Usage
+        usage = PhFormatSize(UInt32x32To64(pagefile->Info.TotalInUse, PAGE_SIZE), ULONG_MAX);
+        PhSetListViewSubItem(ListViewHandle, lvItemIndex, 1, usage->Buffer);
+        PhDereferenceObject(usage);
+
+        // Peak usage
+        usage = PhFormatSize(UInt32x32To64(pagefile->Info.PeakUsage, PAGE_SIZE), ULONG_MAX);
+        PhSetListViewSubItem(ListViewHandle, lvItemIndex, 2, usage->Buffer);
+        PhDereferenceObject(usage);
+
+        // Total
+        usage = PhFormatSize(UInt32x32To64(pagefile->Info.TotalSize, PAGE_SIZE), ULONG_MAX);
+        PhSetListViewSubItem(ListViewHandle, lvItemIndex, 3, usage->Buffer);
+        PhDereferenceObject(usage);
+
+        // Minimum
+        usage = PhFormatSize(UInt32x32To64(pagefile->MinimumSize, PAGE_SIZE), ULONG_MAX);
+        PhSetListViewSubItem(ListViewHandle, lvItemIndex, 4, usage->Buffer);
+        PhDereferenceObject(usage);
+
+        // Maximum
+        usage = PhFormatSize(UInt32x32To64(pagefile->MaximumSize, PAGE_SIZE), ULONG_MAX);
+        PhSetListViewSubItem(ListViewHandle, lvItemIndex, 5, usage->Buffer);
+        PhDereferenceObject(usage);
+
+        pagefile = PH_NEXT_PAGEFILE_EX(pagefile);
+    }
+}
+
 INT_PTR CALLBACK PhpPagefilesDlgProc(
     _In_ HWND hwndDlg,
     _In_ UINT uMsg,
@@ -92,65 +164,156 @@ INT_PTR CALLBACK PhpPagefilesDlgProc(
     _In_ LPARAM lParam
     )
 {
+    PPHP_PAGEFILE_PROPERTIES_CONTEXT context;
+
+    if (uMsg == WM_INITDIALOG)
+    {
+        context = PhAllocateZero(sizeof(PHP_PAGEFILE_PROPERTIES_CONTEXT));
+        context->WindowHandle = hwndDlg;
+
+        PhSetWindowContext(hwndDlg, PH_WINDOW_CONTEXT_DEFAULT, context);
+    }
+    else
+    {
+        context = PhGetWindowContext(hwndDlg, PH_WINDOW_CONTEXT_DEFAULT);
+    }
+
+    if (!context)
+        return FALSE;
+
     switch (uMsg)
     {
     case WM_INITDIALOG:
         {
             NTSTATUS status;
-            HWND lvHandle;
             PVOID pagefiles;
 
-            PhCenterWindow(hwndDlg, GetParent(hwndDlg));
+            SendMessage(hwndDlg, WM_SETICON, ICON_SMALL, (LPARAM)PH_LOAD_SHARED_ICON_SMALL(PhInstanceHandle, MAKEINTRESOURCE(IDI_PROCESSHACKER)));
+            SendMessage(hwndDlg, WM_SETICON, ICON_BIG, (LPARAM)PH_LOAD_SHARED_ICON_LARGE(PhInstanceHandle, MAKEINTRESOURCE(IDI_PROCESSHACKER)));
 
-            lvHandle = GetDlgItem(hwndDlg, IDC_LIST);
+            context->WindowHandle = hwndDlg;
+            context->ListViewHandle = GetDlgItem(hwndDlg, IDC_LIST);
 
-            PhAddListViewColumn(lvHandle, 0, 0, 0, LVCFMT_LEFT, 120, L"File name");
-            PhAddListViewColumn(lvHandle, 1, 1, 1, LVCFMT_LEFT, 100, L"Usage");
-            PhAddListViewColumn(lvHandle, 2, 2, 2, LVCFMT_LEFT, 100, L"Peak usage");
-            PhAddListViewColumn(lvHandle, 3, 3, 3, LVCFMT_LEFT, 100, L"Total");
-            PhSetListViewStyle(lvHandle, FALSE, TRUE);
-            PhSetControlTheme(lvHandle, L"explorer");
+            PhAddListViewColumn(context->ListViewHandle, 0, 0, 0, LVCFMT_LEFT, 120, L"File name");
+            PhAddListViewColumn(context->ListViewHandle, 1, 1, 1, LVCFMT_LEFT, 100, L"Usage");
+            PhAddListViewColumn(context->ListViewHandle, 2, 2, 2, LVCFMT_LEFT, 100, L"Peak usage");
+            PhAddListViewColumn(context->ListViewHandle, 3, 3, 3, LVCFMT_LEFT, 100, L"Total");
 
-            if (NT_SUCCESS(status = PhEnumPagefiles(&pagefiles)))
+            if (WindowsVersion > WINDOWS_8)
             {
-                PhpAddPagefileItems(lvHandle, pagefiles);
-                PhFree(pagefiles);
+                PhAddListViewColumn(context->ListViewHandle, 4, 4, 4, LVCFMT_LEFT, 100, L"Minimum");
+                PhAddListViewColumn(context->ListViewHandle, 5, 5, 5, LVCFMT_LEFT, 100, L"Maximum");
+            }
+
+            PhSetListViewStyle(context->ListViewHandle, FALSE, TRUE);
+            PhSetControlTheme(context->ListViewHandle, L"explorer");
+            PhSetExtendedListView(context->ListViewHandle);
+
+            PhInitializeLayoutManager(&context->LayoutManager, hwndDlg);
+            PhAddLayoutItem(&context->LayoutManager, context->ListViewHandle, NULL, PH_ANCHOR_ALL);
+            PhAddLayoutItem(&context->LayoutManager, GetDlgItem(hwndDlg, IDC_REFRESH), NULL, PH_ANCHOR_RIGHT | PH_ANCHOR_BOTTOM);
+            PhAddLayoutItem(&context->LayoutManager, GetDlgItem(hwndDlg, IDOK), NULL, PH_ANCHOR_RIGHT | PH_ANCHOR_BOTTOM);
+            PhLoadListViewColumnsFromSetting(L"PageFileListViewColumns", context->ListViewHandle);
+
+            ExtendedListView_SetRedraw(context->ListViewHandle, FALSE);
+            ListView_DeleteAllItems(context->ListViewHandle);
+
+            if (WindowsVersion > WINDOWS_8)
+            {
+                if (NT_SUCCESS(status = PhEnumPagefilesEx(&pagefiles)))
+                {
+                    PhpAddPagefileItemsEx(context->ListViewHandle, pagefiles);
+                    PhFree(pagefiles);
+                }
             }
             else
             {
-                PhShowStatus(hwndDlg, L"Unable to get pagefile information", status, 0);
-                EndDialog(hwndDlg, IDCANCEL);
+                if (NT_SUCCESS(status = PhEnumPagefiles(&pagefiles)))
+                {
+                    PhpAddPagefileItems(context->ListViewHandle, pagefiles);
+                    PhFree(pagefiles);
+                }
             }
 
+            ExtendedListView_SetRedraw(context->ListViewHandle, TRUE);
+
+            if (!NT_SUCCESS(status))
+            {
+                PhShowStatus(hwndDlg, L"Unable to query pagefile information.", status, 0);
+                DestroyWindow(hwndDlg);
+            }
+
+            if (PhGetIntegerPairSetting(L"PageFileWindowPosition").X)
+                PhLoadWindowPlacementFromSetting(L"PageFileWindowPosition", L"PageFileWindowSize", hwndDlg);
+            else
+                PhCenterWindow(hwndDlg, GetParent(hwndDlg));
+
             PhSetDialogFocus(hwndDlg, GetDlgItem(hwndDlg, IDOK));
+
+            PhInitializeWindowTheme(hwndDlg, PhEnableThemeSupport);
+        }
+        break;
+    case WM_DESTROY:
+        {
+            PhSaveListViewColumnsToSetting(L"PageFileListViewColumns", context->ListViewHandle);
+            PhSaveWindowPlacementToSetting(L"PageFileWindowPosition", L"PageFileWindowSize", hwndDlg);
+
+            PhUnregisterDialog(PhPageFileWindowHandle);
+            PhPageFileWindowHandle = NULL;
+
+            PhRemoveWindowContext(hwndDlg, PH_WINDOW_CONTEXT_DEFAULT);
+
+            PhDeleteLayoutManager(&context->LayoutManager);
+            PhFree(context);
         }
         break;
     case WM_COMMAND:
         {
-            switch (LOWORD(wParam))
+            switch (GET_WM_COMMAND_ID(wParam, lParam))
             {
             case IDCANCEL:
             case IDOK:
-                EndDialog(hwndDlg, IDOK);
+                DestroyWindow(hwndDlg);
                 break;
             case IDC_REFRESH:
                 {
                     NTSTATUS status;
                     PVOID pagefiles;
 
-                    if (NT_SUCCESS(status = PhEnumPagefiles(&pagefiles)))
+                    ExtendedListView_SetRedraw(context->ListViewHandle, FALSE);
+                    ListView_DeleteAllItems(context->ListViewHandle);
+
+                    if (WindowsVersion > WINDOWS_8)
                     {
-                        ListView_DeleteAllItems(GetDlgItem(hwndDlg, IDC_LIST));
-                        PhpAddPagefileItems(GetDlgItem(hwndDlg, IDC_LIST), pagefiles);
-                        PhFree(pagefiles);
+                        if (NT_SUCCESS(status = PhEnumPagefilesEx(&pagefiles)))
+                        {
+                            PhpAddPagefileItemsEx(context->ListViewHandle, pagefiles);
+                            PhFree(pagefiles);
+                        }
                     }
                     else
                     {
-                        PhShowStatus(hwndDlg, L"Unable to get pagefile information", status, 0);
+                        if (NT_SUCCESS(status = PhEnumPagefiles(&pagefiles)))
+                        {
+                            PhpAddPagefileItems(context->ListViewHandle, pagefiles);
+                            PhFree(pagefiles);
+                        }
+                    }
+
+                    ExtendedListView_SetRedraw(context->ListViewHandle, TRUE);
+
+                    if (!NT_SUCCESS(status))
+                    {
+                        PhShowStatus(hwndDlg, L"Unable to query pagefile information.", status, 0);
                     }
                 }
                 break;
             }
+        }
+        break;
+    case WM_SIZE:
+        {
+            PhLayoutManagerLayout(&context->LayoutManager);
         }
         break;
     }
