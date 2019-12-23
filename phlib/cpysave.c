e5d38e4312c0162572bea24188cdf94a23e6f578
dmex <dmex04@gmail.com>
2019-12-24 04:00:03 +1100

    Add PhGetListViewItemText

--- phlib/cpysave.c
+++ phlib/cpysave.c
@@ -449,7 +449,7 @@ VOID PhaMapDisplayIndexListView(
     *NumberOfColumns = count;
 }
 
-PPH_STRING PhaGetListViewItemText(
+PPH_STRING PhGetListViewItemText(
     _In_ HWND ListViewHandle,
     _In_ INT Index,
     _In_ INT SubItemIndex
@@ -484,9 +484,26 @@ PPH_STRING PhaGetListViewItemText(
     }
 
     PhTrimToNullTerminatorString(buffer);
-    PH_AUTO(buffer);
 
     return buffer;
+}
+
+PPH_STRING PhaGetListViewItemText(
+    _In_ HWND ListViewHandle,
+    _In_ INT Index,
+    _In_ INT SubItemIndex
+    )
+{
+    PPH_STRING value;
+
+    if (value = PhGetListViewItemText(ListViewHandle, Index, SubItemIndex))
+    {
+        PH_AUTO(value);
+
+        return value;
+    }
+
+    return NULL;
 }
 
 PPH_STRING PhGetListViewText(
