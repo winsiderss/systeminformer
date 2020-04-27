b4822c57ed14bac70b4379bcab71a33c829f517c
dmex <dmex04@gmail.com>
2020-04-27 21:01:52 +1000

    Update ntldr.h

--- phnt/include/ntldr.h
+++ phnt/include/ntldr.h
@@ -565,6 +565,22 @@ LdrStandardizeSystemPath(
     _In_ PUNICODE_STRING SystemPath
     );
 
+#if (PHNT_VERSION >= PHNT_WINBLUE)
+typedef struct _LDR_FAILURE_DATA
+{
+    NTSTATUS Status;
+    WCHAR DllName[32];
+    WCHAR FunctionName[32];
+} LDR_FAILURE_DATA, *PLDR_FAILURE_DATA;
+
+NTSYSAPI
+PLDR_FAILURE_DATA
+NTAPI
+LdrGetFailureData(
+    VOID
+    );
+#endif
+
 // private
 typedef struct _PS_MITIGATION_OPTIONS_MAP
 {
