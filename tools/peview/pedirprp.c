94c88b1f2a95751603d1f0b724a4676282ef8386
dmex <dmex04@gmail.com>
2020-05-02 00:17:19 +1000

    peview: Allow reordering extra columns, Update prodid columns

--- tools/peview/pedirprp.c
+++ tools/peview/pedirprp.c
@@ -144,7 +144,7 @@ INT_PTR CALLBACK PvpPeDirectoryDlgProc(
         {
             context->ListViewHandle = GetDlgItem(hwndDlg, IDC_LIST);
 
-            PhSetListViewStyle(context->ListViewHandle, FALSE, TRUE);
+            PhSetListViewStyle(context->ListViewHandle, TRUE, TRUE);
             PhSetControlTheme(context->ListViewHandle, L"explorer");
             PhAddListViewColumn(context->ListViewHandle, 0, 0, 0, LVCFMT_LEFT, 40, L"#");
             PhAddListViewColumn(context->ListViewHandle, 1, 1, 1, LVCFMT_LEFT, 130, L"Name");
