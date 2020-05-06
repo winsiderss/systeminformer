4b9ebeb5f4a23def1ef959e98ed5254f8929bddc
dmex <dmex04@gmail.com>
2020-05-06 21:22:10 +1000

    Add missing copyright

--- phnt/include/subprocesstag.h
+++ phnt/include/subprocesstag.h
@@ -1,7 +1,25 @@
+/*
+ * Process Hacker -
+ *   Subprocess tag information
+ *
+ * This file is part of Process Hacker.
+ *
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _SUBPROCESSTAG_H
 #define _SUBPROCESSTAG_H
-
-// Subprocess tag information
 
 typedef enum _TAG_INFO_LEVEL
 {
