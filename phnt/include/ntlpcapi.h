39a6ee8400456544290a6832d45bfc6ea856eb38
dmex <dmex04@gmail.com>
2020-05-06 21:22:10 +1000

    Add missing copyright

--- phnt/include/ntlpcapi.h
+++ phnt/include/ntlpcapi.h
@@ -1,7 +1,25 @@
+/*
+ * Process Hacker -
+ *   Local Inter-process Communication support functions
+ *
+ * This file is part of Process Hacker.
+ *
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _NTLPCAPI_H
 #define _NTLPCAPI_H
-
-// Local Inter-process Communication
 
 #define PORT_CONNECT 0x0001
 #define PORT_ALL_ACCESS (STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | 0x1)
