39c81f97b0aba2431bae2495eeb7f5db53c44f95
dmex <dmex04@gmail.com>
2020-07-21 02:12:28 +1000

    Update mainwnd.c

--- ProcessHacker/mainwnd.c
+++ ProcessHacker/mainwnd.c
@@ -2017,7 +2017,6 @@ VOID PhMwpLoadSettings(
     PhEnableServiceQueryStage2 = !!PhGetIntegerSetting(L"EnableServiceStage2");
     PhEnableTooltipSupport = !!PhGetIntegerSetting(L"EnableTooltipSupport");
     PhEnableLinuxSubsystemSupport = !!PhGetIntegerSetting(L"EnableLinuxSubsystemSupport");
-    PhEnableNetworkResolveDoHSupport = !!PhGetIntegerSetting(L"EnableNetworkResolveDoH");
     PhMwpNotifyIconNotifyMask = PhGetIntegerSetting(L"IconNotifyMask");
     
     if (PhGetIntegerSetting(L"MainWindowAlwaysOnTop"))
