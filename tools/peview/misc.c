8e6133d980243871115f1be8934a72a641339940
dmex <dmex04@gmail.com>
2020-10-09 16:03:30 +1100

    peview: Add tree to certificate tab

--- tools/peview/misc.c
+++ tools/peview/misc.c
@@ -405,3 +405,453 @@ VOID PvHandleListViewCommandCopy(
         PhFree(listviewItems);
     }
 }
+
+#pragma region copied from appsup.c
+
+VOID PhInitializeTreeNewColumnMenu(
+    _Inout_ PPH_TN_COLUMN_MENU_DATA Data
+    )
+{
+    PhInitializeTreeNewColumnMenuEx(Data, 0);
+}
+
+VOID PhInitializeTreeNewColumnMenuEx(
+    _Inout_ PPH_TN_COLUMN_MENU_DATA Data,
+    _In_ ULONG Flags
+    )
+{
+    PPH_EMENU_ITEM resetSortMenuItem = NULL;
+    PPH_EMENU_ITEM sizeColumnToFitMenuItem;
+    PPH_EMENU_ITEM sizeAllColumnsToFitMenuItem;
+    PPH_EMENU_ITEM hideColumnMenuItem = NULL;
+    PPH_EMENU_ITEM chooseColumnsMenuItem = NULL;
+    ULONG minimumNumberOfColumns;
+
+    Data->Menu = PhCreateEMenu();
+    Data->Selection = NULL;
+    Data->ProcessedId = 0;
+
+    sizeColumnToFitMenuItem = PhCreateEMenuItem(0, PH_TN_COLUMN_MENU_SIZE_COLUMN_TO_FIT_ID, L"Size column to fit", NULL, NULL);
+    sizeAllColumnsToFitMenuItem = PhCreateEMenuItem(0, PH_TN_COLUMN_MENU_SIZE_ALL_COLUMNS_TO_FIT_ID, L"Size all columns to fit", NULL, NULL);
+
+    if (!(Flags & PH_TN_COLUMN_MENU_NO_VISIBILITY))
+    {
+        hideColumnMenuItem = PhCreateEMenuItem(0, PH_TN_COLUMN_MENU_HIDE_COLUMN_ID, L"Hide column", NULL, NULL);
+        chooseColumnsMenuItem = PhCreateEMenuItem(0, PH_TN_COLUMN_MENU_CHOOSE_COLUMNS_ID, L"Choose columns...", NULL, NULL);
+    }
+
+    if (Flags & PH_TN_COLUMN_MENU_SHOW_RESET_SORT)
+    {
+        ULONG sortColumn;
+        PH_SORT_ORDER sortOrder;
+
+        TreeNew_GetSort(Data->TreeNewHandle, &sortColumn, &sortOrder);
+
+        if (sortOrder != Data->DefaultSortOrder || (Data->DefaultSortOrder != NoSortOrder && sortColumn != Data->DefaultSortColumn))
+            resetSortMenuItem = PhCreateEMenuItem(0, PH_TN_COLUMN_MENU_RESET_SORT_ID, L"Reset sort", NULL, NULL);
+    }
+
+    PhInsertEMenuItem(Data->Menu, sizeColumnToFitMenuItem, ULONG_MAX);
+    PhInsertEMenuItem(Data->Menu, sizeAllColumnsToFitMenuItem, ULONG_MAX);
+
+    if (!(Flags & PH_TN_COLUMN_MENU_NO_VISIBILITY))
+    {
+        PhInsertEMenuItem(Data->Menu, hideColumnMenuItem, ULONG_MAX);
+
+        if (resetSortMenuItem)
+            PhInsertEMenuItem(Data->Menu, resetSortMenuItem, ULONG_MAX);
+
+        PhInsertEMenuItem(Data->Menu, PhCreateEMenuSeparator(), ULONG_MAX);
+        PhInsertEMenuItem(Data->Menu, chooseColumnsMenuItem, ULONG_MAX);
+
+        if (TreeNew_GetFixedColumn(Data->TreeNewHandle))
+            minimumNumberOfColumns = 2; // don't allow user to remove all normal columns (the fixed column can never be removed)
+        else
+            minimumNumberOfColumns = 1;
+
+        if (!Data->MouseEvent || !Data->MouseEvent->Column ||
+            Data->MouseEvent->Column->Fixed || // don't allow the fixed column to be hidden
+            TreeNew_GetVisibleColumnCount(Data->TreeNewHandle) < minimumNumberOfColumns + 1
+            )
+        {
+            hideColumnMenuItem->Flags |= PH_EMENU_DISABLED;
+        }
+    }
+    else
+    {
+        if (resetSortMenuItem)
+            PhInsertEMenuItem(Data->Menu, resetSortMenuItem, ULONG_MAX);
+    }
+
+    if (!Data->MouseEvent || !Data->MouseEvent->Column)
+    {
+        sizeColumnToFitMenuItem->Flags |= PH_EMENU_DISABLED;
+    }
+}
+
+VOID PhpEnsureValidSortColumnTreeNew(
+    _Inout_ HWND TreeNewHandle,
+    _In_ ULONG DefaultSortColumn,
+    _In_ PH_SORT_ORDER DefaultSortOrder
+    )
+{
+    ULONG sortColumn;
+    PH_SORT_ORDER sortOrder;
+
+    // Make sure the column we're sorting by is actually visible, and if not, don't sort anymore.
+
+    TreeNew_GetSort(TreeNewHandle, &sortColumn, &sortOrder);
+
+    if (sortOrder != NoSortOrder)
+    {
+        PH_TREENEW_COLUMN column;
+
+        TreeNew_GetColumn(TreeNewHandle, sortColumn, &column);
+
+        if (!column.Visible)
+        {
+            if (DefaultSortOrder != NoSortOrder)
+            {
+                // Make sure the default sort column is visible.
+                TreeNew_GetColumn(TreeNewHandle, DefaultSortColumn, &column);
+
+                if (!column.Visible)
+                {
+                    ULONG maxId;
+                    ULONG id;
+                    BOOLEAN found;
+
+                    // Use the first visible column.
+                    maxId = TreeNew_GetMaxId(TreeNewHandle);
+                    id = 0;
+                    found = FALSE;
+
+                    while (id <= maxId)
+                    {
+                        if (TreeNew_GetColumn(TreeNewHandle, id, &column))
+                        {
+                            if (column.Visible)
+                            {
+                                DefaultSortColumn = id;
+                                found = TRUE;
+                                break;
+                            }
+                        }
+
+                        id++;
+                    }
+
+                    if (!found)
+                    {
+                        DefaultSortColumn = 0;
+                        DefaultSortOrder = NoSortOrder;
+                    }
+                }
+            }
+
+            TreeNew_SetSort(TreeNewHandle, DefaultSortColumn, DefaultSortOrder);
+        }
+    }
+}
+
+BOOLEAN PhHandleTreeNewColumnMenu(
+    _Inout_ PPH_TN_COLUMN_MENU_DATA Data
+    )
+{
+    if (!Data->Selection)
+        return FALSE;
+
+    switch (Data->Selection->Id)
+    {
+    case PH_TN_COLUMN_MENU_RESET_SORT_ID:
+        {
+            TreeNew_SetSort(Data->TreeNewHandle, Data->DefaultSortColumn, Data->DefaultSortOrder);
+        }
+        break;
+    case PH_TN_COLUMN_MENU_SIZE_COLUMN_TO_FIT_ID:
+        {
+            if (Data->MouseEvent && Data->MouseEvent->Column)
+            {
+                TreeNew_AutoSizeColumn(Data->TreeNewHandle, Data->MouseEvent->Column->Id, 0);
+            }
+        }
+        break;
+    case PH_TN_COLUMN_MENU_SIZE_ALL_COLUMNS_TO_FIT_ID:
+        {
+            ULONG maxId;
+            ULONG id;
+
+            maxId = TreeNew_GetMaxId(Data->TreeNewHandle);
+            id = 0;
+
+            while (id <= maxId)
+            {
+                TreeNew_AutoSizeColumn(Data->TreeNewHandle, id, 0);
+                id++;
+            }
+        }
+        break;
+    case PH_TN_COLUMN_MENU_HIDE_COLUMN_ID:
+        {
+            PH_TREENEW_COLUMN column;
+
+            if (Data->MouseEvent && Data->MouseEvent->Column && !Data->MouseEvent->Column->Fixed)
+            {
+                column.Id = Data->MouseEvent->Column->Id;
+                column.Visible = FALSE;
+                TreeNew_SetColumn(Data->TreeNewHandle, TN_COLUMN_FLAG_VISIBLE, &column);
+                PhpEnsureValidSortColumnTreeNew(Data->TreeNewHandle, Data->DefaultSortColumn, Data->DefaultSortOrder);
+                InvalidateRect(Data->TreeNewHandle, NULL, FALSE);
+            }
+        }
+        break;
+    case PH_TN_COLUMN_MENU_CHOOSE_COLUMNS_ID:
+        {
+            //PhShowChooseColumnsDialog(Data->TreeNewHandle, Data->TreeNewHandle, PH_CONTROL_TYPE_TREE_NEW);
+            PhpEnsureValidSortColumnTreeNew(Data->TreeNewHandle, Data->DefaultSortColumn, Data->DefaultSortOrder);
+        }
+        break;
+    default:
+        return FALSE;
+    }
+
+    Data->ProcessedId = Data->Selection->Id;
+
+    return TRUE;
+}
+
+VOID PhDeleteTreeNewColumnMenu(
+    _In_ PPH_TN_COLUMN_MENU_DATA Data
+    )
+{
+    if (Data->Menu)
+    {
+        PhDestroyEMenu(Data->Menu);
+        Data->Menu = NULL;
+    }
+}
+
+VOID PhInitializeTreeNewFilterSupport(
+    _Out_ PPH_TN_FILTER_SUPPORT Support,
+    _In_ HWND TreeNewHandle,
+    _In_ PPH_LIST NodeList
+    )
+{
+    Support->FilterList = NULL;
+    Support->TreeNewHandle = TreeNewHandle;
+    Support->NodeList = NodeList;
+}
+
+VOID PhDeleteTreeNewFilterSupport(
+    _In_ PPH_TN_FILTER_SUPPORT Support
+    )
+{
+    PhDereferenceObject(Support->FilterList);
+}
+
+PPH_TN_FILTER_ENTRY PhAddTreeNewFilter(
+    _In_ PPH_TN_FILTER_SUPPORT Support,
+    _In_ PPH_TN_FILTER_FUNCTION Filter,
+    _In_opt_ PVOID Context
+    )
+{
+    PPH_TN_FILTER_ENTRY entry;
+
+    entry = PhAllocate(sizeof(PH_TN_FILTER_ENTRY));
+    entry->Filter = Filter;
+    entry->Context = Context;
+
+    if (!Support->FilterList)
+        Support->FilterList = PhCreateList(2);
+
+    PhAddItemList(Support->FilterList, entry);
+
+    return entry;
+}
+
+VOID PhRemoveTreeNewFilter(
+    _In_ PPH_TN_FILTER_SUPPORT Support,
+    _In_ PPH_TN_FILTER_ENTRY Entry
+    )
+{
+    ULONG index;
+
+    if (!Support->FilterList)
+        return;
+
+    index = PhFindItemList(Support->FilterList, Entry);
+
+    if (index != ULONG_MAX)
+    {
+        PhRemoveItemList(Support->FilterList, index);
+        PhFree(Entry);
+    }
+}
+
+BOOLEAN PhApplyTreeNewFiltersToNode(
+    _In_ PPH_TN_FILTER_SUPPORT Support,
+    _In_ PPH_TREENEW_NODE Node
+    )
+{
+    BOOLEAN show;
+    ULONG i;
+
+    show = TRUE;
+
+    if (Support->FilterList)
+    {
+        for (i = 0; i < Support->FilterList->Count; i++)
+        {
+            PPH_TN_FILTER_ENTRY entry;
+
+            entry = Support->FilterList->Items[i];
+
+            if (!entry->Filter(Node, entry->Context))
+            {
+                show = FALSE;
+                break;
+            }
+        }
+    }
+
+    return show;
+}
+
+VOID PhApplyTreeNewFilters(
+    _In_ PPH_TN_FILTER_SUPPORT Support
+    )
+{
+    ULONG i;
+
+    for (i = 0; i < Support->NodeList->Count; i++)
+    {
+        PPH_TREENEW_NODE node;
+
+        node = Support->NodeList->Items[i];
+        node->Visible = PhApplyTreeNewFiltersToNode(Support, node);
+
+        if (!node->Visible && node->Selected)
+        {
+            node->Selected = FALSE;
+        }
+    }
+
+    TreeNew_NodesStructured(Support->TreeNewHandle);
+}
+
+#define ID_COPY_CELL 136
+#define ID_SYMBOL_COPY 40201
+
+typedef struct _PH_COPY_CELL_CONTEXT
+{
+    HWND TreeNewHandle;
+    ULONG Id; // column ID
+    PPH_STRING MenuItemText;
+} PH_COPY_CELL_CONTEXT, *PPH_COPY_CELL_CONTEXT;
+
+VOID NTAPI PhpCopyCellEMenuItemDeleteFunction(
+    _In_ struct _PH_EMENU_ITEM *Item
+    )
+{
+    PPH_COPY_CELL_CONTEXT context;
+
+    context = Item->Context;
+    PhDereferenceObject(context->MenuItemText);
+    PhFree(context);
+}
+
+BOOLEAN PhInsertCopyCellEMenuItem(
+    _In_ struct _PH_EMENU_ITEM *Menu,
+    _In_ ULONG InsertAfterId,
+    _In_ HWND TreeNewHandle,
+    _In_ PPH_TREENEW_COLUMN Column
+    )
+{
+    PPH_EMENU_ITEM parentItem;
+    ULONG indexInParent;
+    PPH_COPY_CELL_CONTEXT context;
+    PH_STRINGREF columnText;
+    PPH_STRING escapedText;
+    PPH_STRING menuItemText;
+    PPH_EMENU_ITEM copyCellItem;
+
+    if (!Column)
+        return FALSE;
+
+    if (!PhFindEMenuItemEx(Menu, 0, NULL, InsertAfterId, &parentItem, &indexInParent))
+        return FALSE;
+
+    indexInParent++;
+
+    context = PhAllocate(sizeof(PH_COPY_CELL_CONTEXT));
+    context->TreeNewHandle = TreeNewHandle;
+    context->Id = Column->Id;
+
+    PhInitializeStringRef(&columnText, Column->Text);
+    escapedText = PhEscapeStringForMenuPrefix(&columnText);
+    menuItemText = PhFormatString(L"Copy \"%s\"", escapedText->Buffer);
+    PhDereferenceObject(escapedText);
+    copyCellItem = PhCreateEMenuItem(0, ID_COPY_CELL, menuItemText->Buffer, NULL, context);
+    copyCellItem->DeleteFunction = PhpCopyCellEMenuItemDeleteFunction;
+    context->MenuItemText = menuItemText;
+
+    if (Column->CustomDraw)
+        copyCellItem->Flags |= PH_EMENU_DISABLED;
+
+    PhInsertEMenuItem(parentItem, copyCellItem, indexInParent);
+
+    return TRUE;
+}
+
+BOOLEAN PhHandleCopyCellEMenuItem(
+    _In_ struct _PH_EMENU_ITEM *SelectedItem
+    )
+{
+    PPH_COPY_CELL_CONTEXT context;
+    PH_STRING_BUILDER stringBuilder;
+    ULONG count;
+    ULONG selectedCount;
+    ULONG i;
+    PPH_TREENEW_NODE node;
+    PH_TREENEW_GET_CELL_TEXT getCellText;
+
+    if (!SelectedItem)
+        return FALSE;
+    if (SelectedItem->Id != ID_COPY_CELL)
+        return FALSE;
+
+    context = SelectedItem->Context;
+
+    PhInitializeStringBuilder(&stringBuilder, 0x100);
+    count = TreeNew_GetFlatNodeCount(context->TreeNewHandle);
+    selectedCount = 0;
+
+    for (i = 0; i < count; i++)
+    {
+        node = TreeNew_GetFlatNode(context->TreeNewHandle, i);
+
+        if (node && node->Selected)
+        {
+            selectedCount++;
+
+            getCellText.Flags = 0;
+            getCellText.Node = node;
+            getCellText.Id = context->Id;
+            PhInitializeEmptyStringRef(&getCellText.Text);
+            TreeNew_GetCellText(context->TreeNewHandle, &getCellText);
+
+            PhAppendStringBuilder(&stringBuilder, &getCellText.Text);
+            PhAppendStringBuilder2(&stringBuilder, L"\r\n");
+        }
+    }
+
+    if (stringBuilder.String->Length != 0 && selectedCount == 1)
+        PhRemoveEndStringBuilder(&stringBuilder, 2);
+
+    PhSetClipboardString(context->TreeNewHandle, &stringBuilder.String->sr);
+    PhDeleteStringBuilder(&stringBuilder);
+
+    return TRUE;
+}
+
+#pragma endregion
