607e85a6779c84af6d23376571f50eae72cdc190
dmex <dmex04@gmail.com>
2020-11-25 03:59:12 +1100

    ExtendedNotifications: Retire growl notification support to plugins-extra

--- plugins/ExtendedNotifications/extnoti.h
+++ plugins/ExtendedNotifications/extnoti.h
@@ -2,7 +2,6 @@
 #define EXTNOTI_H
 
 #define PLUGIN_NAME L"ProcessHacker.ExtendedNotifications"
-#define SETTING_NAME_ENABLE_GROWL (PLUGIN_NAME L".EnableGrowl")
 #define SETTING_NAME_LOG_FILENAME (PLUGIN_NAME L".LogFileName")
 #define SETTING_NAME_PROCESS_LIST (PLUGIN_NAME L".ProcessList")
 #define SETTING_NAME_SERVICE_LIST (PLUGIN_NAME L".ServiceList")
