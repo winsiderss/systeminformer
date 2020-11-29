806ecffcfa7c74c50e54d7a934c103273f3e01a2
dmex <dmex04@gmail.com>
2020-11-30 06:33:35 +1100

    Add mxml wrapper

--- phlib/json.c
+++ phlib/json.c
@@ -1,8 +1,8 @@
 /*
  * Process Hacker -
- *   json wrapper
+ *   json and xml wrapper
  *
- * Copyright (C) 2017 dmex
+ * Copyright (C) 2017-2020 dmex
  *
  * This file is part of Process Hacker.
  *
@@ -247,4 +247,243 @@ VOID PhSaveJsonObjectToFile(
     )
 {
     json_object_to_file(FileName, Object);
+}
+
+// XML support
+
+NTSTATUS PhLoadXmlObjectFromFile(
+    _In_ PWSTR FileName,
+    _Out_opt_ PVOID* XmlRootObject
+    )
+{
+    NTSTATUS status;
+    HANDLE fileHandle;
+    LARGE_INTEGER fileSize;
+    mxml_node_t* currentNode;
+
+    status = PhCreateFileWin32(
+        &fileHandle,
+        FileName,
+        FILE_GENERIC_READ,
+        FILE_ATTRIBUTE_NORMAL,
+        FILE_SHARE_READ | FILE_SHARE_DELETE,
+        FILE_OPEN,
+        FILE_NON_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT
+        );
+
+    if (!NT_SUCCESS(status))
+        return status;
+
+    if (NT_SUCCESS(PhGetFileSize(fileHandle, &fileSize)) && fileSize.QuadPart == 0)
+    {
+        // A blank file is OK.
+        NtClose(fileHandle);
+        return STATUS_END_OF_FILE;
+    }
+
+    currentNode = mxmlLoadFd(
+        NULL,
+        fileHandle,
+        MXML_OPAQUE_CALLBACK
+        );
+
+    NtClose(fileHandle);
+
+    if (currentNode)
+    {
+        if (mxmlGetType(currentNode) == MXML_ELEMENT)
+        {
+            if (XmlRootObject)
+                *XmlRootObject = currentNode;
+
+            return STATUS_SUCCESS;
+        }
+
+        mxmlDelete(currentNode);
+    }
+
+    return STATUS_FILE_CORRUPT_ERROR;
+}
+
+NTSTATUS PhSaveXmlObjectToFile(
+    _In_ PWSTR FileName,
+    _In_ PVOID XmlRootObject,
+    _In_opt_ PVOID XmlSaveCallback
+    )
+{
+    NTSTATUS status;
+    HANDLE fileHandle;
+    PPH_STRING fullPath;
+    ULONG indexOfFileName;
+
+    // Create the directory if it does not exist.  
+    if (fullPath = PhGetFullPath(FileName, &indexOfFileName))
+    {
+        if (indexOfFileName != ULONG_MAX)
+        {
+            PPH_STRING directoryName;
+
+            directoryName = PhSubstring(fullPath, 0, indexOfFileName);
+            status = PhCreateDirectory(directoryName);
+
+            if (!NT_SUCCESS(status))
+            {
+                PhDereferenceObject(directoryName);
+                PhDereferenceObject(fullPath);
+                return status;
+            }
+
+            PhDereferenceObject(directoryName);
+        }
+
+        PhDereferenceObject(fullPath);
+    }  
+
+    status = PhCreateFileWin32(
+        &fileHandle,
+        FileName,
+        FILE_GENERIC_WRITE,
+        FILE_ATTRIBUTE_NORMAL,
+        FILE_SHARE_READ,
+        FILE_OVERWRITE_IF,
+        FILE_NON_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT
+        );
+
+    if (!NT_SUCCESS(status))
+        return status;
+
+    if (mxmlSaveFd(XmlRootObject, fileHandle, XmlSaveCallback) == -1)
+    {
+        status = STATUS_UNSUCCESSFUL;
+    }
+
+    NtClose(fileHandle);
+
+    return status;
+}
+
+PVOID PhCreateXmlNode(
+    _In_opt_ PVOID ParentNode,
+    _In_ PSTR Name
+    )
+{
+    return mxmlNewElement(ParentNode, Name);
+}
+
+PVOID PhCreateXmlOpaqueNode(
+    _In_opt_ PVOID ParentNode,
+    _In_ PSTR Value
+    )
+{
+    return mxmlNewOpaque(ParentNode, Value);
+}
+
+VOID PhFreeXmlObject(
+    _In_ PVOID XmlRootObject
+    )
+{
+    mxmlDelete(XmlRootObject);
+}
+
+PVOID PhGetXmlNodeFirstChild(
+    _In_ PVOID XmlNodeObject
+    )
+{
+    return mxmlGetFirstChild(XmlNodeObject);
+}
+
+PVOID PhGetXmlNodeNextChild(
+    _In_ PVOID XmlNodeObject
+    )
+{
+    return mxmlGetNextSibling(XmlNodeObject);
+}
+
+PPH_STRING PhGetOpaqueXmlNodeText(
+    _In_ PVOID XmlNodeObject
+    )
+{
+    PCSTR string;
+
+    if (string = mxmlGetOpaque(XmlNodeObject))
+    {
+        return PhConvertUtf8ToUtf16((PSTR)string);
+    }
+    else
+    {
+        return PhReferenceEmptyString();
+    }
+}
+
+PSTR PhGetXmlNodeElementText(
+    _In_ PVOID XmlNodeObject
+    )
+{
+    return (PSTR)mxmlGetElement(XmlNodeObject);
+}
+
+PPH_STRING PhGetXmlNodeAttributeText(
+    _In_ PVOID XmlNodeObject,
+    _In_ PSTR AttributeName
+    )
+{
+    PCSTR string;
+
+    if (string = mxmlElementGetAttr(XmlNodeObject, AttributeName))
+    {
+        return PhConvertUtf8ToUtf16((PSTR)string);
+    }
+
+    return NULL;
+}
+
+PSTR PhGetXmlNodeAttributeByIndex(
+    _In_ PVOID XmlNodeObject,
+    _In_ INT Index,
+    _Out_ PSTR* AttributeName
+    )
+{
+    return (PSTR)mxmlElementGetAttrByIndex(XmlNodeObject, Index, AttributeName);
+}
+
+VOID PhSetXmlNodeAttributeText(
+    _In_ PVOID XmlNodeObject,
+    _In_ PSTR Name,
+    _In_ PSTR Value
+    )
+{
+    mxmlElementSetAttr(XmlNodeObject, Name, Value);
+}
+
+INT PhGetXmlNodeAttributeCount(
+    _In_ PVOID XmlNodeObject
+    )
+{
+    return mxmlElementGetAttrCount(XmlNodeObject);
+}
+
+typedef BOOLEAN (NTAPI* PPH_ENUM_XML_NODE_CALLBACK)(
+    _In_ PVOID XmlNodeObject,
+    _In_opt_ PVOID Context
+    );
+
+BOOLEAN PhEnumXmlNode(
+    _In_ PVOID XmlNodeObject,
+    _In_ PPH_ENUM_XML_NODE_CALLBACK Callback,
+    _In_opt_ PVOID Context
+    )
+{
+    PVOID currentNode;
+
+    currentNode = PhGetXmlNodeFirstChild(XmlNodeObject);
+
+    while (currentNode)
+    {
+        if (Callback(currentNode, Context))
+            break;
+
+        currentNode = PhGetXmlNodeNextChild(currentNode);
+    }
+
+    return TRUE;
 }
