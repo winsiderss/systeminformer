1b2aab3674013f74c954e9d6b79dce3e71563f2c
dmex <dmex04@gmail.com>
2020-11-30 13:36:01 +1100

    Add custom POWER_INFORMATION_LEVEL

--- phnt/include/ntpoapi.h
+++ phnt/include/ntpoapi.h
@@ -121,6 +121,108 @@ typedef struct _POWER_STATE_NOTIFY_HANDLER
     PVOID Context;
 } POWER_STATE_NOTIFY_HANDLER, *PPOWER_STATE_NOTIFY_HANDLER;
 
+#if (PHNT_MODE != PHNT_MODE_KERNEL)
+// POWER_INFORMATION_LEVEL
+// Note: We don't use an enum for these values to minimize conflicts with the Windows SDK. (dmex)
+#define SystemPowerPolicyAc 0 // SYSTEM_POWER_POLICY // gets policy if InputBuffer is NULL and sets it otherwise.
+#define SystemPowerPolicyDc 1 // SYSTEM_POWER_POLICY
+#define VerifySystemPolicyAc 2 // SYSTEM_POWER_POLICY
+#define VerifySystemPolicyDc 3 // SYSTEM_POWER_POLICY
+#define SystemPowerCapabilities 4 // SYSTEM_POWER_CAPABILITIES
+#define SystemBatteryState 5 // SYSTEM_BATTERY_STATE
+#define SystemPowerStateHandler 6 // (kernel-mode only)
+#define ProcessorStateHandler 7 // (kernel-mode only)
+#define SystemPowerPolicyCurrent 8 // SYSTEM_POWER_POLICY
+#define AdministratorPowerPolicy 9 // ADMINISTRATOR_POWER_POLICY // GET: InputBuffer NULL. SET: not NULL.
+#define SystemReserveHiberFile 10 // BOOLEAN // (requires SeCreatePagefilePrivilege) // TRUE: hibernation file created. FALSE: hibernation file deleted.
+#define ProcessorInformation 11 // PROCESSOR_POWER_INFORMATION
+#define SystemPowerInformation 12 // SYSTEM_POWER_INFORMATION
+#define ProcessorStateHandler2 13 // not implemented
+#define LastWakeTime 14 // ULONGLONG
+#define LastSleepTime 15 // ULONGLONG
+#define SystemExecutionState 16 // EXECUTION_STATE // SetThreadExecutionState
+#define SystemPowerStateNotifyHandler 17 // (kernel-mode only)
+#define ProcessorPowerPolicyAc 18 // not implemented
+#define ProcessorPowerPolicyDc 19 // not implemented
+#define VerifyProcessorPowerPolicyAc 20 // not implemented
+#define VerifyProcessorPowerPolicyDc 21 // not implemented
+#define ProcessorPowerPolicyCurrent 22 // not implemented
+#define SystemPowerStateLogging 23
+#define SystemPowerLoggingEntry 24 // (kernel-mode only)
+#define SetPowerSettingValue 25 // (kernel-mode only)
+#define NotifyUserPowerSetting 26 // not implemented
+#define PowerInformationLevelUnused0 27 // not implemented
+#define SystemMonitorHiberBootPowerOff 28 // NULL (PowerMonitorOff)
+#define SystemVideoState 29 // MONITOR_DISPLAY_STATE
+#define TraceApplicationPowerMessage 30 // (kernel-mode only)
+#define TraceApplicationPowerMessageEnd 31 // (kernel-mode only)
+#define ProcessorPerfStates 32 // (kernel-mode only)
+#define ProcessorIdleStates 33 // (kernel-mode only)
+#define ProcessorCap 34 // (kernel-mode only)
+#define SystemWakeSource 35 // (kernel-mode only)
+#define SystemHiberFileInformation 36
+#define TraceServicePowerMessage 37
+#define ProcessorLoad 38
+#define PowerShutdownNotification 39 // (kernel-mode only)
+#define MonitorCapabilities 40 // (kernel-mode only)
+#define SessionPowerInit 41 // (kernel-mode only)
+#define SessionDisplayState 42 // (kernel-mode only)
+#define PowerRequestCreate 43 // PowerCreateRequest
+#define PowerRequestAction 44 // PowerClearRequest
+#define GetPowerRequestList 45 // powercfg.exe /requests
+#define ProcessorInformationEx 46 // PROCESSOR_POWER_INFORMATION
+#define NotifyUserModeLegacyPowerEvent 47 // (kernel-mode only)
+#define GroupPark 48 // (debug-mode boot only) 
+#define ProcessorIdleDomains 49 // (kernel-mode only)
+#define WakeTimerList 50 // powercfg.exe /waketimers
+#define SystemHiberFileSize 51 // ULONG
+#define ProcessorIdleStatesHv 52
+#define ProcessorPerfStatesHv 53
+#define ProcessorPerfCapHv 54
+#define ProcessorSetIdle 55 // (debug-mode boot only) 
+#define LogicalProcessorIdling 56 // (kernel-mode only)
+#define UserPresence 57 // not implemented
+#define PowerSettingNotificationName 58
+#define GetPowerSettingValue 59 // GUID
+#define IdleResiliency 60 // POWER_IDLE_RESILIENCY
+#define SessionRITState 61 // (kernel-mode only)
+#define SessionConnectNotification 62 // (kernel-mode only)
+#define SessionPowerCleanup 63 // (kernel-mode only)
+#define SessionLockState 64
+#define SystemHiberbootState 65 // BOOLEAN // fast startup supported
+#define PlatformInformation 66 // BOOLEAN // connected standby supported
+#define PdcInvocation 67 // (kernel-mode only)
+#define MonitorInvocation 68 // (kernel-mode only)
+#define FirmwareTableInformationRegistered 69 // (kernel-mode only)
+#define SetShutdownSelectedTime 70 // NULL
+#define SuspendResumeInvocation 71 // (kernel-mode only)
+#define PlmPowerRequestCreate 72
+#define ScreenOff 73 // NULL (PowerMonitorOff)
+#define CsDeviceNotification 74 // (kernel-mode only)
+#define PlatformRole 75 // POWER_PLATFORM_ROLE
+#define LastResumePerformance 76 // RESUME_PERFORMANCE
+#define DisplayBurst 77 // NULL (PowerMonitorOn)
+#define ExitLatencySamplingPercentage 78
+#define RegisterSpmPowerSettings 79 // (kernel-mode only)
+#define PlatformIdleStates 80 // (kernel-mode only)
+#define ProcessorIdleVeto 81 // (kernel-mode only) // deprecated
+#define PlatformIdleVeto 82 // (kernel-mode only) // deprecated
+#define SystemBatteryStatePrecise 83 // SYSTEM_BATTERY_STATE
+#define ThermalEvent 84  // THERMAL_EVENT // PowerReportThermalEvent
+#define PowerRequestActionInternal 85 // (kernel-mode only)
+#define BatteryDeviceState 86
+#define PowerInformationInternal 87
+#define ThermalStandby 88 // NULL // shutdown with thermal standby as reason.
+#define SystemHiberFileType 89 // ULONG // zero ? reduced : full // powercfg.exe /h /type 
+#define PhysicalPowerButtonPress 90 // BOOLEAN // bugcheck with MANUALLY_INITIATED_POWER_BUTTON_HOLD 
+#define QueryPotentialDripsConstraint 91 // (kernel-mode only)
+#define EnergyTrackerCreate 92
+#define EnergyTrackerQuery 93
+#define UpdateBlackBoxRecorder 94
+#define SessionAllowExternalDmaDevices 95
+#define PowerInformationLevelMaximum 96
+#endif
+
 typedef struct _PROCESSOR_POWER_INFORMATION
 {
     ULONG Number;
