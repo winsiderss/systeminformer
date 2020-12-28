b7a83dbc29620cc1919bda5f9c2d5fe08b47ea06
dmex <dmex04@gmail.com>
2020-12-28 11:48:50 +1100

    Add correct BoundaryDescriptor type, Fix SAL annotation

--- plugins/DotNetTools/counters.c
+++ plugins/DotNetTools/counters.c
@@ -513,7 +513,7 @@ BOOLEAN OpenDotNetPublicControlBlock_V4(
 {
     BOOLEAN result = FALSE;
     PPH_STRING legacyBoundaryDescriptorName;
-    PVOID boundaryDescriptorHandle = NULL;
+    HANDLE boundaryDescriptorHandle = NULL;
     HANDLE privateNamespaceHandle = NULL;
     HANDLE blockTableHandle = NULL;
     HANDLE tokenHandle = NULL;
