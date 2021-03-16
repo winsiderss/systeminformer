winsiderss/systeminformer

commit 88368deffd629066d0ca1c1b80eb69b1f68fca88

PhLoadLibrarySafe and ban LoadLibrary (#835)

ProcessHacker/sdk/phdk.h
@@ -6,6 +6,7 @@
 #define PHNT_VERSION PHNT_WIN7
 #define PHAPPAPI __declspec(dllimport)
 
+#include "banned.h"
 #include "ph.h"
 #include "phnet.h"
 #include "provider.h"

ProcessHacker/ProcessHacker.def
@@ -420,6 +420,7 @@ EXPORTS
     PhShowStatus
     PhStringToGuid
     PhUpdateHash
+    PhLoadLibrarySafe
 
 ; circbuf
     PhClearCircularBuffer_FLOAT

ProcessHacker/phsvc/svcapi.c
@@ -1130,7 +1130,7 @@ NTSTATUS PhSvcApiSetTcpEntry(
     {
         HMODULE iphlpapiModule;
 
-        iphlpapiModule = LoadLibrary(L"iphlpapi.dll");
+        iphlpapiModule = PhLoadLibrarySafe(L"iphlpapi.dll");
 
         if (iphlpapiModule)
         {

ProcessHacker/prpgwmi.c
@@ -66,7 +66,7 @@ PVOID PhpGetWmiProviderDllBase(
             if (systemFileName = PhConcatStringRefZ(&systemDirectory->sr, L"\\wbem\\wbemprox.dll"))
             {
                 if (!(imageBaseAddress = PhGetLoaderEntryFullDllBase(PhGetString(systemFileName))))
-                    imageBaseAddress = LoadLibrary(PhGetString(systemFileName));
+                    imageBaseAddress = PhLoadLibrarySafe(PhGetString(systemFileName));
 
                 PhDereferenceObject(systemFileName);
             }

ProcessHacker/runas.c
@@ -231,7 +231,7 @@ BOOLEAN PhShowRunFileDialog(
     //    );
     //PVOID shell32Handle;
     //
-    //if (shell32Handle = LoadLibrary(L"shell32.dll"))
+    //if (shell32Handle = PhLoadLibrarySafe(L"shell32.dll"))
     //{
     //    if (RunFileDlg_I = PhGetDllBaseProcedureAddress(shell32Handle, NULL, 61))
     //    {
@@ -387,7 +387,7 @@ BOOLEAN PhpInitializeNetApi(VOID)
 
     if (PhBeginInitOnce(&initOnce))
     {
-        if (netapiModuleHandle = LoadLibrary(L"netapi32.dll"))
+        if (netapiModuleHandle = PhLoadLibrarySafe(L"netapi32.dll"))
         {
             NetUserEnum_I = PhGetDllBaseProcedureAddress(netapiModuleHandle, "NetUserEnum", 0);
             NetApiBufferFree_I = PhGetDllBaseProcedureAddress(netapiModuleHandle, "NetApiBufferFree", 0);
@@ -415,7 +415,7 @@ BOOLEAN PhpInitializeMRUList(VOID)
 
     if (PhBeginInitOnce(&initOnce))
     {
-        if (comctl32ModuleHandle = LoadLibrary(L"comctl32.dll"))
+        if (comctl32ModuleHandle = PhLoadLibrarySafe(L"comctl32.dll"))
         {
             CreateMRUList_I = PhGetDllBaseProcedureAddress(comctl32ModuleHandle, "CreateMRUListW", 0);
             AddMRUString_I = PhGetDllBaseProcedureAddress(comctl32ModuleHandle, "AddMRUStringW", 0);
@@ -2095,7 +2095,7 @@ BOOLEAN PhpRunFileAsInteractiveUser(
     INT cmdlineArgCount;
     PWSTR* cmdlineArgList;
 
-    if (!(wdcLibraryHandle = LoadLibrary(L"wdc.dll")))
+    if (!(wdcLibraryHandle = PhLoadLibrarySafe(L"wdc.dll")))
         return FALSE;
 
     if (!(WdcRunTaskAsInteractiveUser_I = PhGetDllBaseProcedureAddress(wdcLibraryHandle, "WdcRunTaskAsInteractiveUser", 0)))

ProcessHacker/srvprv.c
@@ -1377,7 +1377,7 @@ VOID PhpInitializeServiceNonPoll(
     {
         PVOID sechostHandle;
 
-        if (sechostHandle = LoadLibrary(L"sechost.dll"))
+        if (sechostHandle = PhLoadLibrarySafe(L"sechost.dll"))
         {
             SubscribeServiceChangeNotifications_I = PhGetDllBaseProcedureAddress(sechostHandle, "SubscribeServiceChangeNotifications", 0);
             UnsubscribeServiceChangeNotifications_I = PhGetDllBaseProcedureAddress(sechostHandle, "UnsubscribeServiceChangeNotifications", 0);

phlib/apiimport.c
@@ -41,7 +41,7 @@ PVOID PhpImportProcedure(
         module = PhGetLoaderEntryDllBase(ModuleName);
 
         if (!module)
-            module = LoadLibrary(ModuleName);
+            module = PhLoadLibrarySafe(ModuleName);
 
         if (module)
         {

phlib/appresolver.c
@@ -82,7 +82,7 @@ static BOOLEAN PhpKernelAppCoreInitialized(
         {
             PVOID kernelBaseModuleHandle;
 
-            if (kernelBaseModuleHandle = LoadLibrary(L"kernelbase.dll")) // kernel.appcore.dll
+            if (kernelBaseModuleHandle = PhLoadLibrarySafe(L"kernelbase.dll")) // kernel.appcore.dll
             {
                 AppContainerDeriveSidFromMoniker_I = PhGetDllBaseProcedureAddress(kernelBaseModuleHandle, "AppContainerDeriveSidFromMoniker", 0);
                 AppContainerLookupMoniker_I = PhGetDllBaseProcedureAddress(kernelBaseModuleHandle, "AppContainerLookupMoniker", 0);

commit 2b317360a9bb44e15aaf416677093c61e9f8f586

Fix build regression from #835

ProcessHacker/sdk/phdk.h
@@ -6,7 +6,6 @@
 #define PHNT_VERSION PHNT_WIN7
 #define PHAPPAPI __declspec(dllimport)
 
-#include "banned.h"
 #include "ph.h"
 #include "phnet.h"
 #include "provider.h"
@@ -23,5 +22,7 @@
 #include "cpysave.h"
 
 #include "phapppub.h"
+
+#include "banned.h"
 
 #endif