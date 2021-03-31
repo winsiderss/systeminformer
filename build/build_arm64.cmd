35b5b61db27b2ce8f5903280599fa7e8f96a1d6d
dmex <dmex04@gmail.com>
2021-03-31 17:33:31 +1100

    Update arm64 build script, Fix ignoring build errors, Rename script build_arm->build_arm64

--- build/build_arm64.cmd
+++ build/build_arm64.cmd
@@ -3,10 +3,17 @@
 @cd /d "%~dp0\..\"
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+
 msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=ARM64 -verbosity:minimal
+if %ERRORLEVEL% neq 0 goto end
+
 msbuild ProcessHacker.sln -property:Configuration=Debug -property:Platform=ARM64 -verbosity:minimal
+if %ERRORLEVEL% neq 0 goto end
+
 mkdir sdk\lib\arm64
 copy bin\DebugARM64\ProcessHacker.lib sdk\lib\arm64
+
 msbuild plugins\Plugins.sln -property:Configuration=Debug -property:Platform=ARM64 -verbosity:minimal
 
+:end
 pause
