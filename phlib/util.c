winsiderss/systeminformer

commit 8dd929f72e8690ec600acce6ea567457773831ff

Disable reparse for namespace handles

phlib/util.c
@@ -5984,7 +5984,7 @@ HANDLE PhGetNamespaceHandle(
         InitializeObjectAttributes(
             &objectAttributes,
             &namespacePathUs,
-            OBJ_OPENIF,
+            OBJ_OPENIF | OBJ_DONT_REPARSE,
             NULL,
             securityDescriptor
             );

commit d80efaa7f8f035cca06362bbabf145ab2e4774aa

Fix namespace regression from 8dd929f7

phlib/util.c
@@ -5984,7 +5984,7 @@ HANDLE PhGetNamespaceHandle(
         InitializeObjectAttributes(
             &objectAttributes,
             &namespacePathUs,
-            OBJ_OPENIF | OBJ_DONT_REPARSE,
+            OBJ_OPENIF | (WindowsVersion < WINDOWS_10 ? 0 : OBJ_DONT_REPARSE),
             NULL,
             securityDescriptor
             );