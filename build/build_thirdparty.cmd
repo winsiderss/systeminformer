5502140731064b2ab0c3950ffd88f6786d9b5576
dmex <dmex04@gmail.com>
2021-06-27 02:41:21 +1000

    Create build_thirdparty.cmd

---
+++ build/build_thirdparty.cmd
@@ -0,0 +1,26 @@
+@echo off
+@setlocal enableextensions
+@cd /d "%~dp0\..\"
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+
+msbuild tools\thirdparty\thirdparty.sln -property:Configuration=Debug -property:Platform=x86 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
+msbuild tools\thirdparty\thirdparty.sln -property:Configuration=Release -property:Platform=x86 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
+msbuild tools\thirdparty\thirdparty.sln -property:Configuration=Debug -property:Platform=x64 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
+msbuild tools\thirdparty\thirdparty.sln -property:Configuration=Release -property:Platform=x64 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
+msbuild tools\thirdparty\thirdparty.sln -property:Configuration=Debug -property:Platform=ARM64 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
+msbuild tools\thirdparty\thirdparty.sln -property:Configuration=Release -property:Platform=ARM64 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
+:end
+pause
