335d32b2ece4f449db1156b9f1542820bebdc956
dmex <dmex04@gmail.com>
2021-07-15 19:28:59 +1000

    Add missing copyright notice

--- phlib/data.c
+++ phlib/data.c
@@ -1,3 +1,26 @@
+/*
+ * Process Hacker -
+ *   miscellaneous typedef declarations 
+ *
+ * Copyright (C) 2010-2016 wj32
+ * Copyright (C) 2017-2021 dmex
+ *
+ * This file is part of Process Hacker.
+ *
+ * Process Hacker is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Process Hacker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <ph.h>
 #include <phdata.h>
 
