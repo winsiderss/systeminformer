e47a93faf03d218b6e5d4026e488ab365f03c243
dmex <dmex04@gmail.com>
2021-08-13 06:48:03 +1000

    Add fixlib_bcd\make_bcd_lib script

---
+++ tools/fixlib_bcd/make_bcd_lib.cmd
@@ -0,0 +1,10 @@
+@echo off
+@setlocal enableextensions
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+
+lib /machine:x86 /def:lib32/bcd.def /out:lib32/bcd.lib
+"../fixlib/bin/Release/fixlib.exe" lib32/bcd.lib
+lib /machine:x64 /def:lib64/bcd.def /out:lib64/bcd.lib
+
+pause
