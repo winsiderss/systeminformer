1e0d0180d0f91301da5c68c2b89253aa0f04b022
dmex <dmex04@gmail.com>
2021-11-09 20:23:02 +1100

    Update build scripts to VS2022

--- build/build_thirdparty.cmd
+++ build/build_thirdparty.cmd
@@ -2,7 +2,7 @@
 @setlocal enableextensions
 @cd /d "%~dp0\..\"
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
 
 msbuild tools\thirdparty\thirdparty.sln -property:Configuration=Debug -property:Platform=x86 -verbosity:normal
 if %ERRORLEVEL% neq 0 goto end
