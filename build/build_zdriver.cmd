0a5691cb27392005f3eae0dff1c5dfd97367413e
dmex <dmex04@gmail.com>
2022-03-26 12:37:13 +1100

    Update build scripts to VS2022

--- build/build_zdriver.cmd
+++ build/build_zdriver.cmd
@@ -7,19 +7,28 @@ set "msvc2019="
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" ( set "msvc2019=1" )
 
 if defined msvc2019 (
+
    call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
 
    msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=Win32 -verbosity:minimal
    if %ERRORLEVEL% neq 0 goto end
+
+   msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=x64 -verbosity:minimal
+   if %ERRORLEVEL% neq 0 goto end
+
+   msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=ARM64 -verbosity:minimal
+   if %ERRORLEVEL% neq 0 goto end
+
 ) else (
+
    call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+
+   msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=x64 /p:TargetVersion="Windows10" -verbosity:minimal
+   if %ERRORLEVEL% neq 0 goto end
+
+   msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=ARM64 /p:TargetVersion="Windows10" -verbosity:minimal
+   if %ERRORLEVEL% neq 0 goto end
 )
-
-msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=x64 -verbosity:minimal
-if %ERRORLEVEL% neq 0 goto end
-
-msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=ARM64 -verbosity:minimal
-if %ERRORLEVEL% neq 0 goto end
 
 :end
 pause
