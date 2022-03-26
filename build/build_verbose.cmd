1980bb9a39e4836c8ab6ec9304de93900f2a2d10
dmex <dmex04@gmail.com>
2022-03-26 12:37:13 +1100

    Update build scripts to VS2022

--- build/build_verbose.cmd
+++ build/build_verbose.cmd
@@ -5,11 +5,18 @@
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
 
 msbuild ProcessHacker.sln -property:Configuration=Debug -property:Platform=Win32 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
 msbuild ProcessHacker.sln -property:Configuration=Debug -property:Platform=x64 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
 
 tools\CustomBuildTool\bin\Release\CustomBuildTool.exe -sdk
 
 msbuild Plugins\Plugins.sln -property:Configuration=Debug -property:Platform=Win32 -verbosity:normal
-msbuild Plugins\Plugins.sln -property:Configuration=Debug -property:Platform=x64 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
 
+msbuild Plugins\Plugins.sln -property:Configuration=Debug -property:Platform=x64 -verbosity:normal
+if %ERRORLEVEL% neq 0 goto end
+
+:end
 pause
