06c41c810a37208afb4628ff586c110f6b7fb82b
dmex <dmex04@gmail.com>
2022-06-05 12:01:41 +1000

    Update ntmisc.h

--- phnt/include/ntmisc.h
+++ phnt/include/ntmisc.h
@@ -70,29 +70,30 @@ NtTraceEvent(
 
 typedef enum _TRACE_CONTROL_INFORMATION_CLASS
 {
-    TraceControlStartLogger = 1,
-    TraceControlStopLogger = 2,
-    TraceControlQueryLogger = 3,
-    TraceControlUpdateLogger = 4,
-    TraceControlFlushLogger = 5,
-    TraceControlIncrementLoggerFile = 6,
-
+    TraceControlStartLogger = 1, // inout WMI_LOGGER_INFORMATION
+    TraceControlStopLogger = 2, // inout WMI_LOGGER_INFORMATION
+    TraceControlQueryLogger = 3, // inout WMI_LOGGER_INFORMATION
+    TraceControlUpdateLogger = 4, // inout WMI_LOGGER_INFORMATION
+    TraceControlFlushLogger = 5, // inout WMI_LOGGER_INFORMATION
+    TraceControlIncrementLoggerFile = 6, // inout WMI_LOGGER_INFORMATION
+    TraceControlUnknown = 7,
+    // unused
     TraceControlRealtimeConnect = 11,
     TraceControlActivityIdCreate = 12,
     TraceControlWdiDispatchControl = 13,
-    TraceControlRealtimeDisconnectConsumerByHandle = 14,
+    TraceControlRealtimeDisconnectConsumerByHandle = 14, // in HANDLE
     TraceControlRegisterGuidsCode = 15,
     TraceControlReceiveNotification = 16,
-    TraceControlSendDataBlock = 17, // EnableGuid
+    TraceControlSendDataBlock = 17, // ETW_ENABLE_NOTIFICATION_PACKET
     TraceControlSendReplyDataBlock = 18,
     TraceControlReceiveReplyDataBlock = 19,
     TraceControlWdiUpdateSem = 20,
-    TraceControlEnumTraceGuidList = 21,
-    TraceControlGetTraceGuidInfo = 22,
+    TraceControlEnumTraceGuidList = 21, // out GUID[]
+    TraceControlGetTraceGuidInfo = 22, // in GUID, out TRACE_GUID_INFO
     TraceControlEnumerateTraceGuids = 23,
     TraceControlRegisterSecurityProv = 24,
     TraceControlQueryReferenceTime = 25,
-    TraceControlTrackProviderBinary = 26,
+    TraceControlTrackProviderBinary = 26, // in HANDLE
     TraceControlAddNotificationEvent = 27,
     TraceControlUpdateDisallowList = 28,
     TraceControlSetEnableAllKeywordsCode = 29,
@@ -100,16 +101,18 @@ typedef enum _TRACE_CONTROL_INFORMATION_CLASS
     TraceControlUseDescriptorTypeCode = 31,
     TraceControlEnumTraceGroupList = 32,
     TraceControlGetTraceGroupInfo = 33,
-    TraceControlTraceSetDisallowList= 34,
+    TraceControlTraceSetDisallowList = 34,
     TraceControlSetCompressionSettings = 35,
-    TraceControlGetCompressionSettings= 36,
+    TraceControlGetCompressionSettings = 36,
     TraceControlUpdatePeriodicCaptureState = 37,
     TraceControlGetPrivateSessionTraceHandle = 38,
     TraceControlRegisterPrivateSession = 39,
     TraceControlQuerySessionDemuxObject = 40,
     TraceControlSetProviderBinaryTracking = 41,
-    TraceControlMaxLoggers = 42,
-    TraceControlMaxPmcCounter = 43
+    TraceControlMaxLoggers = 42, // out ULONG
+    TraceControlMaxPmcCounter = 43, // out ULONG
+    TraceControlQueryUsedProcessorCount = 44, // ULONG // since WIN11
+    TraceControlGetPmcOwnership = 45,
 } TRACE_CONTROL_INFORMATION_CLASS;
 
 #if (PHNT_VERSION >= PHNT_VISTA)
