d18245ea828a854d6920af817c8de9e4672c9b63
dmex <dmex04@gmail.com>
2022-06-09 17:06:02 +1000

    Update build scripts to VS2022

--- build/build_zdriver.cmd
+++ build/build_zdriver.cmd
@@ -2,13 +2,21 @@
 @setlocal enableextensions
 @cd /d "%~dp0\..\"
 
-set "msvc2019="
+for /f "usebackq tokens=*" %%A in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version "[16.0,17.0)" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+   set VSINSTALLPATH2019=%%A
+)
 
-if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" ( set "msvc2019=1" )
+for /f "usebackq tokens=*" %%A in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+   set VSINSTALLPATH=%%A
+)
 
-if defined msvc2019 (
+if defined VSINSTALLPATH2019 (
 
-   call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+   if exist "%VSINSTALLPATH2019%\VC\Auxiliary\Build\vcvarsall.bat" (
+      call "%VSINSTALLPATH2019%\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+   ) else (
+      goto end
+   )
 
    msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=Win32 -verbosity:minimal
    if %ERRORLEVEL% neq 0 goto end
@@ -20,8 +28,16 @@ if defined msvc2019 (
    if %ERRORLEVEL% neq 0 goto end
 
 ) else (
+ 
+   if not defined VSINSTALLPATH (
+      goto end
+   )
 
-   call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+   if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
+      call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
+   ) else (
+      goto end
+   )
 
    msbuild KProcessHacker\KProcessHacker.sln -property:Configuration=Debug -property:Platform=x64 /p:TargetVersion="Windows10" -verbosity:minimal
    if %ERRORLEVEL% neq 0 goto end
