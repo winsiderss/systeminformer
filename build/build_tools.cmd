c27c98b6884236d9b07543d4bda5c16a6016ac8b
dmex <dmex04@gmail.com>
2022-06-09 17:06:02 +1000

    Update build scripts to VS2022

--- build/build_tools.cmd
+++ build/build_tools.cmd
@@ -2,10 +2,21 @@
 @setlocal enableextensions
 @cd /d "%~dp0\..\"
 
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+for /f "usebackq tokens=*" %%a in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+   set "VSINSTALLPATH=%%a"
+)
 
-cd tools\CustomBuildTool\
+if not defined VSINSTALLPATH (
+   goto end
+)
 
-dotnet publish -c Release /p:PublishProfile=Properties\PublishProfiles\32bit.pubxml
+if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
+   call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" x86
+) else (
+   goto end
+)
 
+dotnet publish tools\CustomBuildTool\CustomBuildTool.sln -c Release /p:PublishProfile=Properties\PublishProfiles\32bit.pubxml
+
+:end
 pause
