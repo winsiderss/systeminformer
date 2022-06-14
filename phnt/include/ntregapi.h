bcb29c608f1ff92cb9d85131a163355b7217ed70
Johnny Shaw <johnny.shaw@live.com>
2022-06-14 00:07:50 -0600

    Update file headers and some copyrights (#1259)

--- phnt/include/ntregapi.h
+++ phnt/include/ntregapi.h
@@ -1,21 +1,7 @@
 /*
- * Process Hacker -
- *   Registry support functions
+ * Registry support functions
  *
- * This file is part of Process Hacker.
- *
- * Process Hacker is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Process Hacker is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of System Informer.
  */
 
 #ifndef _NTREGAPI_H
--- phnt/include/phnt_windows.h
+++ phnt/include/phnt_windows.h
@@ -1,21 +1,7 @@
 /*
- * Process Hacker -
- *   Win32 definition support
+ * Win32 definition support
  *
- * This file is part of Process Hacker.
- *
- * Process Hacker is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Process Hacker is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Process Hacker.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of System Informer.
  */
 
 #ifndef _PHNT_WINDOWS_H
