001a3ca0ba4f3960aa7434028a3030bc2248aed3
Johnny Shaw <johnny.shaw@live.com>
2022-09-02 18:51:08 -0600

    build tooling for cab generation (#1359)

---
+++ build/build_zdriver_cab.cmd
@@ -0,0 +1,45 @@
+@echo off
+@setlocal enableextensions
+
+rmdir /q /s %~dp0\output\cab
+
+call %~dp0\build_zdriver.cmd release
+if %ERRORLEVEL% neq 0 (
+    echo [-] Build failed, CAB was not generated. 
+    goto end
+)
+
+mkdir %~dp0\output\cab 
+
+(robocopy %~dp0\..\KSystemInformer\bin %~dp0\output\cab *.sys *.dll *.pdb /mir) ^& if %ERRORLEVEL% lss 8 set ERRORLEVEL = 0
+if %ERRORLEVEL% neq 0 (
+    echo [-] Failed to copy build artifacts to CAB directory.
+    goto end
+)
+
+(robocopy %~dp0\..\KSystemInformer %~dp0\output\cab KSystemInformer.inf) ^& if %ERRORLEVEL% lss 8 set ERRORLEVEL = 0
+if %ERRORLEVEL% neq 0 (
+    echo [-] Failed to copy INF to CAB directory.
+    goto end
+)
+
+pushd
+cd /d %~dp0\output\cab
+makecab /f %~dp0\KSystemInformer.ddf
+if %ERRORLEVEL% neq 0 (
+    echo [-] Failed to generate CAB. 
+    popd
+    goto end
+)
+popd
+
+(robocopy %~dp0\output\cab\disk1 %~dp0\output KSystemInformer.cab) ^& if %ERRORLEVEL% lss 8 set ERRORLEVEL = 0
+if %ERRORLEVEL% neq 0 (
+    echo [-] Failed to copy CAB to output folder.
+    goto end
+)
+
+echo [+] CAB Complete!
+
+:end
+pause
