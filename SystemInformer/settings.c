763fab901183c3e66187a54cb54caae6c71a64fc
dmex <dmex04@gmail.com>
2022-10-12 02:01:02 +1100

    Disable heap reflection by default since reflected processes don't include port heaps

--- SystemInformer/settings.c
+++ SystemInformer/settings.c
@@ -36,7 +36,7 @@ VOID PhAddDefaultSettings(
     PhpAddIntegerSetting(L"EnableCycleCpuUsage", L"1");
     PhpAddIntegerSetting(L"EnableImageCoherencySupport", L"0");
     PhpAddIntegerSetting(L"EnableInstantTooltips", L"0");
-    PhpAddIntegerSetting(L"EnableHeapReflection", L"1");
+    PhpAddIntegerSetting(L"EnableHeapReflection", L"0");
     PhpAddIntegerSetting(L"EnableHeapMemoryTagging", L"0");
     PhpAddIntegerSetting(L"EnableKph", L"0");
     PhpAddIntegerSetting(L"EnableKphWarnings", L"1");
