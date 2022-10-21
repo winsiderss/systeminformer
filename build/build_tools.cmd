57d4846bfc8ec9095b8d5b9809dc6b33d43ef0b9
dmex <dmex04@gmail.com>
2022-10-22 04:51:46 +1100

    Update build_tools.cmd

--- build/build_tools.cmd
+++ build/build_tools.cmd
@@ -17,7 +17,29 @@ if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
    goto end
 )
 
+:: Pre-cleanup (required since dotnet doesn't cleanup)
+if exist "tools\CustomBuildTool\bin\Release\net6.0-x86" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net6.0-x86"
+)
+if exist "tools\CustomBuildTool\bin\Debug" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Debug"
+)
+if exist "tools\CustomBuildTool\bin\obj" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\obj"
+)
+
 dotnet publish tools\CustomBuildTool\CustomBuildTool.sln -c Release /p:PublishProfile=Properties\PublishProfiles\32bit.pubxml
+
+:: Post-cleanup (optional)
+if exist "tools\CustomBuildTool\bin\Release\net6.0-x86" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net6.0-x86"
+)
+if exist "tools\CustomBuildTool\bin\Debug" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Debug"
+)
+if exist "tools\CustomBuildTool\bin\obj" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\obj"
+)
 
 :end
 pause
