winsiderss/systeminformer

commit 24408d0eca39d056151099d8dc05ec9b38d6d1a1

Add PhNtPathSeperatorString and SysAllocStringLen

SystemInformer/appsup.c
@@ -549,14 +549,20 @@ BOOLEAN PhaGetProcessKnownCommandLine(
             // If the DLL name isn't an absolute path, assume it's in system32.
             // TODO: Use a proper search function.
 
-            if (PhDetermineDosPathNameType(dllName->Buffer) == RtlPathTypeRelative)
+            if (PhDetermineDosPathNameType(PhGetString(dllName)) == RtlPathTypeRelative)
             {
-                dllName = PhaConcatStrings(
-                    3,
-                    PH_AUTO_T(PH_STRING, PhGetSystemDirectory())->Buffer,
-                    L"\\",
-                    dllName->Buffer
-                    );
+                PPH_STRING systemDirectory;
+
+                if (systemDirectory = PhGetSystemDirectory())
+                {
+                    dllName = PhConcatStringRef3(
+                        &systemDirectory->sr,
+                        &PhNtPathSeperatorString,
+                        &dllName->sr
+                        );
+
+                    PhDereferenceObject(systemDirectory);
+                }
             }
 
             KnownCommandLine->RunDllAsApp.FileName = dllName;
@@ -2124,7 +2130,8 @@ BOOLEAN PhShellOpenKey2(
 
     memcpy(favoriteName, L"A_SystemInformer", 16 * sizeof(WCHAR));
     PhGenerateRandomAlphaString(&favoriteName[16], 16);
-    PhInitializeStringRefLongHint(&valueName, favoriteName);
+    valueName.Buffer = favoriteName;
+    valueName.Length = sizeof(favoriteName) - sizeof(UNICODE_NULL);
 
     expandedKeyName = PhExpandKeyName(KeyName, FALSE);
     PhSetValueKey(favoritesKeyHandle, &valueName, REG_SZ, expandedKeyName->Buffer, (ULONG)expandedKeyName->Length + sizeof(UNICODE_NULL));

SystemInformer/actions.c
@@ -38,10 +38,11 @@
 #include <srvprv.h>
 #include <thrdprv.h>
 
-static PWSTR DangerousProcesses[] =
+static PH_STRINGREF DangerousProcesses[] =
 {
-    L"csrss.exe", L"dwm.exe", L"logonui.exe", L"lsass.exe", L"lsm.exe",
-    L"services.exe", L"smss.exe", L"wininit.exe", L"winlogon.exe"
+    PH_STRINGREF_INIT(L"csrss.exe"), PH_STRINGREF_INIT(L"dwm.exe"), PH_STRINGREF_INIT(L"logonui.exe"),
+    PH_STRINGREF_INIT(L"lsass.exe"), PH_STRINGREF_INIT(L"lsm.exe"), PH_STRINGREF_INIT(L"services.exe"),
+    PH_STRINGREF_INIT(L"smss.exe"), PH_STRINGREF_INIT(L"wininit.exe"), PH_STRINGREF_INIT(L"winlogon.exe")
 };
 
 static PPH_STRING DebuggerCommand = NULL;
@@ -1508,9 +1509,9 @@ static BOOLEAN PhpIsDangerousProcess(
     )
 {
     NTSTATUS status;
-    PPH_STRING fileName;
     PPH_STRING systemDirectory;
-    ULONG i;
+    PPH_STRING fileName;
+    PPH_STRING fullName;
 
     if (ProcessId == SYSTEM_PROCESS_ID)
         return TRUE;
@@ -1518,19 +1519,22 @@ static BOOLEAN PhpIsDangerousProcess(
     if (!NT_SUCCESS(status = PhGetProcessImageFileNameByProcessId(ProcessId, &fileName)))
         return FALSE;
 
+    systemDirectory = PH_AUTO(PhGetSystemDirectory());
     PhMoveReference(&fileName, PhGetFileName(fileName));
     PH_AUTO(fileName);
 
-    systemDirectory = PH_AUTO(PhGetSystemDirectory());
-
-    for (i = 0; i < sizeof(DangerousProcesses) / sizeof(PWSTR); i++)
+    for (ULONG i = 0; i < RTL_NUMBER_OF(DangerousProcesses); i++)
     {
-        PPH_STRING fullName;
-
-        fullName = PhaConcatStrings(3, systemDirectory->Buffer, L"\\", DangerousProcesses[i]);
+        fullName = PH_AUTO(PhConcatStringRef3(
+            &systemDirectory->sr,
+            &PhNtPathSeperatorString,
+            &DangerousProcesses[i]
+            ));
 
         if (PhEqualString(fileName, fullName, TRUE))
+        {
             return TRUE;
+        }
     }
 
     return FALSE;

SystemInformer/chproc.c
@@ -98,7 +98,7 @@ static VOID PhpRefreshProcessList(
         if (process->UniqueProcessId != SYSTEM_IDLE_PROCESS_ID)
             name = PhCreateStringFromUnicodeString(&process->ImageName);
         else
-            name = PhCreateString(SYSTEM_IDLE_PROCESS_NAME);
+            name = PhCreateStringFromUnicodeString(&SYSTEM_IDLE_PROCESS_NAME);
 
         lvItemIndex = PhAddListViewItem(Context->ListViewHandle, MAXINT, name->Buffer, process->UniqueProcessId);
         PhDereferenceObject(name);

SystemInformer/main.c
@@ -152,7 +152,7 @@ INT WINAPI wWinMain(
         {
             AllowSetForegroundWindow(ASFW_ANY);
 
-            if (SUCCEEDED(PhRunAsAdminTask(SI_RUNAS_ADMIN_TASK_NAME)))
+            if (SUCCEEDED(PhRunAsAdminTask(&SI_RUNAS_ADMIN_TASK_NAME)))
             {
                 PhActivatePreviousInstance();
                 PhExitApplication(STATUS_SUCCESS);
@@ -908,39 +908,39 @@ BOOLEAN PhInitializeMitigationPolicy(
     //        );
     //}
 
-    if (!InitializeProcThreadAttributeList(NULL, 2, 0, &attributeListLength) && GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+    if (!InitializeProcThreadAttributeList(NULL, 1, 0, &attributeListLength) && GetLastError() != ERROR_INSUFFICIENT_BUFFER)
         goto CleanupExit;
 
     startupInfo.lpAttributeList = PhAllocate(attributeListLength);
 
-    if (!InitializeProcThreadAttributeList(startupInfo.lpAttributeList, 2, 0, &attributeListLength))
+    if (!InitializeProcThreadAttributeList(startupInfo.lpAttributeList, 1, 0, &attributeListLength))
         goto CleanupExit;
     if (!UpdateProcThreadAttribute(startupInfo.lpAttributeList, 0, PROC_THREAD_ATTRIBUTE_MITIGATION_POLICY, &(ULONG64){ DEFAULT_MITIGATION_POLICY_FLAGS }, sizeof(ULONG64), NULL, NULL))
         goto CleanupExit;
     //if (!UpdateProcThreadAttribute(startupInfo.lpAttributeList, 0, PROC_THREAD_ATTRIBUTE_JOB_LIST, &jobObjectHandle, sizeof(HANDLE), NULL, NULL))
     //    goto CleanupExit;
-
-    {
-        PROC_THREAD_BNOISOLATION_ATTRIBUTE bnoIsolation;
-        WCHAR alphastring[16] = L"";
-
-        bnoIsolation.IsolationEnabled = TRUE;
-        PhGenerateRandomAlphaString(alphastring, RTL_NUMBER_OF(alphastring));
-        wcscpy_s(bnoIsolation.IsolationPrefix, RTL_NUMBER_OF(bnoIsolation.IsolationPrefix), alphastring);
-
-        if (!UpdateProcThreadAttribute(
-            startupInfo.lpAttributeList,
-            0,
-            PROC_THREAD_ATTRIBUTE_BNO_ISOLATION,
-            &bnoIsolation,
-            sizeof(PROC_THREAD_BNOISOLATION_ATTRIBUTE),
-            NULL,
-            NULL
-            ))
-        {
-            goto CleanupExit;
-        }
-    }
+    //
+    //{
+    //    PROC_THREAD_BNOISOLATION_ATTRIBUTE bnoIsolation;
+    //    WCHAR alphastring[16] = L"";
+    //
+    //    bnoIsolation.IsolationEnabled = TRUE;
+    //    PhGenerateRandomAlphaString(alphastring, RTL_NUMBER_OF(alphastring));
+    //    wcscpy_s(bnoIsolation.IsolationPrefix, RTL_NUMBER_OF(bnoIsolation.IsolationPrefix), alphastring);
+    //
+    //    if (!UpdateProcThreadAttribute(
+    //        startupInfo.lpAttributeList,
+    //        0,
+    //        PROC_THREAD_ATTRIBUTE_BNO_ISOLATION,
+    //        &bnoIsolation,
+    //        sizeof(PROC_THREAD_BNOISOLATION_ATTRIBUTE),
+    //        NULL,
+    //        NULL
+    //        ))
+    //    {
+    //        goto CleanupExit;
+    //    }
+    //}
 
     if (NT_SUCCESS(PhCreateProcessWin32Ex(
         NULL,

SystemInformer/modprv.c
@@ -783,7 +783,7 @@ VOID PhModuleProviderUpdate(
             if (!moduleProvider->CetEnabled)
                 moduleItem->ImageDllCharacteristicsEx &= ~IMAGE_DLLCHARACTERISTICS_EX_CET_COMPAT;
 
-            if (NT_SUCCESS(PhQueryFullAttributesFile(moduleItem->FileName, &networkOpenInfo)))
+            if (NT_SUCCESS(PhQueryFullAttributesFile(&moduleItem->FileName->sr, &networkOpenInfo)))
             {
                 moduleItem->FileLastWriteTime = networkOpenInfo.LastWriteTime;
                 moduleItem->FileEndOfFile = networkOpenInfo.EndOfFile;

SystemInformer/options.c
@@ -1000,7 +1000,7 @@ BOOLEAN PhpIsExploitProtectionEnabled(
 
     PhMoveReference(&path, PhGetBaseDirectory(path));
     PhMoveReference(&apppath, PhGetBaseName(apppath));
-    keypath = PhConcatStrings(3, path->Buffer, L"\\", apppath->Buffer);
+    keypath = PhConcatStringRef3(&path->sr, &PhNtPathSeperatorString, &apppath->sr);
     PhDereferenceObject(apppath);
     PhDereferenceObject(path);
 
@@ -1052,7 +1052,7 @@ NTSTATUS PhpSetExploitProtectionEnabled(
     apppath = PH_AUTO(PhGetApplicationFileName());
     path = PH_AUTO(PhGetBaseDirectory(path));
     apppath = PH_AUTO(PhGetBaseName(apppath));
-    keypath = PH_AUTO(PhConcatStrings(3, path->Buffer, L"\\", apppath->Buffer));
+    keypath = PH_AUTO(PhConcatStringRef3(&path->sr, &PhNtPathSeperatorString, &apppath->sr));
 
     if (Enabled)
     {
@@ -1205,7 +1205,7 @@ NTSTATUS PhpSetSilentProcessNotifyEnabled(
             apppath = PH_AUTO(PhGetApplicationFileName());
             path = PH_AUTO(PhGetBaseDirectory(path));
             apppath = PH_AUTO(PhGetBaseName(apppath));
-            keypath = PH_AUTO(PhConcatStrings(3, path->Buffer, L"\\", apppath->Buffer));
+            keypath = PH_AUTO(PhConcatStringRef3(&path->sr, &PhNtPathSeperatorString, &apppath->sr));
 
             status = PhCreateKey(
                 &keyHandle,
@@ -1261,7 +1261,7 @@ NTSTATUS PhpSetSilentProcessNotifyEnabled(
             apppath = PH_AUTO(PhGetApplicationFileName());
             path = PH_AUTO(PhGetBaseDirectory(path));
             apppath = PH_AUTO(PhGetBaseName(apppath));
-            keypath = PH_AUTO(PhConcatStrings(3, path->Buffer, L"\\", apppath->Buffer));
+            keypath = PH_AUTO(PhConcatStringRef3(&path->sr, &PhNtPathSeperatorString, &apppath->sr));
 
             status = PhOpenKey(
                 &keyHandle,
@@ -1863,6 +1863,7 @@ INT_PTR CALLBACK PhpOptionsGeneralDlgProc(
 
                                         if (applicationFileName = PhGetApplicationFileName())
                                         {
+                                            static PH_STRINGREF seperator = PH_STRINGREF_INIT(L"\"");
                                             HRESULT status;
                                             PPH_STRING quotedFileName;
 #if (PHNT_VERSION >= PHNT_WIN7)
@@ -1870,7 +1871,7 @@ INT_PTR CALLBACK PhpOptionsGeneralDlgProc(
 
                                             if (NT_SUCCESS(RtlQueryElevationFlags(&flags)) && flags.ElevationEnabled)
                                             {
-                                                PH_STRINGREF programFilesPathSr = PH_STRINGREF_INIT(L"%ProgramFiles%\\");
+                                                static PH_STRINGREF programFilesPathSr = PH_STRINGREF_INIT(L"%ProgramFiles%\\");
                                                 PPH_STRING programFilesPath;
 
                                                 if (programFilesPath = PhExpandEnvironmentStrings(&programFilesPathSr))
@@ -1892,7 +1893,11 @@ INT_PTR CALLBACK PhpOptionsGeneralDlgProc(
                                                 }
                                             }
 #endif
-                                            quotedFileName = PH_AUTO(PhConcatStrings(3, L"\"", PhGetStringOrEmpty(applicationFileName), L"\""));
+                                            quotedFileName = PH_AUTO(PhConcatStringRef3(
+                                                &seperator,
+                                                &applicationFileName->sr,
+                                                &seperator
+                                                ));
 
                                             status = PhCreateAdminTask(
                                                 SI_RUNAS_ADMIN_TASK_NAME,

SystemInformer/procprv.c
@@ -1201,7 +1201,7 @@ VOID PhpFillProcessItem(
     if (ProcessItem->ProcessId != SYSTEM_IDLE_PROCESS_ID)
         ProcessItem->ProcessName = PhCreateStringFromUnicodeString(&Process->ImageName);
     else
-        ProcessItem->ProcessName = PhCreateString(SYSTEM_IDLE_PROCESS_NAME);
+        ProcessItem->ProcessName = PhCreateStringFromUnicodeString(&SYSTEM_IDLE_PROCESS_NAME);
 
     if (PH_IS_REAL_PROCESS_ID(ProcessItem->ProcessId))
     {

SystemInformer/proctree.c
@@ -1239,7 +1239,7 @@ static VOID PhpUpdateProcessNodeFileAttributes(
         FILE_NETWORK_OPEN_INFORMATION networkOpenInfo;
 
         if (ProcessNode->ProcessItem->FileName && NT_SUCCESS(PhQueryFullAttributesFile(
-            ProcessNode->ProcessItem->FileName,
+            &ProcessNode->ProcessItem->FileName->sr,
             &networkOpenInfo
             )))
         {

commit 68e10fa2273b65f680754f8a6fd84a5bce50978c

Fix PhShellOpenKey2 regression from commit 24408d0eca39d056151099d8dc05ec9b38d6d1a1

SystemInformer/appsup.c
@@ -2121,7 +2121,7 @@ BOOLEAN PhShellOpenKey2(
         goto CleanupExit;
 
     memcpy(favoriteName, L"A_SystemInformer", 16 * sizeof(WCHAR));
-    PhGenerateRandomAlphaString(&favoriteName[16], 16);
+    PhGenerateRandomAlphaString(&favoriteName[16], ARRAYSIZE(favoriteName) - 16);
     valueName.Buffer = favoriteName;
     valueName.Length = sizeof(favoriteName) - sizeof(UNICODE_NULL);
 