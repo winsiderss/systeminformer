a945c4d27cf77043a6c76bb4efaf4a92306ea854
dmex <dmex04@gmail.com>
2022-11-09 23:38:49 +1100

    Update build_tools.cmd

--- build/build_tools.cmd
+++ build/build_tools.cmd
@@ -12,33 +12,39 @@ if not defined VSINSTALLPATH (
 )
 
 if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
-   call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" x86
+   call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64
 ) else (
    goto end
 )
 
 :: Pre-cleanup (required since dotnet doesn't cleanup)
-if exist "tools\CustomBuildTool\bin\Release\net6.0-x86" (
-   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net6.0-x86"
+if exist "tools\CustomBuildTool\bin\Release\net7.0-x64" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net7.0-x64"
 )
 if exist "tools\CustomBuildTool\bin\Debug" (
    rmdir /S /Q "tools\CustomBuildTool\bin\Debug"
 )
-if exist "tools\CustomBuildTool\bin\obj" (
-   rmdir /S /Q "tools\CustomBuildTool\bin\obj"
+if exist "tools\CustomBuildTool\bin\x64" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\x64"
+)
+if exist "tools\CustomBuildTool\obj" (
+   rmdir /S /Q "tools\CustomBuildTool\obj"
 )
 
-dotnet publish tools\CustomBuildTool\CustomBuildTool.sln -c Release /p:PublishProfile=Properties\PublishProfiles\32bit.pubxml
+dotnet publish tools\CustomBuildTool\CustomBuildTool.sln -c Release /p:PublishProfile=Properties\PublishProfiles\32bit.pubxml --output tools\CustomBuildTool\bin\Release\
 
 :: Post-cleanup (optional)
-if exist "tools\CustomBuildTool\bin\Release\net6.0-x86" (
-   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net6.0-x86"
+if exist "tools\CustomBuildTool\bin\Release\net7.0-x64" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net7.0-x64"
 )
 if exist "tools\CustomBuildTool\bin\Debug" (
    rmdir /S /Q "tools\CustomBuildTool\bin\Debug"
 )
-if exist "tools\CustomBuildTool\bin\obj" (
-   rmdir /S /Q "tools\CustomBuildTool\bin\obj"
+if exist "tools\CustomBuildTool\bin\x64" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\x64"
+)
+if exist "tools\CustomBuildTool\obj" (
+   rmdir /S /Q "tools\CustomBuildTool\obj"
 )
 
 :end
