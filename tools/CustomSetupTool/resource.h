2b0800165e6744164de888b954caffbd3f502653
dmex <dmex04@gmail.com>
2022-11-25 17:14:37 +1100

    Update resource file formats to UTF-8 and include embedded RT_MANIFEST

--- tools/CustomSetupTool/resource.h
+++ tools/CustomSetupTool/resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by resource.rc
 //
+#define IDR_RT_MANIFEST                 1
 #define IDI_ICON                        101
 #define IDR_BIN_DATA                    102
 
