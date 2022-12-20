ba96df6a4abb21879b9297e85dd2ea486e114e03
dmex <dmex04@gmail.com>
2022-12-21 03:33:02 +1100

    Update build_version.cmd

--- tools/versioning/build_version.cmd
+++ tools/versioning/build_version.cmd
@@ -7,4 +7,15 @@ echo;
 
 rc.exe /v /c 65001 /i "%INCLUDE%" /i "%~dp0..\..\systeminformer" /D "_UNICODE" /D "UNICODE" /fo version.res version.rc
 
+echo;
+cvtres.exe /MACHINE:x64 /VERBOSE version.res
+
+echo;
+
+cl.exe version.obj /link /DLL /MACHINE:X64 /SUBSYSTEM:WINDOWS /NOENTRY /out:version.dll
+
+echo;
+
+link.exe /SUBSYSTEM:CONSOLE /MACHINE:x64 /ENTRY:__ImageBase /RELEASE /Brepro /VERBOSE /OUT:version_link.exe version.res
+
 pause
