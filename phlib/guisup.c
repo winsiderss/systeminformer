1aa562b705fa8ce1e27e203486cb7facbde3d84a
dmex <dmex04@gmail.com>
2022-12-23 08:36:21 +1100

    Remove legacy PhGetGlobalTimerQueue

--- phlib/guisup.c
+++ phlib/guisup.c
@@ -1869,23 +1869,6 @@ HICON PhGetInternalWindowIcon(
     return InternalGetWindowIcon_I(WindowHandle, Type);
 }
 
-HANDLE PhGetGlobalTimerQueue(
-    VOID
-    )
-{
-    static HANDLE PhTimerQueueHandle = NULL;
-    static PH_INITONCE PhTimerQueueHandleInitOnce = PH_INITONCE_INIT;
-
-    if (PhBeginInitOnce(&PhTimerQueueHandleInitOnce))
-    {
-        RtlCreateTimerQueue(&PhTimerQueueHandle);
-
-        PhEndInitOnce(&PhTimerQueueHandleInitOnce);
-    }
-
-    return PhTimerQueueHandle;
-}
-
 BOOLEAN PhIsImmersiveProcess(
     _In_ HANDLE ProcessHandle
     )
