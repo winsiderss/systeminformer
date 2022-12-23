winsiderss/systeminformer

commit 0d5c6cc0a03d66c033c9029ebb53d253b04bbaa9

Add service start/continue/stop multiple selection #84

SystemInformer/mainwnd.c
@@ -1594,50 +1594,58 @@ VOID PhMwpOnCommand(
         break;
     case ID_SERVICE_START:
         {
-            PPH_SERVICE_ITEM serviceItem = PhGetSelectedServiceItem();
+            PPH_SERVICE_ITEM* serviceItems;
+            ULONG numberOfServiceItems;
 
-            if (serviceItem)
-            {
-                PhReferenceObject(serviceItem);
-                PhUiStartService(WindowHandle, serviceItem);
-                PhDereferenceObject(serviceItem);
-            }
+            PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
+            PhReferenceObjects(serviceItems, numberOfServiceItems);
+
+            PhUiStartServices(WindowHandle, serviceItems, numberOfServiceItems);
+
+            PhDereferenceObjects(serviceItems, numberOfServiceItems);
+            PhFree(serviceItems);
         }
         break;
     case ID_SERVICE_CONTINUE:
         {
-            PPH_SERVICE_ITEM serviceItem = PhGetSelectedServiceItem();
+            PPH_SERVICE_ITEM* serviceItems;
+            ULONG numberOfServiceItems;
 
-            if (serviceItem)
-            {
-                PhReferenceObject(serviceItem);
-                PhUiContinueService(WindowHandle, serviceItem);
-                PhDereferenceObject(serviceItem);
-            }
+            PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
+            PhReferenceObjects(serviceItems, numberOfServiceItems);
+
+            PhUiContinueServices(WindowHandle, serviceItems, numberOfServiceItems);
+
+            PhDereferenceObjects(serviceItems, numberOfServiceItems);
+            PhFree(serviceItems);
         }
         break;
     case ID_SERVICE_PAUSE:
         {
-            PPH_SERVICE_ITEM serviceItem = PhGetSelectedServiceItem();
+            PPH_SERVICE_ITEM* serviceItems;
+            ULONG numberOfServiceItems;
 
-            if (serviceItem)
-            {
-                PhReferenceObject(serviceItem);
-                PhUiPauseService(WindowHandle, serviceItem);
-                PhDereferenceObject(serviceItem);
-            }
+            PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
+            PhReferenceObjects(serviceItems, numberOfServiceItems);
+
+            PhUiPauseServices(WindowHandle, serviceItems, numberOfServiceItems);
+
+            PhDereferenceObjects(serviceItems, numberOfServiceItems);
+            PhFree(serviceItems);
         }
         break;
     case ID_SERVICE_STOP:
         {
-            PPH_SERVICE_ITEM serviceItem = PhGetSelectedServiceItem();
+            PPH_SERVICE_ITEM* serviceItems;
+            ULONG numberOfServiceItems;
 
-            if (serviceItem)
-            {
-                PhReferenceObject(serviceItem);
-                PhUiStopService(WindowHandle, serviceItem);
-                PhDereferenceObject(serviceItem);
-            }
+            PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
+            PhReferenceObjects(serviceItems, numberOfServiceItems);
+
+            PhUiStopServices(WindowHandle, serviceItems, numberOfServiceItems);
+
+            PhDereferenceObjects(serviceItems, numberOfServiceItems);
+            PhFree(serviceItems);
         }
         break;
     case ID_SERVICE_DELETE:

SystemInformer/actions.c
@@ -3396,6 +3396,44 @@ static VOID PhpShowErrorService(
         );
 }
 
+BOOLEAN PhUiStartServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
+    )
+{
+    BOOLEAN result = TRUE;
+    ULONG i;
+
+    for (i = 0; i < NumberOfServices; i++)
+    {
+        SC_HANDLE serviceHandle;
+        BOOLEAN success = FALSE;
+
+        serviceHandle = PhOpenService(PhGetString(Services[i]->Name), SERVICE_START);
+
+        if (serviceHandle)
+        {
+            if (StartService(serviceHandle, 0, NULL))
+                success = TRUE;
+
+            CloseServiceHandle(serviceHandle);
+        }
+
+        if (!success)
+        {
+            NTSTATUS status;
+
+            status = PhGetLastWin32ErrorAsNtStatus();
+            result = FALSE;
+
+            PhpShowErrorService(WindowHandle, L"start", Services[i], status, 0);
+        }
+    }
+
+    return result;
+}
+
 BOOLEAN PhUiStartService(
     _In_ HWND hWnd,
     _In_ PPH_SERVICE_ITEM Service
@@ -3445,6 +3483,46 @@ BOOLEAN PhUiStartService(
     }
 
     return success;
+}
+
+BOOLEAN PhUiContinueServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
+    )
+{
+    BOOLEAN result = TRUE;
+    ULONG i;
+
+    for (i = 0; i < NumberOfServices; i++)
+    {
+        SC_HANDLE serviceHandle;
+        BOOLEAN success = FALSE;
+
+        serviceHandle = PhOpenService(PhGetString(Services[i]->Name), SERVICE_PAUSE_CONTINUE);
+
+        if (serviceHandle)
+        {
+            SERVICE_STATUS serviceStatus;
+
+            if (ControlService(serviceHandle, SERVICE_CONTROL_CONTINUE, &serviceStatus))
+                success = TRUE;
+
+            CloseServiceHandle(serviceHandle);
+        }
+
+        if (!success)
+        {
+            NTSTATUS status;
+
+            status = PhGetLastWin32ErrorAsNtStatus();
+            result = FALSE;
+
+            PhpShowErrorService(WindowHandle, L"continue", Services[i], status, 0);
+        }
+    }
+
+    return result;
 }
 
 BOOLEAN PhUiContinueService(
@@ -3500,6 +3578,46 @@ BOOLEAN PhUiContinueService(
     return success;
 }
 
+BOOLEAN PhUiPauseServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
+    )
+{
+    BOOLEAN result = TRUE;
+    ULONG i;
+
+    for (i = 0; i < NumberOfServices; i++)
+    {
+        SC_HANDLE serviceHandle;
+        BOOLEAN success = FALSE;
+
+        serviceHandle = PhOpenService(PhGetString(Services[i]->Name), SERVICE_PAUSE_CONTINUE);
+
+        if (serviceHandle)
+        {
+            SERVICE_STATUS serviceStatus;
+
+            if (ControlService(serviceHandle, SERVICE_CONTROL_PAUSE, &serviceStatus))
+                success = TRUE;
+
+            CloseServiceHandle(serviceHandle);
+        }
+
+        if (!success)
+        {
+            NTSTATUS status;
+
+            status = PhGetLastWin32ErrorAsNtStatus();
+            result = FALSE;
+
+            PhpShowErrorService(WindowHandle, L"pause", Services[i], status, 0);
+        }
+    }
+
+    return result;
+}
+
 BOOLEAN PhUiPauseService(
     _In_ HWND hWnd,
     _In_ PPH_SERVICE_ITEM Service
@@ -3552,6 +3670,47 @@ BOOLEAN PhUiPauseService(
 
     return success;
 }
+
+BOOLEAN PhUiStopServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
+    )
+{
+    BOOLEAN result = TRUE;
+    ULONG i;
+
+    for (i = 0; i < NumberOfServices; i++)
+    {
+        SC_HANDLE serviceHandle;
+        BOOLEAN success = FALSE;
+
+        serviceHandle = PhOpenService(PhGetString(Services[i]->Name), SERVICE_STOP);
+
+        if (serviceHandle)
+        {
+            SERVICE_STATUS serviceStatus;
+
+            if (ControlService(serviceHandle, SERVICE_CONTROL_STOP, &serviceStatus))
+                success = TRUE;
+
+            CloseServiceHandle(serviceHandle);
+        }
+
+        if (!success)
+        {
+            NTSTATUS status;
+
+            status = PhGetLastWin32ErrorAsNtStatus();
+            result = FALSE;
+
+            PhpShowErrorService(WindowHandle, L"stop", Services[i], status, 0);
+        }
+    }
+
+    return result;
+}
+
 
 BOOLEAN PhUiStopService(
     _In_ HWND hWnd,

SystemInformer/include/actions.h
@@ -332,9 +332,27 @@ PhUiSetBoostPriorityProcess(
 PHAPPAPI
 BOOLEAN
 NTAPI
+PhUiStartServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
+    );
+
+PHAPPAPI
+BOOLEAN
+NTAPI
 PhUiStartService(
     _In_ HWND hWnd,
     _In_ PPH_SERVICE_ITEM Service
+    );
+
+PHAPPAPI
+BOOLEAN
+NTAPI
+PhUiContinueServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
     );
 
 PHAPPAPI
@@ -348,9 +366,27 @@ PhUiContinueService(
 PHAPPAPI
 BOOLEAN
 NTAPI
+PhUiPauseServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
+    );
+
+PHAPPAPI
+BOOLEAN
+NTAPI
 PhUiPauseService(
     _In_ HWND hWnd,
     _In_ PPH_SERVICE_ITEM Service
+    );
+
+PHAPPAPI
+BOOLEAN
+NTAPI
+PhUiStopServices(
+    _In_ HWND WindowHandle,
+    _In_ PPH_SERVICE_ITEM* Services,
+    _In_ ULONG NumberOfServices
     );
 
 PHAPPAPI

SystemInformer/mwpgsrv.c
@@ -279,6 +279,10 @@ VOID PhMwpInitializeServiceMenu(
     else
     {
         PhSetFlagsAllEMenuItems(Menu, PH_EMENU_DISABLED, PH_EMENU_DISABLED);
+        PhEnableEMenuItem(Menu, ID_SERVICE_START, TRUE);
+        PhEnableEMenuItem(Menu, ID_SERVICE_CONTINUE, TRUE);
+        PhEnableEMenuItem(Menu, ID_SERVICE_PAUSE, TRUE);
+        PhEnableEMenuItem(Menu, ID_SERVICE_STOP, TRUE);
         PhEnableEMenuItem(Menu, ID_SERVICE_COPY, TRUE);
     }
 

commit c0acdf72cd3804e8ab0571fc2977e7802557113a

Fix service elevation prompt regression from commit 0d5c6cc0a03d66c033c9029ebb53d253b04bbaa9

SystemInformer/mainwnd.c
@@ -1656,7 +1656,10 @@ VOID PhMwpOnCommand(
             PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
             PhReferenceObjects(serviceItems, numberOfServiceItems);
 
-            PhUiStartServices(WindowHandle, serviceItems, numberOfServiceItems);
+            if (numberOfServiceItems == 1) // phsvc elevation (dmex)
+                PhUiStartService(WindowHandle, serviceItems[0]);
+            else
+                PhUiStartServices(WindowHandle, serviceItems, numberOfServiceItems);
 
             PhDereferenceObjects(serviceItems, numberOfServiceItems);
             PhFree(serviceItems);
@@ -1670,7 +1673,10 @@ VOID PhMwpOnCommand(
             PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
             PhReferenceObjects(serviceItems, numberOfServiceItems);
 
-            PhUiContinueServices(WindowHandle, serviceItems, numberOfServiceItems);
+            if (numberOfServiceItems == 1) // phsvc elevation (dmex)
+                PhUiContinueService(WindowHandle, serviceItems[0]);
+            else
+                PhUiContinueServices(WindowHandle, serviceItems, numberOfServiceItems);
 
             PhDereferenceObjects(serviceItems, numberOfServiceItems);
             PhFree(serviceItems);
@@ -1684,7 +1690,10 @@ VOID PhMwpOnCommand(
             PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
             PhReferenceObjects(serviceItems, numberOfServiceItems);
 
-            PhUiPauseServices(WindowHandle, serviceItems, numberOfServiceItems);
+            if (numberOfServiceItems == 1) // phsvc elevation (dmex)
+                PhUiPauseService(WindowHandle, serviceItems[0]);
+            else
+                PhUiPauseServices(WindowHandle, serviceItems, numberOfServiceItems);
 
             PhDereferenceObjects(serviceItems, numberOfServiceItems);
             PhFree(serviceItems);
@@ -1698,7 +1707,10 @@ VOID PhMwpOnCommand(
             PhGetSelectedServiceItems(&serviceItems, &numberOfServiceItems);
             PhReferenceObjects(serviceItems, numberOfServiceItems);
 
-            PhUiStopServices(WindowHandle, serviceItems, numberOfServiceItems);
+            if (numberOfServiceItems == 1) // phsvc elevation (dmex)
+                PhUiStopService(WindowHandle, serviceItems[0]);
+            else
+                PhUiStopServices(WindowHandle, serviceItems, numberOfServiceItems);
 
             PhDereferenceObjects(serviceItems, numberOfServiceItems);
             PhFree(serviceItems);