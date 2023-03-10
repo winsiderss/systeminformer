winsiderss/systeminformer

commit f755980bf0093c5932f4ffed436fa02e9abd9d4d

Move process architecture to treenode #1638

SystemInformer/proctree.c
@@ -306,11 +306,6 @@ VOID PhLoadSettingsProcessTreeUpdateMask(
     else
         ClearFlag(PhProcessProviderFlagsMask, PH_PROCESS_PROVIDER_FLAG_AVERAGE);
 
-    if (TreeNew_GetColumn(ProcessTreeListHandle, PHPRTLC_ARCHITECTURE, &column) && column.Visible)
-        SetFlag(PhProcessProviderFlagsMask, PH_PROCESS_PROVIDER_FLAG_ARCHITECTURE);
-    else
-        ClearFlag(PhProcessProviderFlagsMask, PH_PROCESS_PROVIDER_FLAG_ARCHITECTURE);
-
     PhInitializeProcessTreeColumnHeaderCache();
 }
 
@@ -718,7 +713,7 @@ VOID PhTickProcessNodes(
 
         // The name and PID never change, so we don't invalidate that.
         memset(&node->TextCache[2], 0, sizeof(PH_STRINGREF) * (PHPRTLC_MAXIMUM - 2));
-        node->ValidMask &= PHPN_OSCONTEXT | PHPN_IMAGE | PHPN_DPIAWARENESS | PHPN_APPID | PHPN_DESKTOPINFO | PHPN_CODEPAGE | PHPN_GRANTEDACCESS; // Items that always remain valid
+        node->ValidMask &= PHPN_OSCONTEXT | PHPN_IMAGE | PHPN_DPIAWARENESS | PHPN_APPID | PHPN_DESKTOPINFO | PHPN_CODEPAGE | PHPN_ARCHITECTURE | PHPN_GRANTEDACCESS; // Items that always remain valid
 
         // The DPI awareness defaults to unaware if not set or declared in the manifest in which case
         // it can be changed once, so we can only be sure that it won't be changed again if it is different
@@ -1174,6 +1169,7 @@ static VOID PhpUpdateProcessNodeImage(
 
             if (NT_SUCCESS(PhLoadMappedImageHeaderPageSize(&ProcessNode->ProcessItem->FileName->sr, NULL, &mappedImage)))
             {
+                ProcessNode->ImageReserved = mappedImage.NtHeaders->FileHeader.Machine;
                 ProcessNode->ImageTimeDateStamp = mappedImage.NtHeaders->FileHeader.TimeDateStamp;
                 ProcessNode->ImageCharacteristics = mappedImage.NtHeaders->FileHeader.Characteristics;
 
@@ -1497,6 +1493,28 @@ static VOID PhpUpdateProcessNodePowerThrottling(
         }
 
         ProcessNode->ValidMask |= PHPN_POWERTHROTTLING;
+    }
+}
+
+static VOID PhpUpdateProcessNodeArchitecture(
+    _Inout_ PPH_PROCESS_NODE ProcessNode
+    )
+{
+    if (!(ProcessNode->ValidMask & PHPN_ARCHITECTURE))
+    {
+        ProcessNode->ImageReserved = IMAGE_FILE_MACHINE_UNKNOWN;
+
+        if (ProcessNode->ProcessItem->QueryHandle)
+        {
+            USHORT processArchitecture;
+
+            if (NT_SUCCESS(PhGetProcessArchitecture(ProcessNode->ProcessItem->QueryHandle, &processArchitecture)))
+            {
+                ProcessNode->ImageReserved = processArchitecture;
+            }
+        }
+
+        ProcessNode->ValidMask |= PHPN_ARCHITECTURE;
     }
 }
 
@@ -2268,7 +2286,18 @@ END_SORT_FUNCTION
 
 BEGIN_SORT_FUNCTION(Architecture)
 {
-    sortResult = uintcmp(processItem1->Architecture, processItem2->Architecture);
+    if (WindowsVersion >= WINDOWS_11)
+    {
+        PhpUpdateProcessNodeArchitecture(node1);
+        PhpUpdateProcessNodeArchitecture(node2);
+    }
+    else
+    {
+        PhpUpdateProcessNodeImage(node1);
+        PhpUpdateProcessNodeImage(node2);
+    }
+
+    sortResult = uintcmp(node1->ImageReserved, node2->ImageReserved);
 }
 END_SORT_FUNCTION
 
@@ -3488,7 +3517,12 @@ BOOLEAN NTAPI PhpProcessTreeNewCallback(
                 break;
             case PHPRTLC_ARCHITECTURE:
                 {
-                    switch (processItem->Architecture)
+                    if (WindowsVersion >= WINDOWS_11)
+                        PhpUpdateProcessNodeArchitecture(node);
+                    else
+                        PhpUpdateProcessNodeImage(node);
+
+                    switch (node->ImageReserved)
                     {
                     case IMAGE_FILE_MACHINE_I386:
                         PhInitializeStringRef(&getCellText->Text, L"x86");

SystemInformer/include/procprv.h
@@ -36,7 +36,6 @@ typedef enum _PH_PROCESS_PROVIDER_FLAG
     PH_PROCESS_PROVIDER_FLAG_CFGUARD = 0x80000,
     PH_PROCESS_PROVIDER_FLAG_CET = 0x100000,
     PH_PROCESS_PROVIDER_FLAG_AVERAGE = 0x200000,
-    PH_PROCESS_PROVIDER_FLAG_ARCHITECTURE = 0x400000,
 } PH_PROCESS_PROVIDER_FLAG;
 
 extern ULONG PhProcessProviderFlagsMask;
@@ -280,8 +279,6 @@ typedef struct _PH_PROCESS_ITEM
 
     NTSTATUS ImageCoherencyStatus;
     FLOAT ImageCoherency;
-
-    USHORT Architecture; /*!< Process Machine Architecture (IMAGE_FILE_MACHINE_...) */
 
 } PH_PROCESS_ITEM, *PPH_PROCESS_ITEM;
 // end_phapppub

SystemInformer/include/proctree.h
@@ -145,8 +145,9 @@
 #define PHPN_ERRORMODE 0x8000
 #define PHPN_CODEPAGE 0x10000
 #define PHPN_POWERTHROTTLING 0x20000
-#define PHPN_PRIORITYBOOST 0x40000
-#define PHPN_GRANTEDACCESS 0x80000
+#define PHPN_ARCHITECTURE 0x40000
+#define PHPN_PRIORITYBOOST 0x80000
+#define PHPN_GRANTEDACCESS 0x100000
 
 // begin_phapppub
 typedef struct _PH_PROCESS_NODE

SystemInformer/procprv.c
@@ -91,8 +91,7 @@ typedef struct _PH_PROCESS_QUERY_S1_DATA
             ULONG IsBeingDebugged : 1;
             ULONG IsImmersive : 1;
             ULONG IsFilteredHandle : 1;
-            ULONG Architecture : 16; /*!< Process Machine Architecture (IMAGE_FILE_MACHINE_...) */
-            ULONG Spare : 9;
+            ULONG Spare : 25;
         };
     };
 
@@ -871,39 +870,6 @@ VOID PhpProcessQueryStage1(
         }
     }
 
-    // Architecture
-    if (PH_IS_REAL_PROCESS_ID(processItem->ProcessId) && FlagOn(PhProcessProviderFlagsMask, PH_PROCESS_PROVIDER_FLAG_ARCHITECTURE))
-    {
-        status = STATUS_NOT_FOUND;
-
-        //
-        // First try to use the new API if it makes sense to. (jxy-s)
-        //
-        if (WindowsVersion >= WINDOWS_11 && processItem->QueryHandle)
-        {
-            USHORT processArchitecture;
-
-            status = PhGetProcessArchitecture(
-                processItem->QueryHandle,
-                &processArchitecture
-                );
-
-            if (NT_SUCCESS(status))
-            {
-                Data->Architecture = processArchitecture;
-            }
-        }
-
-        //
-        // Check if we succeeded above. (jxy-s)
-        //
-        if (!NT_SUCCESS(status))
-        {
-            // Set the special value for a delayed stage2 query. (dmex)
-            Data->Architecture = USHRT_MAX;
-        }
-    }
-
     if (processItem->Sid && FlagOn(PhProcessProviderFlagsMask, PH_PROCESS_PROVIDER_FLAG_USERNAME))
     {
         // Note: We delay resolving the SID name because the local LSA cache might still be
@@ -950,21 +916,6 @@ VOID PhpProcessQueryStage2(
             Data->ImportModules = ULONG_MAX;
             Data->ImportFunctions = ULONG_MAX;
         }
-
-        //if (processItem->Architecture == USHRT_MAX && FlagOn(PhProcessProviderFlagsMask, PH_PROCESS_PROVIDER_FLAG_ARCHITECTURE))
-        //{
-        //    PH_MAPPED_IMAGE mappedImage;
-        //
-        //    // For backward compatibility we'll read the Machine from the file.
-        //    // If we fail to access the file we could go read from the remote
-        //    // process memory, but for now we only read from the file. (jxy-s)
-        //
-        //    if (NT_SUCCESS(PhLoadMappedImageHeaderPageSize(&processItem->FileName->sr, NULL, &mappedImage)))
-        //    {
-        //        Data->Architecture = (USHORT)mappedImage.NtHeaders->FileHeader.Machine;
-        //        PhUnloadMappedImage(&mappedImage);
-        //    }
-        //}
     }
 
     if (PhEnableImageCoherencySupport && processItem->FileName && !processItem->IsSubsystemProcess)
@@ -1100,7 +1051,6 @@ VOID PhpFillProcessItemStage1(
     processItem->IsBeingDebugged = Data->IsBeingDebugged;
     processItem->IsImmersive = Data->IsImmersive;
     processItem->IsProtectedHandle = Data->IsFilteredHandle;
-    processItem->Architecture = (USHORT)Data->Architecture;
 
     PhSwapReference(&processItem->Record->CommandLine, processItem->CommandLine);
 
@@ -1141,12 +1091,6 @@ VOID PhpFillProcessItemStage2(
     {
         memcpy(&processItem->VersionInfo, &Data->VersionInfo, sizeof(PH_IMAGE_VERSION_INFO));
     }
-
-    // Note: We query the architecture in stage1 so don't overwrite the previous data. (dmex)
-    //if (processItem->Architecture == USHRT_MAX)
-    //{
-    //    processItem->Architecture = Data->Architecture;
-    //}
 }
 
 VOID PhpFillProcessItemExtension(
@@ -2892,26 +2836,6 @@ VOID PhProcessProviderUpdate(
                 {
                     processItem->IsProtectedHandle = filteredHandle;
                     modified = TRUE;
-                }
-            }
-
-            // Architecture
-            if (processItem->QueryHandle && FlagOn(PhProcessProviderFlagsMask, PH_PROCESS_PROVIDER_FLAG_ARCHITECTURE))
-            {
-                if (processItem->Architecture == IMAGE_FILE_MACHINE_UNKNOWN)
-                {
-                    processItem->Architecture = IMAGE_FILE_MACHINE_TARGET_HOST;
-
-                    if (WindowsVersion >= WINDOWS_11)
-                    {
-                        USHORT processArchitecture;
-
-                        if (NT_SUCCESS(PhGetProcessArchitecture(processItem->QueryHandle, &processArchitecture)))
-                        {
-                            processItem->Architecture = processArchitecture;
-                            modified = TRUE;
-                        }
-                    }
                 }
             }
 

SystemInformer/prpggen.c
@@ -6,7 +6,7 @@
  * Authors:
  *
  *     wj32    2009-2016
- *     dmex    2016-2022
+ *     dmex    2016-2023
  *
  */
 
@@ -14,6 +14,7 @@
 #include <procprp.h>
 #include <procprpp.h>
 
+#include <mapimg.h>
 #include <secedit.h>
 #include <verify.h>
 
@@ -86,10 +87,33 @@ PPH_STRING PhGetProcessItemImageTypeText(
     _In_ PPH_PROCESS_ITEM ProcessItem
     )
 {
+    USHORT architecture = IMAGE_FILE_MACHINE_UNKNOWN;
     PWSTR arch = L"";
     PWSTR bits = L"";
 
-    switch (ProcessItem->Architecture)
+    {
+        USHORT processArchitecture;
+
+        if (
+            WindowsVersion >= WINDOWS_11 && ProcessItem->QueryHandle && 
+            NT_SUCCESS(PhGetProcessArchitecture(ProcessItem->QueryHandle, &processArchitecture))
+            )
+        {
+            architecture = processArchitecture;
+        }
+        else if (ProcessItem->FileName)
+        {
+            PH_MAPPED_IMAGE mappedImage;
+
+            if (NT_SUCCESS(PhLoadMappedImageHeaderPageSize(&ProcessItem->FileName->sr, NULL, &mappedImage)))
+            {
+                architecture = mappedImage.NtHeaders->FileHeader.Machine;
+                PhUnloadMappedImage(&mappedImage);
+            }
+        }
+    }
+
+    switch (architecture)
     {
     case IMAGE_FILE_MACHINE_I386:
         arch = L"I386 ";

commit 1c63a436b63b2f6efdcb14ba865806e598c86851

Fix process architecture regression from f755980bf

SystemInformer/proctree.c
@@ -1169,7 +1169,8 @@ static VOID PhpUpdateProcessNodeImage(
 
             if (NT_SUCCESS(PhLoadMappedImageHeaderPageSize(&ProcessNode->ProcessItem->FileName->sr, NULL, &mappedImage)))
             {
-                ProcessNode->ImageReserved = mappedImage.NtHeaders->FileHeader.Machine;
+                if (WindowsVersion < WINDOWS_11)
+                    ProcessNode->ImageReserved = mappedImage.NtHeaders->FileHeader.Machine;
                 ProcessNode->ImageTimeDateStamp = mappedImage.NtHeaders->FileHeader.TimeDateStamp;
                 ProcessNode->ImageCharacteristics = mappedImage.NtHeaders->FileHeader.Characteristics;
 