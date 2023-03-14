winsiderss/systeminformer

commit 5628df2a30861508c631906fc3728711f9be4a82

Remove duplicate handle checks

phlib/hndlinfo.c
@@ -1829,7 +1829,7 @@ NTSTATUS PhGetHandleInformationEx(
 {
     NTSTATUS status = STATUS_SUCCESS;
     NTSTATUS subStatus = STATUS_SUCCESS;
-    HANDLE dupHandle = NULL;
+    HANDLE objectHandle = NULL;
     PPH_STRING typeName = NULL;
     PPH_STRING objectName = NULL;
     PPH_STRING bestObjectName = NULL;
@@ -1853,7 +1853,7 @@ NTSTATUS PhGetHandleInformationEx(
                 ProcessHandle,
                 Handle,
                 NtCurrentProcess(),
-                &dupHandle,
+                &objectHandle,
                 0,
                 0,
                 0
@@ -1864,8 +1864,12 @@ NTSTATUS PhGetHandleInformationEx(
         }
         else
         {
-            dupHandle = Handle;
+            objectHandle = Handle;
         }
+    }
+    else
+    {
+        objectHandle = Handle;
     }
 
     // Get basic information.
@@ -1873,7 +1877,7 @@ NTSTATUS PhGetHandleInformationEx(
     {
         status = PhpGetObjectBasicInformation(
             ProcessHandle,
-            useKph ? Handle : dupHandle,
+            objectHandle,
             BasicInformation
             );
 
@@ -1888,7 +1892,7 @@ NTSTATUS PhGetHandleInformationEx(
     // Get the type name.
     status = PhpGetObjectTypeName(
         ProcessHandle,
-        useKph ? Handle : dupHandle,
+        objectHandle,
         ObjectTypeNumber,
         &typeName
         );
@@ -1906,7 +1910,7 @@ NTSTATUS PhGetHandleInformationEx(
     {
         status = PhpGetObjectName(
             ProcessHandle,
-            useKph ? Handle : dupHandle,
+            objectHandle,
             TRUE,
             &objectName
             );
@@ -1924,7 +1928,7 @@ NTSTATUS PhGetHandleInformationEx(
         // Query the object normally.
         status = PhpGetObjectName(
             ProcessHandle,
-            useKph ? Handle : dupHandle,
+            objectHandle,
             FALSE,
             &objectName
             );
@@ -1979,8 +1983,8 @@ CleanupExit:
             PhSetReference(BestObjectName, bestObjectName);
     }
 
-    if (dupHandle && ProcessHandle != NtCurrentProcess())
-        NtClose(dupHandle);
+    if (objectHandle && ProcessHandle != NtCurrentProcess())
+        NtClose(objectHandle);
 
     PhClearReference(&typeName);
     PhClearReference(&objectName);

commit 6eaae28035eb4cfd5ad1455ed639721bed99b12d

Fix handle leak regression from commit 5628df2a

phlib/hndlinfo.c
@@ -1983,7 +1983,7 @@ CleanupExit:
             PhSetReference(BestObjectName, bestObjectName);
     }
 
-    if (objectHandle && ProcessHandle != NtCurrentProcess())
+    if (!useKph && objectHandle && ProcessHandle != NtCurrentProcess())
         NtClose(objectHandle);
 
     PhClearReference(&typeName);