a1f776a6acb069c7752e4422d5f93173ff35aabb
dmex <dmex04@gmail.com>
2023-04-28 14:18:40 +1000

    thirdparty: Fix warning LNK4078 from commit 1212ad2

--- tools/thirdparty/detours/disasm.cpp
+++ tools/thirdparty/detours/disasm.cpp
@@ -144,8 +144,8 @@
 //      offsets.
 //
 
-#pragma data_seg(".sithirdpartyd")
-#pragma const_seg(".sithirdpartyc")
+#pragma data_seg(".si3rd")
+#pragma const_seg(".si3rc")
 
 //////////////////////////////////////////////////// X86 and X64 Disassembler.
 //
