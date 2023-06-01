bd27b254690d612fff6a3aea0cb400b0fdc859d7
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2023-06-01 15:52:07 +0800

    fix: phnt typo

--- phnt/include/ntlpcapi.h
+++ phnt/include/ntlpcapi.h
@@ -881,7 +881,7 @@ NTAPI
 NtAlpcImpersonateClientOfPort(
     _In_ HANDLE PortHandle,
     _In_ PPORT_MESSAGE Message,
-    _In_ PVOID Flags
+    _In_ ULONG Flags
     );
 
 #if (PHNT_VERSION >= PHNT_THRESHOLD)
