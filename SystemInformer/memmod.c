bfa026ea7948e4915c112f6cd69b8db4e44bad51
dmex <dmex04@gmail.com>
2023-07-06 13:34:02 +1000

    Add refresh for modified pages

--- SystemInformer/memmod.c
+++ SystemInformer/memmod.c
@@ -166,6 +166,52 @@ typedef struct _SYMBOL_LOOKUP_RESULT
     PPH_STRING FileName;
 } SYMBOL_LOOKUP_RESULT, *PSYMBOL_LOOKUP_RESULT;
 
+VOID PhpCleanupPageModifiedLists(
+    _In_ PPH_IMAGE_MODIFIED_DIALOG_CONTEXT Context
+    )
+{
+    PSINGLE_LIST_ENTRY listEntry;
+
+    if (Context->ImageAddressList)
+    {
+        for (ULONG i = 0; i < Context->ImageAddressList->Count; i++)
+        {
+            PPH_IMAGE_MAPPED_BASE_ENTRY entry = Context->ImageAddressList->Items[i];
+            PhClearReference(&entry->FileName);
+            PhFree(entry);
+        }
+        PhClearReference(&Context->ImageAddressList);
+    }
+
+    if (Context->PageTamperingList)
+    {
+        for (ULONG i = 0; i < Context->PageTamperingList->Count; i++)
+            PhFree(Context->PageTamperingList->Items[i]);
+        PhClearReference(&Context->PageTamperingList);
+    }
+
+    // Free all unused results.
+
+    PhAcquireQueuedLockExclusive(&Context->ResultListLock);
+
+    listEntry = Context->ResultListHead.Next;
+
+    while (listEntry)
+    {
+        PSYMBOL_LOOKUP_RESULT result;
+
+        result = CONTAINING_RECORD(listEntry, SYMBOL_LOOKUP_RESULT, ListEntry);
+        listEntry = listEntry->Next;
+
+        PhClearReference(&result->Symbol);
+        PhClearReference(&result->FileName);
+
+        PhFree(result);
+    }
+
+    PhReleaseQueuedLockExclusive(&Context->ResultListLock);
+}
+
 VOID PhpLimitedSymbolReferenceContext(
     _In_ PPH_IMAGE_MODIFIED_DIALOG_CONTEXT Context
     )
@@ -179,53 +225,12 @@ VOID PhpLimitedSymbolDereferenceContext(
 {
     if (_InterlockedDecrement(&Context->RefCount) == 0)
     {
-        PSINGLE_LIST_ENTRY listEntry;
-
         if (Context->SymbolProvider)
             PhDereferenceObject(Context->SymbolProvider);
         if (Context->ProcessHandle)
             NtClose(Context->ProcessHandle);
 
-        if (Context->ImageAddressList)
-        {
-            for (ULONG i = 0; i < Context->ImageAddressList->Count; i++)
-            {
-                PPH_IMAGE_MAPPED_BASE_ENTRY entry = Context->ImageAddressList->Items[i];
-                PhClearReference(&entry->FileName);
-                PhFree(entry);
-            }
-            PhDereferenceObject(Context->ImageAddressList);
-        }
-
-        if (Context->PageTamperingList)
-        {
-            for (ULONG i = 0; i < Context->PageTamperingList->Count; i++)
-                PhFree(Context->PageTamperingList->Items[i]);
-            PhDereferenceObject(Context->PageTamperingList);
-        }
-
-        // Free all unused results.
-
-        PhAcquireQueuedLockExclusive(&Context->ResultListLock);
-
-        listEntry = Context->ResultListHead.Next;
-
-        while (listEntry)
-        {
-            PSYMBOL_LOOKUP_RESULT result;
-
-            result = CONTAINING_RECORD(listEntry, SYMBOL_LOOKUP_RESULT, ListEntry);
-            listEntry = listEntry->Next;
-
-            if (result->Symbol)
-                PhDereferenceObject(result->Symbol);
-            if (result->FileName)
-                PhDereferenceObject(result->FileName);
-
-            PhFree(result);
-        }
-
-        PhReleaseQueuedLockExclusive(&Context->ResultListLock);
+        PhpCleanupPageModifiedLists(Context);
 
         PhDereferenceObject(Context->ProcessItem);
 
@@ -477,36 +482,36 @@ NTSTATUS PhCheckProcessImagesForTampering(
 }
 
 INT_PTR CALLBACK PhPageModifiedDlgProc(
-    _In_ HWND hwndDlg,
-    _In_ UINT uMsg,
+    _In_ HWND WindowHandle,
+    _In_ UINT WindowMessage,
     _In_ WPARAM wParam,
     _In_ LPARAM lParam
     )
 {
     PPH_IMAGE_MODIFIED_DIALOG_CONTEXT context = NULL;
 
-    if (uMsg == WM_INITDIALOG)
+    if (WindowMessage == WM_INITDIALOG)
     {
         context = (PPH_IMAGE_MODIFIED_DIALOG_CONTEXT)lParam;
 
-        PhSetWindowContext(hwndDlg, PH_WINDOW_CONTEXT_DEFAULT, context);
+        PhSetWindowContext(WindowHandle, PH_WINDOW_CONTEXT_DEFAULT, context);
     }
     else
     {
-        context = PhGetWindowContext(hwndDlg, PH_WINDOW_CONTEXT_DEFAULT);
+        context = PhGetWindowContext(WindowHandle, PH_WINDOW_CONTEXT_DEFAULT);
     }
 
     if (!context)
         return FALSE;
 
-    switch (uMsg)
+    switch (WindowMessage)
     {
     case WM_INITDIALOG:
         {
-            context->WindowHandle = hwndDlg;
-            context->ListViewHandle = GetDlgItem(hwndDlg, IDC_LIST);
+            context->WindowHandle = WindowHandle;
+            context->ListViewHandle = GetDlgItem(WindowHandle, IDC_LIST);
 
-            PhSetApplicationWindowIcon(hwndDlg);
+            PhSetApplicationWindowIcon(WindowHandle);
 
             PhSetListViewStyle(context->ListViewHandle, TRUE, TRUE);
             PhSetControlTheme(context->ListViewHandle, L"explorer");
@@ -520,20 +525,20 @@ INT_PTR CALLBACK PhPageModifiedDlgProc(
             PhLoadListViewColumnsFromSetting(L"MemoryModifiedListViewColumns", context->ListViewHandle);
             PhLoadListViewSortColumnsFromSetting(L"MemoryModifiedListViewSort", context->ListViewHandle);
 
-            PhInitializeLayoutManager(&context->LayoutManager, hwndDlg);
+            PhInitializeLayoutManager(&context->LayoutManager, WindowHandle);
             PhAddLayoutItem(&context->LayoutManager, context->ListViewHandle, NULL, PH_ANCHOR_ALL);
-            PhAddLayoutItem(&context->LayoutManager, GetDlgItem(hwndDlg, IDC_REFRESH), NULL, PH_ANCHOR_BOTTOM | PH_ANCHOR_RIGHT);
-            PhAddLayoutItem(&context->LayoutManager, GetDlgItem(hwndDlg, IDOK), NULL, PH_ANCHOR_BOTTOM | PH_ANCHOR_RIGHT);
+            PhAddLayoutItem(&context->LayoutManager, GetDlgItem(WindowHandle, IDC_REFRESH), NULL, PH_ANCHOR_BOTTOM | PH_ANCHOR_RIGHT);
+            PhAddLayoutItem(&context->LayoutManager, GetDlgItem(WindowHandle, IDOK), NULL, PH_ANCHOR_BOTTOM | PH_ANCHOR_RIGHT);
 
             if (PhGetIntegerPairSetting(L"MemoryModifiedWindowPosition").X)
-                PhLoadWindowPlacementFromSetting(L"MemoryModifiedWindowPosition", L"MemoryModifiedWindowSize", hwndDlg);
+                PhLoadWindowPlacementFromSetting(L"MemoryModifiedWindowPosition", L"MemoryModifiedWindowSize", WindowHandle);
             else
-                PhCenterWindow(hwndDlg, context->ParentWindowHandle);
+                PhCenterWindow(WindowHandle, context->ParentWindowHandle);
 
             {
                 PPH_STRING windowTitle;
 
-                windowTitle = PhGetWindowText(hwndDlg);
+                windowTitle = PhGetWindowText(WindowHandle);
                 PhMoveReference(&windowTitle, PhFormatString(
                     L"%s: %s (%lu)",
                     PhGetStringOrEmpty(windowTitle),
@@ -541,13 +546,13 @@ INT_PTR CALLBACK PhPageModifiedDlgProc(
                     HandleToUlong(context->ProcessItem->ProcessId)
                     ));
 
-                PhSetWindowText(hwndDlg, PhGetStringOrEmpty(windowTitle));
+                PhSetWindowText(WindowHandle, PhGetStringOrEmpty(windowTitle));
                 PhDereferenceObject(windowTitle);
             }
 
-            PhSetTimer(hwndDlg, PH_WINDOW_TIMER_DEFAULT, 1000, NULL);
+            PhSetTimer(WindowHandle, PH_WINDOW_TIMER_DEFAULT, 1000, NULL);
 
-            PhInitializeWindowTheme(hwndDlg, PhEnableThemeSupport);
+            PhInitializeWindowTheme(WindowHandle, PhEnableThemeSupport);
 
             {
                 NTSTATUS status;
@@ -556,8 +561,8 @@ INT_PTR CALLBACK PhPageModifiedDlgProc(
 
                 if (!NT_SUCCESS(status))
                 {
-                    PhShowStatus(GetParent(hwndDlg), L"", status, 0);
-                    EndDialog(hwndDlg, IDCANCEL);
+                    PhShowStatus(context->ParentWindowHandle, L"Unable to perform the scan", status, 0);
+                    EndDialog(WindowHandle, IDCANCEL);
                 }
             }
         }
@@ -566,17 +571,17 @@ INT_PTR CALLBACK PhPageModifiedDlgProc(
         {
             PhSaveListViewSortColumnsToSetting(L"MemoryModifiedListViewSort", context->ListViewHandle);
             PhSaveListViewColumnsToSetting(L"MemoryModifiedListViewColumns", context->ListViewHandle);
-            PhSaveWindowPlacementToSetting(L"MemoryModifiedWindowPosition", L"MemoryModifiedWindowSize", hwndDlg);
+            PhSaveWindowPlacementToSetting(L"MemoryModifiedWindowPosition", L"MemoryModifiedWindowSize", WindowHandle);
 
             context->Destroying = TRUE;
 
-            PhKillTimer(hwndDlg, PH_WINDOW_TIMER_DEFAULT);
+            PhKillTimer(WindowHandle, PH_WINDOW_TIMER_DEFAULT);
 
             PhDeleteLayoutManager(&context->LayoutManager);
 
             PhpLimitedSymbolDereferenceContext(context);
 
-            PhRemoveWindowContext(hwndDlg, PH_WINDOW_CONTEXT_DEFAULT);
+            PhRemoveWindowContext(WindowHandle, PH_WINDOW_CONTEXT_DEFAULT);
         }
         break;
     case WM_TIMER:
@@ -595,11 +600,28 @@ INT_PTR CALLBACK PhPageModifiedDlgProc(
             {
             case IDCANCEL:
             case IDOK:
-                DestroyWindow(hwndDlg);
+                DestroyWindow(WindowHandle);
                 break;
-            //case IDC_REFRESH:
-            //    PhpRefreshGdiHandles(context);
-            //    break;
+            case IDC_REFRESH:
+                {
+                    ExtendedListView_SetRedraw(context->ListViewHandle, FALSE);
+                    ListView_DeleteAllItems(context->ListViewHandle);
+                    ExtendedListView_SetRedraw(context->ListViewHandle, TRUE);
+
+                    PhpCleanupPageModifiedLists(context);
+
+                    {
+                        NTSTATUS status;
+
+                        status = PhCheckProcessImagesForTampering(context);
+
+                        if (!NT_SUCCESS(status))
+                        {
+                            PhShowStatus(WindowHandle, L"Unable to perform the scan", status, 0);
+                        }
+                    }
+                }
+                break;
             }
         }
         break;
@@ -639,7 +661,7 @@ INT_PTR CALLBACK PhPageModifiedDlgProc(
 
                     item = PhShowEMenu(
                         menu,
-                        hwndDlg,
+                        WindowHandle,
                         PH_EMENU_SHOW_SEND_COMMAND | PH_EMENU_SHOW_LEFTRIGHT,
                         PH_ALIGN_LEFT | PH_ALIGN_TOP,
                         point.x,
@@ -667,11 +689,11 @@ INT_PTR CALLBACK PhPageModifiedDlgProc(
         }
         break;
     case WM_CTLCOLORBTN:
-        return HANDLE_WM_CTLCOLORBTN(hwndDlg, wParam, lParam, PhWindowThemeControlColor);
+        return HANDLE_WM_CTLCOLORBTN(WindowHandle, wParam, lParam, PhWindowThemeControlColor);
     case WM_CTLCOLORDLG:
-        return HANDLE_WM_CTLCOLORDLG(hwndDlg, wParam, lParam, PhWindowThemeControlColor);
+        return HANDLE_WM_CTLCOLORDLG(WindowHandle, wParam, lParam, PhWindowThemeControlColor);
     case WM_CTLCOLORSTATIC:
-        return HANDLE_WM_CTLCOLORSTATIC(hwndDlg, wParam, lParam, PhWindowThemeControlColor);
+        return HANDLE_WM_CTLCOLORSTATIC(WindowHandle, wParam, lParam, PhWindowThemeControlColor);
     }
 
     return FALSE;
