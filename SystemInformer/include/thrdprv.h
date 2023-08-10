993a53733c9f71dc3aa52f1c9e3b22c072bc7380
dmex <dmex04@gmail.com>
2023-08-10 10:03:05 +1000

    Update copyright

--- SystemInformer/include/thrdprv.h
+++ SystemInformer/include/thrdprv.h
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2022 Winsider Seminars & Solutions, Inc.  All rights reserved.
+ *
+ * This file is part of System Informer.
+ *
+ * Authors:
+ *
+ *     wj32    2009-2016
+ *     dmex    2017-2023
+ *
+ */
+
 #ifndef PH_THRDPRV_H
 #define PH_THRDPRV_H
 
