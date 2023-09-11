904c4c50849a7bd7f602732a4793ea3999547b63
Johnny Shaw <johnny.shaw@live.com>
2023-09-10 22:50:38 -0600

    show enclave load reason

--- phnt/include/ntldr.h
+++ phnt/include/ntldr.h
@@ -1120,7 +1120,7 @@ typedef struct _LDR_SOFTWARE_ENCLAVE
     SIZE_T Size;
     PVOID PreviousBaseAddress;
     LIST_ENTRY Modules; // LDR_DATA_TABLE_ENTRY.InLoadOrderLinks
-    PLDR_DATA_TABLE_ENTRY MainModule;
+    PLDR_DATA_TABLE_ENTRY PrimaryModule;
     PLDR_DATA_TABLE_ENTRY BCryptModule;
     PLDR_DATA_TABLE_ENTRY BCryptPrimitivesModule;
 } LDR_SOFTWARE_ENCLAVE, *PLDR_SOFTWARE_ENCLAVE;
