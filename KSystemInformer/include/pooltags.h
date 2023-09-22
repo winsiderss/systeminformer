173d4a95f8f6d627bdc30a52ca77208bb8d90eee
Johnny Shaw <johnny.shaw@live.com>
2023-09-22 00:19:19 -0600

    kph cleanup pool tags

--- KSystemInformer/include/pooltags.h
+++ KSystemInformer/include/pooltags.h
@@ -13,8 +13,8 @@
 
 // alloc
 
-#define KPH_TAG_PAGED_LOOKASIDE_OBJECT          '0AcK'
-#define KPH_TAG_NPAGED_LOOKASIDE_OBJECT         '1AcK'
+#define KPH_TAG_PAGED_LOOKASIDE_OBJECT          '0ApK'
+#define KPH_TAG_NPAGED_LOOKASIDE_OBJECT         '1ApK'
 
 // comms
 
@@ -84,8 +84,8 @@
 
 // alpc
 
-#define KPH_TAG_ALPC_NAME_QUERY                 '0ApK'
-#define KPH_TAG_ALPC_QUERY                      '1ApK'
+#define KPH_TAG_ALPC_NAME_QUERY                 '0apK'
+#define KPH_TAG_ALPC_QUERY                      '1apK'
 
 // file
 
@@ -94,6 +94,6 @@
 
 // socket
 
-#define KPH_TAG_SOCKET                           '0spK'
-#define KPH_TAG_TLS                              '1spK'
-#define KPH_TAG_TLS_BUFFER                       '2spK'
+#define KPH_TAG_SOCKET                          '0spK'
+#define KPH_TAG_TLS                             '1spK'
+#define KPH_TAG_TLS_BUFFER                      '2spK'
