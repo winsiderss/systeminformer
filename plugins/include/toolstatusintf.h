ff3ec25b0e8b9707a15f77fce8e3817faa269c72
dmex <dmex04@gmail.com>
2024-03-25 05:35:49 +1100

    Update toolstatusintf.h

--- plugins/include/toolstatusintf.h
+++ plugins/include/toolstatusintf.h
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2022 Winsider Seminars & Solutions, Inc.  All rights reserved.
+ *
+ * This file is part of System Informer.
+ *
+ * Authors:
+ *
+ *     wj32    2015
+ *     dmex    2015-2024
+ *
+ */
+
 #ifndef _TOOLSTATUSINTF_H
 #define _TOOLSTATUSINTF_H
 
