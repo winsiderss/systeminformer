7ccb384d40d29cb3ebc1707944f209a270af2300
Johnny Shaw <johnny.shaw@live.com>
2024-03-30 22:11:42 -0600

    ToolStatus searchbox tab unfocus

--- plugins/ToolStatus/resource.h
+++ plugins/ToolStatus/resource.h
@@ -55,9 +55,10 @@
 #define IDC_ENABLE_AUTOCOMPLETE         1016
 #define IDC_ENABLE_AUTOFOCUS_SEARCH     1017
 #define ID_SEARCH                       40011
+#define ID_SEARCH_TAB                   40012
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        147
