a59fbd1cc2bdc18276028c11ee498be0f0394fb1
dmex <dmex04@gmail.com>
2024-04-27 14:20:55 +1000

    BuildTools: Update scripts

--- build/build_tools.cmd
+++ build/build_tools.cmd
@@ -18,8 +18,8 @@ if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
 )
 
 :: Pre-cleanup (required since dotnet doesn't cleanup)
-if exist "tools\CustomBuildTool\bin\Release\net7.0-x64" (
-   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net7.0-x64"
+if exist "tools\CustomBuildTool\bin\Release\net8.0-x64" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net8.0-x64"
 )
 if exist "tools\CustomBuildTool\bin\Debug" (
    rmdir /S /Q "tools\CustomBuildTool\bin\Debug"
@@ -34,8 +34,8 @@ if exist "tools\CustomBuildTool\obj" (
 dotnet publish tools\CustomBuildTool\CustomBuildTool.sln -c Release /p:PublishProfile=Properties\PublishProfiles\64bit.pubxml /p:ContinuousIntegrationBuild=true
 
 :: Post-cleanup (optional)
-if exist "tools\CustomBuildTool\bin\Release\net7.0-x64" (
-   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net7.0-x64"
+if exist "tools\CustomBuildTool\bin\Release\net8.0-x64" (
+   rmdir /S /Q "tools\CustomBuildTool\bin\Release\net8.0-x64"
 )
 if exist "tools\CustomBuildTool\bin\Debug" (
    rmdir /S /Q "tools\CustomBuildTool\bin\Debug"
