winsiderss/systeminformer

commit ff32738d7bbbe8ddcb8dda91b673075bf1267b07

Fix loading plugins from mapped paths #2051

SystemInformer/plugin.c
@@ -52,7 +52,7 @@ VOID PhpExecuteCallbackForAllPlugins(
     );
 
 PH_AVL_TREE PhPluginsByName = PH_AVL_TREE_INIT(PhpPluginsCompareFunction);
-BOOLEAN PhPluginsLoadNative = TRUE;
+BOOLEAN PhPluginsLoadNative = FALSE;
 static PH_CALLBACK GeneralCallbacks[GeneralCallbackMaximum];
 static ULONG NextPluginId = IDPLUGINS + 1;
 static PH_STRINGREF PhpDefaultPluginName[] =
@@ -191,39 +191,9 @@ PPH_STRING PhpGetPluginDirectoryPath(
     VOID
     )
 {
-    PPH_STRING pluginsDirectory;
-    PPH_STRING applicationDirectory;
-    SIZE_T returnLength;
-    PH_FORMAT format[2];
-    WCHAR pluginsDirectoryBuffer[MAX_PATH];
+    static PH_STRINGREF pluginsDirectory = PH_STRINGREF_INIT(L"plugins\\");
 
-    applicationDirectory = PhGetApplicationDirectory();
-    PhInitFormatSR(&format[0], applicationDirectory->sr);
-    PhInitFormatS(&format[1], L"plugins\\");
-
-    if (PhFormatToBuffer(
-        format,
-        RTL_NUMBER_OF(format),
-        pluginsDirectoryBuffer,
-        sizeof(pluginsDirectoryBuffer),
-        &returnLength
-        ))
-    {
-        PH_STRINGREF pluginsDirectoryPath;
-
-        pluginsDirectoryPath.Buffer = pluginsDirectoryBuffer;
-        pluginsDirectoryPath.Length = returnLength - sizeof(UNICODE_NULL);
-
-        pluginsDirectory = PhCreateString2(&pluginsDirectoryPath);
-    }
-    else
-    {
-        pluginsDirectory = PhFormat(format, RTL_NUMBER_OF(format), MAX_PATH);
-    }
-
-    PhDereferenceObject(applicationDirectory);
-
-    return pluginsDirectory;
+    return PhGetApplicationDirectoryFileName(&pluginsDirectory, PhPluginsLoadNative);
 }
 
 //PPH_STRING PhpGetPluginDirectoryPath(
@@ -438,7 +408,7 @@ VOID PhLoadPlugins(
     PPH_STRING pluginsDirectory;
     PPH_LIST pluginLoadErrors;
 
-    PhPluginsLoadNative = !PhGetIntegerSetting(L"EnablePluginsNative");
+    PhPluginsLoadNative = !!PhGetIntegerSetting(L"EnablePluginsNative");
 
     if (!(pluginsDirectory = PhpGetPluginDirectoryPath()))
         return;
@@ -567,26 +537,21 @@ NTSTATUS PhLoadPlugin(
     _In_ PPH_STRINGREF FileName
     )
 {
+    NTSTATUS status;
+
     if (PhPluginsLoadNative)
     {
-        NTSTATUS status;
-        PPH_STRING fileName;
-
-        if (fileName = PhResolveDevicePrefix(FileName))
-        {
-            if (LoadLibraryEx(PhGetString(fileName), NULL, 0))
-                status = STATUS_SUCCESS;
-            else
-                status = PhGetLastWin32ErrorAsNtStatus();
-
-            PhDereferenceObject(fileName);
-            return status;
-        }
-
-        return STATUS_UNSUCCESSFUL;
+        status = PhLoadPluginImage(FileName, NULL);
+    }
+    else
+    {
+        if (LoadLibraryEx(PhGetStringRefZ(FileName), NULL, 0))
+            status = STATUS_SUCCESS;
+        else
+            status = PhGetLastWin32ErrorAsNtStatus();
     }
 
-    return PhLoadPluginImage(FileName, NULL);
+    return status;
 }
 
 VOID PhInvokeCallbackForAllPlugins(

commit 34489e621f80fb0fb928037e2899bfe6ae6320c5

Fix plugin enumeration regression from ff32738d7bbbe8ddcb8dda91b673075bf1267b07

SystemInformer/plugin.c
@@ -420,7 +420,12 @@ VOID PhLoadPlugins(
         if (PhIsPluginDisabled(&PhpDefaultPluginName[i]))
             continue;
 
-        if (fileName = PhConcatStringRef2(&pluginsDirectory->sr, &PhpDefaultPluginName[i]))
+        if (PhPluginsLoadNative)
+            fileName = PhConcatStringRef3(&PhWin32ExtendedPathPrefix, &pluginsDirectory->sr, &PhpDefaultPluginName[i]);
+        else
+            fileName = PhConcatStringRef2(&pluginsDirectory->sr, &PhpDefaultPluginName[i]);
+
+        if (fileName)
         {
             status = PhLoadPlugin(&fileName->sr);
 
@@ -447,17 +452,34 @@ VOID PhLoadPlugins(
 
     if (!PhGetIntegerSetting(L"EnableDefaultSafePlugins"))
     {
-        HANDLE pluginsDirectoryHandle;
+        HANDLE pluginsDirectoryHandle = NULL;
 
-        if (NT_SUCCESS(PhCreateFile(
-            &pluginsDirectoryHandle,
-            &pluginsDirectory->sr,
-            FILE_LIST_DIRECTORY | SYNCHRONIZE,
-            FILE_ATTRIBUTE_DIRECTORY,
-            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-            FILE_OPEN,
-            FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT
-            )))
+        if (PhPluginsLoadNative)
+        {
+            PhCreateFile(
+                &pluginsDirectoryHandle,
+                &pluginsDirectory->sr,
+                FILE_LIST_DIRECTORY | SYNCHRONIZE,
+                FILE_ATTRIBUTE_DIRECTORY,
+                FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+                FILE_OPEN,
+                FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT
+                );
+        }
+        else
+        {
+            PhCreateFileWin32(
+                &pluginsDirectoryHandle,
+                PhGetString(pluginsDirectory),
+                FILE_LIST_DIRECTORY | SYNCHRONIZE,
+                FILE_ATTRIBUTE_DIRECTORY,
+                FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+                FILE_OPEN,
+                FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT
+                );
+        }
+
+        if (pluginsDirectoryHandle)
         {
             static UNICODE_STRING pluginsSearchPattern = RTL_CONSTANT_STRING(L"*.dll");
 
@@ -846,6 +868,12 @@ VOID PhPluginGetSystemStatistics(
     _Out_ PPH_PLUGIN_SYSTEM_STATISTICS Statistics
     )
 {
+    if (!PhProcessStatisticsInitialized)
+    {
+        memset(Statistics, 0, sizeof(PH_PLUGIN_SYSTEM_STATISTICS));
+        return;
+    }
+
     Statistics->Performance = &PhPerfInformation;
 
     Statistics->NumberOfProcesses = PhTotalProcesses;