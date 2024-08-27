574a5be38df40d4409a525655d919c0cb33f6ec7
Johnny Shaw <johnny.shaw@live.com>
2024-08-27 00:23:28 -0600

    update build scripts

--- build/build_sdk_rebuild.cmd
+++ build/build_sdk_rebuild.cmd
@@ -2,6 +2,13 @@
 @setlocal enableextensions
 @cd /d "%~dp0\..\"
 
-start /B /W "" "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe" "-cleansdk"
+set BUILD_TOOL="tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe"
+
+if not exist %BUILD_TOOL% (
+    echo Build tool not found. Run build\build_init.cmd first.
+    exit /b 1
+)
+
+start /B /W "" %BUILD_TOOL% "-cleansdk"
 
 pause
