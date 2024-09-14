winsiderss/systeminformer

commit 0c0c938f8818f9a43eb55f7c2248b93ed6cf1302

Add single type formatting

plugins/ExtendedTools/pooldialog.c
@@ -149,17 +149,14 @@ BOOLEAN EtPoolTagTreeFilterCallback(
 }
 
 VOID NTAPI EtPoolMonProcessesUpdatedCallback(
-    _In_opt_ PVOID Parameter,
+    _In_ PVOID Parameter,
     _In_ PVOID Context
     )
 {
     PPOOLTAG_CONTEXT context = Context;
 
-    if (context->ProcessesUpdatedCount < 2)
-    {
-        context->ProcessesUpdatedCount++;
+    if (PtrToUlong(Parameter) < 3)
         return;
-    }
 
     EtUpdatePoolTagTable(Context);
 }

SystemInformer/appsup.c
@@ -1792,7 +1792,10 @@ VOID PhApplyTreeNewFilters(
         }
     }
 
-    TreeNew_NodesStructured(Support->TreeNewHandle);
+    if (Support->NodeList->Count)
+    {
+        TreeNew_NodesStructured(Support->TreeNewHandle);
+    }
 }
 
 VOID NTAPI PhpCopyCellEMenuItemDeleteFunction(

SystemInformer/include/procprv.h
@@ -298,6 +298,7 @@ typedef struct _PH_PROCESS_ITEM
     FLOAT ImageCoherency;
 
     ULONG LxssProcessId;
+    HANDLE FreezeHandle;
 
 } PH_PROCESS_ITEM, *PPH_PROCESS_ITEM;
 // end_phapppub
@@ -523,11 +524,12 @@ PhGetProcessSmallImageList(
     );
 
 // Note: Can only be called from same thread as process provider. (dmex)
+_Success_(return)
 PHAPPAPI
 BOOLEAN
 NTAPI
 PhDuplicateProcessInformation(
-    _Out_ PPVOID ProcessInformation
+    _Outptr_ PPVOID ProcessInformation
     );
 // end_phapppub
 

SystemInformer/mainwnd.c
@@ -468,8 +468,8 @@ VOID PhMwpInitializeControls(
         WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | TCS_MULTILINE,
         0,
         0,
-        3,
-        3,
+        0,
+        0,
         WindowHandle,
         NULL,
         PhInstanceHandle,
@@ -482,8 +482,8 @@ VOID PhMwpInitializeControls(
         WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | TN_STYLE_ICONS | TN_STYLE_DOUBLE_BUFFERED | TN_STYLE_ANIMATE_DIVIDER | thinRows | treelistBorder | treelistCustomColors | treelistCustomHeaderDraw,
         0,
         0,
-        3,
-        3,
+        0,
+        0,
         WindowHandle,
         NULL,
         PhInstanceHandle,
@@ -496,8 +496,8 @@ VOID PhMwpInitializeControls(
         WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | TN_STYLE_ICONS | TN_STYLE_DOUBLE_BUFFERED | thinRows | treelistBorder | treelistCustomColors,
         0,
         0,
-        3,
-        3,
+        0,
+        0,
         WindowHandle,
         NULL,
         PhInstanceHandle,
@@ -510,8 +510,8 @@ VOID PhMwpInitializeControls(
         WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | TN_STYLE_ICONS | TN_STYLE_DOUBLE_BUFFERED | thinRows | treelistBorder | treelistCustomColors,
         0,
         0,
-        3,
-        3,
+        0,
+        0,
         WindowHandle,
         NULL,
         PhInstanceHandle,

SystemInformer/mwpgproc.c
@@ -770,7 +770,7 @@ VOID PhMwpInitializeProcessMenu(
 
         if (WindowsVersion >= WINDOWS_11 && PH_IS_REAL_PROCESS_ID(Processes[0]->ProcessId))
         {
-            if (PhIsProcessStateFrozen(Processes[0]->ProcessId))
+            if (!PhIsNullOrInvalidHandle(Processes[0]->FreezeHandle))
             {
                 if (item = PhFindEMenuItem(Menu, 0, NULL, ID_PROCESS_FREEZE))
                     PhDestroyEMenuItem(item);

SystemInformer/netlist.c
@@ -197,9 +197,9 @@ VOID PhLoadSettingsNetworkTreeList(
     PhDereferenceObject(sortSettings);
 
     if (PhGetIntegerSetting(L"EnableInstantTooltips"))
-    {
         SendMessage(TreeNew_GetTooltips(NetworkTreeListHandle), TTM_SETDELAYTIME, TTDT_INITIAL, 0);
-    }
+    else
+        SendMessage(TreeNew_GetTooltips(NetworkTreeListHandle), TTM_SETDELAYTIME, TTDT_AUTOPOP, MAXSHORT);
 
     PhLoadSettingsNetworkTreeUpdateMask();
 }
@@ -222,8 +222,10 @@ VOID PhReloadSettingsNetworkTreeList(
     VOID
     )
 {
-    SendMessage(TreeNew_GetTooltips(NetworkTreeListHandle), TTM_SETDELAYTIME, TTDT_INITIAL,
-        PhGetIntegerSetting(L"EnableInstantTooltips") ? 0 : -1);
+    if (PhGetIntegerSetting(L"EnableInstantTooltips"))
+        SendMessage(TreeNew_GetTooltips(NetworkTreeListHandle), TTM_SETDELAYTIME, TTDT_INITIAL, 0);
+    else
+        SendMessage(TreeNew_GetTooltips(NetworkTreeListHandle), TTM_SETDELAYTIME, TTDT_AUTOPOP, MAXSHORT);
 
     PhLoadSettingsNetworkTreeUpdateMask();
 }

SystemInformer/notifico.c
@@ -1983,7 +1983,7 @@ VOID PhNfpCpuUsageIconUpdateCallback(
     }
 
     PhInitFormatS(&format[0], L"CPU usage: ");
-    PhInitFormatF(&format[1], (PhCpuKernelUsage + PhCpuUserUsage) * 100.f, PhMaxPrecisionUnit);
+    PhInitFormatF(&format[1], (PhCpuKernelUsage + PhCpuUserUsage) * 100, PhMaxPrecisionUnit);
     PhInitFormatC(&format[2], L'%');
     if (maxCpuText) PhInitFormatSR(&format[3], maxCpuText->sr);
     else PhInitFormatC(&format[3], L' ');
@@ -2077,7 +2077,7 @@ VOID PhNfpCpuUsageTextIconUpdateCallback(
     }
 
     PhInitFormatS(&format[0], L"CPU usage: ");
-    PhInitFormatF(&format[1], (PhCpuKernelUsage + PhCpuUserUsage) * 100, PhMaxPrecisionUnit);
+    PhInitFormatF(&format[1], (PhCpuKernelUsage + PhCpuUserUsage) * 100.f, PhMaxPrecisionUnit);
     PhInitFormatC(&format[2], L'%');
     if (maxCpuText) PhInitFormatSR(&format[3], maxCpuText->sr);
     else PhInitFormatC(&format[3], L' ');

SystemInformer/options.c
@@ -1392,6 +1392,7 @@ typedef enum _PHP_OPTIONS_INDEX
     PHP_OPTIONS_INDEX_ENABLE_DRIVER,
     PHP_OPTIONS_INDEX_ENABLE_MONOSPACE,
     PHP_OPTIONS_INDEX_ENABLE_PLUGINS,
+    PHP_OPTIONS_INDEX_ENABLE_AVX_EXTENSIONS,
     PHP_OPTIONS_INDEX_ENABLE_UNDECORATE_SYMBOLS,
     PHP_OPTIONS_INDEX_ENABLE_COLUMN_HEADER_TOTALS,
     PHP_OPTIONS_INDEX_ENABLE_CYCLE_CPU_USAGE,
@@ -1438,6 +1439,7 @@ static VOID PhpAdvancedPageLoad(
     PhAddListViewItem(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_MONOSPACE, L"Enable monospace fonts", NULL);
     PhAddListViewItem(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_PLUGINS, L"Enable plugins", NULL);
     PhAddListViewItem(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_UNDECORATE_SYMBOLS, L"Enable undecorated symbols", NULL);
+    PhAddListViewItem(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_AVX_EXTENSIONS, L"Enable AVX extensions (experimental)", NULL);
     PhAddListViewItem(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_COLUMN_HEADER_TOTALS, L"Enable column header totals (experimental)", NULL);
 #ifdef _ARM64_
     // see: PhpEstimateIdleCyclesForARM (jxy-s)
@@ -1474,6 +1476,7 @@ static VOID PhpAdvancedPageLoad(
     SetLvItemCheckForSetting(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_WARNINGS, L"EnableWarnings");
     SetLvItemCheckForSetting(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_PLUGINS, L"EnablePlugins");
     SetLvItemCheckForSetting(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_UNDECORATE_SYMBOLS, L"DbgHelpUndecorate");
+    SetLvItemCheckForSetting(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_AVX_EXTENSIONS, L"EnableAvxSupport");
     SetLvItemCheckForSetting(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_COLUMN_HEADER_TOTALS, L"TreeListEnableHeaderTotals");
     SetLvItemCheckForSetting(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_GRAPH_SCALING, L"EnableGraphMaxScale");
 #ifdef _ARM64_
@@ -1660,6 +1663,7 @@ static VOID PhpAdvancedPageSave(
     SetSettingForLvItemCheck(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_WARNINGS, L"EnableWarnings");
     SetSettingForLvItemCheckRestartRequired(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_PLUGINS, L"EnablePlugins");
     SetSettingForLvItemCheck(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_UNDECORATE_SYMBOLS, L"DbgHelpUndecorate");
+    SetSettingForLvItemCheck(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_AVX_EXTENSIONS, L"EnableAvxSupport");
     SetSettingForLvItemCheckRestartRequired(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_COLUMN_HEADER_TOTALS, L"TreeListEnableHeaderTotals");
     SetSettingForLvItemCheckRestartRequired(listViewHandle, PHP_OPTIONS_INDEX_ENABLE_GRAPH_SCALING, L"EnableGraphMaxScale");
 #ifdef _ARM64_

commit d3717d151e10a26cb0b506269b707c4830ebde56

Fix pool table auto refresh
Broken by 0c0c938

plugins/ExtendedTools/pooldialog.c
@@ -113,7 +113,7 @@ VOID NTAPI EtPoolMonProcessesUpdatedCallback(
 {
     PPOOLTAG_CONTEXT context = Context;
 
-    if (PtrToUlong(Parameter) < 3)
+    if (ProcessesUpdatedCount != 3)
         return;
 
     EtUpdatePoolTagTable(Context);