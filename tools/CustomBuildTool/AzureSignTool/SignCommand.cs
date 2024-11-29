d7847e908ec487bc53a94d6c582ac50a13cdd1f4
dmex <dmex04@gmail.com>
2024-11-30 07:11:05 +1100

    Update SignCommand.cs

--- tools/CustomBuildTool/AzureSignTool/SignCommand.cs
+++ tools/CustomBuildTool/AzureSignTool/SignCommand.cs
@@ -4,35 +4,14 @@
     {
         internal static readonly string CodeSigningOid = "1.3.6.1.5.5.7.3.3";
 
-        private static X509Certificate2 LoadAuthenticode(string path)
+        private static bool IsAuthenticodeSignrf(string path)
         {
             if (X509Certificate2.GetCertContentType(path) == X509ContentType.Authenticode)
             {
-#pragma warning disable SYSLIB0057
-                return new X509Certificate2(path);
-#pragma warning restore SYSLIB0057
+                return true;
             }
 
-            return null;
-        }
-
-        internal static bool IsSigned(string filePath)
-        {
-            try
-            {
-                var certificate =  new X509Certificate2(LoadAuthenticode(filePath));
-
-                // check if file contains a code signing cert.
-                // Note that this does not check validity of the signature
-                return certificate.Extensions
-                    .Select(extension => extension as X509EnhancedKeyUsageExtension)
-                    .Select(enhancedExtension => enhancedExtension?.EnhancedKeyUsages)
-                    .Any(oids => oids?[CodeSigningOid] != null);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            return false;
         }
 
         private static X509Certificate2Collection GetAdditionalCertificates(IEnumerable<string> paths)
