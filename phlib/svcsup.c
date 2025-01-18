winsiderss/systeminformer

commit 0424133b79b74f9104cf51c6e882dfa7f7f6beb5

Add PPCH_STRINGREF

phlib/svcsup.c
@@ -15,7 +15,7 @@
 #include <svcsup.h>
 #include <mapldr.h>
 
-static PH_STRINGREF PhpServiceUnknownString = PH_STRINGREF_INIT(L"Unknown");
+static CONST PH_STRINGREF PhpServiceUnknownString = PH_STRINGREF_INIT(L"Unknown");
 
 static CONST PH_KEY_VALUE_PAIR PhpServiceStatePairs[] =
 {
@@ -345,7 +345,7 @@ NTSTATUS PhOpenServiceKey(
     _In_ PPH_STRINGREF ServiceName
     )
 {
-    static PH_STRINGREF servicesKeyName = PH_STRINGREF_INIT(L"System\\CurrentControlSet\\Services");
+    static CONST PH_STRINGREF servicesKeyName = PH_STRINGREF_INIT(L"System\\CurrentControlSet\\Services");
     static PH_INITONCE initOnce = PH_INITONCE_INIT;
     static HANDLE servicesKeyHandle = NULL;
     NTSTATUS status = STATUS_UNSUCCESSFUL;
@@ -906,25 +906,13 @@ BOOLEAN PhGetServiceTriggerInfo(
     return FALSE;
 }
 
-PPH_STRINGREF PhGetServiceStateString(
+PPCH_STRINGREF PhGetServiceStateString(
     _In_ ULONG ServiceState
     )
 {
-    switch (ServiceState)
-    {
-    case SERVICE_STOPPED:
-    case SERVICE_START_PENDING:
-    case SERVICE_STOP_PENDING:
-    case SERVICE_RUNNING:
-    case SERVICE_CONTINUE_PENDING:
-    case SERVICE_PAUSE_PENDING:
-    case SERVICE_PAUSED:
-        return PhpServiceStatePairs[ServiceState - 1].Key;
-    }
+    PPCH_STRINGREF string;
 
-    PPH_STRINGREF string;
-
-    if (PhFindStringRefSiKeyValuePairs(
+    if (PhIndexStringRefSiKeyValuePairs(
         PhpServiceStatePairs,
         sizeof(PhpServiceStatePairs),
         ServiceState,
@@ -937,11 +925,11 @@ PPH_STRINGREF PhGetServiceStateString(
     return &PhpServiceUnknownString;
 }
 
-PPH_STRINGREF PhGetServiceTypeString(
+PPCH_STRINGREF PhGetServiceTypeString(
     _In_ ULONG ServiceType
     )
 {
-    PPH_STRINGREF string;
+    PPCH_STRINGREF string;
 
     if (PhFindStringRefSiKeyValuePairs(
         PhpServiceTypePairs,
@@ -973,23 +961,13 @@ ULONG PhGetServiceTypeInteger(
         return ULONG_MAX;
 }
 
-PPH_STRINGREF PhGetServiceStartTypeString(
+PPCH_STRINGREF PhGetServiceStartTypeString(
     _In_ ULONG ServiceStartType
     )
 {
-    switch (ServiceStartType)
-    {
-    case SERVICE_BOOT_START:
-    case SERVICE_SYSTEM_START:
-    case SERVICE_AUTO_START:
-    case SERVICE_DEMAND_START:
-    case SERVICE_DISABLED:
-        return PhpServiceStartTypePairs[ServiceStartType].Key;
-    }
+    PPCH_STRINGREF string;
 
-    PPH_STRINGREF string;
-
-    if (PhFindStringRefSiKeyValuePairs(
+    if (PhIndexStringRefSiKeyValuePairs(
         PhpServiceStartTypePairs,
         sizeof(PhpServiceStartTypePairs),
         ServiceStartType,
@@ -1019,22 +997,13 @@ ULONG PhGetServiceStartTypeInteger(
         return ULONG_MAX;
 }
 
-PPH_STRINGREF PhGetServiceErrorControlString(
+PPCH_STRINGREF PhGetServiceErrorControlString(
     _In_ ULONG ServiceErrorControl
     )
 {
-    switch (ServiceErrorControl)
-    {
-    case SERVICE_ERROR_IGNORE:
-    case SERVICE_ERROR_NORMAL:
-    case SERVICE_ERROR_SEVERE:
-    case SERVICE_ERROR_CRITICAL:
-        return PhpServiceErrorControlPairs[ServiceErrorControl].Key;
-    }
+    PPCH_STRINGREF string;
 
-    PPH_STRINGREF string;
-
-    if (PhFindStringRefSiKeyValuePairs(
+    if (PhIndexStringRefSiKeyValuePairs(
         PhpServiceErrorControlPairs,
         sizeof(PhpServiceErrorControlPairs),
         ServiceErrorControl,
@@ -1174,7 +1143,7 @@ PPH_STRING PhGetServiceKeyName(
     _In_ PPH_STRINGREF ServiceName
     )
 {
-    static PH_STRINGREF servicesKeyName = PH_STRINGREF_INIT(L"System\\CurrentControlSet\\Services\\");
+    static CONST PH_STRINGREF servicesKeyName = PH_STRINGREF_INIT(L"System\\CurrentControlSet\\Services\\");
 
     return PhConcatStringRef2(&servicesKeyName, ServiceName);
 }
@@ -1183,8 +1152,8 @@ PPH_STRING PhGetServiceParametersKeyName(
     _In_ PPH_STRINGREF ServiceName
     )
 {
-    static PH_STRINGREF servicesKeyName = PH_STRINGREF_INIT(L"System\\CurrentControlSet\\Services\\");
-    static PH_STRINGREF parametersKeyName = PH_STRINGREF_INIT(L"\\Parameters");
+    static CONST PH_STRINGREF servicesKeyName = PH_STRINGREF_INIT(L"System\\CurrentControlSet\\Services\\");
+    static CONST PH_STRINGREF parametersKeyName = PH_STRINGREF_INIT(L"\\Parameters");
 
     return PhConcatStringRef3(&servicesKeyName, ServiceName, &parametersKeyName);
 }

SystemInformer/actions.c
@@ -1316,12 +1316,6 @@ BOOLEAN PhUiLogoffSession(
     return FALSE;
 }
 
-typedef struct _PH_PLUGIN_IS_DANGEROUS_PROCESS
-{
-    HANDLE ProcessId;
-    BOOLEAN DangerousProcess;
-} PH_PLUGIN_IS_DANGEROUS_PROCESS, *PPH_PLUGIN_IS_DANGEROUS_PROCESS;
-
 /**
  * Determines if a process is a system process.
  *
@@ -1352,8 +1346,8 @@ BOOLEAN PhIsDangerousProcess(
     if (!NT_SUCCESS(PhGetProcessImageFileNameByProcessId(ProcessId, &fileName)))
         return FALSE;
 
-    PhMoveReference(&fileName, PhGetFileName(fileName));
     hash = PhHashStringRefEx(&fileName->sr, TRUE, PH_STRING_HASH_X65599);
+    PhMoveReference(&fileName, PhGetBaseName(fileName));
     PhDereferenceObject(fileName);
 
     for (ULONG i = 0; i < RTL_NUMBER_OF(DangerousProcesses); i++)
@@ -2624,9 +2618,9 @@ BOOLEAN PhUiDebugProcess(
     _In_ PPH_PROCESS_ITEM Process
     )
 {
-    static PH_STRINGREF aeDebugKeyName = PH_STRINGREF_INIT(L"Software\\Microsoft\\Windows NT\\CurrentVersion\\AeDebug");
+    static CONST PH_STRINGREF aeDebugKeyName = PH_STRINGREF_INIT(L"Software\\Microsoft\\Windows NT\\CurrentVersion\\AeDebug");
 #ifdef _WIN64
-    static PH_STRINGREF aeDebugWow64KeyName = PH_STRINGREF_INIT(L"Software\\Wow6432Node\\Microsoft\\Windows NT\\CurrentVersion\\AeDebug");
+    static CONST PH_STRINGREF aeDebugWow64KeyName = PH_STRINGREF_INIT(L"Software\\Wow6432Node\\Microsoft\\Windows NT\\CurrentVersion\\AeDebug");
 #endif
     NTSTATUS status;
     BOOLEAN cont = FALSE;

SystemInformer/hndlprp.c
@@ -486,13 +486,13 @@ VOID PhpUpdateHandleGeneral(
     ULONG numberOfAccessEntries;
     WCHAR string[PH_INT64_STR_LEN_1];
 
-    PhSetIListViewGroupSubItem(Context->ListViewClass, PH_HANDLE_GENERAL_CATEGORY_BASICINFO, PH_HANDLE_GENERAL_INDEX_NAME, 1, PhGetStringOrEmpty(Context->HandleItem->BestObjectName));
-    PhSetIListViewGroupSubItem(Context->ListViewClass, PH_HANDLE_GENERAL_CATEGORY_BASICINFO, PH_HANDLE_GENERAL_INDEX_TYPE, 1, PhGetStringOrEmpty(Context->HandleItem->TypeName));
+    PhSetIListViewSubItem(Context->ListViewClass, PH_HANDLE_GENERAL_INDEX_NAME, 1, PhGetStringOrEmpty(Context->HandleItem->BestObjectName));
+    PhSetIListViewSubItem(Context->ListViewClass, PH_HANDLE_GENERAL_INDEX_TYPE, 1, PhGetStringOrEmpty(Context->HandleItem->TypeName));
 
     if (Context->HandleItem->Object)
     {
         PhPrintPointer(string, Context->HandleItem->Object);
-        PhSetIListViewGroupSubItem(Context->ListViewClass, PH_HANDLE_GENERAL_CATEGORY_BASICINFO, PH_HANDLE_GENERAL_INDEX_OBJECT, 1, string);
+        PhSetIListViewSubItem(Context->ListViewClass, PH_HANDLE_GENERAL_INDEX_OBJECT, 1, string);
     }
 
     if (PhGetAccessEntries(
@@ -1749,7 +1749,7 @@ VOID PhpUpdateHandleGeneral(
             if (exitStatus != STATUS_PENDING)
             {
                 PhLargeIntegerToLocalSystemTime(&time, (PLARGE_INTEGER)&times.ExitTime);
-                PhSetListViewSubItem(Context->ListViewHandle, Context->ListViewRowCache[PH_HANDLE_GENERAL_INDEX_PROCESSTHREADEXITTIME], 1, PhaFormatDateTime(&time)->Buffer);
+                PhSetListViewSubItem(Context->ListViewHandle, PH_HANDLE_GENERAL_INDEX_PROCESSTHREADEXITTIME, 1, PhaFormatDateTime(&time)->Buffer);
             }
         }
 

SystemInformer/include/phplug.h
@@ -162,6 +162,12 @@ typedef struct _PH_PLUGIN_OBJECT_PROPERTIES
     HPROPSHEETPAGE *Pages;
 } PH_PLUGIN_OBJECT_PROPERTIES, *PPH_PLUGIN_OBJECT_PROPERTIES;
 
+typedef struct _PH_PLUGIN_IS_DANGEROUS_PROCESS
+{
+    HANDLE ProcessId;
+    BOOLEAN DangerousProcess;
+} PH_PLUGIN_IS_DANGEROUS_PROCESS, *PPH_PLUGIN_IS_DANGEROUS_PROCESS;
+
 typedef enum _PH_PLUGIN_HANDLE_GENERAL_CATEGORY
 {
     // common

SystemInformer/options.c
@@ -126,7 +126,7 @@ static HWND ContainerControl = NULL;
 static BOOLEAN RestartRequired = FALSE;
 
 // General
-static PH_STRINGREF CurrentUserRunKeyName = PH_STRINGREF_INIT(L"Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+static CONST PH_STRINGREF CurrentUserRunKeyName = PH_STRINGREF_INIT(L"Software\\Microsoft\\Windows\\CurrentVersion\\Run");
 static BOOLEAN CurrentUserRunPresent = FALSE;
 static HFONT CurrentFontInstance = NULL;
 static HFONT CurrentFontMonospaceInstance = NULL;
@@ -134,7 +134,7 @@ static PPH_STRING NewFontSelection = NULL;
 static PPH_STRING NewFontMonospaceSelection = NULL;
 
 // Advanced
-static PH_STRINGREF TaskMgrImageOptionsKeyName = PH_STRINGREF_INIT(L"Software\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\taskmgr.exe");
+static CONST PH_STRINGREF TaskMgrImageOptionsKeyName = PH_STRINGREF_INIT(L"Software\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\taskmgr.exe");
 static PPH_STRING OldTaskMgrDebugger = NULL;
 static HWND WindowHandleForElevate = NULL;
 

SystemInformer/plugin.c
@@ -191,7 +191,7 @@ PPH_STRING PhpGetPluginDirectoryPath(
     VOID
     )
 {
-    static PH_STRINGREF pluginsDirectory = PH_STRINGREF_INIT(L"plugins\\");
+    static CONST PH_STRINGREF pluginsDirectory = PH_STRINGREF_INIT(L"plugins\\");
 
     return PhGetApplicationDirectoryFileName(&pluginsDirectory, PhPluginsLoadNative);
 }

SystemInformer/procprv.c
@@ -761,7 +761,7 @@ VOID PhpProcessQueryStage1(
 
         if (PhEnableCycleCpuUsage && processId == INTERRUPTS_PROCESS_ID)
         {
-            static PH_STRINGREF descriptionText = PH_STRINGREF_INIT(L"Interrupts and DPCs");
+            static CONST PH_STRINGREF descriptionText = PH_STRINGREF_INIT(L"Interrupts and DPCs");
             PhMoveReference(&Data->VersionInfo.FileDescription, PhCreateString2(&descriptionText));
         }
     }

SystemInformer/sessshad.c
@@ -13,8 +13,6 @@
 #include <settings.h>
 #include <winsta.h>
 
-#define SIP(String, Integer) { (String), (PVOID)(Integer) }
-
 static CONST PH_KEY_VALUE_PAIR VirtualKeyPairs[] =
 {
     SIP(L"0", '0'),

commit 2a1465829411c081c52576cab6a72c647c255ecf

Fix service index for PhIndexStringRefSiKeyValuePairs regression from 0424133b

phlib/svcsup.c
@@ -19,6 +19,7 @@ static CONST PH_STRINGREF PhpServiceUnknownString = PH_STRINGREF_INIT(L"Unknown"
 
 static CONST PH_KEY_VALUE_PAIR PhpServiceStatePairs[] =
 {
+    SIP(SREF(L"Unknown"), 0),
     SIP(SREF(L"Stopped"), SERVICE_STOPPED),
     SIP(SREF(L"Start pending"), SERVICE_START_PENDING),
     SIP(SREF(L"Stop pending"), SERVICE_STOP_PENDING),