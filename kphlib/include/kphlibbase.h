ee053c4e053624572a26b2bb8a4db47c148f73d6
Johnny Shaw <johnny.shaw@live.com>
2025-01-29 17:51:21 -0700

    kph ring buffer

--- kphlib/include/kphlibbase.h
+++ kphlib/include/kphlibbase.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2022 Winsider Seminars & Solutions, Inc.  All rights reserved.
+ *
+ * This file is part of System Informer.
+ *
+ * Authors:
+ *
+ *     jxy-s   2025
+ *
+ */
+
 #pragma once
 
 #include <sistatus.h>
