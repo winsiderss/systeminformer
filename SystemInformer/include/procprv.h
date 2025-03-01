39b1510a91f13ad4f9fb306c530f3b2b3f5442a2
dmex <dmex04@gmail.com>
2025-03-01 22:47:04 +1100

    Fix 32bit WorkingSetPrivateSize

--- SystemInformer/include/procprv.h
+++ SystemInformer/include/procprv.h
@@ -271,7 +271,7 @@ typedef struct _PH_PROCESS_ITEM
 
     VM_COUNTERS_EX VmCounters;
     IO_COUNTERS IoCounters;
-    SIZE_T WorkingSetPrivateSize; // since VISTA
+    ULONGLONG WorkingSetPrivateSize; // since VISTA
     ULONG PeakNumberOfThreads; // since WIN7
     ULONG HardFaultCount; // since WIN7
 
