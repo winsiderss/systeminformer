14c12615e5a0131a6209e79d49943b13f6b73353
dmex <dmex04@gmail.com>
2025-03-07 16:59:58 +1100

    Update etwmini.h

--- plugins/ExtendedTools/etwmini.h
+++ plugins/ExtendedTools/etwmini.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2022 Winsider Seminars & Solutions, Inc.  All rights reserved.
+ *
+ * This file is part of System Informer.
+ *
+ * Authors:
+ *
+ *     wj32    2015
+ *
+ */
+
 #ifndef ETWMINI_H
 #define ETWMINI_H
 
