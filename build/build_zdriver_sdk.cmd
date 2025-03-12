384e30c65037dcf459cdfa6b1e8c35780a3323c5
dmex <dmex04@gmail.com>
2025-03-12 18:43:22 +1100

    Fix build

--- build/build_zdriver_sdk.cmd
+++ build/build_zdriver_sdk.cmd
@@ -4,7 +4,21 @@
 
 if "%1"=="" (
    echo:
-   echo Usage: build_dyndata.cmd [BUILD_OUTPUT_PATH]
+   echo Usage: build_zdriver_sdk.cmd [FILE_PATH] [CONFIGURATION] [PLATFORM]
+   echo:
+   pause
+   goto end
+)
+if "%2"=="" (
+   echo:
+   echo Usage: build_zdriver_sdk.cmd [FILE_PATH] [CONFIGURATION] [PLATFORM]
+   echo:
+   pause
+   goto end
+)
+if "%3"=="" (
+   echo:
+   echo Usage: build_zdriver_sdk.cmd [FILE_PATH] [CONFIGURATION] [PLATFORM]
    echo:
    pause
    goto end
@@ -12,8 +26,9 @@ if "%1"=="" (
 
 if exist "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe" (
    echo:
-   start /B /W "" "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe" "-dyndata" "%1"
+   start /B /W "" "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe" "-kphsign" "%1"
    echo:
+   start /B /W "" "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe" "-sdk" "%2" "%3"
 ) else (
    echo CustomBuildTool.exe not found in tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE% folder.
 )
--- build/build_zsign.cmd
+++ build/build_zsign.cmd
@@ -4,21 +4,7 @@
 
 if "%1"=="" (
    echo:
-   echo Usage: build_zdriver_sdk.cmd [FILE_PATH] [CONFIGURATION] [PLATFORM]
-   echo:
-   pause
-   goto end
-)
-if "%2"=="" (
-   echo:
-   echo Usage: build_zdriver_sdk.cmd [FILE_PATH] [CONFIGURATION] [PLATFORM]
-   echo:
-   pause
-   goto end
-)
-if "%3"=="" (
-   echo:
-   echo Usage: build_zdriver_sdk.cmd [FILE_PATH] [CONFIGURATION] [PLATFORM]
+   echo Usage: build_zsign.cmd [FILE_PATH] [CONFIGURATION] [PLATFORM]
    echo:
    pause
    goto end
@@ -28,7 +14,6 @@ if exist "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuild
    echo:
    start /B /W "" "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe" "-kphsign" "%1"
    echo:
-   start /B /W "" "tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE%\CustomBuildTool.exe" "-sdk" "%2" "%3"
 ) else (
    echo CustomBuildTool.exe not found in tools\CustomBuildTool\bin\Release\%PROCESSOR_ARCHITECTURE% folder.
 )
