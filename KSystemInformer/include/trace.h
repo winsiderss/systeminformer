c53bce089ed14f833a3db6b87f8da99d31e7ad00
Johnny Shaw <johnny.shaw@live.com>
2025-04-13 15:55:38 -0600

    kph trace minor cleanup

--- KSystemInformer/include/trace.h
+++ KSystemInformer/include/trace.h
@@ -11,17 +11,17 @@
 
 #pragma once
 
-#define WPP_CONTROL_GUIDS                                                     \
-    WPP_DEFINE_CONTROL_GUID(                                                  \
-        KSystemInformer, (f64b58a2, 8214, 4037, 8c7d, b96ce6098f3d),          \
-        WPP_DEFINE_BIT(GENERAL)     /* bit  0 = 0x00000001 */                 \
-        WPP_DEFINE_BIT(UTIL)        /* bit  1 = 0x00000002 */                 \
-        WPP_DEFINE_BIT(COMMS)       /* bit  2 = 0x00000004 */                 \
-        WPP_DEFINE_BIT(INFORMER)    /* bit  3 = 0x00000008 */                 \
-        WPP_DEFINE_BIT(VERIFY)      /* bit  4 = 0x00000010 */                 \
-        WPP_DEFINE_BIT(HASH)        /* bit  5 = 0x00000020 */                 \
-        WPP_DEFINE_BIT(TRACKING)    /* bit  5 = 0x00000040 */                 \
-        WPP_DEFINE_BIT(PROTECTION)  /* bit  5 = 0x00000080 */                 \
+#define WPP_CONTROL_GUIDS                                                      \
+    WPP_DEFINE_CONTROL_GUID(                                                   \
+        KSystemInformer, (f64b58a2, 8214, 4037, 8c7d, b96ce6098f3d),           \
+        WPP_DEFINE_BIT(GENERAL)     /* bit  0 = 0x00000001 */                  \
+        WPP_DEFINE_BIT(UTIL)        /* bit  1 = 0x00000002 */                  \
+        WPP_DEFINE_BIT(COMMS)       /* bit  2 = 0x00000004 */                  \
+        WPP_DEFINE_BIT(INFORMER)    /* bit  3 = 0x00000008 */                  \
+        WPP_DEFINE_BIT(VERIFY)      /* bit  4 = 0x00000010 */                  \
+        WPP_DEFINE_BIT(HASH)        /* bit  5 = 0x00000020 */                  \
+        WPP_DEFINE_BIT(TRACKING)    /* bit  5 = 0x00000040 */                  \
+        WPP_DEFINE_BIT(PROTECTION)  /* bit  5 = 0x00000080 */                  \
         )
 
 #define WPP_LEVEL_EVENT_LOGGER(level,event) WPP_LEVEL_LOGGER(event)
