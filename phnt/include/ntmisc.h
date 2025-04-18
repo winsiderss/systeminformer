02c680b9fdf86f488541e03c712e3f1bdd3331bd
diversenok <diversenok@gmail.com>
2025-04-18 08:27:36 +0200

    PHNT fixes (#2514)

--- phnt/include/ntmisc.h
+++ phnt/include/ntmisc.h
@@ -767,7 +767,7 @@ WINAPI
 GetCurrentPackageApplicationResourcesContext(
     _In_ ULONG Index,
     _Reserved_ ULONG_PTR Unused,
-    _Out_ PACKAGE_APPLICATION_CONTEXT_REFERENCE *PackageResourcesContext
+    _Out_ PACKAGE_RESOURCES_CONTEXT_REFERENCE *PackageResourcesContext
     );
 
 // rev
@@ -778,7 +778,7 @@ GetPackageApplicationResourcesContext(
     _In_ PVOID PackageInfoReference, // PACKAGE_INFO_REFERENCE
     _In_ ULONG Index,
     _Reserved_ ULONG_PTR Unused,
-    _Out_ PACKAGE_APPLICATION_CONTEXT_REFERENCE *PackageResourcesContext
+    _Out_ PACKAGE_RESOURCES_CONTEXT_REFERENCE *PackageResourcesContext
     );
 
 // rev
@@ -786,10 +786,11 @@ WINBASEAPI
 LONG
 WINAPI
 GetPackageResourcesProperty(
-    _In_ PACKAGE_APPLICATION_CONTEXT_REFERENCE PackageResourcesContext,
+    _In_ PACKAGE_RESOURCES_CONTEXT_REFERENCE PackageResourcesContext,
     _In_ PackageResourcesProperty PropertyId,
     _Inout_ PULONG BufferSize,
-    _Out_writes_bytes_(BufferSize) PVOID Buffer
+    _Out_writes_bytes_(BufferSize) PVOID Buffer,
+    _Out_opt_ PULONG Flags
     );
 
 //
