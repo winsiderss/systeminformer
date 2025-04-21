55549871b3282917e37a1b2037d471f368272a9d
Johnny Shaw <johnny.shaw@live.com>
2025-04-20 23:07:12 -0600

    kph ntfill cleanup

--- KSystemInformer/include/ntfill.h
+++ KSystemInformer/include/ntfill.h
@@ -608,7 +608,6 @@ typedef enum _PS_PROTECTED_TYPE
     PsProtectedTypeNone = 0,
     PsProtectedTypeProtectedLight = 1,
     PsProtectedTypeProtected = 2
-
 } PS_PROTECTED_TYPE, *PPS_PROTECTED_TYPE;
 
 typedef enum _PS_PROTECTED_SIGNER
