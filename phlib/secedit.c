winsiderss/systeminformer

commit a14742ac91a97af56aabd97682487f51f742b1b2

fix handle security if object name cannot be retrieved (#2318)
* fix PhEditSecurity for NULL strings

* fix handle security if object name cannot be retrieved

* Update prpghndl.c

* finally fix handle security

* Update objmgr.c

---------

Co-authored-by: dmex <dmex04@gmail.com>
Co-authored-by: Johnny Shaw <johnny.shaw@live.com>

phlib/secedit.c
@@ -254,7 +254,7 @@ ISecurityInformation *PhSecurityInformation_Create(
     info->RefCount = 1;
 
     info->WindowHandle = WindowHandle;
-    info->ObjectName = PhCreateString(ObjectName);
+    info->ObjectName = ObjectName ? PhCreateString(ObjectName) : PhReferenceEmptyString();
     info->ObjectType = PhCreateString(ObjectType);
     info->ObjectTypeMask = PhSecurityObjectType(info->ObjectType);
     info->OpenObject = OpenObject;

commit ed928fb21a1f8b67e7c7fc5c676892e317bfba02

Fix showing ACUI for unnamed handles regression from PR #2318

phlib/secedit.c
@@ -255,7 +255,7 @@ ISecurityInformation *PhSecurityInformation_Create(
     info->RefCount = 1;
 
     info->WindowHandle = WindowHandle;
-    info->ObjectNameString = ObjectName ? PhCreateString(ObjectName) : PhReferenceEmptyString();
+    info->ObjectNameString = ObjectName ? PhCreateString(ObjectName) : NULL;
     info->ObjectTypeString = PhCreateString(ObjectType);
     info->ObjectType = PhSecurityObjectType(info->ObjectTypeString);
     info->OpenObject = OpenObject;
@@ -293,6 +293,12 @@ ISecurityInformation *PhSecurityInformation_Create(
             memcpy(&info->GenericMapping, &genericMapping, sizeof(genericMapping));
             info->HaveGenericMapping = TRUE;
         }
+    }
+
+    if (PhIsNullOrEmptyString(info->ObjectNameString))
+    {
+        // Note: The ACUI dialog doesn't allow empty strings for the object name. (dmex)  
+        PhMoveReference(&info->ObjectNameString, PhCreateString(L"Unknown"));
     }
 
     return (ISecurityInformation *)info;