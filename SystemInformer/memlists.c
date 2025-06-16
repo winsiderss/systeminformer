e3a775fba75be5afc369c993d69b1d1f819d42ed
Johnny Shaw <johnny.shaw@live.com>
2025-06-16 16:22:21 -0600

    fix PhShowMemoryListCommand from breaking commit ac2ed17

--- SystemInformer/memlists.c
+++ SystemInformer/memlists.c
@@ -561,9 +561,9 @@ VOID PhShowMemoryListCommand(
             if (!PhIsNullOrEmptyString(message))
             {
                 if (showError)
-                    PhShowError2(ParentWindow, L"Unable to empty the memory list.", L"%s", PhGetString(message));
+                    PhShowError2(ParentWindow, L"Memory Command", L"%s", PhGetString(message));
                 else
-                    PhShowInformation2(ParentWindow, L"Unable to empty the memory list.", L"%s", PhGetString(message));
+                    PhShowInformation2(ParentWindow, L"Memory Command", L"%s", PhGetString(message));
             }
 
             PhClearReference(&message);
@@ -598,7 +598,7 @@ VOID PhShowMemoryListCommand(
 
             message = PhFinalStringBuilderString(&stringBuilder);
             if (!PhIsNullOrEmptyString(message))
-                PhShowInformation2(ParentWindow, L"Unable to empty the memory list.", L"%s", PhGetString(message));
+                PhShowInformation2(ParentWindow, L"Memory Command", L"%s", PhGetString(message));
             PhClearReference(&message);
         }
     }
