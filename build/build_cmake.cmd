f0ac91e3a2017dc2a4c6abb4f7df75766d370a3e
Johnny Shaw <johnny.shaw@live.com>
2025-08-06 16:51:21 -0600

    cmake build script specify toolchain

--- build/build_cmake.cmd
+++ build/build_cmake.cmd
@@ -2,11 +2,18 @@
 @setlocal enableextensions
 @cd /d "%~dp0\..\"
 
+set "TIB=false"
+if "%GITHUB_ACTIONS%"=="true" (
+    set "TIB=true"
+) else if "%TF_BUILD%"=="true" (
+    set "TIB=true"
+)
+
 set GENERATOR=%~1
 set CONFIG=%~2
-set PLATFORM=%~3
+set TOOLCHAIN=%~3
 set ACTION=%~4
-set TOOLCHAIN=
+set TOOLCHAIN_FILE=
 set BUILD_DIR=
 
 for /f "usebackq tokens=*" %%a in (`call "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
@@ -29,21 +36,39 @@ if /i "%CONFIG%"=="Debug" (
     goto end
 )
 
-if /i "%PLATFORM%"=="Win32" (
+if /i "%TOOLCHAIN%"=="msvc-x86" (
+    set PLATFORM=Win32
     set VCVARS_ARCH=amd64_x86
-    set BUILD_DIR="%CD%\%BUILD_DIR%-32"
-    set TOOLCHAIN="%CD%\cmake\toolchain\msvc-x86.cmake"
-) else if /i "%PLATFORM%"=="x64" (
+    set BUILD_DIR="%CD%\%BUILD_DIR%-msvc-32"
+    set TOOLCHAIN_FILE="%CD%\cmake\toolchain\msvc-x86.cmake"
+) else if /i "%TOOLCHAIN%"=="msvc-amd64" (
+    set PLATFORM=x64
     set VCVARS_ARCH=amd64
-    set BUILD_DIR="%CD%\%BUILD_DIR%-64"
-    set TOOLCHAIN="%CD%\cmake\toolchain\msvc-amd64.cmake"
-) else if /i "%PLATFORM%"=="ARM64" (
+    set BUILD_DIR="%CD%\%BUILD_DIR%-msvc-64"
+    set TOOLCHAIN_FILE="%CD%\cmake\toolchain\msvc-amd64.cmake"
+) else if /i "%TOOLCHAIN%"=="msvc-arm64" (
+    set PLATFORM=ARM64
     set VCVARS_ARCH=amd64_arm64
-    set BUILD_DIR="%CD%\%BUILD_DIR%-arm64"
-    set TOOLCHAIN="%CD%\cmake\toolchain\msvc-arm64.cmake"
+    set BUILD_DIR="%CD%\%BUILD_DIR%-msvc-arm64"
+    set TOOLCHAIN_FILE="%CD%\cmake\toolchain\msvc-arm64.cmake"
+) else if /i "%TOOLCHAIN%"=="clang-msvc-x86" (
+    set PLATFORM=Win32
+    set VCVARS_ARCH=amd64_x86
+    set BUILD_DIR="%CD%\%BUILD_DIR%-clang-msvc-32"
+    set TOOLCHAIN_FILE="%CD%\cmake\toolchain\clang-msvc-x86.cmake"
+) else if /i "%TOOLCHAIN%"=="clang-msvc-amd64" (
+    set PLATFORM=x64
+    set VCVARS_ARCH=amd64
+    set BUILD_DIR="%CD%\%BUILD_DIR%-clang-msvc-64"
+    set TOOLCHAIN_FILE="%CD%\cmake\toolchain\clang-msvc-amd64.cmake"
+) else if /i "%TOOLCHAIN%"=="clang-msvc-arm64" (
+    set PLATFORM=ARM64
+    set VCVARS_ARCH=amd64_arm64
+    set BUILD_DIR="%CD%\%BUILD_DIR%-clang-msvc-arm64"
+    set TOOLCHAIN_FILE="%CD%\cmake\toolchain\clang-msvc-arm64.cmake"
 ) else (
-    echo Error: Unsupported platform: %PLATFORM%
-    echo Supported platforms: Win32, x64, ARM64
+    echo Error: Unsupported toolchain: %TOOLCHAIN%
+    echo Supported toolchains: msvc-x86, msvc-amd64, msvc-arm64, clang-msvc-x86, clang-msvc-amd64, clang-msvc-arm64
     set ERRORLEVEL=1
     goto end
 )
@@ -57,10 +82,10 @@ echo Action:        %ACTION%
 echo Generator:     %GENERATOR%
 echo Configuration: %CONFIG%
 echo Platform:      %PLATFORM%
+echo Toolchain:     %TOOLCHAIN%
 echo VCVARS:        %VCVARS_ARCH%
 echo Source:        %SOURCE_DIR%
 echo Build:         %BUILD_DIR%
-echo Toolchain:     %TOOLCHAIN%
 echo ===============================================
 
 set CMAKE_GEN_OPTS=
@@ -81,7 +106,7 @@ if /i "%ACTION%"=="clean" (
     if %ERRORLEVEL% neq 0 goto end
     if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
     cmake -G "%GENERATOR%" -S %SOURCE_DIR% -B %BUILD_DIR% ^
-        --toolchain %TOOLCHAIN% ^
+        --toolchain %TOOLCHAIN_FILE% ^
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
         -DSI_WITH_CORE=ON ^
         -DSI_WITH_PLUGINS=ON ^
@@ -99,4 +124,4 @@ if /i "%ACTION%"=="clean" (
 )
 
 :end
-pause
+if "%TIB%"=="false" pause
