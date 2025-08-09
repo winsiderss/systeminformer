87bdd04bbd633e18c2bc60af2b0bcf0a6eb08c8b
Johnny Shaw <johnny.shaw@live.com>
2025-08-09 11:33:36 -0600

    phbase add clang diagnostic helpers

--- phlib/include/phbase.h
+++ phlib/include/phbase.h
@@ -26,6 +26,18 @@
 #define PHLIBAPI
 #endif
 
+#ifdef __clang__
+#define PH_CLANG_STRINGIFYX(x)         #x
+#define PH_CLANG_STRINGIFY(x)          TMH_STRINGIFYX(x)
+#define PH_CLANG_DIAGNOSTIC_PUSH       _Pragma("clang diagnostic push")
+#define PH_CLANG_DIAGNOSTIC_IGNORED(x) _Pragma(PH_CLANG_STRINGIFY(clang diagnostic ignored x))
+#define PH_CLANG_DIAGNOSTIC_POP        _Pragma("clang diagnostic pop")
+#else
+#define PH_CLANG_DIAGNOSTIC_PUSH
+#define PH_CLANG_DIAGNOSTIC_IGNORED(x)
+#define PH_CLANG_DIAGNOSTIC_POP
+#endif
+
 #include <phnt_windows.h>
 #include <phnt.h>
 #include <phsup.h>
