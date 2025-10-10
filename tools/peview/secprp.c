101fe15a22f1d7df50ff79a94c705ad95334d7a1
kmlch-git <kmlch@sina.com>
2025-10-11 06:46:49 +0800

    Revert "Refix bug#2709（In "certificate node-> algorithm = data;", the string length in the "data" variable in this line is also calculated by the length of a UNICODE_NULL terminator.）"
    This reverts commit effcf6fe839435edf3c0867d7c3c7d4e512c31c2.

--- tools/peview/secprp.c
+++ tools/peview/secprp.c
@@ -1035,10 +1035,7 @@ BOOLEAN PvpPeFillNodeCertificateInfo(
             PPH_STRING data = PhCreateStringEx(NULL, dataLength);
 
             if (CertGetCertificateContextProperty(CertificateContext, CERT_SIGN_HASH_CNG_ALG_PROP_ID, data->Buffer, &dataLength))
-            {
                 CertificateNode->Algorithm = data;
-                PhTrimToNullTerminatorString(data);
-            }
             else
                 PhDereferenceObject(data);
         }
