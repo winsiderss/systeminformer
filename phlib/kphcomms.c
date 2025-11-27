3552ef5d31a88c08021baaeb38ba34d2e1548409
Johnny Shaw <johnny.shaw@live.com>
2025-11-27 11:43:57 -0700

    kph optimize port disconnect detection

--- phlib/kphcomms.c
+++ phlib/kphcomms.c
@@ -122,7 +122,16 @@ VOID WINAPI KphpCommsIoCallback(
     msg = CONTAINING_RECORD(ApcContext, KPH_UMESSAGE, Overlapped);
 
     if (IoSB->Status != STATUS_SUCCESS)
+    {
+        if (IoSB->Status == STATUS_PORT_DISCONNECTED)
+        {
+            KphpCommsPortDisconnected = TRUE;
+            PhReleaseRundownProtection(&KphpCommsRundown);
+            return;
+        }
+
         goto QueueIoOperation;
+    }
 
     assert(IoSB->Information >= UFIELD_OFFSET(KPH_UMESSAGE, Message));
 
@@ -195,13 +204,7 @@ QueueIoOperation:
         assert(status == STATUS_PORT_DISCONNECTED);
 
         if (status == STATUS_PORT_DISCONNECTED)
-        {
-            //
-            // Mark the port disconnected so KphCommsIsConnected returns false.
-            // This can happen if the driver goes away before the client.
-            //
             KphpCommsPortDisconnected = TRUE;
-        }
 
         TpCancelAsyncIoOperation(KphpCommsThreadPoolIo);
     }
@@ -547,13 +550,7 @@ NTSTATUS KphCommsReplyMessage(
         );
 
     if (status == STATUS_PORT_DISCONNECTED)
-    {
-        //
-        // Mark the port disconnected so KphCommsIsConnected returns false.
-        // This can happen if the driver goes away before the client.
-        //
         KphpCommsPortDisconnected = TRUE;
-    }
 
 CleanupExit:
 
@@ -595,13 +592,7 @@ NTSTATUS KphCommsSendMessage(
         );
 
     if (status == STATUS_PORT_DISCONNECTED)
-    {
-        //
-        // Mark the port disconnected so KphCommsIsConnected returns false.
-        // This can happen if the driver goes away before the client.
-        //
         KphpCommsPortDisconnected = TRUE;
-    }
 
     return status;
 }
