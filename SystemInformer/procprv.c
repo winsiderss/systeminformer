winsiderss/systeminformer

commit 7e9aa37378bcf2838a418c2c4a18b1ec3b413e19

Fix affinity update for single groups

SystemInformer/procprv.c
@@ -2900,23 +2900,36 @@ VOID PhProcessProviderUpdate(
             {
                 ULONG affinityPopulationCount = 0;
 
-                for (USHORT i = 0; i < PhSystemProcessorInformation.NumberOfProcessorGroups; i++)
+                if (PhSystemProcessorInformation.SingleProcessorGroup)
                 {
-                    GROUP_AFFINITY affinity;
+                    KAFFINITY affinityMask;
 
-                    affinity.Group = i;
-
-                    if (processItem->QueryHandle &&
-                        NT_SUCCESS(PhGetProcessGroupAffinity(processItem->QueryHandle, &affinity)))
+                    if (processItem->QueryHandle && NT_SUCCESS(PhGetProcessAffinityMask(processItem->QueryHandle, &affinityMask)))
                     {
-                        processItem->AffinityMasks[i] = affinity.Mask;
+                        processItem->AffinityMasks = &affinityMask;
+                        affinityPopulationCount = PhCountBitsUlongPtr(processItem->AffinityMasks[0]);
                     }
-                    else
+                }
+                else
+                {
+                    for (USHORT i = 0; i < PhSystemProcessorInformation.NumberOfProcessorGroups; i++)
                     {
-                        processItem->AffinityMasks[i] = PhSystemProcessorInformation.ActiveProcessorsAffinityMasks[i];
-                    }
+                        GROUP_AFFINITY affinity;
 
-                    affinityPopulationCount = PhCountBitsUlongPtr(processItem->AffinityMasks[i]);
+                        affinity.Group = i;
+
+                        if (processItem->QueryHandle &&
+                            NT_SUCCESS(PhGetProcessGroupAffinity(processItem->QueryHandle, &affinity)))
+                        {
+                            processItem->AffinityMasks[i] = affinity.Mask;
+                        }
+                        else
+                        {
+                            processItem->AffinityMasks[i] = PhSystemProcessorInformation.ActiveProcessorsAffinityMasks[i];
+                        }
+
+                        affinityPopulationCount = PhCountBitsUlongPtr(processItem->AffinityMasks[i]);
+                    }
                 }
 
                 if (processItem->AffinityPopulationCount != affinityPopulationCount)

commit 1215d1d15d1a3abdf87976cf021eb740815e9962

Fix affinity regression from 7e9aa37 #2788

SystemInformer/procprv.c
@@ -3121,7 +3121,7 @@ VOID PhProcessProviderUpdate(
                             processItem->AffinityMasks[i] = PhSystemProcessorInformation.ActiveProcessorsAffinityMasks[i];
                         }
 
-                        affinityPopulationCount = PhCountBitsUlongPtr(processItem->AffinityMasks[i]);
+                        affinityPopulationCount += PhCountBitsUlongPtr(processItem->AffinityMasks[i]);
                     }
                 }
 