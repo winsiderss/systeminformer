5d72e6ae0590de86ec51fc26311ca9afeab3cff9
dmex <dmex04@gmail.com>
2026-01-14 05:39:45 +1100

    Update procprp.c

--- SystemInformer/procprp.c
+++ SystemInformer/procprp.c
@@ -527,12 +527,12 @@ LRESULT CALLBACK PhpOptionsButtonWndProc(
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_SETCRITICAL, L"&Critical", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_DETACHFROMDEBUGGER, L"&Detach from debugger", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_ECOMODE, L"Efficiency mode", NULL, NULL), ULONG_MAX);
-                PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_TIMER, L"Timer coaseling", NULL, NULL), ULONG_MAX);
-                PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_RESOLUTION, L"Timer resolution", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_EXECUTIONREQUIRED, L"Execution required", NULL, NULL), ULONG_MAX);
+                PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_GDIHANDLES, L"GDI &handles...", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_HEAPS, L"Heaps...", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_LOCKS, L"Locks...", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_FLUSHHEAPS, L"Flush heaps", NULL, NULL), ULONG_MAX);
+                PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_PAGESMODIFIED, L"Modified pages...", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_REDUCEWORKINGSET, L"Reduce working &set", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_RUNAS, L"&Run as...", NULL, NULL), ULONG_MAX);
                 PhInsertEMenuItem(menuItem, PhCreateEMenuItem(0, ID_MISCELLANEOUS_RUNASTHISUSER, L"Run &as this user...", NULL, NULL), ULONG_MAX);
@@ -647,6 +647,15 @@ LRESULT CALLBACK PhpOptionsButtonWndProc(
                         break;
                     case ID_MISCELLANEOUS_DETACHFROMDEBUGGER:
                         PhUiDetachFromDebuggerProcess(WindowHandle, processItem);
+                        break;
+                    case ID_MISCELLANEOUS_ECOMODE:
+                        PhUiSetEcoModeProcess(WindowHandle, processItem);
+                        break;
+                    case ID_MISCELLANEOUS_EXECUTIONREQUIRED:
+                        PhUiSetExecutionRequiredProcess(WindowHandle, processItem);
+                        break;
+                    case ID_MISCELLANEOUS_GDIHANDLES:
+                        PhShowGdiHandlesDialog(WindowHandle, processItem);
                         break;
                     case ID_MISCELLANEOUS_HEAPS:
                         PhShowProcessHeapsDialog(WindowHandle, processItem);
