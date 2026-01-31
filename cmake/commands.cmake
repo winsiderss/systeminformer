169e52cacd7be399d39a1cd82c1325e44fe07e5f
dmex <dmex04@gmail.com>
2026-01-31 17:52:22 +1100

    Update commands.cmake

--- cmake/commands.cmake
+++ cmake/commands.cmake
@@ -20,6 +20,10 @@ set(SI_UM_CLANG_NO_DIAGNOSTICS
     -Wno-unused-function
     -Wno-unused-variable
     -Wno-visibility
+    -Wno-defaulted-function-deleted
+    -Wno-nontrivial-memcall
+    -Wno-class-conversion
+    -Wno-microsoft-explicit-constructor-call
 )
 
 #
