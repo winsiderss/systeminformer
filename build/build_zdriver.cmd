34a936c19f80e126f40c334429d2450b38d15213
dmex <dmex04@gmail.com>
2026-02-15 16:04:16 +1100

    Add EWDK support to build scripts

--- build/build_zdriver.cmd
+++ build/build_zdriver.cmd
@@ -59,6 +59,14 @@ for /f "usebackq tokens=*" %%A in (`call "%ProgramFiles(x86)%\Microsoft Visual S
     set VSINSTALLPATH=%%A
 )
 
+if not defined VSINSTALLPATH if defined WindowsSdkDir (
+   set "VSINSTALLPATH=%WindowsSdkDir%"
+)
+
+if not defined VSINSTALLPATH if defined EWDK_ROOT (
+   set "VSINSTALLPATH=%EWDK_ROOT%"
+)
+
 if not defined VSINSTALLPATH (
     echo [-] Visual Studio not found
     goto end
@@ -67,8 +75,12 @@ if not defined VSINSTALLPATH (
 if exist "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" (
    call "%VSINSTALLPATH%\VC\Auxiliary\Build\vcvarsall.bat" amd64_arm64
 ) else (
-    echo [-] Failed to set up build environment
-    goto end
+   if not defined INCLUDE (
+      if not defined LIB (
+         echo Warning: vcvarsall.bat not found and compilation environment variables ^(INCLUDE/LIB^) are not defined.
+         echo The build may fail if the environment is not properly initialized.
+      )
+   )
 )
 
 echo [+] Building... [WIN64] %BUILD_CONFIGURATION% %BUILD_TARGET% %PREFAST_ANALYSIS% %LEGACY_BUILD%
