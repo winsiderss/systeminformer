7366f9ad90ebd674ec2f78ac3752dbedc5dbae72
dmex <dmex04@gmail.com>
2026-04-30 20:09:18 +1000

    Update exlfprp.c

--- tools/peview/exlfprp.c
+++ tools/peview/exlfprp.c
@@ -103,7 +103,7 @@ VOID PvExlfProperties(
 
     if (!PhExtractIcon(PvFileName->Buffer, &PvImageLargeIcon, &PvImageSmallIcon))
     {
-        PhGetStockApplicationIcon(&PvImageSmallIcon, &PvImageLargeIcon, PhGetSystemDpi());
+        PhGetStockApplicationIcon(&PvImageSmallIcon, &PvImageLargeIcon, USER_DEFAULT_SCREEN_DPI);
     }
 
     if (propContext = PvCreatePropContext(PvFileName))
