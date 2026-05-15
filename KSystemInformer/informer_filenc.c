83fe59bd5dcd5cd663e84bb1d70681d199c8fee2
Johnny Shaw <johnny.shaw@live.com>
2026-05-15 08:54:23 -0600

    kph informer file always defer cached name delete

--- KSystemInformer/informer_filenc.c
+++ KSystemInformer/informer_filenc.c
@@ -890,6 +890,7 @@ VOID KphpFltInitializeFileNameCache(
     typeInfo.Delete = NULL;
     typeInfo.Free = KphpFltFreeCachedFileName;
     typeInfo.Flags = 0;
+    typeInfo.DeferDelete = TRUE;
 
     KphCreateObjectType(&KphpCachedFileNameTypeName,
                         &typeInfo,
